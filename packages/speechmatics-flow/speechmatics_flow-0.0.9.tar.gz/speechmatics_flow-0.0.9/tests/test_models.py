0b144dde256bcc0d3300744de471df596561c261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:03:53 +0000

    Add package speechmatics-flow/0.0.9/speechmatics_flow-0.0.9.tar.gz

---
+++ packages/speechmatics-flow/speechmatics_flow-0.0.9.tar.gz/speechmatics_flow-0.0.9/tests/test_models.py
@@ -0,0 +1,59 @@
+from dataclasses import asdict
+
+import pytest
+
+from speechmatics_flow import models
+
+TEMPLATE_VARS = {
+    "persona": "You are an aging English butler named Humphrey.",
+    "style": "Be charming but unpredictable.",
+    "context": "You are taking a customer's order for fast food.",
+}
+
+
+@pytest.mark.parametrize(
+    "config, want",
+    [
+        ({}, {"type": "raw", "encoding": "pcm_s16le", "sample_rate": 16000}),
+        (
+            {"encoding": "pcm_f32le", "sample_rate": 44100},
+            {"type": "raw", "encoding": "pcm_f32le", "sample_rate": 44100},
+        ),
+    ],
+)
+def test_audio_settings(config, want):
+    audio_settings = models.AudioSettings(**config)
+    got = audio_settings.asdict()
+    assert got == want
+
+
+@pytest.mark.parametrize(
+    "config, want",
+    [
+        ({}, {"template_id": "default"}),
+        (
+            {"template_id": "test", "template_variables": TEMPLATE_VARS},
+            {"template_id": "test", "template_variables": TEMPLATE_VARS},
+        ),
+    ],
+)
+def test_conversation_config(config, want):
+    conversation_config = models.ConversationConfig(**config)
+    got = conversation_config.asdict()
+    assert got == want
+
+
+@pytest.mark.parametrize(
+    "config, want",
+    [
+        ({}, {"buffering": 10, "sample_rate": 16000, "chunk_size": 256}),
+        (
+            {"buffering": 20, "sample_rate": 8000, "chunk_size": 512},
+            {"buffering": 20, "sample_rate": 8000, "chunk_size": 512},
+        ),
+    ],
+)
+def test_playback_settings(config, want):
+    audio_settings = models.PlaybackSettings(**config)
+    got = asdict(audio_settings)
+    assert got == want
