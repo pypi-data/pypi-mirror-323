e37299ee90922e8249b7a33d256b93656798480c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:53 +0000

    Add package redisfacade/1.0.0/redisfacade-1.0.0.tar.gz

---
+++ packages/redisfacade/redisfacade-1.0.0.tar.gz/redisfacade-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='redisfacade',
+    version='1.0.0',
+    description='A package to implement a facade for accessing Redis databases',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Alan Medina',
+    author_email='amedina.escom@hotmail.com',
+    url='https://github.com/amedinag32/redisfacade',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'async-timeout==5.0.1',
+        'redis==5.2.1'
+    ],
+)
