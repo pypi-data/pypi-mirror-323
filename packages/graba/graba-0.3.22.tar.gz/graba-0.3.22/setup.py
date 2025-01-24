0364576f33394fde360df10582ba5c950dc45574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:32 +0000

    Add package graba/0.3.22/graba-0.3.22.tar.gz

---
+++ packages/graba/graba-0.3.22.tar.gz/graba-0.3.22/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="graba",
+    version="0.3.22",
+    description="A library to generate random value for different concepts",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Francisco Jose Albert Albusac",
+    author_email="francis.jaa@protonmail.com",
+    url="https://github.com/tatitati/graba",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "python-dateutil>=2.8.2",
+    ],
+)
