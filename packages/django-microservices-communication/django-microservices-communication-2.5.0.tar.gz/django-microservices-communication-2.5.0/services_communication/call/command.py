a690d8b453489b32341ab99af04a8c01475ae5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:03:36 +0000

    Add package django-microservices-communication/2.5.0/django-microservices-communication-2.5.0.tar.gz

---
+++ packages/django-microservices-communication/django-microservices-communication-2.5.0.tar.gz/django-microservices-communication-2.5.0/services_communication/call/command.py
@@ -0,0 +1,23 @@
+import json
+from datetime import timedelta
+
+from django.core.serializers.json import DjangoJSONEncoder
+from django.utils.timezone import now
+
+from services_communication.process.publisher import build_publisher_by_settings
+from services_communication.utils import camelize
+
+
+def send_command(service_name, arguments, command_name='', timeout=None):
+    meta = {}
+
+    if timeout:
+        meta['expired_time'] = now() + timedelta(seconds=timeout)
+
+    arguments['meta'] = meta
+
+    build_publisher_by_settings()._publish(
+        exchange=service_name,
+        routing_key=command_name,
+        body=json.dumps(camelize(arguments), cls=DjangoJSONEncoder)
+    )
