79af7f79866da81b0272cdccb7ba78b62edc289c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:45 +0000

    Add package deeply-learnable-package/0.1.0/deeply_learnable_package-0.1.0.tar.gz

---
+++ packages/deeply-learnable-package/deeply_learnable_package-0.1.0.tar.gz/deeply_learnable_package-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="deeply_learnable_package",  # Package name
+    version="0.1.0",    # Initial version
+    author="Konstantin",
+    description="A package for regression, image processing, and classification",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "scikit-learn>=0.24.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
