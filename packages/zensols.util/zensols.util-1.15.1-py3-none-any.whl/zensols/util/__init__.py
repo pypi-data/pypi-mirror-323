bd023dc1a38c5d1583046e21ba449a852ba7e089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:45 +0000

    Add package zensols.util/1.15.1/zensols.util-1.15.1-py3-none-any.whl

---
+++ packages/zensols.util/zensols.util-1.15.1-py3-none-any.whl/zensols/util/__init__.py
@@ -0,0 +1,11 @@
+from .writable import *
+from .fail import *
+from .std import *
+from .time import *
+from .hasher import *
+from .log import *
+from .executor import *
+from .pkgres import *
+
+# add a ``logging.TRACE`` logging level
+add_trace_level()
