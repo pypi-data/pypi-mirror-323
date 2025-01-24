df2a76606cd5cab82ca6aed9fb26839b93f4caf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:26 +0000

    Add package autobex/0.1.147/autobex-0.1.147.tar.gz

---
+++ packages/autobex/autobex-0.1.147.tar.gz/autobex-0.1.147/autobex/__init__.py
@@ -0,0 +1,6 @@
+from .osm_search_plus import OSMSearchPlus, OSMSearchError
+
+__version__ = "0.1.147"
+
+# Make OSMSearchPlus available at package level
+__all__ = ['OSMSearchPlus', 'OSMSearchError'] 
