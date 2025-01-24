d87f5b9d037c01766556303359551739ddbd72bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:03:52 +0000

    Add package django-spire/0.1.8/django_spire-0.1.8.tar.gz

---
+++ packages/django-spire/django_spire-0.1.8.tar.gz/django_spire-0.1.8/django_spire/user_account/tests/factories.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from django_spire.permission.models import PortalUser
+
+
+def create_user(username: str, password: str) -> PortalUser:
+    user = PortalUser.objects.create_user(username=username, password=password)
+    user.save()
+    return user
+
+
+def create_super_user() -> PortalUser:
+    user = PortalUser.objects.create_user(
+        username='bobert@email.com',
+        password='12345'
+    )
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
+    return user
