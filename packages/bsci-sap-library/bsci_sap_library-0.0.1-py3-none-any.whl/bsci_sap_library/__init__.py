2ee586e9b764f6e4a2bc41859cefd4f019be401e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:16 +0000

    Add package bsci-sap-library/0.0.1/bsci_sap_library-0.0.1-py3-none-any.whl

---
+++ packages/bsci-sap-library/bsci_sap_library-0.0.1-py3-none-any.whl/bsci_sap_library/__init__.py
@@ -0,0 +1,5 @@
+raise RuntimeError(
+    "This package name is reserved and cannot be used. "
+    "If you installed this by mistake, please uninstall it with:\n\n"
+    "    pip uninstall mypackage"
+)
