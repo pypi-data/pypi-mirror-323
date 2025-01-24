b6f190e944ff92246cc850aca51f36fadbc3c083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:32 +0000

    Add package automonisaur/0.6.8/automonisaur-0.6.8-py3-none-any.whl

---
+++ packages/automonisaur/automonisaur-0.6.8-py3-none-any.whl/automon/integrations/instagram/tests/test_instagram_browser.py
@@ -0,0 +1,28 @@
+import unittest
+
+from automon.integrations.instagram.client_browser import InstagramBrowserClient
+from automon.integrations.seleniumWrapper import ChromeWrapper
+
+
+class InstagramClientTest(unittest.TestCase):
+    c = InstagramBrowserClient(headless=False)
+    c.browser.config.webdriver_wrapper = ChromeWrapper()
+
+    if c.browser.run():
+
+        c.browser.get(c.urls.login_page)
+
+        # user
+        login_user = c.browser.wait_for_xpath(c.xpaths.login_user)
+        c.browser.action_click(login_user, 'user')
+        c.browser.action_type(c.login)
+
+        # password
+        password = c.browser.wait_for_xpath(c.xpaths.login_pass)
+        c.browser.action_click(password, 'password')
+
+        c.browser.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
