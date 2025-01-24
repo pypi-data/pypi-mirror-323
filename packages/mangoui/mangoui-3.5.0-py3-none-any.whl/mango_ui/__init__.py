6a703cda0056e74715b9c0d1083a1dbd427c1d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:41 +0000

    Add package mangoui/3.5.0/mangoui-3.5.0-py3-none-any.whl

---
+++ packages/mangoui/mangoui-3.5.0-py3-none-any.whl/mango_ui/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description: 
+# @Time   : 2024-10-08 16:09
+# @Author : 毛鹏
+from mango_ui.components import *
+from mango_ui.enums.enums import *
+from mango_ui.models.models import *
+from mango_ui.settings.settings import THEME
+from mango_ui.widgets import *
+from mango_ui.widget_tool import WidgetTool
+from mango_ui.resources.app_rc import *
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+from PySide6.QtSvgWidgets import *
+
+# from PySide6.QtCore import *
+# from PySide6.QtGui import *
+# from PySide6.QtWidgets import *
+# from PySide6.QtSvgWidgets import *
