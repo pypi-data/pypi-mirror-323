d7a4322b2e413bc2cbb7587cf7f356775c95bfbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:32 +0000

    Add package bsci-email-library/0.0.1/bsci_email_library-0.0.1.tar.gz

---
+++ packages/bsci-email-library/bsci_email_library-0.0.1.tar.gz/bsci_email_library-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="bsci_email_library",
+    version="0.0.1",
+    description="This package does nothing.",
+    long_description="This package is reserved and does nothing.",
+    long_description_content_type="text/plain",
+    packages=["bsci_email_library"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
