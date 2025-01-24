648997fd1863221008f1b656ac127e15da7267b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:25 +0000

    Add package socket-sdk-python/1.0.15/socket_sdk_python-1.0.15-py3-none-any.whl

---
+++ packages/socket-sdk-python/socket_sdk_python-1.0.15-py3-none-any.whl/socketdev/repositories/__init__.py
@@ -0,0 +1,23 @@
+import socketdev
+from typing import TypedDict
+
+
+class Repo(TypedDict):
+    name: str
+    description: str
+    homepage: str
+    visibility: str
+    archived: bool
+    default_branch: str
+
+
+class Repositories:
+    @staticmethod
+    def list() -> dict:
+        path = "repos"
+        response = socketdev.do_request(path=path)
+        if response.status_code == 200:
+            repos = response.json()
+        else:
+            repos = {}
+        return repos
