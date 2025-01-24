a6de9e7a7c79cc443c13120d7eea964a8e1969ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:58 +0000

    Add package auto-feishu/0.0.8/auto_feishu-0.0.8-py3-none-any.whl

---
+++ packages/auto-feishu/auto_feishu-0.0.8-py3-none-any.whl/feishu/models/group.py
@@ -0,0 +1,13 @@
+from typing import Literal, Optional
+
+from pydantic import BaseModel
+
+
+class GroupInfo(BaseModel):
+    name: str = ""
+    description: str = ""
+    owner_id: str = ""  # 群主为机器人时无返回值
+    owner_id_type: str = ""  # 群主为机器人时无返回值
+    tenant_key: str = ""
+    external: Optional[bool] = None
+    chat_status: Optional[Literal["normal", "dissolved", "dissolved_save"]] = None
