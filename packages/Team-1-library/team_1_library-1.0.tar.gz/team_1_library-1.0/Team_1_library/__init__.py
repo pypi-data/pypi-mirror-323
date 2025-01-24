33bf2c472154fa669d0b20e8fffd3625780aa407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:03:59 +0000

    Add package Team-1-library/1.0/team_1_library-1.0.tar.gz

---
+++ packages/Team-1-library/team_1_library-1.0.tar.gz/team_1_library-1.0/Team_1_library/__init__.py
@@ -0,0 +1,2 @@
+from .functions import  Automatic_Preprocess
+from .functions import Manual_Preprocess
---
+++ packages/Team-1-library/team_1_library-1.0.tar.gz/team_1_library-1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+name="Team_1_library",
+version="1.0",
+description="Data prerpocess library with missing value imputation and outlier correction functions.",
+long_description=open("README.md").read(),
+long_description_content_type="text/markdown",
+author="Jokin Agirre, Irene Alvarez, Uxue Auzmendi, Jon Lorenzo, Manex Ugarte ",
+author_email="manex.ugarte@alumni.mondragon.edu",
+url="https://github.com/Manex14/Team_1_library",
+packages=find_packages(),
+include_package_data=True,
+install_requires=[
+        "pandas",
+        "scipy",
+        "numpy",
+        "scikit-learn",
+        "fuzzywuzzy"
+    ],
+classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+],
+python_requires=">=3.8",
+)
