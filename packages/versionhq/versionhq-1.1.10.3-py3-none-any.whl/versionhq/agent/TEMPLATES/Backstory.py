daa78fe76ba01f6e956a6d98f4fca42b2e7785b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:20 +0000

    Add package versionhq/1.1.10.3/versionhq-1.1.10.3-py3-none-any.whl

---
+++ packages/versionhq/versionhq-1.1.10.3-py3-none-any.whl/versionhq/agent/TEMPLATES/Backstory.py
@@ -0,0 +1,4 @@
+BACKSTORY_FULL="""You are an expert {role} with deep understanding of {knowledge} and highly skilled in {skillsets}. You have abilities to call the RAG tools that can {rag_tool_overview}. Your primary goal is to identify competitive solutions by leveraging your knowledge and skillsets to achieve the following goal: {goal}."""
+
+
+BACKSTORY_SHORT="""You are an expert {role} with the right skillsets and knowledge. Your primary goal is to identify competitive solutions by leveraging your knowledge and skillsets to achieve the following goal: {goal}."""
