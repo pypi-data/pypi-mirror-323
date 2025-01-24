c18a81cc43a8f6bed696ee6f6add60c4a77100ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:03:41 +0000

    Add package django-spire/0.0.1/django_spire-0.0.1.tar.gz

---
+++ packages/django-spire/django_spire-0.0.1.tar.gz/django_spire-0.0.1/django_spire/user_account/factories.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from typing_extensions import TYPE_CHECKING
+
+from django_spire.permission.models import PortalUser
+
+
+def register_new_user(
+    first_name: str,
+    last_name: str,
+    email: str,
+    password: str
+) -> PortalUser:
+    return PortalUser.objects.create_user(
+        username=email.lower(),
+        email=email.lower(),
+        password=password,
+        first_name=first_name,
+        last_name=last_name
+    )
---
+++ packages/django-spire/django_spire-0.0.1.tar.gz/django_spire-0.0.1/testing/settings.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+
+import secrets
+
+from pathlib import Path
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.staticfiles',
+
+    'django_spire.ai',
+    'django_spire.core',
+    'django_spire.authentication',
+    'django_spire.authentication.mfa',
+    'django_spire.breadcrumb',
+    'django_spire.comment',
+    'django_spire.file',
+    'django_spire.form',
+    'django_spire.gamification',
+    'django_spire.help',
+    'django_spire.history',
+    'django_spire.maintenance',
+    'django_spire.notification',
+    'django_spire.options',
+    'django_spire.pagination',
+    'django_spire.permission',
+    'django_spire.search',
+    'django_spire.user_account',
+]
+
+DEBUG = True
+SECRET_KEY = secrets.token_urlsafe(50)
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SENDGRID_TEMPLATE_ID = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.db',
+    }
+}
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_glue.middleware.GlueMiddleware'
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            str(BASE_DIR / 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django_glue.context_processors.glue'
+            ],
+            'builtins': [
+            ],
+            'debug': DEBUG,
+        },
+    },
+]
