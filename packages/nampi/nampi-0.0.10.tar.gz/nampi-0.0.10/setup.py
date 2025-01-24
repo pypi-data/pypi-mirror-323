0959ea84d04a2430ace292083a3225169a031277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:45 +0000

    Add package nampi/0.0.10/nampi-0.0.10.tar.gz

---
+++ packages/nampi/nampi-0.0.10.tar.gz/nampi-0.0.10/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name='nampi',
+  version='0.0.10',
+  author='lalala',
+  author_email='vikki.flove@gmail.com',
+  description='///',
+  long_description='///',
+  long_description_content_type='text/markdown',
+  url='https://github.com/Vika-K228',
+  packages= ['nampi'],
+  python_requires='>=3.0'
+)
