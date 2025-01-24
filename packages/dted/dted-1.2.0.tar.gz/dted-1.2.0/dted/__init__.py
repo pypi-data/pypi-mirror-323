fef1a6e3be86c4a031fbe67242083ff10de6dca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:16 +0000

    Add package dted/1.2.0/dted-1.2.0.tar.gz

---
+++ packages/dted/dted-1.2.0.tar.gz/dted-1.2.0/dted/__init__.py
@@ -0,0 +1,11 @@
+""" Simplified imports for the DTED parsing module. """
+
+import importlib.metadata as metadata
+from .latlon import LatLon
+from .tile import Tile
+
+__version__ = ""
+try:
+    __version__ = metadata.version(__name__)  # type: ignore
+except FileNotFoundError:
+    pass
