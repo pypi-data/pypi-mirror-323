82cda8f08f0df1b525a368ca4d44c91a4ab5339f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:03:50 +0000

    Add package hestia-earth-models/0.65.9/hestia_earth_models-0.65.9-py3-none-any.whl

---
+++ packages/hestia-earth-models/hestia_earth_models-0.65.9-py3-none-any.whl/tests/models/poschEtAl2008/test_terrestrialEutrophicationPotentialAccumulatedExceedance.py
@@ -0,0 +1,58 @@
+import json
+from unittest.mock import patch
+
+from hestia_earth.models.poschEtAl2008.terrestrialEutrophicationPotentialAccumulatedExceedance import MODEL, TERM_ID, \
+    run
+from tests.utils import fixtures_path, fake_new_indicator
+
+class_path = f"hestia_earth.models.{MODEL}.{TERM_ID}"
+fixtures_folder = f"{fixtures_path}/{MODEL}/{TERM_ID}"
+
+
+def fake_rounded_indicator(value: float):
+    indicator = fake_new_indicator(TERM_ID, MODEL)
+    indicator['value'] = round(value, 7)
+    return indicator
+
+
+@patch(f"{class_path}._new_indicator", side_effect=fake_new_indicator)
+def test_run(*args):
+    with open(f"{fixtures_folder}/impact-assessment.jsonld", encoding='utf-8') as f:
+        impactassessment = json.load(f)
+
+    with open(f"{fixtures_folder}/Belarus/result.jsonld", encoding='utf-8') as f:
+        expected = json.load(f)
+
+    value = run(impactassessment)
+    assert value == expected
+
+
+@patch(f"{class_path}._indicator", side_effect=fake_rounded_indicator)
+def test_lookup_to_bad_country(*args):
+    """
+    Should default to region-world value
+    """
+    with open(f"{fixtures_folder}/impact-assessment.jsonld", encoding='utf-8') as f:
+        impactassessment = json.load(f)
+
+    impactassessment['country']['@id'] = "example-land-not-real"
+
+    with open(f"{fixtures_folder}/region-world/result.jsonld", encoding='utf-8') as f:
+        expected = json.load(f)
+
+    value = run(impactassessment)
+    assert value == expected
+
+
+@patch(f"{class_path}._new_indicator", side_effect=fake_new_indicator)
+def test_run_no_emissions(*args):
+    """
+    Impact assessment with no emissions should return a indicator of 0
+    """
+    with open(f"{fixtures_folder}/impact-assessment.jsonld", encoding='utf-8') as f:
+        impactassessment = json.load(f)
+
+    del impactassessment['emissionsResourceUse']
+
+    value = run(impactassessment)
+    assert value['value'] == 0
