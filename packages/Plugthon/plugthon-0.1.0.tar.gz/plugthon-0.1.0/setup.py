4a9209e6582f5b72b096897271f6bd1996b39e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:22 +0000

    Add package Plugthon/0.1.0/plugthon-0.1.0.tar.gz

---
+++ packages/Plugthon/plugthon-0.1.0.tar.gz/plugthon-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "Plugthon",
+    version = "0.1.0",
+    packages = find_packages(),
+    author = "iniridwanul",
+    author_email = "iniridwanul@gmail.com",
+    description = "Plugthon is a developer-friendly library for building modular and scalable Telegram bots.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Plugthon/Plugthon",
+    license = "AGPL",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    keywords = ["Plugthon", "Python", "Telethon", "Telegram bots", "Userbot", "Telegram"],
+    python_requires = ">=3.6",
+    install_requires = ["Telethon"]
+)
