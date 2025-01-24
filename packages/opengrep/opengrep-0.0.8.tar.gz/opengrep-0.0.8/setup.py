ed133f845c67ef4b62f4d599ee5516093e8da0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:03:56 +0000

    Add package opengrep/0.0.8/opengrep-0.0.8.tar.gz

---
+++ packages/opengrep/opengrep-0.0.8.tar.gz/opengrep-0.0.8/setup.py
@@ -0,0 +1,33 @@
+VERSION = "0.0.8"
+
+from setuptools import setup, find_packages
+
+setup(
+    name='opengrep',
+    version=VERSION,   
+    description='',
+    long_description="opengrep",
+    long_description_content_type="text/markdown",
+    url='',
+    author='Zerooo',
+    author_email='',
+    include_package_data=True,
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'tests.*', 'release']),
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'opengrep=opengrep.main:main',
+        ],
+    },
+    license='MIT',
+    install_requires=[
+        'requests',
+    ],
+    extras_require={
+        "dev": ["twine>=4.0.2"],
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+    ],
+    keywords='opengrep grep',
+)
