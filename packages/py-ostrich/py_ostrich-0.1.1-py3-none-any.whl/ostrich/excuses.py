a7b70cd54a8e41676563cd6cdea25460e2c01d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:19 +0000

    Add package py-ostrich/0.1.1/py_ostrich-0.1.1-py3-none-any.whl

---
+++ packages/py-ostrich/py_ostrich-0.1.1-py3-none-any.whl/ostrich/excuses.py
@@ -0,0 +1,23 @@
+## src/ostrich/excuses.py
+FUNNY_EXCUSES = [
+    "¯\_(ツ)_/¯ It works on my machine!",
+    "TODO: Fix this... (maybe in 2025)",
+    "If you're reading this, it's already too late",
+    "This code is like my dating life - needs work but I'm ignoring it",
+    "Here lies the dreams of clean code",
+    "Legend says the original dev is still 'getting to it'",
+    "This function is like my gym membership - unused but full of potential",
+    "Protected by copy-paste-driven development",
+    "60% of the time, it works every time",
+    "Error 404: Motivation not found",
+    "This bug is now a feature",
+    "Coffee.exe stopped working",
+    "it's not a bug, it's an undocumented feature",
+    "Days without technical debt: -42",
+    "I'll fix this when Bitcoin hits 1mil",
+    "TODO: Make this better (last modified 3 years ago)",
+    "This code is like my todo list - growing but ignored",
+    "Technical debt? More like technical investment!",
+    "This function runs on hopes and prayers",
+    "Powered by Stack Overflow"
+]
