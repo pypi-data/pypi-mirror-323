c973ae79d8e391cc67a9658913f03e84e1c775ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:42 +0000

    Add package boto3-stubs-full/1.36.5/boto3_stubs_full-1.36.5-py3-none-any.whl

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.36.5-py3-none-any.whl/mypy_boto3_mailmanager/__init__.pyi
@@ -0,0 +1,66 @@
+"""
+Main interface for mailmanager service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_mailmanager import (
+        Client,
+        ListAddonInstancesPaginator,
+        ListAddonSubscriptionsPaginator,
+        ListArchiveExportsPaginator,
+        ListArchiveSearchesPaginator,
+        ListArchivesPaginator,
+        ListIngressPointsPaginator,
+        ListRelaysPaginator,
+        ListRuleSetsPaginator,
+        ListTrafficPoliciesPaginator,
+        MailManagerClient,
+    )
+
+    session = Session()
+    client: MailManagerClient = session.client("mailmanager")
+
+    list_addon_instances_paginator: ListAddonInstancesPaginator = client.get_paginator("list_addon_instances")
+    list_addon_subscriptions_paginator: ListAddonSubscriptionsPaginator = client.get_paginator("list_addon_subscriptions")
+    list_archive_exports_paginator: ListArchiveExportsPaginator = client.get_paginator("list_archive_exports")
+    list_archive_searches_paginator: ListArchiveSearchesPaginator = client.get_paginator("list_archive_searches")
+    list_archives_paginator: ListArchivesPaginator = client.get_paginator("list_archives")
+    list_ingress_points_paginator: ListIngressPointsPaginator = client.get_paginator("list_ingress_points")
+    list_relays_paginator: ListRelaysPaginator = client.get_paginator("list_relays")
+    list_rule_sets_paginator: ListRuleSetsPaginator = client.get_paginator("list_rule_sets")
+    list_traffic_policies_paginator: ListTrafficPoliciesPaginator = client.get_paginator("list_traffic_policies")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import MailManagerClient
+from .paginator import (
+    ListAddonInstancesPaginator,
+    ListAddonSubscriptionsPaginator,
+    ListArchiveExportsPaginator,
+    ListArchiveSearchesPaginator,
+    ListArchivesPaginator,
+    ListIngressPointsPaginator,
+    ListRelaysPaginator,
+    ListRuleSetsPaginator,
+    ListTrafficPoliciesPaginator,
+)
+
+Client = MailManagerClient
+
+__all__ = (
+    "Client",
+    "ListAddonInstancesPaginator",
+    "ListAddonSubscriptionsPaginator",
+    "ListArchiveExportsPaginator",
+    "ListArchiveSearchesPaginator",
+    "ListArchivesPaginator",
+    "ListIngressPointsPaginator",
+    "ListRelaysPaginator",
+    "ListRuleSetsPaginator",
+    "ListTrafficPoliciesPaginator",
+    "MailManagerClient",
+)
