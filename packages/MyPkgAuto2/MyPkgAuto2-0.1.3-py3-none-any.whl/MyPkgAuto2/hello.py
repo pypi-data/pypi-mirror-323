c4a5a169eae3d8b6b3679226335ca2dabb8be76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:04:19 +0000

    Add package MyPkgAuto2/0.1.3/MyPkgAuto2-0.1.3-py3-none-any.whl

---
+++ packages/MyPkgAuto2/MyPkgAuto2-0.1.3-py3-none-any.whl/MyPkgAuto2/hello.py
@@ -0,0 +1,7 @@
+def auto2_say_hello(name="World"):
+    """打印 Hello, World! 或其他名字"""
+    return f"Hello, {name}!"
+
+def auto2_say_hello2(name="World"):
+    """打印 Hello, World! 或其他名字"""
+    return f"{name}, {name}, Hello!"
