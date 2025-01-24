02e00081044216f38726b97b51229e9e287758fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:38 +0000

    Add package setlogging/0.3.3/setlogging-0.3.3.tar.gz

---
+++ packages/setlogging/setlogging-0.3.3.tar.gz/setlogging-0.3.3/tests/__init__.py
@@ -0,0 +1,13 @@
+# src/setlogging/__init__.py
+
+from setlogging.logger import CustomFormatter, setup_logging, get_logger
+
+__version__ = "0.3.2"
+__author__ = "Jie Yan"
+__email__ = "kiki3890528@gmail.com"
+
+__all__ = [
+    "CustomFormatter",
+    "setup_logging",
+    "get_logger",
+]
