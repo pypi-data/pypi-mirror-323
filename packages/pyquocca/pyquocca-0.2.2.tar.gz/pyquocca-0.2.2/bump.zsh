7132aef89f50ba990718a717423879029c57bd17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:04:00 +0000

    Add package pyquocca/0.2.2/pyquocca-0.2.2.tar.gz

---
+++ packages/pyquocca/pyquocca-0.2.2.tar.gz/pyquocca-0.2.2/bump.zsh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+
+cd $(dirname "$0")
+
+new="pyquocca==$(yq '.project.version' pyproject.toml)"
+echo Replacing existing pyquocca requirements with $new...
+
+# Update parent module challenges
+cd ../../../challenges
+
+for file in **/requirements.txt; do
+    old=$(grep -E 'pyquocca==\d+\.\d+\.\d+' $file)
+    if [[ $? == '0' ]]; then
+        echo $(dirname $file): $old '-->' $new
+        safe=$(sed 's/\./\\./g' <<<$old)
+        sed -i.bak s/$safe/$new/g $file
+        rm $file.bak
+    else
+        echo $(dirname $file): no pyquocca requirement
+    fi
+done
