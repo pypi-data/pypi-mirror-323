f04a6d5f1b9c69db893bb2a0c209a324df28ee45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:04:04 +0000

    Add package G4-Lib/0.1.1/g4_lib-0.1.1.tar.gz

---
+++ packages/G4-Lib/g4_lib-0.1.1.tar.gz/g4_lib-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup( 
+ name="G4_Lib",
+ version="0.1.1",
+ description="Automatic preprocessing library for dataset cleaning",
+ long_description=open("README.md").read(),
+ long_description_content_type="text/markdown",
+ author="Group 4: Aiora De La Lama, Katalin Garcia, Sofia Garlito, Maren Lakalle, Julen Pagola, Naia Sanz",
+ author_email="sofiamaria.garlito@mondragon.alumni.edu",
+ packages=find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+ python_requires=">=3.6",
+ install_requires=[
+     "pandas>=1.0.0",
+     "numpy>=1.18.0",
+     "scikit-learn>0.24.0",
+     "scipy>=1.0.0"
+ ]
+)
