76ce94c8d44dab503d49ecde099a521961c1406b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:21 +0000

    Add package streamlit-auth-mfa/2.0.2/streamlit_auth_mfa-2.0.2.tar.gz

---
+++ packages/streamlit-auth-mfa/streamlit_auth_mfa-2.0.2.tar.gz/streamlit_auth_mfa-2.0.2/streamlit_auth/authentication/__init__.py
@@ -0,0 +1,23 @@
+__all__ = [
+    'Authenticate',  # Classe de autenticação
+    
+    'pagina_gerenciar_permissao',  # Função para gerenciar permissões
+    'pagina_gerenciar_usuarios',  # Função para gerenciar usuários
+    'main_page_gerenciar',  # Pagina principal
+    
+    'TbUsuarioStreamlit',  # Modelo de usuário
+    'TbSessaoStreamlit',  # Modelo de sessão
+    'TbPermissaoUsuariosStreamlit',  # Modelo de permissões
+]
+
+from .backend.auth import Authenticate
+from .frontend.perms import pagina_gerenciar_permissao
+from .frontend.users import pagina_gerenciar_usuarios
+from .frontend.main_page import main_page_gerenciar
+
+# Modelos de banco de dados
+from .backend.models import (
+    TbUsuarioStreamlit,
+    TbSessaoStreamlit,
+    TbPermissaoUsuariosStreamlit
+    )
