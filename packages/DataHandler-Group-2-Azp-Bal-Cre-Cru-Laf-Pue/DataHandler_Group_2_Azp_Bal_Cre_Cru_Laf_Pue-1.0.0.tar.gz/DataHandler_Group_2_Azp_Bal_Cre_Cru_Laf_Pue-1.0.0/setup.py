1e51201340734f180097031475e7f2f141ff5689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:03:44 +0000

    Add package DataHandler-Group-2-Azp-Bal-Cre-Cru-Laf-Pue/1.0.0/DataHandler_Group_2_Azp_Bal_Cre_Cru_Laf_Pue-1.0.0.tar.gz

---
+++ packages/DataHandler-Group-2-Azp-Bal-Cre-Cru-Laf-Pue/DataHandler_Group_2_Azp_Bal_Cre_Cru_Laf_Pue-1.0.0.tar.gz/DataHandler_Group_2_Azp_Bal_Cre_Cru_Laf_Pue-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name="DataHandler_Group_2_Azp_Bal_Cre_Cru_Laf_Pue",
+    version="1.0.0",
+    description="Dataset handler of group 2",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    author="Irati Azpilgain, Mikel Ballesteros, Ander Crespo, Estibaliz Cruz, Lucas Lafuente y Mikel Puerta",
+    author_email="mikel.puerta@alumni.mondragon.edu",
+    url="https://github.com/Group_2_DataHandler/Group_2_DataHandler",
+    packages=find_packages(),
+    py_modules=["DataHandler_Group_2_Azp_Bal_Cre_Cru_Laf_Pue"],
+    install_requires=["pandas","numpy","seaborn","matplotlib","scikit-learn","openpyxl","xlrd"],
+    classifiers=["Programming Language :: Python :: 3",    "License :: OSI Approved :: MIT License","Operating System :: OS Independent",],
+    python_requires=">=3.6",
+)
