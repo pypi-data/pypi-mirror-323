313a34268a95299f604fa7c19576cf3650e29f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:05:02 +0000

    Add package netspresso/1.10.0b13/netspresso-1.10.0b13-py3-none-any.whl

---
+++ packages/netspresso/netspresso-1.10.0b13-py3-none-any.whl/netspresso/metadata/quantizer.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass, field
+from typing import Dict, Union
+
+from netspresso.enums import TaskType
+from netspresso.metadata.common import BaseMetadata, ModelInfo
+from qai_hub import QuantizeDtype
+
+
+@dataclass
+class QuantizeInfo:
+    quantize_task_uuid: str = ""
+    model_file_name: str = ""
+    input_model_uuid: str = ""
+    output_model_uuid: str = ""
+
+
+@dataclass
+class QuantizerMetadata(BaseMetadata):
+    task_type: TaskType = TaskType.QUANTIZE
+    input_model_path: str = ""
+    quantized_model_path: str = ""
+    model_info: ModelInfo = field(default_factory=ModelInfo)
+    quantize_info: QuantizeInfo = field(default_factory=QuantizeInfo)
