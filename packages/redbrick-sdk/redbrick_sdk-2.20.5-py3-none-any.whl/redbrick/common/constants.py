4db0470786b8ff83d72b5a5a11e6483e01b41e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:03:59 +0000

    Add package redbrick-sdk/2.20.5/redbrick_sdk-2.20.5-py3-none-any.whl

---
+++ packages/redbrick-sdk/redbrick_sdk-2.20.5-py3-none-any.whl/redbrick/common/constants.py
@@ -0,0 +1,20 @@
+"""Constants."""
+
+MAX_CONCURRENCY = 30
+MAX_FILE_BATCH_SIZE = 5
+MAX_RETRY_ATTEMPTS = 3
+REQUEST_TIMEOUT = 30
+
+DEFAULT_URL = "https://api.redbrickai.com"
+
+PEERLESS_ERRORS = (
+    KeyboardInterrupt,
+    PermissionError,
+    TimeoutError,
+    ConnectionError,
+    ValueError,
+    SystemError,
+    SystemExit,
+)
+
+DUMMY_FILE_PATH = "https://test"
---
+++ packages/redbrick-sdk/redbrick_sdk-2.20.5-py3-none-any.whl/redbrick/common/upload.py
@@ -0,0 +1,150 @@
+"""Abstract interface to upload."""
+
+from typing import List, Dict, Optional, Any, Sequence
+from abc import ABC, abstractmethod
+
+import aiohttp
+
+from redbrick.types.task import InputTask
+
+
+class UploadControllerInterface(ABC):
+    """Abstract interface to define methods for Upload."""
+
+    @abstractmethod
+    async def create_datapoint_async(  # pylint: disable=too-many-locals
+        self,
+        aio_client: aiohttp.ClientSession,
+        org_id: str,
+        project_id: str,
+        storage_id: str,
+        name: str,
+        items: List[str],
+        heat_maps: Optional[List[Dict]],
+        transforms: Optional[List[Dict]],
+        centerlines: Optional[List[Dict]],
+        labels_data: Optional[str],
+        labels_map: Optional[List[Dict]] = None,
+        series_info: Optional[List[Dict]] = None,
+        meta_data: Optional[Dict] = None,
+        is_ground_truth: bool = False,
+        pre_assign: Optional[Dict] = None,
+        priority: Optional[float] = None,
+        attributes: Optional[List[Dict]] = None,
+    ) -> Dict:
+        """
+        Create a datapoint and returns its taskId.
+
+        Name must be unique in the project.
+        """
+
+    @abstractmethod
+    async def update_items_async(
+        self,
+        aio_client: aiohttp.ClientSession,
+        org_id: str,
+        project_id: str,
+        storage_id: str,
+        task_id: str,
+        items: Optional[List[str]] = None,
+        series_info: Optional[List[Dict]] = None,
+        heat_maps: Optional[List[Dict]] = None,
+        transforms: Optional[List[Dict]] = None,
+        centerlines: Optional[List[Dict]] = None,
+        meta_data: Optional[Dict] = None,
+    ) -> Dict:
+        """Update items in a datapoint."""
+
+    @abstractmethod
+    def items_upload_presign(
+        self, org_id: str, project_id: str, files: List[str], file_type: List[str]
+    ) -> List[Dict[Any, Any]]:
+        """Get a presigned url for uploading items."""
+
+    @abstractmethod
+    async def delete_tasks(
+        self,
+        aio_client: aiohttp.ClientSession,
+        org_id: str,
+        project_id: str,
+        task_ids: List[str],
+    ) -> bool:
+        """Delete tasks in a project."""
+
+    @abstractmethod
+    async def delete_tasks_by_name(
+        self,
+        aio_client: aiohttp.ClientSession,
+        org_id: str,
+        project_id: str,
+        task_names: List[str],
+    ) -> bool:
+        """Delete tasks in a project by task names."""
+
+    @abstractmethod
+    async def generate_items_list(
+        self,
+        aio_client: aiohttp.ClientSession,
+        files: List[str],
+        import_type: str,
+        as_study: bool = False,
+        windows: bool = False,
+    ) -> str:
+        """Generate direct upload items list."""
+
+    @abstractmethod
+    async def validate_and_convert_to_import_format(
+        self,
+        aio_client: aiohttp.ClientSession,
+        data: List[InputTask],
+        convert: Optional[bool] = None,
+        storage_id: Optional[str] = None,
+    ) -> Dict:
+        """Validate and convert tasks format."""
+
+    @abstractmethod
+    def import_tasks_from_workspace(
+        self,
+        org_id: str,
+        project_id: str,
+        source_project_id: str,
+        task_search: List[Dict],
+        with_labels: bool = False,
+    ) -> Dict:
+        """Import tasks from another project in the same workspace."""
+
+    @abstractmethod
+    async def update_priority(
+        self,
+        session: aiohttp.ClientSession,
+        org_id: str,
+        project_id: str,
+        tasks: List[Dict],
+    ) -> Optional[str]:
+        """Update tasks priorities."""
+
+    @abstractmethod
+    async def update_labels(
+        self,
+        session: aiohttp.ClientSession,
+        org_id: str,
+        project_id: str,
+        task_id: str,
+        labels: str,
+        labels_map: Optional[Sequence[Optional[Dict]]] = None,
+        finalize: bool = False,
+        time_spent_ms: Optional[int] = None,
+        extra_data: Optional[Dict] = None,
+    ) -> None:
+        """Update tasks labels."""
+
+    @abstractmethod
+    async def send_tasks_to_stage(
+        self,
+        session: aiohttp.ClientSession,
+        org_id: str,
+        project_id: str,
+        task_ids: List[str],
+        stage_name: str,
+    ) -> Optional[str]:
+        """Send tasks to different stage."""
