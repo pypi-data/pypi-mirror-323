62c64cb978fbf5d578b363a05782b95b3a41e246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:03:41 +0000

    Add package shruti-package/0.1/shruti-package-0.1.tar.gz

---
+++ packages/shruti-package/shruti-package-0.1.tar.gz/shruti-package-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shruti-package",  # Replace with your package name
+    version="0.1",  # Version of your package
+    packages=find_packages(),  # Automatically finds all sub-packages
+    install_requires=[],  # List any dependencies your package needs
+    long_description=open('README.md').read(),  # Optional: Read the README
+    long_description_content_type='text/markdown',  # Optional: If you're using Markdown for README
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # If you're using MIT License
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',  # Define Python version requirements
+   entry_points={
+    'console_scripts': [
+        'run-my-script = shruti_package.script:run_script',
+    ],
+}
+)
+
