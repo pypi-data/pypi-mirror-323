7a1ccc2a6d9951c765c781dd62f521b9e3c03792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:07 +0000

    Add package commanding/0.0.2/commanding-0.0.2.tar.gz

---
+++ packages/commanding/commanding-0.0.2.tar.gz/commanding-0.0.2/commanding/__init__.py
@@ -0,0 +1,10 @@
+from commanding.cli_manager import CLIManager
+from commanding.cli_manager import info, warn, error, debug
+
+__all__ = [
+    "CLIManager",
+    "info",
+    "warn",
+    "error",
+    "debug"
+]
