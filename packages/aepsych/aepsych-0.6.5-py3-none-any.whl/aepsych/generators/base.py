2ddb9a2f94ff3e0646c37263b446e8a58c485b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:25 +0000

    Add package aepsych/0.6.5/aepsych-0.6.5-py3-none-any.whl

---
+++ packages/aepsych/aepsych-0.6.5-py3-none-any.whl/aepsych/generators/base.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+import abc
+import re
+import warnings
+from inspect import _empty, signature
+from typing import Any, Dict, Generic, Optional, Protocol, runtime_checkable, TypeVar
+
+import torch
+from aepsych.config import Config
+from aepsych.models.base import AEPsychMixin
+from botorch.acquisition import (
+    AcquisitionFunction,
+    LogNoisyExpectedImprovement,
+    NoisyExpectedImprovement,
+    qLogNoisyExpectedImprovement,
+    qNoisyExpectedImprovement,
+)
+
+AEPsychModelType = TypeVar("AEPsychModelType", bound=AEPsychMixin)
+
+
+@runtime_checkable
+class AcqArgProtocol(Protocol):
+    @classmethod
+    def from_config(cls, config: Config) -> Any:
+        pass
+
+
+class AEPsychGenerator(abc.ABC, Generic[AEPsychModelType]):
+    """Abstract base class for generators, which are responsible for generating new points to sample."""
+
+    _requires_model = True
+    baseline_requiring_acqfs = [
+        qNoisyExpectedImprovement,
+        NoisyExpectedImprovement,
+        qLogNoisyExpectedImprovement,
+        LogNoisyExpectedImprovement,
+    ]
+    stimuli_per_trial = 1
+    max_asks: Optional[int] = None
+
+    acqf: AcquisitionFunction
+    acqf_kwargs: Dict[str, Any]
+
+    def __init__(
+        self,
+    ) -> None:
+        pass
+
+    @abc.abstractmethod
+    def gen(self, num_points: int, model: AEPsychModelType) -> torch.Tensor:
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    def from_config(cls, config: Config) -> Any:
+        pass
+
+    @classmethod
+    def _get_acqf_options(
+        cls, acqf: AcquisitionFunction, config: Config
+    ) -> Dict[str, Any]:
+        """Get the extra arguments for the acquisition function from the config.
+
+        Args:
+            acqf (AcquisitionFunction): The acquisition function to get arguments for.
+            config (Config): The configuration object.
+
+        Returns:
+            Dict[str, Any]: The extra arguments for the acquisition function.
+        """
+
+        if acqf is not None:
+            acqf_name = acqf.__name__
+
+            # model is not an extra arg, it's a default arg
+            acqf_kwargs = signature(acqf).parameters
+            acqf_args_expected = [i for i in list(acqf_kwargs.keys()) if i != "model"]
+
+            # this is still very ugly
+            extra_acqf_args = {}
+            if acqf_name in config:
+                full_section = config[acqf_name]
+                for k in acqf_args_expected:
+                    # if this thing is configured
+                    if k in full_section.keys():
+                        v = config.get(acqf_name, k)
+                        # if it's an object make it an object
+                        if full_section[k] in Config.registered_names.keys():
+                            extra_acqf_args[k] = config.getobj(acqf_name, k)
+                        elif re.search(
+                            r"^\[.*\]$", v, flags=re.DOTALL
+                        ):  # use regex to check if the value is a list
+                            extra_acqf_args[k] = config._str_to_list(v)  # type: ignore
+                        else:
+                            # otherwise try a float
+                            try:
+                                extra_acqf_args[k] = config.getfloat(acqf_name, k)
+                            # finally just return a string
+                            except ValueError:
+                                extra_acqf_args[k] = config.get(acqf_name, k)
+
+            # next, do more processing
+            for k, v in extra_acqf_args.items():
+                if hasattr(v, "from_config"):  # configure if needed
+                    assert isinstance(v, AcqArgProtocol)  # make mypy happy
+                    extra_acqf_args[k] = v.from_config(config)
+                elif isinstance(v, type):  # instaniate a class if needed
+                    extra_acqf_args[k] = v()
+
+            # Final checks, bandaid
+            for key, value in acqf_kwargs.items():
+                if key == "model":  # Model is handled separately
+                    continue
+                if value.default == _empty and key not in extra_acqf_args:
+                    if key not in config["common"]:
+                        # HACK: Not actually sure why some required args can be missing
+                        warnings.warn(
+                            f"{acqf_name} requires the {key} option but we could not find it.",
+                            UserWarning,
+                        )
+                        continue
+
+                    # A required parameter is missing! Look for it in common
+                    config_str = config.get("common", key)
+
+                    # An object that we know about
+                    if config_str in Config.registered_names.keys():
+                        extra_acqf_args[key] = config.getobj("common", key)
+
+                    # Some sequence
+                    if "[" in config_str and "]" in config_str:
+                        # Try to turn it into a tensor or fallback to list
+                        try:
+                            extra_acqf_args[key] = config.gettensor("common", key)
+                        except ValueError:
+                            extra_acqf_args[key] = config.getlist("common", key)
+
+        else:
+            extra_acqf_args = {}
+
+        return extra_acqf_args
---
+++ packages/aepsych/aepsych-0.6.5-py3-none-any.whl/aepsych/generators/optimize_acqf_generator.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+from __future__ import annotations
+
+import inspect
+import time
+from typing import Any, Dict, Optional
+
+import torch
+from aepsych.config import Config
+from aepsych.generators.base import AEPsychGenerator
+from aepsych.models.base import ModelProtocol
+from aepsych.utils_logging import getLogger
+from botorch.acquisition import (
+    AcquisitionFunction,
+    LogNoisyExpectedImprovement,
+    NoisyExpectedImprovement,
+    qLogNoisyExpectedImprovement,
+    qNoisyExpectedImprovement,
+)
+from botorch.acquisition.preference import AnalyticExpectedUtilityOfBestOption
+from botorch.optim import optimize_acqf
+
+logger = getLogger()
+
+
+class OptimizeAcqfGenerator(AEPsychGenerator):
+    """Generator that chooses points by minimizing an acquisition function."""
+
+    baseline_requiring_acqfs = [
+        NoisyExpectedImprovement,
+        LogNoisyExpectedImprovement,
+        qNoisyExpectedImprovement,
+        qLogNoisyExpectedImprovement,
+    ]
+
+    def __init__(
+        self,
+        lb: torch.Tensor,
+        ub: torch.Tensor,
+        acqf: AcquisitionFunction,
+        acqf_kwargs: Optional[Dict[str, Any]] = None,
+        restarts: int = 10,
+        samps: int = 1000,
+        max_gen_time: Optional[float] = None,
+        stimuli_per_trial: int = 1,
+    ) -> None:
+        """Initialize OptimizeAcqfGenerator.
+        Args:
+            lb (torch.Tensor): Lower bounds for the optimization.
+            ub (torch.Tensor): Upper bounds for the optimization.
+            acqf (AcquisitionFunction): Acquisition function to use.
+            acqf_kwargs (Dict[str, object], optional): Extra arguments to
+                pass to acquisition function. Defaults to no arguments.
+            restarts (int): Number of restarts for acquisition function optimization. Defaults to 10.
+            samps (int): Number of samples for quasi-random initialization of the acquisition function optimizer. Defaults to 1000.
+            max_gen_time (float, optional): Maximum time (in seconds) to optimize the acquisition function. Defaults to None.
+            stimuli_per_trial (int): Number of stimuli per trial. Defaults to 1.
+        """
+
+        if acqf_kwargs is None:
+            acqf_kwargs = {}
+        self.acqf = acqf
+        self.acqf_kwargs = acqf_kwargs
+        self.restarts = restarts
+        self.samps = samps
+        self.max_gen_time = max_gen_time
+        self.stimuli_per_trial = stimuli_per_trial
+        self.lb = lb
+        self.ub = ub
+
+    def _instantiate_acquisition_fn(self, model: ModelProtocol) -> AcquisitionFunction:
+        """
+        Instantiates the acquisition function with the specified model and additional arguments.
+
+        Args:
+            model (ModelProtocol): The model to use with the acquisition function.
+
+        Returns:
+            AcquisitionFunction: Configured acquisition function.
+        """
+        if self.acqf == AnalyticExpectedUtilityOfBestOption:
+            return self.acqf(pref_model=model)
+
+        if hasattr(model, "device"):
+            if "lb" in self.acqf_kwargs:
+                if not isinstance(self.acqf_kwargs["lb"], torch.Tensor):
+                    self.acqf_kwargs["lb"] = torch.tensor(self.acqf_kwargs["lb"])
+
+                self.acqf_kwargs["lb"] = self.acqf_kwargs["lb"].to(model.device)
+
+            if "ub" in self.acqf_kwargs:
+                if not isinstance(self.acqf_kwargs["ub"], torch.Tensor):
+                    self.acqf_kwargs["ub"] = torch.tensor(self.acqf_kwargs["ub"])
+
+                self.acqf_kwargs["ub"] = self.acqf_kwargs["ub"].to(model.device)
+
+        if self.acqf in self.baseline_requiring_acqfs:
+            return self.acqf(model, model.train_inputs[0], **self.acqf_kwargs)
+        else:
+            return self.acqf(model=model, **self.acqf_kwargs)
+
+    def gen(self, num_points: int, model: ModelProtocol, **gen_options) -> torch.Tensor:
+        """Query next point(s) to run by optimizing the acquisition function.
+        Args:
+            num_points (int): Number of points to query.
+            model (ModelProtocol): Fitted model of the data.
+        Returns:
+            torch.Tensor: Next set of point(s) to evaluate, [num_points x dim].
+        """
+
+        if self.stimuli_per_trial == 2:
+            qbatch_points = self._gen(
+                num_points=num_points * 2, model=model, **gen_options
+            )
+
+            # output of super() is (q, dim) but the contract is (num_points, dim, 2)
+            # so we need to split q into q and pairs and then move the pair dim to the end
+            return qbatch_points.reshape(num_points, 2, -1).swapaxes(-1, -2)
+
+        else:
+            return self._gen(num_points=num_points, model=model, **gen_options)
+
+    def _gen(
+        self, num_points: int, model: ModelProtocol, **gen_options: Dict[str, Any]
+    ) -> torch.Tensor:
+        """
+        Generates the next query points by optimizing the acquisition function.
+
+        Args:
+            num_points (int): Number of points to query.
+            model (ModelProtocol): Fitted model of the data.
+            gen_options (Dict[str, Any]): Additional options for generating points, such as custom configurations.
+
+        Returns:
+            torch.Tensor: Next set of points to evaluate, with shape [num_points x dim].
+        """
+        # eval should be inherited from superclass
+        model.eval()  # type: ignore
+        acqf = self._instantiate_acquisition_fn(model)
+
+        if hasattr(model, "device"):
+            self.lb = self.lb.to(model.device)
+            self.ub = self.ub.to(model.device)
+
+        logger.info("Starting gen...")
+        starttime = time.time()
+
+        new_candidate, _ = optimize_acqf(
+            acq_function=acqf,
+            bounds=torch.stack([self.lb, self.ub]),
+            q=num_points,
+            num_restarts=self.restarts,
+            raw_samples=self.samps,
+            timeout_sec=self.max_gen_time,
+            **gen_options,
+        )
+
+        logger.info(f"Gen done, time={time.time()-starttime}")
+        return new_candidate
+
+    @classmethod
+    def from_config(cls, config: Config) -> "OptimizeAcqfGenerator":
+        """
+        Creates an instance of OptimizeAcqfGenerator from a configuration object.
+
+        Args:
+            config (Config): Configuration object containing initialization parameters.
+
+        Returns:
+            OptimizeAcqfGenerator: A configured instance of OptimizeAcqfGenerator with specified acquisition function,
+            restart and sample parameters, maximum generation time, and stimuli per trial.
+        """
+        classname = cls.__name__
+        lb = config.gettensor(classname, "lb")
+        ub = config.gettensor(classname, "ub")
+        acqf = config.getobj(classname, "acqf", fallback=None)
+        extra_acqf_args = cls._get_acqf_options(acqf, config)
+        stimuli_per_trial = config.getint(classname, "stimuli_per_trial")
+        restarts = config.getint(classname, "restarts", fallback=10)
+        samps = config.getint(classname, "samps", fallback=1000)
+        max_gen_time = config.getfloat(classname, "max_gen_time", fallback=None)
+
+        return cls(
+            lb=lb,
+            ub=ub,
+            acqf=acqf,
+            acqf_kwargs=extra_acqf_args,
+            restarts=restarts,
+            samps=samps,
+            max_gen_time=max_gen_time,
+            stimuli_per_trial=stimuli_per_trial,
+        )
