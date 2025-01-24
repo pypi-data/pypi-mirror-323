3dc1f6a282e1aea047a2616be9d32ec7c4f4035c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:03:16 +0000

    Add package osiris-utils/1.0.0/osiris_utils-1.0.0.tar.gz

---
+++ packages/osiris-utils/osiris_utils-1.0.0.tar.gz/osiris_utils-1.0.0/osiris_utils/__init__.py
@@ -0,0 +1,3 @@
+from .utils import (time_estimation, filesize_estimation, transverse_average, integrate, animate_2D,
+                    save_data, read_data, mft_decomposition, courant2D)
+from .data import create_dataset, open1D, open2D, open3D, read_osiris_file
