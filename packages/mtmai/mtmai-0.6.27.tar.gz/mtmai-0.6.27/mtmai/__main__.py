c2eb908a4ee029a3b2c1e39d2d5b6b790fca0b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:03:44 +0000

    Add package mtmai/0.6.27/mtmai-0.6.27.tar.gz

---
+++ packages/mtmai/mtmai-0.6.27.tar.gz/mtmai-0.6.27/mtmai/__main__.py
@@ -0,0 +1,50 @@
+import asyncio
+
+import click
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import mtmai.core.bootstraps as bootstraps
+
+bootstraps.bootstrap_core()
+
+
+def main():
+    @click.group()
+    def cli():
+        pass
+
+    @cli.command()
+    def serve():
+        from mtmai.core.config import settings
+        from mtmai.core.logging import get_logger
+        from mtmai.server import serve
+
+        logger = get_logger()
+        logger.info("🚀 call serve : %s:%s", settings.HOSTNAME, settings.PORT)
+        asyncio.run(serve())
+
+    # @cli.command()
+    # @click.option("--url", required=False)
+    # def worker(url):
+    #     from mtmai.worker import WorkerApp
+
+    #     worker_app = WorkerApp(url)
+    #     asyncio.run(worker_app.deploy_mtmai_workers())
+
+    @cli.command()
+    def gradio():
+        from mtmai.gradio_app import demo
+
+        demo.launch(
+            share=True,
+            server_name="0.0.0.0",
+            server_port=18089,
+        )
+
+    cli()
+
+
+if __name__ == "__main__":
+    main()
