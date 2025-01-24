1195d4972d1a513f0225de69f66b53caa66791fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:36 +0000

    Add package rcquant-sdk/0.0.250124.11/rcquant_sdk-0.0.250124.11-py3-none-any.whl

---
+++ packages/rcquant-sdk/rcquant_sdk-0.0.250124.11-py3-none-any.whl/rcquant_sdk/data/trade/read_instrument_param_data.py
@@ -0,0 +1,45 @@
+from typing import List
+from ...interface import IData
+from ...packer.trade.read_instrument_param_data_packer import ReadInstrumentParamDataPacker
+from .instrument_data import InstrumentData
+
+
+class ReadInstrumentParamData(IData):
+    def __init__(self, exchange_id: List[str] = [], instrument_id: List[str] = [], unicode: List[str] = [], base_path=''):
+        super().__init__(ReadInstrumentParamDataPacker(self))
+        self._ExchangeID: List[str] = exchange_id.copy()
+        self._InstrumentID: List[str] = instrument_id.copy()
+        self._UniCode: List[str] = unicode.copy()
+        self._DataList: List[InstrumentData] = []
+
+    @property
+    def ExchangeID(self):
+        return self._ExchangeID
+
+    @ExchangeID.setter
+    def ExchangeID(self, value: List[str]):
+        self._ExchangeID = value
+
+    @property
+    def InstrumentID(self):
+        return self._InstrumentID
+
+    @InstrumentID.setter
+    def InstrumentID(self, value: List[str]):
+        self._InstrumentID = value
+
+    @property
+    def UniCode(self):
+        return self._UniCode
+
+    @UniCode.setter
+    def UniCode(self, value: List[str]):
+        self._IUniCode = value
+
+    @property
+    def DataList(self):
+        return self._DataList
+
+    @DataList.setter
+    def DataList(self, value: List[InstrumentData]):
+        self._DataList = value
