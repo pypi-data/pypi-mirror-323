dae3e3df0fdfda68a8b07d9a24e7c8302b699271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:03:39 +0000

    Add package sparkplug-api-feature-flags/1.0.2/sparkplug_api_feature_flags-1.0.2.tar.gz

---
+++ packages/sparkplug-api-feature-flags/sparkplug_api_feature_flags-1.0.2.tar.gz/sparkplug_api_feature_flags-1.0.2/sparkplug_feature_flags/serializers/flag_access_read.py
@@ -0,0 +1,45 @@
+# django
+from django.contrib.auth.base_user import AbstractBaseUser
+
+# contrib
+from rest_framework.serializers import (
+    ModelSerializer,
+    SlugRelatedField,
+)
+
+# app
+from ..models import (
+    FeatureFlag,
+    FlagAccess,
+)
+from .feature_flag_teaser import FeatureFlagTeaser
+
+
+class FlagAccessRead(
+    ModelSerializer["FlagAccess"],
+):
+    feature_flag_uuid: "SlugRelatedField[FeatureFlag]" = SlugRelatedField(
+        slug_field="uuid",
+        source="feature_flag",
+        read_only=True,
+    )
+
+    feature_flag = FeatureFlagTeaser(read_only=True)
+
+    user_uuid: "SlugRelatedField[type[AbstractBaseUser]]" = SlugRelatedField(
+        slug_field="uuid",
+        source="user",
+        read_only=True,
+    )
+
+    class Meta:
+        model = FlagAccess
+
+        fields = (
+            "uuid",
+            "feature_flag_uuid",
+            "feature_flag",
+            "user_uuid",
+        )
+
+        read_only_fields = ("__all__",)
