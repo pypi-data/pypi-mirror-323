18a21d4ecd259c7ca3673e5459a37243a62ac7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:04:54 +0000

    Add package softwareai/0.5.50/SoftwareAI-0.5.50.tar.gz

---
+++ packages/softwareai/SoftwareAI-0.5.50.tar.gz/SoftwareAI-0.5.50/softwareai/CoreApp/SoftwareAI/Functions_Submit_Outputs/Software_Support/TicketProblem.py
@@ -0,0 +1,114 @@
+#########################################
+# IMPORT SoftwareAI Libs 
+from softwareai.CoreApp._init_libs_ import *
+#########################################
+# IMPORT SoftwareAI Core
+from softwareai.CoreApp._init_core_ import * 
+#########################################
+
+# IMPORT SoftwareAI Functions
+from softwareai.CoreApp._init_functions_ import *
+#########################################
+
+tool_outputs = []
+def submit_output_TicketProblem(function_name,
+                                function_arguments,
+                                tool_call,
+                                threead_id,
+                                client,
+                                run,
+                                appfb
+                                ):
+
+    global tool_outputs
+    if function_name == 'OpenSupportTicketProblem':
+        args = json.loads(function_arguments)
+        result = OpenSupportTicketProblem(
+            user_email=args['user_email'],
+            issue_description=args['issue_description'],
+            appfb=appfb
+        )
+        tool_call_id = tool_call.id
+        client.beta.threads.runs.submit_tool_outputs(
+            thread_id=threead_id,
+            run_id=run.id,
+            tool_outputs=[
+                {
+                    "tool_call_id": tool_call_id, 
+                    "output": json.dumps(result)  
+                }
+            ]
+        )
+
+
+
+
+    if function_name == 'CloseSupportTicketProblem':
+        args = json.loads(function_arguments)
+        result = CloseSupportTicketProblem(
+            ticketid=args['ticketid'],
+            appfb=appfb
+        )
+        tool_outputs.append({
+        "tool_call_id": tool_call.id,
+        "output": result
+        })
+
+        if tool_outputs:
+            try:
+                run = client.beta.threads.runs.submit_tool_outputs_and_poll(
+                thread_id=threead_id,
+                run_id=run.id,
+                tool_outputs=tool_outputs
+                )
+                print("Tool outputs submitted successfully.")
+                tool_outputs = []
+                return True
+            except Exception as e:
+                print("Failed to submit tool outputs:", e)
+        else:
+            print("No tool outputs to submit.")
+
+
+    if function_name == 'RecordCSAT':
+        args = json.loads(function_arguments)
+        result = RecordCSAT(
+            ticketid=args['ticketid'], 
+            csat_score=args['csat_score'],
+            appfb=appfb
+        )
+        tool_outputs.append({
+        "tool_call_id": tool_call.id,
+        "output": result
+        })
+
+        if tool_outputs:
+            try:
+                run = client.beta.threads.runs.submit_tool_outputs_and_poll(
+                thread_id=threead_id,
+                run_id=run.id,
+                tool_outputs=tool_outputs
+                )
+                print("Tool outputs submitted successfully.")
+                tool_outputs = []
+                return True
+            except Exception as e:
+                print("Failed to submit tool outputs:", e)
+        else:
+            print("No tool outputs to submit.")
+
+
+    if tool_outputs:
+        try:
+            run = client.beta.threads.runs.submit_tool_outputs_and_poll(
+            thread_id=threead_id,
+            run_id=run.id,
+            tool_outputs=tool_outputs
+            )
+            print("Tool outputs submitted successfully.")
+            tool_outputs = []
+            return True
+        except Exception as e:
+            print("Failed to submit tool outputs:", e)
+    else:
+        print("No tool outputs to submit.")
