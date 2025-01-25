c09ea9c393ed2139874e941d7e987b19e9e52906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:32 +0000

    Add package chap-core/0.1.2/chap_core-0.1.2.tar.gz

---
+++ packages/chap-core/chap_core-0.1.2.tar.gz/chap_core-0.1.2/chap_core/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for chap-core."""
+
+__author__ = """Sandvelab"""
+__email__ = "knutdrand@gmail.com"
+__version__ = "0.1.2"
+
+from . import fetch
+from . import data
+
+__all__ = ["fetch", "data"]
