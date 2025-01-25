4e4085a07138a302d07d6eb8a58f695e4b597c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:03:52 +0000

    Add package somali-stopwords/0.1/somali_stopwords-0.1.tar.gz

---
+++ packages/somali-stopwords/somali_stopwords-0.1.tar.gz/somali_stopwords-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='somali_stopwords',
+    version='0.1',
+    packages=find_packages(),
+    description='A library to handle Somali stopwords',
+    author='R&D',
+    author_email='abdoldevtra@gmail.com',
+    install_requires=[],
+)
