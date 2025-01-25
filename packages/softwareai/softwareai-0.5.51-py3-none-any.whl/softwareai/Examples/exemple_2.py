65046b644b46b0d09308798b4193d76209698e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:34 +0000

    Add package softwareai/0.5.51/softwareai-0.5.51-py3-none-any.whl

---
+++ packages/softwareai/softwareai-0.5.51-py3-none-any.whl/softwareai/Examples/exemple_2.py
@@ -0,0 +1,18 @@
+#########################################
+# IMPORT SoftwareAI Agents
+from softwareai.CoreApp._init_agents_ import AgentInitializer
+#########################################
+# IMPORT SoftwareAI Libs 
+from softwareai.CoreApp._init_libs_ import *
+#########################################
+# IMPORT SoftwareAI Core
+from softwareai.CoreApp._init_core_ import * 
+#########################################
+
+name_app = "appx"
+appfb = FirebaseKeysinit._init_app_(name_app)
+
+byte_manager = AgentInitializer.get_agent('ByteManager') 
+mensagem = "solicito uma atualização do repositorio https://github.com/A-I-O-R-G/trontechnicalanalysistool"
+owner_response = byte_manager.AI_1_ByteManager_Company_Owners(mensagem, appfb)
+print(owner_response)
