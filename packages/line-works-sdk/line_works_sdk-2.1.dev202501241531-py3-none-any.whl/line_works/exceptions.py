da4e76873668454852f5f70317a2f192af97d528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:05:16 +0000

    Add package line-works-sdk/2.1.dev202501241531/line_works_sdk-2.1.dev202501241531-py3-none-any.whl

---
+++ packages/line-works-sdk/line_works_sdk-2.1.dev202501241531-py3-none-any.whl/line_works/exceptions.py
@@ -0,0 +1,18 @@
+class LineWorksException(Exception):
+    pass
+
+
+class LogicException(LineWorksException):
+    pass
+
+
+class LoginException(LineWorksException):
+    pass
+
+
+class GetMyInfoException(LineWorksException):
+    pass
+
+
+class SendMessageException(LineWorksException):
+    pass
