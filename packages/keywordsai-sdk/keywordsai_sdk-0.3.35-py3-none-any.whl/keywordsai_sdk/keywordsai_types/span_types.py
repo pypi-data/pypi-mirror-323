d15ee64ce19f630a41edb6891a499bc1a21b2ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:05:32 +0000

    Add package keywordsai-sdk/0.3.35/keywordsai_sdk-0.3.35-py3-none-any.whl

---
+++ packages/keywordsai-sdk/keywordsai_sdk-0.3.35-py3-none-any.whl/keywordsai_sdk/keywordsai_types/span_types.py
@@ -0,0 +1,21 @@
+from keywordsai_sdk.keywordsai_types._internal_types import KeywordsAIBaseModel
+from enum import Enum
+
+class KeywordsAISpanAttributes(Enum):
+    # Span attributes
+    KEYWORDSAI_SPAN_CUSTOM_ID = "keywordsai.span_params.custom_identifier"
+
+    # Customer params
+    KEYWORDSAI_CUSTOMER_PARAMS_ID = "keywordsai.customer_params.customer_identifier"
+    KEYWORDSAI_CUSTOMER_PARAMS_EMAIL = "keywordsai.customer_params.email"
+    KEYWORDSAI_CUSTOMER_PARAMS_NAME = "keywordsai.customer_params.name"
+    
+    # Evaluation params
+    KEYWORDSAI_EVALUATION_PARAMS_ID = "keywordsai.evaluation_params.evaluation_identifier"
+
+    # Threads
+    KEYWORDSAI_THREADS_ID = "keywordsai.threads.thread_identifier"
+
+    # Metadata
+    KEYWORDSAI_METADATA = "keywordsai.metadata" # This is a pattern, it can be  any "keywordsai.metadata.key" where key is customizable
+
