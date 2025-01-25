484c59a6e2ca133af86c81b876b26c61a2ce2553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:46 +0000

    Add package invenio-pure/1.2.0/invenio-pure-1.2.0.tar.gz

---
+++ packages/invenio-pure/invenio-pure-1.2.0.tar.gz/invenio-pure-1.2.0/invenio_pure/tasks.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021-2024 Graz University of Technology.
+#
+# invenio-pure is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Scheduled tasks for celery."""
+
+
+from celery import shared_task
+from flask import current_app
+from invenio_access.permissions import system_identity
+
+from .proxies import current_pure
+
+
+@shared_task(ignore_result=True)
+def import_records_from_pure() -> None:
+    """Import records from pure."""
+    import_func = current_app.config["PURE_IMPORT_FUNC"]
+    filter_records = current_app.config["PURE_FILTER_RECORDS"]
+
+    pure_service = current_pure.pure_rest_service
+    ids = pure_service.fetch_all_ids(system_identity, filter_records)
+
+    for pure_id in ids:
+        try:
+            import_func(system_identity, pure_id, pure_service)
+        except RuntimeError as e:
+            msg = "ERROR pure pure_id: %s with message: %s"
+            current_app.logger.error(msg, pure_id, str(e))
---
+++ packages/invenio-pure/invenio-pure-1.2.0.tar.gz/invenio-pure-1.2.0/tests/conftest.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2022 Technische Universität Graz.
+#
+# invenio-pure is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Pytest configuration.
+
+See https://pytest-invenio.readthedocs.io/ for documentation on which test
+fixtures are available.
+"""
+
+
+from collections.abc import Callable
+
+import pytest
+from flask import Flask
+
+from invenio_pure import InvenioPure
+
+
+@pytest.fixture(scope="module")
+def create_app(instance_path: str) -> Callable:
+    """Application factory fixture."""
+
+    def factory(**config: dict) -> Flask:
+        app = Flask("testapp", instance_path=instance_path)
+        app.config.update(**config)
+        InvenioPure(app)
+        return app
+
+    return factory
