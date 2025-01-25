686d26647d7f546ddd2853ed121b4d0a4edbcbf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:04:11 +0000

    Add package rtmlib-flip/1.0.10/rtmlib_flip-1.0.10-py3-none-any.whl

---
+++ packages/rtmlib-flip/rtmlib_flip-1.0.10-py3-none-any.whl/rtmlib/tools/__init__.py
@@ -0,0 +1,8 @@
+from .object_detection import YOLOX, RTMDet, RTMDetRegional
+from .pose_estimation import RTMO, RTMPose
+from .solution import Body, Hand, PoseTracker, Wholebody, BodyWithFeet
+
+__all__ = [
+    'RTMDet', 'RTMPose', 'YOLOX', 'Wholebody', 'Body', 'Hand', 'PoseTracker',
+    'RTMO', 'BodyWithFeet', 'RTMDetRegional'
+]
