36dc9e9872331f53cddae0840cf9b42568e91862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 16:04:23 +0000

    Add package tinkoff-voicekit-ai/0.3.3.post1/tinkoff_voicekit_ai-0.3.3.post1-py3-none-any.whl

---
+++ packages/tinkoff-voicekit-ai/tinkoff_voicekit_ai-0.3.3.post1-py3-none-any.whl/tinkoff_voicekit_ai/STT/__init__.py
@@ -0,0 +1,2 @@
+from tinkoff_voicekit_ai.STT.client_stt import ClientSTT
+from tinkoff_voicekit_ai.STT import aio_client_stt as aio
---
+++ packages/tinkoff-voicekit-ai/tinkoff_voicekit_ai-0.3.3.post1-py3-none-any.whl/tinkoff_voicekit_ai/TTS/__init__.py
@@ -0,0 +1,2 @@
+from tinkoff_voicekit_ai.TTS.client_tts import ClientTTS
+from tinkoff_voicekit_ai.TTS import aio_client_tts as aio
