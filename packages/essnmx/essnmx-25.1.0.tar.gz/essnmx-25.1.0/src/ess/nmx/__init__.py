f5d62ff573353a97b0e816fde13cfd3dbb4f9926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:38 +0000

    Add package essnmx/25.1.0/essnmx-25.1.0.tar.gz

---
+++ packages/essnmx/essnmx-25.1.0.tar.gz/essnmx-25.1.0/src/ess/nmx/__init__.py
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
+# ruff: noqa: E402, F401
+
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("essnmx")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+del importlib
+
+from .data import small_mcstas_3_sample
+from .reduction import NMXData, NMXReducedData
+from .types import MaximumProbability
+
+default_parameters = {MaximumProbability: 10000}
+
+del MaximumProbability
+
+__all__ = [
+    "small_mcstas_3_sample",
+    "NMXReducedData",
+    "NMXData",
+    "default_parameters",
+]
