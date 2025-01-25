3e5541449b10b730ccf06162002f13c0d6a4df52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:04:06 +0000

    Add package httpx-curl-cffi/0.1.3/httpx_curl_cffi-0.1.3-py3-none-any.whl

---
+++ packages/httpx-curl-cffi/httpx_curl_cffi-0.1.3-py3-none-any.whl/httpx_curl_cffi/__init__.py
@@ -0,0 +1,23 @@
+__version__ = "0.1.3"
+
+from .transport import (
+    AsyncCurlTransport,
+    BrowserTypeLiteral,
+    CurlHttpVersion,
+    CurlInfo,
+    CurlOpt,
+    CurlTransport,
+    ExtraFingerprints,
+    ExtraFpDict,
+)
+
+__all__ = [
+    "AsyncCurlTransport",
+    "BrowserTypeLiteral",
+    "CurlHttpVersion",
+    "CurlInfo",
+    "CurlOpt",
+    "CurlTransport",
+    "ExtraFingerprints",
+    "ExtraFpDict",
+]
