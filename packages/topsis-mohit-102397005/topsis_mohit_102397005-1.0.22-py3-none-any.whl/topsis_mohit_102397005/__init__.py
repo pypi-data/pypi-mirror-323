a29fd1401f3cf8649ead22db1d2f32c0909ea6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:04:14 +0000

    Add package topsis-mohit-102397005/1.0.22/topsis_mohit_102397005-1.0.22-py3-none-any.whl

---
+++ packages/topsis-mohit-102397005/topsis_mohit_102397005-1.0.22-py3-none-any.whl/topsis_mohit_102397005/__init__.py
@@ -0,0 +1,5 @@
+from topsis_mohit_102397005.topsis_102397005 import topsis
+
+def run_topsis(inputFileName,weights,impacts,outputFileName):
+    topsis(inputFileName,weights,impacts,outputFileName)
+    
