a3cd4420cf612d78d1fcb5683ed76cd6c0cca3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:04:35 +0000

    Add package hypha/0.20.47.post2/hypha-0.20.47.post2-py3-none-any.whl

---
+++ packages/hypha/hypha-0.20.47.post2-py3-none-any.whl/hypha/__main__.py
@@ -0,0 +1,10 @@
+"""Main module for hypha."""
+
+from hypha.server import get_argparser, start_server
+
+
+def main():
+    """Main function."""
+    arg_parser = get_argparser()
+    opt = arg_parser.parse_args()
+    start_server(opt)
