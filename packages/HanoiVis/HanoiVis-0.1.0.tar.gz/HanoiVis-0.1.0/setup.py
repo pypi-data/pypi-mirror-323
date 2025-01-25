91d6a4d53910dc1b390d43693964cec0e4e57896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:05:29 +0000

    Add package HanoiVis/0.1.0/HanoiVis-0.1.0.tar.gz

---
+++ packages/HanoiVis/HanoiVis-0.1.0.tar.gz/HanoiVis-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='HanoiVis',
+    version='0.1.0',
+    author='Arvind Olag',
+    author_email='arvindolag@gmail.com',
+    packages=['hanoivis', 'hanoivis.test'],
+    url='http://pypi.python.org/pypi/HanoiVis/',
+    license='LICENSE.txt',
+    description='Tower of Hanoi visualiser',
+    long_description=open('README.rst').read()
+)
