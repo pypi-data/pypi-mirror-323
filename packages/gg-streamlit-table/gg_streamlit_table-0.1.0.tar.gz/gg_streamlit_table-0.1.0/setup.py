28e1dc100149e50ed0e849a94d9cc19ae810b65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:03:55 +0000

    Add package gg-streamlit-table/0.1.0/gg_streamlit_table-0.1.0.tar.gz

---
+++ packages/gg-streamlit-table/gg_streamlit_table-0.1.0.tar.gz/gg_streamlit_table-0.1.0/setup.py
@@ -0,0 +1,29 @@
+"""
+This is a setup file for the gg-streamlit-table package.
+"""
+
+import setuptools
+
+setuptools.setup(
+    name="gg-streamlit-table",
+    version="0.1.0",
+    author="Gaurang Kulkarni",
+    author_email="gaurangak@example.com",
+    description="A custom table component for Streamlit with actions and styling",
+    long_description="""# Streamlit Custom Table
+    A custom table component for Streamlit with actions, styling, and Material icons.""",
+    long_description_content_type="text/markdown",
+    url="https://github.com/gaurangak/gg-streamlit-table",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "streamlit>=1.0.0",
+        "pandas>=1.0.0",
+    ],
+)
