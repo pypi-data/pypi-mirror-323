489728f5aa4017afdf9493b62a2de8c2d6e83af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:04:21 +0000

    Add package tieval/0.1.5/tieval-0.1.5.tar.gz

---
+++ packages/tieval/tieval-0.1.5.tar.gz/tieval-0.1.5/tests/test_base.py
@@ -0,0 +1,40 @@
+
+from pathlib import Path
+
+from tieval.base import Sentence
+from tieval.links import TLink
+from tieval.entities import Entity
+from tieval.datasets.readers import TempEval3DocumentReader
+
+
+DATA_PATH = Path(__file__).parent / "files"
+
+
+class TestDocument:
+
+    doc = TempEval3DocumentReader(DATA_PATH / "tempeval_3.tml").read()
+
+    def test_print(self):
+        print(self.doc)
+        
+    def test_temporal_closure(self):
+        tlinks = self.doc.temporal_closure
+        tlink = next(iter(tlinks))
+        assert isinstance(tlink, TLink)
+        assert isinstance(tlink.source, Entity)
+        assert isinstance(tlink.target, Entity)
+        
+    def test_temporal_closure_length(self):
+        assert len(self.doc.temporal_closure) == 306
+
+
+class TestSentence:
+
+    sent = Sentence(
+        content="Hi, my name is.",
+        offsets=(100, 115)
+    )
+
+    def test_tokens(self):
+        tkns = self.sent.tokens
+        assert len(tkns) == 6
