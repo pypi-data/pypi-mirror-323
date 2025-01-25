24e596500727c444a4b9eab09755b0d6d53f149b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:04:26 +0000

    Add package acuvity/0.5.4/acuvity-0.5.4-py3-none-any.whl

---
+++ packages/acuvity/acuvity-0.5.4-py3-none-any.whl/acuvity/guard/__init__.py
@@ -0,0 +1,10 @@
+from .config import Guard, GuardConfig
+from .constants import GuardName
+from .threshold import Threshold
+
+__all__ = [
+    'Guard',
+    'GuardConfig',
+    'GuardName',
+    'Threshold'
+]
---
+++ packages/acuvity/acuvity-0.5.4-py3-none-any.whl/acuvity/response/result.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import List
+
+from acuvity.guard.constants import GuardName
+
+
+class ResponseMatch(str, Enum):
+    """Enumeration for check matches."""
+    YES = "YES"
+    NO = "NO"
+
+@dataclass
+class GuardMatch:
+    """Result of a single check operation."""
+    response_match: ResponseMatch
+    guard_name: GuardName
+    actual_value: float
+    threshold: str
+    match_count: int = 0
+    match_values: List[str]= field(default_factory=list)
+
+@dataclass
+class Matches:
+    """Result of processing multiple checks or a configuration."""
+    input_data: str
+    response_match: ResponseMatch
+    matched_checks: List[GuardMatch]
+    all_checks: List[GuardMatch]
