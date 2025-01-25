912c1ef79d8d9ba7a95af1a86e360d9ae49a72e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:05:08 +0000

    Add package dockipy/0.0.59/dockipy-0.0.59.tar.gz

---
+++ packages/dockipy/dockipy-0.0.59.tar.gz/dockipy-0.0.59/src/dockipy/dockishell.py
@@ -0,0 +1,23 @@
+import dockipy.utils as utils
+import pathlib, platform, subprocess
+
+def dockishell():
+    work_dir, project_root, target_root = utils.find_project_root()
+
+    command, remote, clean = utils.argsparse()
+
+    docki_config = utils.get_docki_config(project_root, remote)
+
+    image, client = utils.build_docker_image(target_root, docki_config, clean)
+
+    try:
+        utils.setup_venv(project_root, target_root, client, image, docki_config, clean)
+        container = utils.run_container(client, image, command, docki_config, work_dir, project_root, target_root)
+        utils.print_logs(container)
+    except KeyboardInterrupt:
+        print("Shutting down the container")
+    except Exception as e:
+        print(e)
+    finally:
+        container.stop()
+        container.remove(force=True)
