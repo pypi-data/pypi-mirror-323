0830287d0b2e79d6ac7808f5456a493c253748e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:53 +0000

    Add package usaugment/1.0.1/usaugment-1.0.1-py3-none-any.whl

---
+++ packages/usaugment/usaugment-1.0.1-py3-none-any.whl/usaugment/albumentations/__init__.py
@@ -0,0 +1,5 @@
+# ruff: noqa: F401
+from .depth_attenuation import DepthAttenuation
+from .gaussian_shadow import GaussianShadow
+from .haze_artifact import HazeArtifact
+from .speckle_reduction import SpeckleReduction
