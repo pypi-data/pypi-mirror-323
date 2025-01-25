4b2300f7102c0a9043f765f6a27a559268824080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:03:51 +0000

    Add package ner-span-annotator/0.0.1/ner_span_annotator-0.0.1-py3-none-any.whl

---
+++ packages/ner-span-annotator/ner_span_annotator-0.0.1-py3-none-any.whl/ner_span_annotator/__init__.py
@@ -0,0 +1,49 @@
+import os
+import streamlit.components.v1 as components
+
+_RELEASE = True
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        "ner_span_annotator",
+        url="http://localhost:3001",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("ner_span_annotator", path=build_dir)
+
+
+def ner_span_annotator(name=None, tokens=None, spans=None, labels=None, key=None, default=0):
+    """
+    Create a new instance of "my_component", passing tokens and spans.
+
+    Parameters
+    ----------
+    name : str or None
+        Optional name/string to pass to the component.
+    tokens : list[str] or None
+        List of tokens to render.
+    spans : list[dict] or None
+        List of spans with keys like {"start_token", "end_token", "label"}.
+    labels : list[str] or None
+        List of labels to work with.
+    key : str or None
+        An optional key that uniquely identifies this component.
+    default : Any
+        The initial return value of the component before user interaction.
+
+    Returns
+    -------
+    Any
+        The component's return value (set via Streamlit.setComponentValue).
+    """
+    component_value = _component_func(
+        name=name,
+        tokens=tokens,
+        spans=spans,
+        labels=labels,
+        key=key,
+        default=default
+    )
+    return component_value
