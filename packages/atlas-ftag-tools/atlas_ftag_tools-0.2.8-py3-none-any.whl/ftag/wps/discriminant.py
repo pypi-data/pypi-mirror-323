f27c8cdd3d4c09aa0988c3357199dbf1cae8db25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:04:25 +0000

    Add package atlas-ftag-tools/0.2.8/atlas_ftag_tools-0.2.8-py3-none-any.whl

---
+++ packages/atlas-ftag-tools/atlas_ftag_tools-0.2.8-py3-none-any.whl/ftag/wps/discriminant.py
@@ -0,0 +1,131 @@
+from __future__ import annotations
+
+from typing import Callable
+
+import numpy as np
+
+from ftag import Flavours
+from ftag.labels import Label, remove_suffix
+
+
+def discriminant(
+    jets: np.ndarray,
+    tagger: str,
+    signal: Label,
+    fxs: dict[str, float],
+    epsilon: float = 1e-10,
+) -> np.ndarray:
+    """
+    Get the tagging discriminant.
+
+    Calculated as the logarithm of the ratio of a specified signal probability
+    to a weighted sum ofbackground probabilities.
+
+    Parameters
+    ----------
+    jets : np.ndarray
+        Structed jet array containing tagger scores.
+    tagger : str
+        Name of the tagger, used to construct field names.
+    signal : str
+        Type of signal.
+    fxs : dict[str, float]
+        Dict of background probability names and their fractions.
+        If a fraction is None, it is calculated as (1 - sum of provided fractions).
+    epsilon : float, optional
+        A small value added to probabilities to prevent division by zero, by default 1e-10.
+
+    Returns
+    -------
+    np.ndarray
+        The tagger discriminant values for the jets.
+
+    Raises
+    ------
+    ValueError
+        If a fraction is specified for a denominator that is not present in the input array.
+    """
+    denominator = 0.0
+    for d, fx in fxs.items():
+        name = f"{tagger}_{d}"
+        if fx > 0 and name not in jets.dtype.names:
+            raise ValueError(f"Nonzero fx for {d}, but '{name}' not found in input array.")
+        denominator += jets[name] * fx if name in jets.dtype.names else 0
+    signal_field = f"{tagger}_{signal.px}"
+    if signal_field not in jets.dtype.names:
+        signal_field = f"{tagger}_p{remove_suffix(signal.name, 'jets')}"
+    return np.log((jets[signal_field] + epsilon) / (denominator + epsilon))
+
+
+def tautag_dicriminant(jets, tagger, fb, fc, epsilon=1e-10):
+    fxs = {"pb": fb, "pc": fc, "pu": 1 - fb - fc}
+    return discriminant(jets, tagger, Flavours.taujets, fxs, epsilon=epsilon)
+
+
+def btag_discriminant(jets, tagger, fc, ftau=0, epsilon=1e-10):
+    fxs = {"pc": fc, "ptau": ftau, "pu": 1 - fc - ftau}
+    return discriminant(jets, tagger, Flavours.bjets, fxs, epsilon=epsilon)
+
+
+def ghostbtag_discriminant(jets, tagger, fc, ftau=0, epsilon=1e-10):
+    fxs = {"pghostc": fc, "pghosttau": ftau, "pghostu": 1 - fc - ftau}
+    return discriminant(jets, tagger, Flavours.ghostbjets, fxs, epsilon=epsilon)
+
+
+def ctag_discriminant(jets, tagger, fb, ftau=0, epsilon=1e-10):
+    fxs = {"pb": fb, "ptau": ftau, "pu": 1 - fb - ftau}
+    return discriminant(jets, tagger, Flavours.cjets, fxs, epsilon=epsilon)
+
+
+def hbb_discriminant(jets, tagger, ftop=0.25, fhcc=0.02, epsilon=1e-10):
+    fxs = {"phcc": fhcc, "ptop": ftop, "pqcd": 1 - ftop - fhcc}
+    return discriminant(jets, tagger, Flavours.hbb, fxs, epsilon=epsilon)
+
+
+def hcc_discriminant(jets, tagger, ftop=0.25, fhbb=0.3, epsilon=1e-10):
+    fxs = {"phbb": fhbb, "ptop": ftop, "pqcd": 1 - ftop - fhbb}
+    return discriminant(jets, tagger, Flavours.hcc, fxs, epsilon=epsilon)
+
+
+def get_discriminant(
+    jets: np.ndarray, tagger: str, signal: Label | str, epsilon: float = 1e-10, **fxs
+):
+    """Calculate the b-tag or c-tag discriminant for a given tagger.
+
+    Parameters
+    ----------
+    jets : np.ndarray
+        Structured array of jets containing tagger outputs
+    tagger : str
+        Name of the tagger
+    signal : Label
+        Signal flavour (bjets/cjets or hbb/hcc)
+    epsilon : float, optional
+        Small number to avoid division by zero, by default 1e-10
+    **fxs : dict
+        Fractions for the different background flavours.
+
+    Returns
+    -------
+    np.ndarray
+        Array of discriminant values.
+
+    Raises
+    ------
+    ValueError
+        If the signal flavour is not recognised.
+    """
+    tagger_funcs: dict[str, Callable] = {
+        "bjets": btag_discriminant,
+        "cjets": ctag_discriminant,
+        "taujets": tautag_dicriminant,
+        "hbb": hbb_discriminant,
+        "hcc": hcc_discriminant,
+        "ghostbjets": ghostbtag_discriminant,
+    }
+
+    if str(signal) not in tagger_funcs:
+        raise ValueError(f"Signal flavour must be one of {list(tagger_funcs.keys())}, not {signal}")
+
+    func: Callable = tagger_funcs[str(Flavours[signal])]
+    return func(jets, tagger, **fxs, epsilon=epsilon)
---
+++ packages/atlas-ftag-tools/atlas_ftag_tools-0.2.8-py3-none-any.whl/ftag/wps/working_points.py
@@ -0,0 +1,316 @@
+"""Calculate tagger working points."""
+
+from __future__ import annotations
+
+import argparse
+from pathlib import Path
+
+import numpy as np
+import yaml
+
+from ftag import Flavours
+from ftag.cli_utils import HelpFormatter
+from ftag.cuts import Cuts
+from ftag.hdf5 import H5Reader
+from ftag.wps.discriminant import get_discriminant
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=HelpFormatter,
+    )
+    parser.add_argument(
+        "--ttbar",
+        required=True,
+        type=Path,
+        help="path to ttbar sample (supports globbing)",
+    )
+    parser.add_argument(
+        "--zprime",
+        required=False,
+        type=Path,
+        help="path to zprime (supports globbing). WPs from ttbar will be reused for zprime",
+    )
+    parser.add_argument(
+        "-e",
+        "--effs",
+        nargs="+",
+        type=float,
+        help="efficiency working point(s). If -r is specified, values should be 1/efficiency",
+    )
+    parser.add_argument(
+        "-t",
+        "--tagger",
+        nargs="+",
+        required=True,
+        type=str,
+        help="tagger name(s)",
+    )
+    parser.add_argument(
+        "-s",
+        "--signal",
+        default="bjets",
+        choices=["bjets", "cjets", "hbb", "hcc"],
+        type=str,
+        help='signal flavour ("bjets" or "cjets" for b-tagging, "hbb" or "hcc" for Xbb)',
+    )
+    parser.add_argument(
+        "-r",
+        "--rejection",
+        default=None,
+        choices=["ujets", "cjets", "bjets", "hbb", "hcc", "top", "qcd"],
+        help="use rejection of specified background class to determine working points",
+    )
+    parser.add_argument(
+        "-d",
+        "--disc_cuts",
+        nargs="+",
+        type=float,
+        help="D_x value(s) to calculate efficiency at",
+    )
+    parser.add_argument(
+        "-n",
+        "--num_jets",
+        default=1_000_000,
+        type=int,
+        help="use this many jets (post selection)",
+    )
+    parser.add_argument(
+        "--ttbar_cuts",
+        nargs="+",
+        default=["pt > 20e3"],
+        type=list,
+        help="selection to apply to ttbar (|eta| < 2.5 is always applied)",
+    )
+    parser.add_argument(
+        "--zprime_cuts",
+        nargs="+",
+        default=["pt > 250e3"],
+        type=list,
+        help="selection to apply to zprime (|eta| < 2.5 is always applied)",
+    )
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        type=Path,
+        help="save results to yaml instead of printing",
+    )
+    parser.add_argument(
+        "--xbb",
+        action="store_true",
+        help="Enable Xbb tagging which expects two fx values ftop and fhcc/fhbb for each tagger",
+    )
+    parser.add_argument(
+        "--fb",
+        nargs="+",
+        type=float,
+        help="fb value(s) for each tagger",
+    )
+    parser.add_argument(
+        "--fc",
+        nargs="+",
+        type=float,
+        help="fc value(s) for each tagger",
+    )
+    parser.add_argument(
+        "--ftau",
+        nargs="+",
+        type=float,
+        help="ftau value(s) for each tagger",
+    )
+    parser.add_argument(
+        "--ftop",
+        nargs="+",
+        type=float,
+        help="ftop value(s) for each tagger",
+    )
+    parser.add_argument(
+        "--fhbb",
+        nargs="+",
+        type=float,
+        help="fhbb value(s) for each tagger",
+    )
+    parser.add_argument(
+        "--fhcc",
+        nargs="+",
+        type=float,
+        help="fhcc value(s) for each tagger",
+    )
+    args = parser.parse_args(args)
+
+    args.signal = Flavours[args.signal]
+
+    if args.effs and args.disc_cuts:
+        raise ValueError("Cannot specify both --effs and --disc_cuts")
+    if not args.effs and not args.disc_cuts:
+        raise ValueError("Must specify either --effs or --disc_cuts")
+
+    if args.xbb:
+        if args.signal not in {Flavours.hbb, Flavours.hcc}:
+            raise ValueError("Xbb tagging only supports hbb or hcc signal flavours")
+        if args.fb or args.fc or args.ftau:
+            raise ValueError("For Xbb tagging, fb, fc and ftau should not be specified")
+        if not args.ftop:
+            raise ValueError("For Xbb tagging, ftop should be specified")
+        if args.signal == "hbb" and not args.fhcc:
+            raise ValueError("For hbb tagging, fhcc should be specified")
+        if args.signal == "hcc" and not args.fhbb:
+            raise ValueError("For hcc tagging, fhbb should be specified")
+    else:
+        if args.ftop or args.fhbb or args.fhcc:
+            raise ValueError("For single-b tagging, ftop, fhbb and fhcc should not be specified")
+        if args.signal == "bjets" and not args.fc:
+            raise ValueError("For bjets tagging, fc should be specified")
+        if args.signal == "cjets" and not args.fb:
+            raise ValueError("For cjets tagging, fb should be specified")
+        if args.ftau is None:
+            args.ftau = [0.0] * len(args.tagger)
+
+    for fx in ["fb", "fc", "ftau", "ftop", "fhbb", "fhcc"]:
+        if getattr(args, fx) and len(getattr(args, fx)) != len(args.tagger):
+            raise ValueError(f"Number of {fx} values must match number of taggers")
+
+    return args
+
+
+def get_fxs_from_args(args):
+    if args.signal == Flavours.bjets:
+        fxs = {"fc": args.fc, "ftau": args.ftau}
+    elif args.signal == Flavours.cjets:
+        fxs = {"fb": args.fb, "ftau": args.ftau}
+    elif args.signal == Flavours.hbb:
+        fxs = {"ftop": args.ftop, "fhcc": args.fhcc}
+    elif args.signal == Flavours.hcc:
+        fxs = {"ftop": args.ftop, "fhbb": args.fhbb}
+    assert fxs is not None
+    return [{k: v[i] for k, v in fxs.items()} for i in range(len(args.tagger))]
+
+
+def get_eff_rej(jets, disc, wp, flavs):
+    out = {"eff": {}, "rej": {}}
+    for bkg in list(flavs):
+        bkg_disc = disc[bkg.cuts(jets).idx]
+        eff = sum(bkg_disc > wp) / len(bkg_disc)
+        out["eff"][str(bkg)] = float(f"{eff:.3g}")
+        out["rej"][str(bkg)] = float(f"{1 / eff:.3g}")
+    return out
+
+
+def get_rej_eff_at_disc(jets, tagger, signal, disc_cuts, **fxs):
+    disc = get_discriminant(jets, tagger, signal, **fxs)
+    d = {}
+    flavs = Flavours.by_category("single-btag")
+    for dcut in disc_cuts:
+        d[str(dcut)] = {"eff": {}, "rej": {}}
+        for f in flavs:
+            e_discs = disc[f.cuts(jets).idx]
+            eff = sum(e_discs > dcut) / len(e_discs)
+            d[str(dcut)]["eff"][str(f)] = float(f"{eff:.3g}")
+            d[str(dcut)]["rej"][str(f)] = 1 / float(f"{eff:.3g}")
+    return d
+
+
+def setup_common_parts(args):
+    flavs = Flavours.by_category("single-btag") if not args.xbb else Flavours.by_category("xbb")
+    default_cuts = Cuts.from_list(["eta > -2.5", "eta < 2.5"])
+    ttbar_cuts = Cuts.from_list(args.ttbar_cuts) + default_cuts
+    zprime_cuts = Cuts.from_list(args.zprime_cuts) + default_cuts
+
+    # prepare to load jets
+    all_vars = list(set(sum((flav.cuts.variables for flav in flavs), [])))
+    reader = H5Reader(args.ttbar)
+    jet_vars = reader.dtypes()["jets"].names
+    for tagger in args.tagger:
+        all_vars += [f"{tagger}_{f.px}" for f in flavs if (f"{tagger}_{f.px}" in jet_vars)]
+
+    # load jets
+    jets = reader.load({"jets": all_vars}, args.num_jets, cuts=ttbar_cuts)["jets"]
+    zp_jets = None
+    if args.zprime:
+        zp_reader = H5Reader(args.zprime)
+        zp_jets = zp_reader.load({"jets": all_vars}, args.num_jets, cuts=zprime_cuts)["jets"]
+
+    return jets, zp_jets, flavs
+
+
+def get_working_points(args=None):
+    jets, zp_jets, flavs = setup_common_parts(args)
+    fxs = get_fxs_from_args(args)
+
+    # loop over taggers
+    out = {}
+    for i, tagger in enumerate(args.tagger):
+        # calculate discriminant
+        out[tagger] = {"signal": str(args.signal), **fxs[i]}
+        disc = get_discriminant(jets, tagger, args.signal, **fxs[i])
+
+        # loop over efficiency working points
+        for eff in args.effs:
+            d = out[tagger][f"{eff:.0f}"] = {}
+
+            wp_flavour = args.signal
+            if args.rejection:
+                eff = 100 / eff  # noqa: PLW2901
+                wp_flavour = args.rejection
+
+            wp_disc = disc[flavs[wp_flavour].cuts(jets).idx]
+            wp = d["cut_value"] = round(float(np.percentile(wp_disc, 100 - eff)), 3)
+
+            # calculate eff and rej for each flavour
+            d["ttbar"] = get_eff_rej(jets, disc, wp, flavs)
+
+            # calculate for zprime
+            if args.zprime:
+                zp_disc = get_discriminant(zp_jets, tagger, Flavours[args.signal], **fxs[i])
+                d["zprime"] = get_eff_rej(zp_jets, zp_disc, wp, flavs)
+
+    if args.outfile:
+        with open(args.outfile, "w") as f:
+            yaml.dump(out, f, sort_keys=False)
+            return None
+    else:
+        return out
+
+
+def get_efficiencies(args=None):
+    jets, zp_jets, _ = setup_common_parts(args)
+    fxs = get_fxs_from_args(args)
+
+    out = {}
+    for i, tagger in enumerate(args.tagger):
+        out[tagger] = {"signal": str(args.signal), **fxs[i]}
+
+        out[tagger]["ttbar"] = get_rej_eff_at_disc(
+            jets, tagger, args.signal, args.disc_cuts, **fxs[i]
+        )
+        if args.zprime:
+            out[tagger]["zprime"] = get_rej_eff_at_disc(
+                zp_jets, tagger, args.signal, args.disc_cuts, **fxs[i]
+            )
+
+    if args.outfile:
+        with open(args.outfile, "w") as f:
+            yaml.dump(out, f, sort_keys=False)
+            return None
+    else:
+        return out
+
+
+def main(args=None):
+    args = parse_args(args)
+
+    if args.effs:
+        out = get_working_points(args)
+    elif args.disc_cuts:
+        out = get_efficiencies(args)
+
+    if out:
+        print(yaml.dump(out, sort_keys=False))
+        return out
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
