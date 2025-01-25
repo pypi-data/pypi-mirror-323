3315683b07c88878638000d1bda7365add430a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:54 +0000

    Add package skewnormlib/0.1.1/skewnormlib-0.1.1-py3-none-any.whl

---
+++ packages/skewnormlib/skewnormlib-0.1.1-py3-none-any.whl/skewnorm/__init__.py
@@ -0,0 +1,9 @@
+# skewnormlib/__init__.py
+
+"""
+skewnorm: A library for skew-weighted normalization.
+
+Modules:
+- normalization: Contains the SkewWeightedNormalization transformer class.
+"""
+from .normalization import SkewWeightedNormalization
