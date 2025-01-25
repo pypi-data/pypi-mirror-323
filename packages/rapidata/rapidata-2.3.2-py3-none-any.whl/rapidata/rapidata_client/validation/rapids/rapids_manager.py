ff3b965c4d068c81136ec9f46bcdc0e593a61feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:05:18 +0000

    Add package rapidata/2.3.2/rapidata-2.3.2-py3-none-any.whl

---
+++ packages/rapidata/rapidata-2.3.2-py3-none-any.whl/rapidata/rapidata_client/validation/rapids/rapids_manager.py
@@ -0,0 +1,195 @@
+from rapidata.rapidata_client.assets.data_type_enum import RapidataDataTypes
+from rapidata.rapidata_client.validation.rapids.rapids import (
+    ClassificationRapid, 
+    CompareRapid, 
+    SelectWordsRapid, 
+    LocateRapid, 
+    DrawRapid,
+    TimestampRapid)
+from rapidata.rapidata_client.assets import MediaAsset, TextAsset, MultiAsset
+from rapidata.rapidata_client.metadata import Metadata
+from rapidata.rapidata_client.validation.rapids.box import Box
+
+from typing import Sequence
+
+class RapidsManager:
+    """
+    Can be used to build different types of rapids. That can then be added to Validation sets
+    """
+    def __init__(self):
+        pass
+    
+    def classification_rapid(self,
+            instruction: str,
+            answer_options: list[str],
+            datapoint: str,
+            truths: list[str],
+            data_type: str = RapidataDataTypes.MEDIA,
+            metadata: Sequence[Metadata] = [],
+    ) -> ClassificationRapid:
+        """Build a classification rapid
+        
+        Args:
+            instruction (str): The instruction/question to be shown to the labeler.
+            answer_options (list[str]): The options that the labeler can choose from to answer the question.
+            datapoint (str): The datapoint that the labeler will be labeling.
+            truths (list[str]): The correct answers to the question.
+            data_type (str, optional): The type of the datapoint. Defaults to RapidataDataTypes.MEDIA.
+            metadata (Sequence[Metadata], optional): The metadata that is attached to the rapid. Defaults to [].
+        """
+        
+        if data_type == RapidataDataTypes.MEDIA:
+            asset = MediaAsset(datapoint)
+        elif data_type == RapidataDataTypes.TEXT:
+            asset = TextAsset(datapoint)
+        else:
+            raise ValueError(f"Unsupported data type: {data_type}")
+
+        return ClassificationRapid(
+                instruction=instruction,
+                answer_options=answer_options,
+                asset=asset,
+                truths=truths,
+                metadata=metadata,
+                )
+    
+    def compare_rapid(self,
+            instruction: str,
+            truth: str,
+            datapoint: list[str],
+            data_type: str = RapidataDataTypes.MEDIA,
+            metadata: Sequence[Metadata] = [],
+    ) -> CompareRapid:
+        """Build a compare rapid
+
+        Args:
+            instruction (str): The instruction that the labeler will be comparing the assets on.
+            truth (str): The correct answer to the comparison. (has to be one of the assets)
+            datapoint (list[str]): The two assets that the labeler will be comparing.
+            data_type (str, optional): The type of the datapoint. Defaults to RapidataDataTypes.MEDIA.
+            metadata (Sequence[Metadata], optional): The metadata that is attached to the rapid. Defaults to [].
+        """
+
+        if data_type == RapidataDataTypes.MEDIA:
+            assets = [MediaAsset(image) for image in datapoint]
+        elif data_type == RapidataDataTypes.TEXT:
+            assets = [TextAsset(text) for text in datapoint]
+        else:
+            raise ValueError(f"Unsupported data type: {data_type}")
+        
+        asset = MultiAsset(assets)
+
+        return CompareRapid(
+                instruction=instruction,
+                asset=asset,
+                truth=truth,
+                metadata=metadata,
+                )
+    
+    def select_words_rapid(self,
+            instruction: str,
+            truths: list[int],
+            datapoint: str,
+            sentence: str,
+            required_precision: float = 1,
+            required_completeness: float = 1,
+            metadata: Sequence[Metadata] = [],
+    ) -> SelectWordsRapid:
+        """Build a select words rapid
+
+        Args:
+            instruction (str): The instruction for the labeler.
+            truths (list[int]): The indices of the words that are the correct answers.
+            datapoint (str): The asset that the labeler will be selecting words from.
+            sentence (str): The sentence that the labeler will be selecting words from. (split up by spaces)
+            required_precision (float): The required precision for the labeler to get the rapid correct (minimum ratio of the words selected that need to be correct). defaults to 1. (no wrong words can be selected)
+            required_completeness (float): The required completeness for the labeler to get the rapid correct (miminum ratio of total correct words selected). defaults to 1. (all correct words need to be selected)
+            metadata (Sequence[Metadata], optional): The metadata that is attached to the rapid. Defaults to [].
+        """
+        
+        asset = MediaAsset(datapoint)
+
+        return SelectWordsRapid(
+                instruction=instruction,
+                truths=truths,
+                asset=asset,
+                sentence=sentence,
+                required_precision=required_precision,
+                required_completeness=required_completeness,
+                metadata=metadata,
+                )
+    
+    def locate_rapid(self,
+            instruction: str,
+            truths: list[Box],
+            datapoint: str,
+            metadata: Sequence[Metadata] = [],
+    ) -> LocateRapid:
+        """Build a locate rapid
+
+        Args:
+            instruction (str): The instruction on what the labeler should do.
+            truths (list[Box]): The bounding boxes of the object that the labeler ought to be locating.
+            datapoint (str): The asset that the labeler will be locating the object in.
+            metadata (Sequence[Metadata], optional): The metadata that is attached to the rapid. Defaults to [].
+        """
+        
+        asset = MediaAsset(datapoint)
+
+        return LocateRapid(
+                instruction=instruction,
+                truths=truths,
+                asset=asset,
+                metadata=metadata,
+                )
+    
+    def draw_rapid(self,
+            instruction: str,
+            truths: list[Box],
+            datapoint: str,
+            metadata: Sequence[Metadata] = [],
+    ) -> DrawRapid:
+        """Build a draw rapid
+
+        Args:
+            instruction (str): The instructions on what the labeler
+            truths (list[Box]): The bounding boxes of the object that the labeler ought to be drawing.
+            datapoint (str): The asset that the labeler will be drawing the object in.
+            metadata (Sequence[Metadata], optional): The metadata that is attached to the rapid. Defaults to [].
+        """
+        
+        asset = MediaAsset(datapoint)
+
+        return DrawRapid(
+                instruction=instruction,
+                truths=truths,
+                asset=asset,
+                metadata=metadata,
+                )
+    
+    def timestamp_rapid(self,
+            instruction: str,
+            truths: list[tuple[int, int]],
+            datapoint: str,
+            metadata: Sequence[Metadata] = []
+    ) -> TimestampRapid:
+        """Build a timestamp rapid
+
+        Args:
+            instruction (str): The instruction for the labeler.
+            truths (list[tuple[int, int]]): The possible accepted timestamps intervals for the labeler (in miliseconds).
+                The first element of the tuple is the start of the interval and the second element is the end of the interval.
+            datapoint (str): The asset that the labeler will be timestamping.
+            metadata (Sequence[Metadata], optional): The metadata that is attached to the rapid. Defaults to [].
+        """
+        
+        asset = MediaAsset(datapoint)
+
+        return TimestampRapid(
+                instruction=instruction,
+                truths=truths,
+                asset=asset,
+                metadata=metadata,
+                )
+
+        
