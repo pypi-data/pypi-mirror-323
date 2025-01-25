57996845accecd6a2e30f1a4acfc312bfe764d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:04:51 +0000

    Add package skribble-sdk/0.12/skribble_sdk-0.12-py3-none-any.whl

---
+++ packages/skribble-sdk/skribble_sdk-0.12-py3-none-any.whl/skribble/signature_request/__init__.py
@@ -0,0 +1,27 @@
+from .operations import (
+    create,
+    get,
+    delete,
+    list,
+    update,
+    add_signer,
+    remove_signer,
+    replace_signers,
+    remind,
+    withdraw,
+    get_attachment
+)
+
+__all__ = [
+    "create",
+    "get",
+    "delete",
+    "list",
+    "update",
+    "add_signer",
+    "remove_signer",
+    "replace_signers",
+    "remind",
+    "withdraw",
+    "get_attachment"
+]
