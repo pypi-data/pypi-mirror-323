409a66d6f87e2a78f3a3cbeb57f87ac2296d0a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:04:33 +0000

    Add package open-space-toolkit-astrodynamics/14.0.0/open_space_toolkit_astrodynamics-14.0.0-py313-none-manylinux2014_x86_64.whl

---
+++ packages/open-space-toolkit-astrodynamics/open_space_toolkit_astrodynamics-14.0.0-py313-none-manylinux2014_x86_64.whl/ostk/astrodynamics/test/flight/test_system.py
@@ -0,0 +1,55 @@
+# Apache License 2.0
+
+import pytest
+
+import numpy as np
+
+import ostk.mathematics as mathematics
+
+import ostk.physics as physics
+
+import ostk.astrodynamics as astrodynamics
+
+Cuboid = mathematics.geometry.d3.object.Cuboid
+Composite = mathematics.geometry.d3.object.Composite
+Point = mathematics.geometry.d3.object.Point
+
+Mass = physics.unit.Mass
+
+System = astrodynamics.flight.System
+
+
+@pytest.fixture
+def system_default_inputs():
+    mass = Mass(90.0, Mass.Unit.Kilogram)
+    geometry = Composite(
+        Cuboid(
+            Point(0.0, 0.0, 0.0),
+            [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]],
+            [1.0, 0.0, 0.0],
+        )
+    )
+
+    return (mass, geometry)
+
+
+@pytest.fixture
+def system(system_default_inputs) -> System:
+    return System(*system_default_inputs)
+
+
+class TestSatelliteSystem:
+    def test_constructors(self, system: System):
+        assert system is not None
+        assert isinstance(system, System)
+        assert system.is_defined()
+
+    def test_comparators(self, system: System):
+        assert (system == system) is True
+        assert (system != system) is False
+
+    def test_getters(self, system_default_inputs, system: System):
+        (mass, geometry) = system_default_inputs
+
+        assert system.get_mass() == mass
+        assert system.get_geometry() == geometry
