41c5ae54149dfb697fb1c7311842f75faf52f3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:04:46 +0000

    Add package APIMakeSens/1.4.11/apimakesens-1.4.11.tar.gz

---
+++ packages/APIMakeSens/apimakesens-1.4.11.tar.gz/apimakesens-1.4.11/MakeSens/__main__.py
@@ -0,0 +1,5 @@
+def main():
+    print("To make use of the MakeSens API import the package to your project")
+    
+if __name__ == "__main__":
+    main()
---
+++ packages/APIMakeSens/apimakesens-1.4.11.tar.gz/apimakesens-1.4.11/README.md
@@ -0,0 +1,2 @@
+# MakeSensAPI_Python
+En este repositorio, se plantea documentar la API para extraer datos de Makesens-Cloud Prueba
