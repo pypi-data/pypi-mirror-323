b912b71846186b02d059105433eeea9fc059d458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:04:40 +0000

    Add package dfeqa/0.0.3/dfeqa-0.0.3-py3-none-any.whl

---
+++ packages/dfeqa/dfeqa-0.0.3-py3-none-any.whl/dfeqa/__init__.py
@@ -0,0 +1,4 @@
+from .db import load_census
+from .data_validation import valid_name_regex, valid_upn
+from .summaries import summarise_str_lengths, fd, freqchart, parse_text
+from .data_transformation import year_group
