d1a4101d4ed6c75a21ff621cbefe6932146191f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:04:30 +0000

    Add package helpful-pys/0.1.2/helpful_pys-0.1.2-py3-none-any.whl

---
+++ packages/helpful-pys/helpful_pys-0.1.2-py3-none-any.whl/helpful-pys/creds.py
@@ -0,0 +1,21 @@
+redshift_user = {
+'usr':'lawrence.chin',
+'pwd':'3KbC9zVQcWEXLrjxWWKw',
+'host':'dataeng-prod.cqyxh8rl6vlx.us-west-2.redshift.amazonaws.com',
+'port':5439,
+'db':'data_warehouse_rc1'
+}
+
+gspread_creds = {
+  "type": "service_account",
+  "project_id": "central-muse-438817-j2",
+  "private_key_id": "cf2e0903831ffbe1771f39d475f18d3d86e343e9",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCibRRsvs6XRWa+\nPotSkhE/JxAThgayApOxZJaQyoFrT0+l/igIe7wfVVoFhMVbNqV+CXyZFNMf49OD\n/PFFXR9YEn4AZmS/STBQrBc+fjk7wPODX+J7GLsz3/i9IOTDGQswNJhahOiUo4r4\n9IcwaCjHZuYTV1QgOS26+FmgjW9q3WlMtc6OaA+l3VQxAj8VXLqjfdZ3GXG+yIp/\nrxOl+PZKfB7AuLkwFfsd15Kz14RfXyffGHnijWFkx/R6adn8G5zBZDkHCJ2IcOUJ\nwUPG9I4qqO3CruD1gvK2wMu/av05Ajwmu27OAkJ8aNeh+hnhjr+3BFPWemnzZ41K\n/OrXqsnVAgMBAAECggEAA7AIzZg+aKpOH8XGlChrT6eozTIBib3rvFRi47opRTZZ\ngf9ayX3aV/bFiAhgklvTrDXLGE75T8kfIVPB5uKAm+yy43pE/B/NWEOJSpADPM36\naVzpBNpF+R1IZdjZlhawjZa1b8wChh9kFWy4e1rzXrrsFdEpgIrJH5cpsm62R8g3\nn7YL0L3yJtQl0xTMaabIjTFW4nfIqIZUShMJj4xnmTl9Hib8fYfMcMl/kZs2FeID\nJPcx6qOVf8pPyLC9GRYRAB6hp1WLCbqMpTfJ+DE32ImzMmBU7t5fZW/5ad0UCWyf\nimw8CXOrTF4svKtEPNFBi03ZdzNGqkQLv3J9kIWpAQKBgQDiFbTx+uhM8qaOCk+b\nu0zyg/CbCJgikjckNDMin98nZAk/yhiQW2gZuEJO3yFR9PncfHjdrnVxKoL3Y/+D\nek/cJXQM6WGbLOYbimcojngyeuiRs6nL5BEPTjt3P1O9P8PODzkjOMy4mOCZzn0b\nP8QvrOyCpfUmD+wchTYgVINRAQKBgQC36wb3Bc/P30eUhICvv4n50mejweSa3f/Y\nq0o5RWwRLPJU7JmY0eBtCA3mDv1Hbs8o1cwe0exMnONhBWguP4cAn6wFaO9AE+VW\nTiRSbJIzyJrpgGIftxuO7zehhZVlXa5FMVdYq3gFyfysp6hA1+589bzIl7mT+M8g\noDphNsRk1QKBgH90IUfxXRhUCxU0NuDiXGSzgObFl46Vw0RDsTgpzJHWUWiGtAZd\noXguaiMwowYjBuEHwtOmUEr13lM9KoSiZXcEHkJRJ1dlnmuUQMeaWaSLqaTKFkT9\nU6i/P8LgDoM3+VinOFzk3nSKqsPKpri/4AfIgqS8jlFg1IjXiEX9nvIBAoGAel9h\nNP+i9DcmU4WOcmHJlnYPC4C5cOoBj9L5Af5VF2CGpUcANBSBdUqEpOyYTe1G3W7T\n8DXROnCxQh7s4MjYNOwI81iMyF/0mmZH+KfQ56+0k2Ue5Oda6NH2e/x8GQQqLlPU\nQGVm6MMq4l5zryF9x5tiTz3U9sX4W0cBUSm1pzkCgYA4Amb/dZZ86DRdsa1tiQZA\nlhbBnUFe+2zyNulcvCMkWGXkegmgM+NBJXZvpbcak4Vp2DcsDBCg02TNru07P5Xt\nsnBiNOs4JbMMZHleiZqnGHtuo/smw89yXDBHRbH7LKgft3R2KmqtRE7yoIfgvkaV\n8lzvqQyrAtC00Zdjf8pH+Q==\n-----END PRIVATE KEY-----\n",
+  "client_email": "gspread-access-bot@central-muse-438817-j2.iam.gserviceaccount.com",
+  "client_id": "106600733365406096449",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/gspread-access-bot%40central-muse-438817-j2.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
