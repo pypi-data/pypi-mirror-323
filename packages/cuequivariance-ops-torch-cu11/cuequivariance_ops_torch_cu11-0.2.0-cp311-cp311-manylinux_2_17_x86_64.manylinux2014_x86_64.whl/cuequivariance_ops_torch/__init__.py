fdaf2d4f1a036a3ec7c85f302c394a87f1a6c6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:04:28 +0000

    Add package cuequivariance-ops-torch-cu11/0.2.0/cuequivariance_ops_torch_cu11-0.2.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cuequivariance-ops-torch-cu11/cuequivariance_ops_torch_cu11-0.2.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cuequivariance_ops_torch/__init__.py
@@ -0,0 +1,52 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+#
+# NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
+# property and proprietary rights in and to this material, related
+# documentation and any modifications thereto. Any use, reproduction,
+# disclosure or distribution of this material and related documentation
+# without an express license agreement from NVIDIA CORPORATION or
+# its affiliates is strictly prohibited.
+
+import torch
+
+from cuequivariance_ops_torch._version import __git_commit__, __version__
+
+from cuequivariance_ops_torch import _ext
+
+
+from cuequivariance_ops_torch.segmented_transpose import (
+    segmented_transpose,
+)
+
+
+from cuequivariance_ops_torch.fused_tensor_product import (
+    fused_tensor_product,
+    FusedTensorProductOp3,
+    FusedTensorProductOp4,
+    tensor_product_info_as_ctype,
+    int_mappings_to_mode,
+)
+
+
+from cuequivariance_ops_torch.symmetric_tensor_contraction import (
+    SymmetricTensorContraction,
+)
+
+from cuequivariance_ops_torch.tensor_product_uniform_1d import (
+    TensorProductUniform4x1d,
+    TensorProductUniform1d,
+)
+
+
+__all__ = [
+    "segmented_transpose",
+    "fused_tensor_product",
+    "FusedTensorProductOp3",
+    "FusedTensorProductOp4",
+    "tensor_product_info_as_ctype",
+    "int_mappings_to_mode",
+    "SymmetricTensorContraction",
+    "TensorProductUniform4x1d",
+    "TensorProductUniform1d",
+]
