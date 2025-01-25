c46caee9cf5699e043651c8f27166a35f1150ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:04:47 +0000

    Add package prometheus-client-python-speedups/0.1.0/prometheus_client_python_speedups-0.1.0-cp39-cp39-musllinux_1_2_aarch64.whl

---
+++ packages/prometheus-client-python-speedups/prometheus_client_python_speedups-0.1.0-cp39-cp39-musllinux_1_2_aarch64.whl/prometheus_client_python_speedups/__init__.py
@@ -0,0 +1,5 @@
+from .prometheus_client_python_speedups import *
+
+__doc__ = prometheus_client_python_speedups.__doc__
+if hasattr(prometheus_client_python_speedups, "__all__"):
+    __all__ = prometheus_client_python_speedups.__all__
