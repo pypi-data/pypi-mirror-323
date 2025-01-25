5881716e18e7cbcc0124b348af101c82136477e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:04:20 +0000

    Add package sparkplug-api-feature-flags/1.0.1/sparkplug_api_feature_flags-1.0.1.tar.gz

---
+++ packages/sparkplug-api-feature-flags/sparkplug_api_feature_flags-1.0.1.tar.gz/sparkplug_api_feature_flags-1.0.1/sparkplug_feature_flags/managers/flag_access_manager.py
@@ -0,0 +1,42 @@
+# python
+from typing import TYPE_CHECKING, TypeVar
+
+# django
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models
+
+# app
+from .flag_access_qs import FlagAccessQS
+
+if TYPE_CHECKING:
+    from ..models import FlagAccess  # noqa: F401
+
+
+User = TypeVar("User", bound=AbstractBaseUser)
+
+
+class FlagAccessManager(
+    models.Manager["FlagAccess"],
+):
+    # Define a default queryset for the manager.
+    # This is used by self.all()
+    def get_queryset(self) -> FlagAccessQS:
+        return FlagAccessQS(self.model, using=self._db)
+
+    def feature_flag_ids(self, *, user: User) -> FlagAccessQS:
+        return (
+            self.all()
+            .filter(user=user)
+            .values_list(
+                "feature_flag__id",
+                flat=True,
+            )
+        )
+
+    def get_for_user(self, *, user: User) -> FlagAccessQS:
+        return (
+            self.all()
+            .filter(user=user)
+            .select_related("user")
+            .select_related("feature_flag")
+        )
