dd51c8f5c552277c3a6710c01accdb12050fac7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:04:26 +0000

    Add package cog/0.13.7/cog-0.13.7-py3-none-any.whl

---
+++ packages/cog/cog-0.13.7-py3-none-any.whl/cog/__init__.py
@@ -0,0 +1,39 @@
+import mimetypes
+
+from pydantic import BaseModel
+
+from .base_predictor import BasePredictor
+from .mimetypes_ext import install_mime_extensions
+from .server.scope import current_scope, emit_metric
+from .types import (
+    AsyncConcatenateIterator,
+    ConcatenateIterator,
+    ExperimentalFeatureWarning,
+    File,
+    Input,
+    Path,
+    Secret,
+)
+
+install_mime_extensions(mimetypes)
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0+unknown"
+
+
+__all__ = [
+    "__version__",
+    "current_scope",
+    "emit_metric",
+    "AsyncConcatenateIterator",
+    "BaseModel",
+    "BasePredictor",
+    "ConcatenateIterator",
+    "ExperimentalFeatureWarning",
+    "File",
+    "Input",
+    "Path",
+    "Secret",
+]
