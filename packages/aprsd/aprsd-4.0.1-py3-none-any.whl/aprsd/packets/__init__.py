2c3848ed42a72831c30c633351f9f24c654069de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 07:03:57 +0000

    Add package aprsd/4.0.1/aprsd-4.0.1-py3-none-any.whl

---
+++ packages/aprsd/aprsd-4.0.1-py3-none-any.whl/aprsd/packets/__init__.py
@@ -0,0 +1,30 @@
+from aprsd.packets import collector
+from aprsd.packets.core import (  # noqa: F401
+    AckPacket,
+    BeaconPacket,
+    BulletinPacket,
+    GPSPacket,
+    MessagePacket,
+    MicEPacket,
+    ObjectPacket,
+    Packet,
+    RejectPacket,
+    StatusPacket,
+    ThirdPartyPacket,
+    UnknownPacket,
+    WeatherPacket,
+    factory,
+)
+from aprsd.packets.packet_list import PacketList  # noqa: F401
+from aprsd.packets.seen_list import SeenList  # noqa: F401
+from aprsd.packets.tracker import PacketTrack  # noqa: F401
+from aprsd.packets.watch_list import WatchList  # noqa: F401
+
+# Register all the packet tracking objects.
+collector.PacketCollector().register(PacketList)
+collector.PacketCollector().register(SeenList)
+collector.PacketCollector().register(PacketTrack)
+collector.PacketCollector().register(WatchList)
+
+
+NULL_MESSAGE = -1
---
+++ packages/aprsd/aprsd-4.0.1-py3-none-any.whl/aprsd/threads/__init__.py
@@ -0,0 +1,12 @@
+import queue
+
+# Make these available to anyone importing
+# aprsd.threads
+from .aprsd import APRSDThread, APRSDThreadList  # noqa: F401
+from .rx import (  # noqa: F401
+    APRSDDupeRXThread,
+    APRSDProcessPacketThread,
+    APRSDRXThread,
+)
+
+packet_queue = queue.Queue(maxsize=20)
