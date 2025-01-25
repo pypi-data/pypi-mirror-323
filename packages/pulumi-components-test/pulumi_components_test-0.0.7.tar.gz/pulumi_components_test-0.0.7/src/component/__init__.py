a2fd00a2ba79fa519a0da19202107ca88e923fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:04:10 +0000

    Add package pulumi-components-test/0.0.7/pulumi_components_test-0.0.7.tar.gz

---
+++ packages/pulumi-components-test/pulumi_components_test-0.0.7.tar.gz/pulumi_components_test-0.0.7/src/component/__init__.py
@@ -0,0 +1,5 @@
+from .host import componentProviderHost
+from .metadata import Metadata
+from .provider import ComponentProvider
+
+__all__ = ["ComponentProvider", "componentProviderHost", "Metadata"]
