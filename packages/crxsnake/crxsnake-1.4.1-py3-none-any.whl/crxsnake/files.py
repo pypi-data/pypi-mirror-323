742269508cf10fdc3d27dc6dd9321707581694f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:20 +0000

    Add package crxsnake/1.4.1/crxsnake-1.4.1-py3-none-any.whl

---
+++ packages/crxsnake/crxsnake-1.4.1-py3-none-any.whl/crxsnake/files.py
@@ -0,0 +1,29 @@
+from typing import Optional, Any
+from json import loads, dumps
+from aiofiles import open
+
+
+async def read_json(path: str, *keys: str) -> Optional[Any]:
+    try:
+        async with open(path, "r", encoding="utf-8") as file:
+            file_content = await file.read()
+            file_data = loads(file_content)
+
+            for key in keys:
+                if key not in file_data:
+                    return None
+                file_data = file_data[key]
+
+            return file_data
+
+    except Exception:
+        raise
+
+
+async def write_json(path: str, data: Any) -> bool:
+    try:
+        async with open(path, "w", encoding="utf-8") as file:
+            await file.write(dumps(data, ensure_ascii=False, indent=2))
+            return True
+    except Exception:
+        raise
