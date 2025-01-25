9b72c5391a7ddd0ee743c807e165bd1cda3cf287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:04:14 +0000

    Add package pyliblo3/0.16.3/pyliblo3-0.16.3-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyliblo3/pyliblo3-0.16.3-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyliblo3/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+import platform
+import os
+
+if platform.system() == "Windows":
+    libspath = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'pyliblo3.libs'))
+    if os.path.exists(libspath) and os.path.isdir(libspath):
+        os.add_dll_directory(libspath)
+    else:
+        possible_paths = ['C:/Program Files/liblo/lib', 'C:/Program Files/liblo/bin']
+        for path in ['C:/Program Files/liblo/lib', 'C:/Program Files/liblo/bin']:
+            if os.path.exists(path) and os.path.isdir(path):
+                os.add_dll_directory(path)
+del platform
+del os
+
+
+from ._liblo import *
