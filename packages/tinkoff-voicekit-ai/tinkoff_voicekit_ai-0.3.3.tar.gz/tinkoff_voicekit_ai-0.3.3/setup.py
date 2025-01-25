e473cf0488887ec511f338dbd5b8fb38ca938743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 16:04:16 +0000

    Add package tinkoff-voicekit-ai/0.3.3/tinkoff_voicekit_ai-0.3.3.tar.gz

---
+++ packages/tinkoff-voicekit-ai/tinkoff_voicekit_ai-0.3.3.tar.gz/tinkoff_voicekit_ai-0.3.3/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='tinkoff_voicekit_ai',
+    version='0.3.3',
+    packages=['tinkoff_voicekit_ai'],
+    include_package_data=True,
+    license="Apache-2.0",
+    author="Albert Toma & Tinkoff Speech Team",
+    author_email="cornil@mail.ru",
+)
