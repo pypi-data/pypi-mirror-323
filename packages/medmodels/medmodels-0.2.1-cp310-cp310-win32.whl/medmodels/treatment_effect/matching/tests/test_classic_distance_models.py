c6b1b8bdd7eeb4a163eb5cdc4b1e610f58e0590d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:04:37 +0000

    Add package medmodels/0.2.1/medmodels-0.2.1-cp310-cp310-win32.whl

---
+++ packages/medmodels/medmodels-0.2.1-cp310-cp310-win32.whl/medmodels/treatment_effect/matching/tests/test_classic_distance_models.py
@@ -0,0 +1,58 @@
+import unittest
+
+import numpy as np
+import polars as pl
+import pytest
+
+from medmodels.treatment_effect.matching.algorithms import (
+    classic_distance_models as cdm,
+)
+
+
+class TestClassicDistanceModels(unittest.TestCase):
+    def test_nearest_neighbor(self) -> None:
+        ###########################################
+        # 1D example
+        c_set = pl.DataFrame({"a": [1, 5, 1, 3]})
+        t_set = pl.DataFrame({"a": [1, 4]})
+
+        expected_result = pl.DataFrame({"a": [1.0, 5.0]})
+        result = cdm.nearest_neighbor(t_set, c_set)
+        assert result.equals(expected_result)
+
+        ###########################################
+        # 3D example with covariates
+        cols = ["a", "b", "c"]
+        array = np.array([[1, 3, 5], [5, 2, 1], [1, 4, 10]])
+        c_set = pl.DataFrame(array, schema=cols)
+        t_set = pl.DataFrame([[1, 4, 2]], schema=cols, orient="row")
+        covs = ["a", "c"]
+
+        expected_result = pl.DataFrame([[1.0, 3.0, 5.0]], schema=cols, orient="row")
+        result = cdm.nearest_neighbor(t_set, c_set, covariates=covs)
+        assert result.equals(expected_result)
+
+        # 2 nearest neighbors
+        expected_abs_2nn = pl.DataFrame(
+            [[1.0, 3.0, 5.0], [1.0, 4.0, 10.0]], schema=cols, orient="row"
+        )
+        result_abs_2nn = cdm.nearest_neighbor(
+            t_set, c_set, covariates=covs, number_of_neighbors=2
+        )
+        assert result_abs_2nn.equals(expected_abs_2nn)
+
+    def test_nearest_neighbor_value_error(self) -> None:
+        """Checking the ValueError when all control units have been matched."""
+        c_set = pl.DataFrame({"a": [1, 2]})
+        t_set = pl.DataFrame({"a": [1, 2, 3]})
+
+        with pytest.raises(
+            ValueError,
+            match="The treated set is too large for the given number of neighbors",
+        ):
+            cdm.nearest_neighbor(t_set, c_set, number_of_neighbors=2)
+
+
+if __name__ == "__main__":
+    run_test = unittest.TestLoader().loadTestsFromTestCase(TestClassicDistanceModels)
+    unittest.TextTestRunner(verbosity=2).run(run_test)
