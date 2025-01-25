97830dd90a0cc44dc736feae37a5e1eb0e2a3389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:31 +0000

    Add package notramp/1.1.8/notramp-1.1.8.tar.gz

---
+++ packages/notramp/notramp-1.1.8.tar.gz/notramp-1.1.8/notramp/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2022, Simon Magin (simakro)
+# BSD-2 license
+# see LICENSE file for license details
+
+"""
+NoTrAmp is a Tool for read-depth normalization and trimming of amplicon
+ reads generated with long read technologies (ONT/PacBio).
+"""
+
+from .version import __version__
+
+
+VERSION = __version__
