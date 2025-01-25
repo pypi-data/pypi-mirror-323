18a0d60bb578e96f85aece0510e0573c673fa9ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 16:04:19 +0000

    Add package polaris-studio/24.3.330/polaris_studio-24.3.330-py3-none-manylinux_2_35_x86_64.whl

---
+++ packages/polaris-studio/polaris_studio-24.3.330-py3-none-manylinux_2_35_x86_64.whl/polaris/version.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025, UChicago Argonne, LLC
+# BSD OPEN SOURCE LICENSE. Full license can be found in LICENSE.md
+# from datetime import datetime, date
+
+
+year_release = 2024
+month_release = 3
+day_release = 330
+
+__version__ = f"{str(year_release)[-2:]}.{str(month_release).zfill(2)}.{str(day_release).zfill(3)}"
