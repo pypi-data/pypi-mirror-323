f564b31af0ee1ae33f2d82a3ff40450e39f2b456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:05:01 +0000

    Add package unstructured-platform/0.4.0/unstructured_platform-0.4.0-py3-none-any.whl

---
+++ packages/unstructured-platform/unstructured_platform-0.4.0-py3-none-any.whl/unstructured_platform_sdk/__init__.py
@@ -0,0 +1,7 @@
+"""Python SDK for the Unstructured Platform API."""
+
+from unstructured_platform_sdk.client import UnstructuredPlatformClient
+
+__version__ = "0.1.0"
+
+__all__ = ["UnstructuredPlatformClient"] 
