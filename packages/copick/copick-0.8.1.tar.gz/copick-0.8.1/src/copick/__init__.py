bfdbe3a1f0474d3100a626a2d495aea78a3c92f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 07:03:47 +0000

    Add package copick/0.8.1/copick-0.8.1.tar.gz

---
+++ packages/copick/copick-0.8.1.tar.gz/copick-0.8.1/src/copick/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.8.1"
+
+from copick.ops.open import from_czcdp_datasets, from_file, from_string
+
+__all__ = [
+    "from_file",
+    "from_string",
+    "from_czcdp_datasets",
+    "from_string",
+    "__version__",
+]
