02e12bf761b1b1b61706190b4c374545c38ebf0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 16:04:16 +0000

    Add package yookassa_api/0.1.1/yookassa_api-0.1.1-py3-none-any.whl

---
+++ packages/yookassa_api/yookassa_api-0.1.1-py3-none-any.whl/yookassa_api/api/methods.py
@@ -0,0 +1,127 @@
+from typing import TypeVar, Generic, Optional, List
+from yookassa_api.schemas import PaymentAmount, Receipt, Airline, Transfer, Deal
+
+
+T = TypeVar('T') 
+
+
+class APIMethod(Generic[T]):
+    """
+    Base API Method
+    """
+    http_method: str = "GET"
+    path: str
+
+
+class PaymentMethod(APIMethod):
+    """
+    Payment Method
+    """
+    http_method = "GET"
+    path = "/payments"
+
+    def __init__(self, path: str):
+        self.path = path
+
+    @classmethod
+    def build(cls, payment_id):
+        """
+        Build method
+        :param payment_id: Payment ID
+        :return: Method
+        """
+        path = cls.path.format(payment_id=payment_id)
+        return cls(path=path)
+
+
+class CreatePayment(APIMethod):
+    """
+    Create payment
+    """
+    http_method = "POST"
+    path = "/payments"
+
+    @staticmethod
+    def build_params(**kwargs):
+        if confirmation := kwargs.get("confirmation"):
+            kwargs["confirmation"] = confirmation.model_dump(exclude_none=True)
+        params = {
+            "amount": kwargs.get("amount").model_dump(),
+            "description": kwargs.get("description"),
+            "receipt": kwargs.get("receipt").model_dump() if kwargs.get("receipt") else None,
+            "recipient": kwargs.get("recipient").model_dump() if kwargs.get("recipient") else None,
+            "payment_token": kwargs.get("payment_token"),
+            "payment_method_id": kwargs.get("payment_method_id"),
+            "payment_method_data": kwargs.get("payment_method_data").model_dump() if kwargs.get("payment_method_data") else None,
+            "confirmation": kwargs.get("confirmation"),
+            "save_payment_method": kwargs.get("save_payment_method"),
+            "capture": kwargs.get("capture"),
+            "client_ip": kwargs.get("client_ip"),
+            "metadata": kwargs.get("metadata"),
+            "airline": kwargs.get("airline").model_dump() if kwargs.get("airline") else None,
+            "transfers": kwargs.get("transfers").model_dump() if kwargs.get("transfers") else None,
+            "deal": kwargs.get("deal").model_dump() if kwargs.get("deal") else None,
+            "merchant_customer_id": kwargs.get("merchant_customer_id"),
+        }
+        return params
+
+
+class GetPayments(APIMethod):
+    """
+    Get Payments
+    """
+    http_method = "GET"
+    path = "/payments"
+
+    @staticmethod
+    def build_params(created_at, captured_at,
+                     payment_method, status,
+                     limit, cursor, **kwargs):
+        params = {
+            "created_at_gte": created_at,
+            "captured_at_gte": captured_at,
+            "payment_method": payment_method.value if payment_method else None,
+            "status": status.value if status else None,
+            "limit": limit,
+            "cursor": cursor,
+            **kwargs
+        }
+        return params
+
+
+class GetPayment(PaymentMethod):
+    """
+    Get Payment
+    """
+    http_method = "GET"
+    path = "/payments/{payment_id}"
+
+
+class CapturePayment(PaymentMethod):
+    """
+    Capture Payment
+    """
+    http_method = "POST"
+    path = "/payments/{payment_id}/capture"
+
+    @staticmethod
+    def build_params(amount: Optional[PaymentAmount],
+                     receipt: Optional[Receipt],
+                     airline: Optional[Airline],
+                     transfers: Optional[List[Transfer]],
+                     deal: Optional[Deal]):
+        params = {
+            "amount": amount.model_dump() if amount else None,
+            "receipt": receipt.model_dump() if receipt else None,
+            "airline": airline.model_dump() if airline else None,
+            "transfers": [transfer.model_dump() for transfer in transfers] if transfers else None,
+            "deal": deal.model_dump() if deal else None,
+        }
+        return params
+
+
+class CancelPayment(PaymentMethod):
+    http_method = "POST"
+    path = "/payments/{payment_id}/cancel"
+    
+
