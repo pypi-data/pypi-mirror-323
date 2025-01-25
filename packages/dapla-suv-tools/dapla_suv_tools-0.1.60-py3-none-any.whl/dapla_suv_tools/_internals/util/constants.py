6a2c934d93d075d5ee8a1e191a5072a0c0cd97c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:45 +0000

    Add package dapla-suv-tools/0.1.60/dapla_suv_tools-0.1.60-py3-none-any.whl

---
+++ packages/dapla-suv-tools/dapla_suv_tools-0.1.60-py3-none-any.whl/dapla_suv_tools/_internals/util/constants.py
@@ -0,0 +1,21 @@
+OPERATION_OK = "OK"
+OPERATION_ERROR = "ERROR"
+
+LOG_DIAGNOSTIC = "diag"
+LOG_INFO = "info"
+LOG_WARNING = "warn"
+LOG_ERROR = "error"
+
+LOG_LEVELS = [LOG_DIAGNOSTIC, LOG_INFO, LOG_WARNING, LOG_ERROR]
+
+PERIODE_PATH = "/periode"
+PULJE_PATH = "/pulje"
+UTSENDING_PATH = "/utsending"
+UTSENDINGSTYPE_PATH = "/utsendingstype"
+SKJEMA_PATH = "/skjemadata/skjema"
+PREFILL_META_PATH = "/skjemadata/prefill-meta"
+INSTANCE_PATH = "/altinn/instance"
+INSTANTIATOR_RESEND_PATH = "/instantiator/resend"
+INNKVITTERING_RESEND_PATH = "/innkvittering/resend"
+PREFILL_API_STAT_PATH = "/public/prefill"
+SFU_PATH = "/sfu"
