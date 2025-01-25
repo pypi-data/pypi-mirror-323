10bec971cc939ddf5c014774c523466ab8d10327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:04:21 +0000

    Add package otter-grader/6.0.5/otter_grader-6.0.5.tar.gz

---
+++ packages/otter-grader/otter_grader-6.0.5.tar.gz/otter_grader-6.0.5/otter/generate/templates/common/run_otter.py
@@ -0,0 +1,7 @@
+"""Runs Otter-Grader's autograding process"""
+
+from otter.run.run_autograder import main as run_autograder
+
+
+if __name__ == "__main__":
+    run_autograder("{{ autograder_dir }}")
