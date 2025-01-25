f16cc7c98f4ea71c71826f77e76953ceda6214f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:30 +0000

    Add package openobd-protocol/1.18.0rc0/openobd_protocol-1.18.0rc0-py3-none-any.whl

---
+++ packages/openobd-protocol/openobd_protocol-1.18.0rc0-py3-none-any.whl/openobd_protocol/FunctionBroker/FunctionBrokerServices_pb2.pyi
@@ -0,0 +1,7 @@
+from openobd_protocol.SessionController.Messages import SessionController_pb2 as _SessionController_pb2
+from openobd_protocol.FunctionBroker.Messages import FunctionBroker_pb2 as _FunctionBroker_pb2
+from openobd_protocol.Session.Messages import Session_pb2 as _Session_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
