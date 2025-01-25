d0f89f012bac042c96e5d60ac99eb2e1850797a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:04:26 +0000

    Add package unstructured-ingest/0.4.1/unstructured_ingest-0.4.1-py3-none-any.whl

---
+++ packages/unstructured-ingest/unstructured_ingest-0.4.1-py3-none-any.whl/unstructured_ingest/v2/interfaces/process.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class BaseProcess(ABC):
+    def is_async(self) -> bool:
+        return False
+
+    def precheck(self) -> None:
+        pass
+
+    @abstractmethod
+    def run(self, **kwargs: Any) -> Any:
+        pass
+
+    async def run_async(self, **kwargs: Any) -> Any:
+        return self.run(**kwargs)
