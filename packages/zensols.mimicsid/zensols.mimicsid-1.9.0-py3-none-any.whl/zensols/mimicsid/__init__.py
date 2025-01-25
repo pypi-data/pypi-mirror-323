6473d14138bbdac22e906f7a43c4790a59736802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:04:30 +0000

    Add package zensols.mimicsid/1.9.0/zensols.mimicsid-1.9.0-py3-none-any.whl

---
+++ packages/zensols.mimicsid/zensols.mimicsid-1.9.0-py3-none-any.whl/zensols/mimicsid/__init__.py
@@ -0,0 +1,21 @@
+"""MIMIC-III corpus parsing and section prediction with MedSecId.
+
+"""
+__author__ = 'Paul Landes'
+
+
+def suppress_warnings():
+    """The pretrained model uses a deprecated API."""
+    import warnings
+    import zensols.mednlp
+    warnings.filterwarnings(
+        'ignore', message='remove_empty_sentences is deprecated.*')
+    zensols.mednlp.surpress_warnings()
+
+
+suppress_warnings()
+
+
+from .domain import *
+from .app import *
+from .cli import *
