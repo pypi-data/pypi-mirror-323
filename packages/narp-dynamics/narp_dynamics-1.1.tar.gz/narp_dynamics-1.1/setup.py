15eb751f028ab815cd63c0819a21fb79e2504ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:04:42 +0000

    Add package narp-dynamics/1.1/narp_dynamics-1.1.tar.gz

---
+++ packages/narp-dynamics/narp_dynamics-1.1.tar.gz/narp_dynamics-1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = 'narp-dynamics',
+    version = '1.1',
+    py_modules = ['narp_functions', 'narp_plotting_functions'],
+    description = 'Implementation of NARP Pulses and Their Interaction Dynamics with Two-Level Systems',
+    long_description = open('README.md', encoding = 'utf-8').read(),
+    long_description_content_type = 'text/markdown',
+    author = 'Juan E. Ardila-García',
+    author_email = 'juardilag@unal.edu.co',
+    url = 'https://gitlab.com/juardilag/narp-dynaimics',
+    install_requires = [
+        'jax',
+        'matplotlib',
+        'cmap',
+        'tqdm'
+    ]
+)
