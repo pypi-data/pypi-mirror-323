979d356412290e7f5a9e05963dfa22a328a32642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:04:43 +0000

    Add package dash2html/1.0/dash2html-1.0.tar.gz

---
+++ packages/dash2html/dash2html-1.0.tar.gz/dash2html-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dash2html',
+    packages=find_packages(),
+    version='1.0',
+    description='Convert Plotly Dash App To Static HTML File',
+    author='Jenil sheth',
+    author_email="shethjeniljigneshbhai@gmail.com"
+)
