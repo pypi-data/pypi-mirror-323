c94a5be44b9d76f4a9eed07d015527a516d0a38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:04:29 +0000

    Add package screening-ai/1.0.0/screening_ai-1.0.0-py3-none-any.whl

---
+++ packages/screening-ai/screening_ai-1.0.0-py3-none-any.whl/screening_ai/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from screening_ai.api.platform_screening_jobs_api import PlatformScreeningJobsApi
+from screening_ai.api.platform_screening_submissions_api import PlatformScreeningSubmissionsApi
+from screening_ai.api.platform_screening_templates_api import PlatformScreeningTemplatesApi
+
