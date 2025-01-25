1c0ce9209793f6db3114ac85546db7bb523b7473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:37 +0000

    Add package pytrilogy/0.0.3.0/pytrilogy-0.0.3.0.tar.gz

---
+++ packages/pytrilogy/pytrilogy-0.0.3.0.tar.gz/pytrilogy-0.0.3.0/tests/test_executor.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from trilogy import Dialects, parse
+from trilogy.core.models.environment import Environment
+
+
+def test_file_parsing():
+    target = Path(__file__).parent / "test_env.preql"
+    parsed = Dialects.DUCK_DB.default_executor().parse_file(target)
+    assert len(list(parsed)) == 1
+
+
+def test_can_handle_everything():
+    env = Environment(working_path=Path(__file__).parent)
+    env, queries = parse(
+        """
+import test_env as test_env;
+
+key x int;
+
+merge test_env.id into x;
+                         
+RAW_SQL('''select 1 ''');
+""",
+        environment=env,
+    )
+    execs = Dialects.DUCK_DB.default_executor(environment=env)
+    for q in queries:
+        execs.execute_query(q)
---
+++ packages/pytrilogy/pytrilogy-0.0.3.0.tar.gz/pytrilogy-0.0.3.0/tests/test_imports.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+from trilogy.core.models.environment import Environment
+
+
+def test_multi_environment():
+    basic = Environment()
+
+    basic.parse(
+        """
+const pi <- 3.14;
+                     
+
+""",
+        namespace="math",
+    )
+
+    basic.parse(
+        """
+                
+            select math.pi;
+                """
+    )
+
+    assert basic.concepts["math.pi"].name == "pi"
+
+
+def test_test_alias_free_import():
+    basic = Environment(working_path=Path(__file__).parent)
+
+    basic.parse(
+        """
+import test_env;
+
+key id2 int;
+
+
+""",
+    )
+
+    assert basic.concepts["id"].name == "id"
+    assert basic.concepts["id2"].name == "id2"
+    assert basic.concepts["id"].namespace == basic.concepts["id2"].namespace
