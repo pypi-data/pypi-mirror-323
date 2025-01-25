1bff072892a4ad96532185ec9a6fbf6ce0664e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 07:04:02 +0000

    Add package litechat/0.0.59/litechat-0.0.59-py3-none-any.whl

---
+++ packages/litechat/litechat-0.0.59-py3-none-any.whl/litechat/__init__.py
@@ -0,0 +1,6 @@
+from ._core import completion,pp_completion,genai,LiteAI,OpenAI,litechat_server,litechat_model,litellm_model,OPENAI_COMPATIBLE_BASE_URL,HFChatModels
+from ._codegpt import litechat_codegpt_server
+from ._gmail_auto import GmailAutomation,gmail,automail
+from ._llamacpp_oai_server import llamacpp_server
+from .server import server
+from ._proj_to_prompt import ProjectToPrompt
