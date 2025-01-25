f1c7b5eb454d97ac56968369052437c5aff186d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:04:13 +0000

    Add package near-py-builder/0.1.0/near_py_builder-0.1.0.tar.gz

---
+++ packages/near-py-builder/near_py_builder-0.1.0.tar.gz/near_py_builder-0.1.0/near_py_builder/assets/micropython/ports/webassembly-near/export_wrappers.c
@@ -0,0 +1,12 @@
+/* THIS FILE IS GENERATED, DO NOT EDIT */
+
+void run_frozen_fn(const char *file_name, const char *fn_name);
+
+void echo() {
+  run_frozen_fn("contract.py", "echo");
+}
+
+void hello_world() {
+  run_frozen_fn("contract.py", "hello_world");
+}
+
