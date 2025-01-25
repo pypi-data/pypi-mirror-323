cb597892f9b43b0ae0a880fe181cc21d088adf58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:04:12 +0000

    Add package agentjo/0.0.4/agentjo-0.0.4.tar.gz

---
+++ packages/agentjo/agentjo-0.0.4.tar.gz/agentjo-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="agentjo",
+    version="0.0.4",
+    packages=find_packages(),
+    install_requires=[
+        "openai>=1.3.6",
+        "langchain",
+        "dill>=0.3.7",
+        "termcolor>=2.3.0",
+        "requests",
+        "PyPDF2",
+        "python-docx",
+        "pandas",
+        "xlrd"
+    ],
+)
