45061fce0b1b1dfc72bca3e3792b15890a04ea98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:06:07 +0000

    Add package dv-flow-libhdlim/0.0.1.12960691532a1/dv_flow_libhdlim-0.0.1.12960691532a1-py3-none-any.whl

---
+++ packages/dv-flow-libhdlim/dv_flow_libhdlim-0.0.1.12960691532a1-py3-none-any.whl/dv_flow/libhdlsim/vlt_sim_image.py
@@ -0,0 +1,33 @@
+import os
+from typing import List
+from dv_flow.mgr import Task, TaskData
+from dv_flow.libhdlsim.vl_sim_image import VlSimImage
+
+class SimImage(VlSimImage):
+
+    def getRefTime(self):
+        if os.path.isfile(os.path.join(self.rundir, 'obj_dir/simv')):
+            print("Returning timestamp")
+            return os.path.getmtime(os.path.join(self.rundir, 'obj_dir/simv'))
+        else:
+            raise Exception("simv file (%s) does not exist" % os.path.join(self.rundir, 'obj_dir/simv'))
+    
+    async def build(self, files : List[str], incdirs : List[str]):
+        cmd = ['verilator', '--binary', '-o', 'simv']
+
+        for incdir in incdirs:
+            cmd.append('+incdir+%s' % incdir)
+
+        cmd.extend(files)
+
+        for top in self.params.top:
+            cmd.extend(['--top-module', top])
+
+        print("self.basedir=%s" % self.rundir)
+        proc = await self.session.create_subprocess(*cmd,
+                                                        cwd=self.rundir)
+        await proc.wait()
+
+        if proc.returncode != 0:
+            raise Exception("Verilator failed (%d)" % proc.returncode)
+
---
+++ packages/dv-flow-libhdlim/dv_flow_libhdlim-0.0.1.12960691532a1-py3-none-any.whl/dv_flow/libhdlsim/xcm_sim_run.py
@@ -0,0 +1,35 @@
+
+import os
+import shutil
+from typing import List
+from dv_flow.mgr import Task, TaskData, FileSet
+from dv_flow.libhdlsim.vl_sim_image import VlSimImage
+
+class SimRun(Task):
+
+    async def run(self, input : TaskData) -> TaskData:
+        vl_fileset = input.getFileSets("simDir")
+
+        build_dir = vl_fileset[0].basedir
+
+        # First  things first: link in the library
+        os.symlink(
+            src=os.path.join(build_dir, "xcelium.d"),
+            dst=self.rundir)
+
+        cmd = ['xmsim', '-64bit', 'simv:snap' ]
+
+        fp = open(os.path.join(self.rundir, 'sim.log'), "w")
+        proc = await self.session.create_subprocess(*cmd,
+                                                    cwd=self.rundir,
+                                                    stdout=fp)
+
+        await proc.wait()
+
+        fp.close()
+
+        output = TaskData()
+        output.addFileSet(FileSet(src=self.name, type="simRunDir", basedir=self.rundir))
+
+        return output
+    pass
