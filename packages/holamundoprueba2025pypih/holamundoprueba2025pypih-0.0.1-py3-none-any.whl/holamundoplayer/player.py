57a20d087129cc04e7b67b7506046d21e77eeeaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:42 +0000

    Add package holamundoprueba2025pypih/0.0.1/holamundoprueba2025pypih-0.0.1-py3-none-any.whl

---
+++ packages/holamundoprueba2025pypih/holamundoprueba2025pypih-0.0.1-py3-none-any.whl/holamundoplayer/player.py
@@ -0,0 +1,22 @@
+"""Este es el modulo que incluye la clase de reproductor de musica"""
+
+class Player():
+    """
+    Esta clase crea un Reproductor de musica
+    """
+    
+    def play(self, song):
+        """
+        Reproduce la musica que recibio como parametro
+        
+        Parameters:
+        song (str): este es un string con el path de la cancion
+        
+        Returns:
+        int: devuelve 1 si reproduce con exito, en caso de fallo devuelve 0
+        """
+        
+        print('reproduciendo cancion')
+        
+    def stop():
+        print('stoping')
