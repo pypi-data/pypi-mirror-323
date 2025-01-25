00377e41032846c4cf48e66c68083c48071bad50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:05:26 +0000

    Add package fast-hard/0.1.0/fast_hard-0.1.0.tar.gz

---
+++ packages/fast-hard/fast_hard-0.1.0.tar.gz/fast_hard-0.1.0/README.md
@@ -0,0 +1,12 @@
+# Fast Hard
+
+Um pacote para gerar projetos FastAPI com estrutura básica.
+
+## Como usar
+
+Instale o pacote:
+
+```bash
+pip install fast_hard
+
+fast-hard meu_novo_projeto
---
+++ packages/fast-hard/fast_hard-0.1.0.tar.gz/fast_hard-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="fast_hard",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+    ],
+    entry_points={
+        "console_scripts": [
+            "fast-hard=fast_hard.cli:create_project",
+        ],
+    },
+    author="Felipe Hardmann",
+    author_email="fashardmann@gmail.com",
+    description="Um pacote para gerar projetos FastAPI com estrutura básica.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/seu-usuario/fast_hard",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
