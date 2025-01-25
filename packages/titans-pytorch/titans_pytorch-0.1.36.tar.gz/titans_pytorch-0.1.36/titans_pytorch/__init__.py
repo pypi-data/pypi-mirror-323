80b489b2386d8a44cc288f99dbd2fdd59164e3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:04:09 +0000

    Add package titans-pytorch/0.1.36/titans_pytorch-0.1.36.tar.gz

---
+++ packages/titans-pytorch/titans_pytorch-0.1.36.tar.gz/titans_pytorch-0.1.36/titans_pytorch/__init__.py
@@ -0,0 +1,11 @@
+from titans_pytorch.neural_memory import (
+    NeuralMemory,
+    MemoryMLP,
+    MemoryAttention,
+    FactorizedMemoryMLP,
+    GatedResidualMemoryMLP
+)
+
+from titans_pytorch.mac_transformer import (
+    MemoryAsContextTransformer
+)
