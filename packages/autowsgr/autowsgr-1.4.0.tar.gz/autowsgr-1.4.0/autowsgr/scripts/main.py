980673edf3b7490ca0e309a4cc3b0efa31852491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:05:56 +0000

    Add package autowsgr/1.4.0/autowsgr-1.4.0.tar.gz

---
+++ packages/autowsgr/autowsgr-1.4.0.tar.gz/autowsgr-1.4.0/autowsgr/scripts/main.py
@@ -0,0 +1,40 @@
+import logging
+
+from airtest.core.settings import Settings as ST  # noqa
+
+from autowsgr.game.build import BuildManager
+from autowsgr.timer import Timer
+from autowsgr.user_config import UserConfig
+from autowsgr.utils.io import yaml_to_dict
+from autowsgr.utils.logger import Logger
+from autowsgr.utils.update import check_for_updates
+
+
+def start_script(settings_path=None) -> Timer:
+    """启动脚本, 返回一个 Timer 记录器.
+    :如果模拟器没有运行, 会尝试启动模拟器,
+    :如果游戏没有运行, 会自动启动游戏,
+    :如果游戏在后台, 会将游戏转到前台
+    Returns:
+        Timer: 该模拟器的记录器
+    """
+    # airtest全局设置
+    ST.CVSTRATEGY = ['tpl']
+
+    # config
+    config_dict = {} if settings_path is None else yaml_to_dict(settings_path)
+    config = UserConfig.from_dict(config_dict)
+    config.pprint()
+
+    if config.check_update:
+        check_for_updates()
+
+    # logger
+    logging.getLogger('airtest').setLevel(logging.ERROR)
+    logger = Logger(config.log_dir, config.log_level)
+    logger.save_config(config)
+
+    timer = Timer(config, logger)
+    timer.port.factory = BuildManager(timer)
+
+    return timer
