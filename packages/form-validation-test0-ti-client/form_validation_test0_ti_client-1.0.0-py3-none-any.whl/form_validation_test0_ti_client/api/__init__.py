8d3e3bfa3266d477aaec140f3d20fc02a630501c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:04:30 +0000

    Add package form-validation-test0-ti-client/1.0.0/form_validation_test0_ti_client-1.0.0-py3-none-any.whl

---
+++ packages/form-validation-test0-ti-client/form_validation_test0_ti_client-1.0.0-py3-none-any.whl/form_validation_test0_ti_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from form_validation_test0_ti_client.api.pet_api import PetApi
+from form_validation_test0_ti_client.api.store_api import StoreApi
+from form_validation_test0_ti_client.api.user_api import UserApi
+
