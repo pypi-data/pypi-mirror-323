3b249c749ee619c25e13a59d02c5f66e147fa0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:04:44 +0000

    Add package dipdup/8.2.0rc1/dipdup-8.2.0rc1.tar.gz

---
+++ packages/dipdup/dipdup-8.2.0rc1.tar.gz/dipdup-8.2.0rc1/src/dipdup/config/http.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from pydantic import ConfigDict
+from pydantic.dataclasses import dataclass
+
+from dipdup.config import DatasourceConfig
+from dipdup.config import HttpConfig
+
+
+@dataclass(config=ConfigDict(extra='forbid', defer_build=True), kw_only=True)
+class HttpDatasourceConfig(DatasourceConfig):
+    """Generic HTTP datasource config
+
+    :param kind: always 'http'
+    :param url: URL to fetch data from
+    :param http: HTTP client configuration
+    """
+
+    kind: Literal['http']
+    url: str
+    http: HttpConfig | None = None
