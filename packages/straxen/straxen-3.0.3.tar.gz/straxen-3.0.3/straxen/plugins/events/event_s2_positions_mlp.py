95709680a7cb0ccea29a86a537c6065085b66bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:04:12 +0000

    Add package straxen/3.0.3/straxen-3.0.3.tar.gz

---
+++ packages/straxen/straxen-3.0.3.tar.gz/straxen-3.0.3/straxen/plugins/events/event_s2_positions_mlp.py
@@ -0,0 +1,30 @@
+import strax
+import straxen
+from straxen.plugins.events._event_s2_positions_base import EventS2PositionBase
+
+
+export, __all__ = strax.exporter()
+
+
+@export
+class EventS2PositionMLP(EventS2PositionBase):
+    """MLP neural net for S2 position reconstruction at event level."""
+
+    algorithm = "mlp"
+    provides = "event_s2_positions_mlp"
+
+    tf_model_mlp = straxen.URLConfig(
+        default=(
+            "tf://"
+            "resource://"
+            "xedocs://posrec_models"
+            "?attr=value"
+            "&fmt=abs_path"
+            "&kind=mlp"
+            "&run_id=plugin.run_id"
+            "&version=ONLINE"
+        ),
+        help='MLP model. Should be opened using the "tf" descriptor. '
+        'Set to "None" to skip computation',
+        cache=3,
+    )
