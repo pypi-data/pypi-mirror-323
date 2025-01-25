ac4a0305e8d662429465db48880c873e5b927050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:05:30 +0000

    Add package metronome-sdk/0.1.0b4/metronome_sdk-0.1.0b4.tar.gz

---
+++ packages/metronome-sdk/metronome_sdk-0.1.0b4.tar.gz/metronome_sdk-0.1.0b4/src/metronome/types/shared/contract_without_amendments.py
@@ -0,0 +1,139 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+from datetime import datetime
+from typing_extensions import Literal
+
+from .commit import Commit
+from .credit import Credit
+from .discount import Discount
+from .override import Override
+from ..._models import BaseModel
+from .pro_service import ProService
+from .scheduled_charge import ScheduledCharge
+from .base_usage_filter import BaseUsageFilter
+
+__all__ = [
+    "ContractWithoutAmendments",
+    "Transition",
+    "UsageStatementSchedule",
+    "ResellerRoyalty",
+    "UsageFilter",
+    "UsageFilterUpdate",
+]
+
+
+class Transition(BaseModel):
+    from_contract_id: str
+
+    to_contract_id: str
+
+    type: Literal["SUPERSEDE", "RENEWAL"]
+
+
+class UsageStatementSchedule(BaseModel):
+    billing_anchor_date: datetime
+    """Contract usage statements follow a selected cadence based on this date."""
+
+    frequency: Literal["MONTHLY", "QUARTERLY", "ANNUAL"]
+
+
+class ResellerRoyalty(BaseModel):
+    fraction: float
+
+    netsuite_reseller_id: str
+
+    reseller_type: Literal["AWS", "AWS_PRO_SERVICE", "GCP", "GCP_PRO_SERVICE"]
+
+    starting_at: datetime
+
+    applicable_product_ids: Optional[List[str]] = None
+
+    applicable_product_tags: Optional[List[str]] = None
+
+    aws_account_number: Optional[str] = None
+
+    aws_offer_id: Optional[str] = None
+
+    aws_payer_reference_id: Optional[str] = None
+
+    ending_before: Optional[datetime] = None
+
+    gcp_account_id: Optional[str] = None
+
+    gcp_offer_id: Optional[str] = None
+
+    reseller_contract_value: Optional[float] = None
+
+
+class UsageFilterUpdate(BaseModel):
+    group_key: str
+
+    group_values: List[str]
+
+    starting_at: datetime
+
+
+class UsageFilter(BaseModel):
+    current: Optional[BaseUsageFilter] = None
+
+    initial: BaseUsageFilter
+
+    updates: List[UsageFilterUpdate]
+
+
+class ContractWithoutAmendments(BaseModel):
+    commits: List[Commit]
+
+    created_at: datetime
+
+    created_by: str
+
+    overrides: List[Override]
+
+    scheduled_charges: List[ScheduledCharge]
+
+    starting_at: datetime
+
+    transitions: List[Transition]
+
+    usage_statement_schedule: UsageStatementSchedule
+
+    credits: Optional[List[Credit]] = None
+
+    discounts: Optional[List[Discount]] = None
+    """This field's availability is dependent on your client's configuration."""
+
+    ending_before: Optional[datetime] = None
+
+    name: Optional[str] = None
+
+    net_payment_terms_days: Optional[float] = None
+
+    netsuite_sales_order_id: Optional[str] = None
+    """This field's availability is dependent on your client's configuration."""
+
+    professional_services: Optional[List[ProService]] = None
+    """This field's availability is dependent on your client's configuration."""
+
+    rate_card_id: Optional[str] = None
+
+    reseller_royalties: Optional[List[ResellerRoyalty]] = None
+    """This field's availability is dependent on your client's configuration."""
+
+    salesforce_opportunity_id: Optional[str] = None
+    """This field's availability is dependent on your client's configuration."""
+
+    scheduled_charges_on_usage_invoices: Optional[Literal["ALL"]] = None
+    """
+    Determines which scheduled and commit charges to consolidate onto the Contract's
+    usage invoice. The charge's `timestamp` must match the usage invoice's
+    `ending_before` date for consolidation to occur. This field cannot be modified
+    after a Contract has been created. If this field is omitted, charges will appear
+    on a separate invoice from usage charges.
+    """
+
+    total_contract_value: Optional[float] = None
+    """This field's availability is dependent on your client's configuration."""
+
+    usage_filter: Optional[UsageFilter] = None
