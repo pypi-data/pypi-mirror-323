2982db5449917db61776c7a651a10363112a1a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:54 +0000

    Add package discordme.py/0.1.0/discordme_py-0.1.0.tar.gz

---
+++ packages/discordme.py/discordme_py-0.1.0.tar.gz/discordme_py-0.1.0/tests/test_main.py
@@ -0,0 +1,5 @@
+# test_import.py
+from discordme import get_token, send_mp, add_reaction, change_status, remove_status
+
+token = get_token("wathd_developement@proton.me", "QWERTYETOXEM@123")
+remove_status(token)
---
+++ packages/discordme.py/discordme_py-0.1.0.tar.gz/discordme_py-0.1.0/tests/tests_for_add.py
@@ -0,0 +1,4 @@
+import requests
+from discordme import get_token, change_bio
+
+token = get_token("wathd_developement@proton.me", "QWERTYETOXEM@123")
