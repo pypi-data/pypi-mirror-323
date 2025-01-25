68fc91f5256473027119ef24d7f34c87607b8781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 16:04:20 +0000

    Add package neurodb/0.0.1/neurodb-0.0.1-py3-none-any.whl

---
+++ packages/neurodb/neurodb-0.0.1-py3-none-any.whl/neurodb/__init__.py
@@ -0,0 +1,6 @@
+"""
+My package initialization file.
+"""
+from .main import my_function
+
+__version__ = "0.0.1"
---
+++ packages/neurodb/neurodb-0.0.1-py3-none-any.whl/neurodb/main.py
@@ -0,0 +1,5 @@
+def my_function():
+    """
+    Example function that returns a greeting.
+    """
+    return "Hello from my_package!"
