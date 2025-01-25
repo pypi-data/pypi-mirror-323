e332452f436c8519e11a81201a85797f23cc9183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:45 +0000

    Add package invenio-records-marc21/0.23.0/invenio_records_marc21-0.23.0-py2.py3-none-any.whl

---
+++ packages/invenio-records-marc21/invenio_records_marc21-0.23.0-py2.py3-none-any.whl/invenio_records_marc21/services/config.py
@@ -0,0 +1,234 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+#
+# Copyright (C) 2021-2023 Graz University of Technology.
+#
+# Invenio-Records-Marc21 is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Marc21 Record Service config."""
+
+from invenio_drafts_resources.services.records.components import DraftFilesComponent
+from invenio_drafts_resources.services.records.config import (
+    RecordServiceConfig,
+    SearchDraftsOptions,
+    SearchOptions,
+    SearchVersionsOptions,
+    is_draft,
+    is_record,
+)
+from invenio_i18n import gettext as _
+from invenio_indexer.api import RecordIndexer
+from invenio_rdm_records.services import facets as rdm_facets
+from invenio_rdm_records.services.components import AccessComponent
+from invenio_rdm_records.services.config import has_doi, is_record_and_has_doi
+from invenio_records_resources.services import (
+    ConditionalLink,
+    FileServiceConfig,
+    pagination_links,
+)
+from invenio_records_resources.services.base.config import (
+    ConfiguratorMixin,
+    FromConfig,
+    FromConfigSearchOptions,
+    SearchOptionsMixin,
+)
+from invenio_records_resources.services.base.links import Link
+from invenio_records_resources.services.files.links import FileLink
+from invenio_records_resources.services.records.links import RecordLink
+
+from ..records import Marc21Draft, Marc21Parent, Marc21Record
+from . import facets
+from .components import DefaultRecordsComponents
+from .customizations import FromConfigPIDsProviders, FromConfigRequiredPIDs
+from .permissions import Marc21RecordPermissionPolicy
+from .schemas import Marc21ParentSchema, Marc21RecordSchema
+
+
+class Marc21SearchOptions(SearchOptions, SearchOptionsMixin):
+    """Search options for record search."""
+
+    facets = {
+        "access_status": rdm_facets.access_status,
+        "resource_type": facets.resource_type,
+    }
+
+
+class Marc21SearchDraftsOptions(SearchDraftsOptions, SearchOptionsMixin):
+    """Search options for drafts search."""
+
+    facets = {
+        "access_status": rdm_facets.access_status,
+        "is_published": facets.is_published,
+        "resource_type": facets.resource_type,
+    }
+
+
+class Marc21SearchVersionsOptions(SearchVersionsOptions, SearchOptionsMixin):
+    """Search options for record versioning search."""
+
+
+class Marc21RecordServiceConfig(RecordServiceConfig, ConfiguratorMixin):
+    """Marc21 record service config."""
+
+    # Record class
+    record_cls = Marc21Record
+    # Draft class
+    draft_cls = Marc21Draft
+    # Parent class
+    parent_record_cls = Marc21Parent
+
+    indexer_cls = RecordIndexer
+    indexer_queue_name = "marc21-records"
+    draft_indexer_cls = RecordIndexer
+    draft_indexer_queue_name = "marc21-records-drafts"
+
+    # Schemas
+    schema = Marc21RecordSchema
+    schema_parent = Marc21ParentSchema
+
+    schema_secret_link = None
+    review = None
+
+    permission_policy_cls = FromConfig(
+        "MARC21_PERMISSION_POLICY",
+        default=Marc21RecordPermissionPolicy,
+        import_string=True,
+    )
+    # Search
+    search = FromConfigSearchOptions(
+        "MARC21_SEARCH",
+        "MARC21_SORT_OPTIONS",
+        "MARC21_FACETS",
+        search_option_cls=Marc21SearchOptions,
+    )
+    search_drafts = FromConfigSearchOptions(
+        "MARC21_SEARCH_DRAFTS",
+        "MARC21_SORT_OPTIONS",
+        "MARC21_FACETS",
+        search_option_cls=Marc21SearchDraftsOptions,
+    )
+    search_versions = FromConfigSearchOptions(
+        "MARC21_SEARCH_VERSIONING",
+        "MARC21_SORT_OPTIONS",
+        "MARC21_FACETS",
+        search_option_cls=Marc21SearchVersionsOptions,
+    )
+
+    # Permission policy
+    default_files_enabled = FromConfig("MARC21_DEFAULT_FILES_ENABLED", default=True)
+
+    permission_policy_cls = FromConfig(
+        "MARC21_PERMISSION_POLICY",
+        default=Marc21RecordPermissionPolicy,
+        import_string=True,
+    )
+    links_search = pagination_links("{+api}/publications{?args*}")
+
+    links_search_drafts = pagination_links("{+api}/user/publications{?args*}")
+
+    links_search_versions = pagination_links(
+        "{+api}/publications/{id}/versions{?args*}"
+    )
+
+    components = FromConfig(
+        "MARC21_RECORDS_SERVICE_COMPONENTS",
+        default=DefaultRecordsComponents,
+    )
+
+    links_item = {
+        "self": ConditionalLink(
+            cond=is_record,
+            if_=RecordLink("{+api}/publications/{id}"),
+            else_=RecordLink("{+api}/publications/{id}/draft"),
+        ),
+        "self_html": ConditionalLink(
+            cond=is_record,
+            if_=RecordLink("{+ui}/publications/{id}"),
+            else_=RecordLink("{+ui}/publications/uploads/{id}"),
+        ),
+        "self_doi": Link(
+            "{+ui}/publications/{+pid_doi}",
+            when=is_record_and_has_doi,
+            vars=lambda record, vars: vars.update(
+                {
+                    f"pid_{scheme}": pid["identifier"].split("/")[1]
+                    for (scheme, pid) in record.pids.items()
+                }
+            ),
+        ),
+        "doi": Link(
+            "https://doi.org/{+pid_doi}",
+            when=has_doi,
+            vars=lambda record, vars: vars.update(
+                {
+                    f"pid_{scheme}": pid["identifier"]
+                    for (scheme, pid) in record.pids.items()
+                }
+            ),
+        ),
+        "files": ConditionalLink(
+            cond=is_record,
+            if_=RecordLink("{+api}/publications/{id}/files"),
+            else_=RecordLink("{+api}/publications/{id}/draft/files"),
+        ),
+        "latest": RecordLink("{+api}/publications/{id}/versions/latest"),
+        "latest_html": RecordLink("{+ui}/publications/{id}/latest"),
+        "draft": RecordLink("{+api}/publications/{id}/draft", when=is_record),
+        "publish": RecordLink(
+            "{+api}/publications/{id}/draft/actions/publish", when=is_draft
+        ),
+        "versions": RecordLink("{+api}/publications/{id}/versions"),
+    }
+
+    # PIDs providers - set from config in customizations.
+    pids_providers = FromConfigPIDsProviders(
+        persistent_identifiers="MARC21_PERSISTENT_IDENTIFIERS",
+        persistent_identifier_providers="MARC21_PERSISTENT_IDENTIFIER_PROVIDERS",
+    )
+    pids_required = FromConfigRequiredPIDs(
+        persistent_identifiers="MARC21_PERSISTENT_IDENTIFIERS",
+    )
+
+
+#
+# Record files
+#
+class Marc21RecordFilesServiceConfig(FileServiceConfig, ConfiguratorMixin):
+    """Marc21 record files service configuration."""
+
+    record_cls = Marc21Record
+    permission_policy_cls = Marc21RecordPermissionPolicy
+    permission_action_prefix = ""
+
+    file_links_list = {
+        "self": RecordLink("{+api}/publications/{id}/files"),
+    }
+
+    file_links_item = {
+        "self": FileLink("{+api}/publications/{id}/files/{key}"),
+        "content": FileLink("{+api}/publications/{id}/files/{key}/content"),
+    }
+
+
+#
+# Draft files
+#
+class Marc21DraftFilesServiceConfig(FileServiceConfig, ConfiguratorMixin):
+    """Marc21 draft files service configuration."""
+
+    record_cls = Marc21Draft
+    permission_policy_cls = Marc21RecordPermissionPolicy
+    permission_action_prefix = "draft_"
+
+    file_links_list = {
+        "self": RecordLink("{+api}/publications/{id}/draft/files"),
+    }
+
+    file_links_item = {
+        "self": FileLink("{+api}/publications/{id}/draft/files/{key}"),
+        "content": FileLink("{+api}/publications/{id}/draft/files/{key}/content"),
+        "commit": FileLink("{+api}/publications/{id}/draft/files/{key}/commit"),
+    }
---
+++ packages/invenio-records-marc21/invenio_records_marc21-0.23.0-py2.py3-none-any.whl/invenio_records_marc21/services/record/metadata.py
@@ -0,0 +1,300 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+#
+# Copyright (C) 2021-2025 Graz University of Technology.
+#
+# Invenio-Records-Marc21 is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Marc21 record class."""
+
+from __future__ import annotations
+
+from contextlib import suppress
+from xml.etree.ElementTree import Element
+
+
+class QName:
+    """Local Rewrite for lxml.etree.QName."""
+
+    def __init__(self, node):
+        """Constructor for QName."""
+        self.node = node
+
+    @property
+    def localname(self):
+        """Return localname from node with xpath."""
+        return self.node.tag.split("}")[-1]
+
+    @property
+    def namespace(self):
+        """Return namespace from node with xpath."""
+        return self.node.tag.split("}")[0][1:]
+
+
+def convert_json_to_marc21xml(record):
+    """Convert marc21 json to marc21 xml."""
+    visitor = JsonToXmlVisitor(record["leader"])
+    visitor.visit(record["fields"])
+    return visitor.get_xml_record()
+
+
+class JsonToXmlVisitor:
+    """JsonToXmlVisitor class."""
+
+    def __init__(self, leader_):
+        """Constructor."""
+        self.namespace = "http://www.loc.gov/MARC21/slim"
+        self.record = Element("record", xmlns=self.namespace)
+
+        leader = Element(f"{{{self.namespace}}}leader")
+        leader.text = leader_
+        self.record.append(leader)
+
+    def get_xml_record(self):
+        """Get xml record."""
+        return self.record
+
+    def visit(self, fields):
+        """Default visit method."""
+        for category, items in fields.items():
+            if category == "AVA" or category == "AVE":
+                self.visit_datafield(category, items)
+            elif int(category) < 10:
+                self.visit_controlfield(category, items)
+            else:
+                self.visit_datafield(category, items)
+
+    def visit_controlfield(self, category, value):
+        """Visit controlfield."""
+        controlfield = Element(f"{{{self.namespace}}}controlfield", {"tag": category})
+        controlfield.text = value
+        self.record.append(controlfield)
+
+    def visit_datafield(self, category, items):
+        """Visit datafield."""
+        for item in items:
+            ind1 = item["ind1"].replace("_", " ")
+            ind2 = item["ind2"].replace("_", " ")
+            datafield = Element(
+                f"{{{self.namespace}}}datafield",
+                {
+                    "tag": category,
+                    "ind1": ind1,
+                    "ind2": ind2,
+                },
+            )
+            for subfn, subfv in sorted(
+                item["subfields"].items(),
+                key=lambda x: f"zz{x}" if x[0].isnumeric() else x[0],
+            ):
+                subfield = Element(f"{{{self.namespace}}}subfield", {"code": subfn})
+                subfield.text = " ".join(subfv)
+                datafield.append(subfield)
+
+            self.record.append(datafield)
+
+
+class Marc21Metadata:
+    """MARC21 Record class to facilitate storage of records in MARC21 format."""
+
+    def __init__(self, *, json=None):
+        """Default constructor of the class."""
+        self.set_default()
+
+        if json:
+            self.json = json
+
+    def set_default(self):
+        """Set default marc21 structure."""
+        self._json = {
+            "leader": "00000nam a2200000zca4500",
+            "fields": {},
+        }
+
+    @property
+    def json(self):
+        """Metadata json getter method."""
+        return {"metadata": self._json}
+
+    @json.setter
+    def json(self, _json):
+        """Metadata json setter method."""
+        if not (
+            isinstance(_json, dict)
+            and "leader" in _json
+            and "fields" in _json
+            and isinstance(_json["fields"], dict)
+        ):
+            msg = (
+                "Marc21Metadata should get a dictionary with at least leader and fields"
+            )
+            raise TypeError(msg)
+
+        self._json = _json["metadata"] if "metadata" in _json else _json
+
+    def get_fields(
+        self,
+        category: str,
+        ind1: str = None,
+        ind2: str = None,
+        return_type: str = "xml",
+    ) -> tuple[list[Element], list[Element]]:
+        """Return fields found by category.
+
+        The return value could be found more precisely by defining ind1, ind2
+        and subf_code.
+        """
+        if not category.isnumeric():
+            return ([], [])
+
+        try:
+            if int(category) < 10:
+                controlfields = [self._json["fields"][category]]
+            else:
+                controlfields = []
+        except KeyError:
+            controlfields = []
+
+        def fix_underline(ind):
+            # the problem is that the codebase introduced "_" as a replacement
+            # for " " and this has to be fixed somewhere in the future
+            return [ind, " " if ind == "_" else None, "_" if ind == " " else None]
+
+        def ind_condition(d):
+            ind1_ = d["ind1"] in fix_underline(ind1) if ind1 else True
+            ind2_ = d["ind2"] in fix_underline(ind2) if ind2 else True
+            return ind1_ and ind2_
+
+        try:
+            datafields = [d for d in self._json["fields"][category] if ind_condition(d)]
+        except KeyError:
+            datafields = []
+
+        return (controlfields, datafields)
+
+    def get_value(
+        self,
+        category: str,
+        ind1: str = None,
+        ind2: str = None,
+        subf_code: str = None,
+    ) -> str:
+        """Get the value of the found field."""
+        controlfields, datafields = self.get_fields(category, ind1, ind2)
+
+        if len(controlfields) > 0:
+            return controlfields[0]
+
+        if len(datafields) > 0:
+            try:
+                return " ".join(datafields[0]["subfields"][subf_code])
+            except KeyError:
+                return ""
+
+        return ""
+
+    def get_values(
+        self,
+        category: str,
+        ind1: str = None,
+        ind2: str = None,
+        subf_code: str = None,
+    ) -> list[str]:
+        """Get values of the found field."""
+        controlfields, datafields = self.get_fields(category, ind1, ind2)
+
+        values = []
+
+        for controlfield in controlfields:
+            values.append(controlfield)
+
+        for datafield in datafields:
+            if subf_code is None:
+                for value in datafield["subfields"].values():
+                    values.extend(value)
+
+            with suppress(KeyError):
+                values.extend(datafield["subfields"][subf_code])
+
+        return values
+
+    def exists_field(
+        self,
+        category: str,
+        ind1: str = None,
+        ind2: str = None,
+        subf_code: str = None,
+        subf_value: str = None,
+    ) -> bool:
+        """Check if the field exists."""
+        values = self.get_values(category, ind1, ind2, subf_code)
+        return any((value == subf_value for value in values))
+
+    def emplace_leader(self, value=""):
+        """Change leader string in record."""
+        self._json["leader"] = value
+
+    def emplace_controlfield(self, tag="", value=""):
+        """Add value to record for given datafield and subfield."""
+        if int(tag) > 9:
+            raise RuntimeError("ERROR: controlfields are < 10")
+
+        controlfield = {tag: value}
+        self._json["fields"].update(controlfield)
+
+    def emplace_datafield(self, selector, *, value=None, subfs=None) -> None:
+        """Add value to record for given datafield and subfield.
+
+        :params selector e.g. "100...a", "100"
+        """
+        tag, ind1, ind2, code = selector.split(".")
+
+        if not ind1:
+            ind1 = "_"
+
+        if not ind2:
+            ind2 = "_"
+
+        if not code:
+            code = "a"
+
+        datafield = {
+            tag: [
+                {
+                    "ind1": ind1,
+                    "ind2": ind2,
+                    "subfields": {},
+                }
+            ],
+        }
+
+        if value:
+            datafield[tag][0]["subfields"][code] = [value]
+        elif subfs:
+            for key, val in sorted(
+                subfs.items(),
+                key=lambda x: f"zz{x}" if x[0].isnumeric() else x[0],
+            ):
+                datafield[tag][0]["subfields"][key] = (
+                    val if isinstance(val, list) else [val]
+                )
+        else:
+            raise RuntimeError("Neither of value or subfs is set.")
+
+        if tag not in self._json["fields"]:
+            self._json["fields"].update(datafield)
+        else:
+            # if the tag already exists it has to be found the correct ind1/ind2
+            # combination to update subfields. dict does not deep update as
+            # intended
+            datafields = self._json["fields"][tag]
+            for d in datafields:
+                if d["ind1"] == ind1 and d["ind2"] == ind2:
+                    for code, value in datafield[tag][0]["subfields"].items():
+                        if code in d["subfields"]:
+                            d["subfields"][code].extend(value)
+                        else:
+                            d["subfields"][code] = value
