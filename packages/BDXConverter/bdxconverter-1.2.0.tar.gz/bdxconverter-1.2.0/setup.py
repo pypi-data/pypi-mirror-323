d2cc0bc47cd93b28bcb9e9d4414aff355e93c45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:04:05 +0000

    Add package BDXConverter/1.2.0/bdxconverter-1.2.0.tar.gz

---
+++ packages/BDXConverter/bdxconverter-1.2.0.tar.gz/bdxconverter-1.2.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('requirements.txt', 'r+', encoding='utf-8') as file:
+    dependences = file.read().strip().split('\n')
+
+with open('README.md', 'r+', encoding='utf-8') as file:
+    long_description = file.read()
+
+with open('version', 'r+', encoding='utf-8') as file:
+    version = file.read().strip()
+
+setuptools.setup(
+    name='BDXConverter',
+    version=version,
+    author='Minecraft Muti-Media Organization',
+    author_email='TriM-Organization@hotmail.com',
+    description='A code library to marshal, unmarshal, visual and reverse visualization of BDX files',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/TriM-Organization/BDXConverter',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=dependences,
+    python_requires='>=3.9',
+)
