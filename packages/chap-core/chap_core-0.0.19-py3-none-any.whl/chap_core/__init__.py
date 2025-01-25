38cefa90c3a1633cc05a54c2c8f0687f0e5114e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:04:52 +0000

    Add package chap-core/0.0.19/chap_core-0.0.19-py3-none-any.whl

---
+++ packages/chap-core/chap_core-0.0.19-py3-none-any.whl/chap_core/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for chap-core."""
+
+__author__ = """Sandvelab"""
+__email__ = "knutdrand@gmail.com"
+__version__ = "0.0.19"
+
+from . import fetch
+from . import data
+
+__all__ = ["fetch", "data"]
