31be83f8a31e7fabaaac097d51b4c7f275ee8af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:04:52 +0000

    Add package f3dasm/2.0.0/f3dasm-2.0.0.tar.gz

---
+++ packages/f3dasm/f3dasm-2.0.0.tar.gz/f3dasm-2.0.0/src/f3dasm/_src/optimization/__init__.py
@@ -0,0 +1,37 @@
+"""
+Module for optimization
+"""
+#                                                                       Modules
+# =============================================================================
+
+# Standard
+from typing import Callable, List
+
+from .numpy_implementations import random_search
+from .optimizer_factory import _optimizer_factory, available_optimizers
+from .scipy_implementations import cg, lbfgsb, nelder_mead
+
+# Local
+
+
+#                                                          Authorship & Credits
+# =============================================================================
+__author__ = 'Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)'
+__credits__ = ['Martin van der Schelling']
+__status__ = 'Stable'
+# =============================================================================
+#
+# =============================================================================
+
+
+# =============================================================================
+
+
+__all__ = [
+    '_optimizer_factory',
+    'cg',
+    'lbfgsb',
+    'nelder_mead',
+    'random_search',
+    'available_optimizers',
+]
