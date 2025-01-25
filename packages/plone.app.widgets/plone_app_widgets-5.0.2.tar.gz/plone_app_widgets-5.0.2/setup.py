aa0363865892b3fe70cc81bee847e144238c274a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:04:23 +0000

    Add package plone.app.widgets/5.0.2/plone_app_widgets-5.0.2.tar.gz

---
+++ packages/plone.app.widgets/plone_app_widgets-5.0.2.tar.gz/plone_app_widgets-5.0.2/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+
+version = '5.0.2'
+
+setup(
+    name='plone.app.widgets',
+    version=version,
+    description="better plone widgets",
+    long_description='%s\n%s' % (
+        open("README.rst").read(),
+        open("CHANGES.rst").read(),
+    ),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords='plone widgets z3cform',
+    author='Nathan Van Gheem',
+    author_email='vangheem@gmail.com',
+    url='https://github.com/plone/plone.app.widgets',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.8',
+    install_requires=[
+        'setuptools',
+        # needed because users vocabulary was added here
+        'plone.app.vocabularies>=2.1.12',
+        'plone.base',
+        'six',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.robotframework[debug]',
+            'plone.app.testing>=4.2.4',  # we need ROBOT_TEST_LEVEL
+            'mock',
+        ],
+        'dexterity': [
+            'pytz',
+            'plone.app.dexterity',
+            'plone.app.contenttypes>=1.1',
+            'plone.app.event>=1.2',
+        ],
+    },
+)
