02d338169d5f827e7a0404f7df59cdc8b08452ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:03:48 +0000

    Add package sybil-engine/9.5.1/sybil_engine-9.5.1.tar.gz

---
+++ packages/sybil-engine/sybil_engine-9.5.1.tar.gz/sybil_engine-9.5.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r", encoding="utf-8") as file:
+    requirements = file.readlines()
+
+setup(
+    name='sybil_engine',
+    version='9.5.1',
+    py_modules=['sybil_engine'],
+    packages=find_packages(),
+    install_requires=requirements,
+    data_files=[('', ['requirements.txt'])],
+    author='Indeoo',
+    author_email='indeooars@gmail.com',
+    description='Engine for web3 smart contracts automatization.',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Indeoo/sybil-engine/',
+)
