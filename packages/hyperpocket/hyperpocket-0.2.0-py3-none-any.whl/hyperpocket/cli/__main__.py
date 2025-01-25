33661c542323b75b7908e082fcd7cc30bdb20714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:04:05 +0000

    Add package hyperpocket/0.2.0/hyperpocket-0.2.0-py3-none-any.whl

---
+++ packages/hyperpocket/hyperpocket-0.2.0-py3-none-any.whl/hyperpocket/cli/__main__.py
@@ -0,0 +1,26 @@
+import click
+
+from hyperpocket.cli.auth import create_token_auth_template
+from hyperpocket.cli.eject import eject
+from hyperpocket.cli.pull import pull
+from hyperpocket.cli.sync import sync
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.group()
+def devtool():
+    pass
+
+
+cli.add_command(devtool)
+devtool.add_command(create_token_auth_template)
+
+cli.add_command(pull)
+cli.add_command(sync)
+cli.add_command(eject)
+
+cli()
