13e36d3604a5705cda9e246b6581eeb763b386e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:04:29 +0000

    Add package pyarallel/0.1.0/pyarallel-0.1.0-py3-none-any.whl

---
+++ packages/pyarallel/pyarallel-0.1.0-py3-none-any.whl/pyarallel/__init__.py
@@ -0,0 +1,10 @@
+from .core import parallel, RateLimit
+
+__version__ = "0.1.0"
+__all__ = ["parallel", "RateLimit"]
+
+__doc__ = f"""
+pyarallel v{__version__}
+
+Simple parallel processing framework for Python.
+"""
