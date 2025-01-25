18fbb4ecec8967286b1697d639b17a5fa425ee7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:06:07 +0000

    Add package keywordsai-sdk/0.3.36/keywordsai_sdk-0.3.36.tar.gz

---
+++ packages/keywordsai-sdk/keywordsai_sdk-0.3.36.tar.gz/keywordsai_sdk-0.3.36/src/keywordsai_sdk/keywordsai_types/span_types.py
@@ -0,0 +1,30 @@
+from keywordsai_sdk.keywordsai_types._internal_types import KeywordsAIParams
+from enum import Enum
+
+class KeywordsAISpanAttributes(Enum):
+    # Span attributes
+    KEYWORDSAI_SPAN_CUSTOM_ID = "keywordsai.span_params.custom_identifier"
+
+    # Customer params
+    KEYWORDSAI_CUSTOMER_PARAMS_ID = "keywordsai.customer_params.customer_identifier"
+    KEYWORDSAI_CUSTOMER_PARAMS_EMAIL = "keywordsai.customer_params.email"
+    KEYWORDSAI_CUSTOMER_PARAMS_NAME = "keywordsai.customer_params.name"
+    
+    # Evaluation params
+    KEYWORDSAI_EVALUATION_PARAMS_ID = "keywordsai.evaluation_params.evaluation_identifier"
+
+    # Threads
+    KEYWORDSAI_THREADS_ID = "keywordsai.threads.thread_identifier"
+
+    # Metadata
+    KEYWORDSAI_METADATA = "keywordsai.metadata" # This is a pattern, it can be  any "keywordsai.metadata.key" where key is customizable
+
+KEYWORDSAI_SPAN_ATTRIBUTES_MAP = {
+    "customer_identifier": KeywordsAISpanAttributes.KEYWORDSAI_CUSTOMER_PARAMS_ID,
+    "customer_email": KeywordsAISpanAttributes.KEYWORDSAI_CUSTOMER_PARAMS_EMAIL,
+    "customer_name": KeywordsAISpanAttributes.KEYWORDSAI_CUSTOMER_PARAMS_NAME,
+    "evaluation_identifier": KeywordsAISpanAttributes.KEYWORDSAI_EVALUATION_PARAMS_ID,
+    "thread_identifier": KeywordsAISpanAttributes.KEYWORDSAI_THREADS_ID,
+    "custom_identifier": KeywordsAISpanAttributes.KEYWORDSAI_SPAN_CUSTOM_ID,
+    "metadata": KeywordsAISpanAttributes.KEYWORDSAI_METADATA,
+}
