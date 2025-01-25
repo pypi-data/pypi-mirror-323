7199e8c95b4f3e4d30bba55a56216a3bfcb60615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 16:04:23 +0000

    Add package soundfile/0.13.1/soundfile-0.13.1.tar.gz

---
+++ packages/soundfile/soundfile-0.13.1.tar.gz/soundfile-0.13.1/doc/fake__soundfile.py
@@ -0,0 +1,24 @@
+"""Mock module for Sphinx autodoc."""
+
+import ctypes
+import ctypes.util
+
+# Monkey-patch ctypes to disable searching for libsndfile
+ctypes.util.find_library = lambda _: NotImplemented
+
+
+class ffi(object):
+
+    def dlopen(self, _):
+        return self
+
+    def string(self, _):
+        return b'not implemented'
+
+    def sf_version_string(self):
+        return NotImplemented
+
+    SFC_GET_FORMAT_INFO = NotImplemented
+
+
+ffi = ffi()
---
+++ packages/soundfile/soundfile-0.13.1.tar.gz/soundfile-0.13.1/doc/index.rst
@@ -0,0 +1,17 @@
+.. currentmodule:: soundfile
+
+.. include:: ../README.rst
+
+.. include:: ../CONTRIBUTING.rst
+
+API Documentation
+=================
+
+.. automodule:: soundfile
+   :members:
+   :undoc-members:
+
+Index
+=====
+
+* :ref:`genindex`
