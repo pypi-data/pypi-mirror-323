072e76ba30acf6e7a92e90b3da5e87fdf6f512a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:04:59 +0000

    Add package pytrilogy/0.0.3.3/pytrilogy-0.0.3.3-py3-none-any.whl

---
+++ packages/pytrilogy/pytrilogy-0.0.3.3-py3-none-any.whl/trilogy/engine.py
@@ -0,0 +1,49 @@
+from typing import Protocol
+
+from sqlalchemy.engine import Connection, CursorResult, Engine
+
+
+class EngineResult(Protocol):
+    pass
+
+    def fetchall(self) -> list[tuple]:
+        pass
+
+
+class EngineConnection(Protocol):
+    pass
+
+    def execute(self, statement: str) -> EngineResult:
+        pass
+
+
+class ExecutionEngine(Protocol):
+    pass
+
+    def connect(self) -> EngineConnection:
+        pass
+
+
+### Begin default SQLAlchemy implementation
+class SqlAlchemyResult(EngineResult):
+    def __init__(self, result: CursorResult):
+        self.result = result
+
+    def fetchall(self):
+        return self.result.fetchall()
+
+
+class SqlAlchemyConnection(EngineConnection):
+    def __init__(self, connection: Connection):
+        self.connection = connection
+
+    def execute(self, statement: str) -> SqlAlchemyResult:
+        return SqlAlchemyResult(self.connection.execute(statement))
+
+
+class SqlAlchemyEngine(ExecutionEngine):
+    def __init__(self, engine: Engine):
+        self.engine = engine
+
+    def connect(self) -> SqlAlchemyConnection:
+        return SqlAlchemyConnection(self.engine.connect())
