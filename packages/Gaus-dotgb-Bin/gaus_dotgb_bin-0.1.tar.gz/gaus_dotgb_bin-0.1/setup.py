c32dbcab97aaa1f669ac135a8f8697ec5cf70e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:06:09 +0000

    Add package Gaus-dotgb-Bin/0.1/gaus_dotgb_bin-0.1.tar.gz

---
+++ packages/Gaus-dotgb-Bin/gaus_dotgb_bin-0.1.tar.gz/gaus_dotgb_bin-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Gaus_dotgb_Bin',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Gaus_dotgb_Bin'],
+      author = 'dotgb',
+      author_email = 'myemail@something.com',
+      zip_safe=False)
+
