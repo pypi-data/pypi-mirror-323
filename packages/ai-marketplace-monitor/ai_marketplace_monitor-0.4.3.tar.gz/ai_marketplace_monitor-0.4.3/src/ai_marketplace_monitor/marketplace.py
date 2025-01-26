6615454ac982c5d08d3eb8b9b1f876e154bb5788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:12 +0000

    Add package ai-marketplace-monitor/0.4.3/ai_marketplace_monitor-0.4.3.tar.gz

---
+++ packages/ai-marketplace-monitor/ai_marketplace_monitor-0.4.3.tar.gz/ai_marketplace_monitor-0.4.3/src/ai_marketplace_monitor/marketplace.py
@@ -0,0 +1,29 @@
+from logging import Logger
+from typing import Any, ClassVar, Dict, List, Type
+
+from playwright.sync_api import Browser, Page
+
+from .items import SearchedItem
+
+
+class Marketplace:
+    allowed_config_keys: ClassVar = {}
+
+    def __init__(self: "Marketplace", name: str, browser: Browser, logger: Logger) -> None:
+        self.name = name
+        self.browser = browser
+        self.logger = logger
+        self.page: Page | None = None
+
+    def configure(self: "Marketplace", config: Dict[str, Any]) -> None:
+        self.config = config
+
+    @classmethod
+    def validate(cls: Type["Marketplace"], config: Dict[str, Any]) -> None:
+        # if there are other keys in config, raise an error
+        for key in config:
+            if key not in cls.allowed_config_keys:
+                raise ValueError(f"Marketplace contains an invalid key {key}.")
+
+    def search(self: "Marketplace", item: Dict[str, Any]) -> List[SearchedItem]:
+        raise NotImplementedError("Search method must be implemented by subclasses.")
