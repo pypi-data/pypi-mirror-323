26afadb408764d2a31c995351f481d884fb41e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 03:06:42 +0000

    Add package spanns/1.1.1/spanns-1.1.1.tar.gz

---
+++ packages/spanns/spanns-1.1.1.tar.gz/spanns-1.1.1/src/spanns/__init__.py
@@ -0,0 +1,11 @@
+from .spanns import spanns, spanns_core, spanns_routine
+
+import importlib
+__version__ = "1.1.1"
+
+__all__ = [
+    "__version__",
+    "spanns",
+    "spanns_routine",
+    "spanns_core",
+]
