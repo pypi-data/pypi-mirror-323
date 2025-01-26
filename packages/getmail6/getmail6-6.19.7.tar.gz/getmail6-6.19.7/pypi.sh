0bdd69273b6d715d409f41f5db64f98f9e8c69db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 15:04:22 +0000

    Add package getmail6/6.19.7/getmail6-6.19.7.tar.gz

---
+++ packages/getmail6/getmail6-6.19.7.tar.gz/getmail6-6.19.7/pypi.sh
@@ -0,0 +1,13 @@
+# docs/COPYING 2a + DRY: https://github.com/getmail6/getmail6
+# Please refer to the git history regarding who changed what and when in this file.
+
+if git status | grep nothing; then
+    make dist
+    make up6
+    sed -i -e "s/name.*=.*getmail6./name = 'getmail'/g" setup.py
+    make dist
+    make up
+    git reset --hard
+else
+    echo Commit changes first!
+fi
