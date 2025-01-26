4ee5f5b3b7ec0bb76e28d415d344e0bea6c17cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:35 +0000

    Add package embedding-suite/0.1.5/embedding_suite-0.1.5.tar.gz

---
+++ packages/embedding-suite/embedding_suite-0.1.5.tar.gz/embedding_suite-0.1.5/embedding_suite/providers/voyageai_provider.py
@@ -0,0 +1,20 @@
+import voyageai
+from embedding_suite.provider import Provider
+from embedding_suite.utils.config import get_api_key
+
+# https://docs.voyageai.com/docs/embeddings
+
+
+class VoyageaiProvider(Provider):
+    def __init__(self, **config):
+        self.api_key = config.get("api_key") or get_api_key("voyageai")
+        if not self.api_key:
+            raise ValueError("VoyageAI API key is missing.")
+
+    def generate_embeddings(self, model, inputs, **kwargs):
+
+        vo = voyageai.Client(api_key=self.api_key)
+
+        result = vo.embed(inputs, model=model, input_type="document")
+
+        return result.embeddings
