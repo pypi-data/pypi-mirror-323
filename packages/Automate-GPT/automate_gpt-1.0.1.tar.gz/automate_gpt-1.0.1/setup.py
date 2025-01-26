e259042f8e73fc2cc910a8e6c12dacbdf22b03af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 03:07:14 +0000

    Add package Automate-GPT/1.0.1/automate_gpt-1.0.1.tar.gz

---
+++ packages/Automate-GPT/automate_gpt-1.0.1.tar.gz/automate_gpt-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Automate_GPT',
+    packages=find_packages(),
+    version='1.0.1',
+    description='Chat GPT Automation',
+    author='Jenil sheth',
+    author_email="shethjeniljigneshbhai@gmail.com",
+    install_requires=["undetected_chromedriver","selenium","pyperclip"],
+    long_description=open('chatgptautomation.md').read(),
+    long_description_content_type="text/markdown",
+)
