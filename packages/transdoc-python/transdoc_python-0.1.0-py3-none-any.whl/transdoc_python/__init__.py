9c66751df9968064b80b2b96057938537b1bacf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 03:07:08 +0000

    Add package transdoc-python/0.1.0/transdoc_python-0.1.0-py3-none-any.whl

---
+++ packages/transdoc-python/transdoc_python-0.1.0-py3-none-any.whl/transdoc_python/__init__.py
@@ -0,0 +1,51 @@
+"""
+# Transdoc Python
+
+A Transdoc handler for Python docstrings, using libcst to rewrite
+documentation.
+"""
+
+from importlib.metadata import version
+from pathlib import Path
+from typing import IO
+import libcst as cst
+from libcst import MetadataWrapper
+from transdoc import TransdocHandler, TransdocTransformer
+
+from transdoc_python.__visitor import DocstringVisitor
+
+
+__version__ = version("transdoc-python")
+
+
+class TransdocPythonHandler:
+    """
+    A Transdoc handler for Python docstrings.
+    """
+
+    def matches_file(self, file_path: str) -> bool:
+        return Path(file_path).suffix in [".py", ".pyi"]
+
+    def transform_file(
+        self,
+        transformer: TransdocTransformer,
+        in_path: str,
+        in_file: IO,
+        out_file: IO | None,
+    ) -> None:
+        parsed = MetadataWrapper(cst.parse_module(in_file.read()))
+        visitor = DocstringVisitor(transformer, in_path)
+        updated_cst = parsed.visit(visitor)
+        visitor.raise_errors()
+        if out_file is not None:
+            out_file.write(updated_cst.code)
+
+
+__all__ = [
+    "__version__",
+    "TransdocPythonHandler",
+]
+
+
+if __name__ == "__main__":
+    handler: TransdocHandler = TransdocPythonHandler()
