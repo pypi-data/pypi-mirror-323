5f0a6ad14e82fad9a916f6a47a441de59bf1ff76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 15:04:29 +0000

    Add package Topsis-Kunal-102216002/1.0.0/Topsis_Kunal_102216002-1.0.0.tar.gz

---
+++ packages/Topsis-Kunal-102216002/Topsis_Kunal_102216002-1.0.0.tar.gz/Topsis_Kunal_102216002-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Topsis_Kunal_102216002',  # Replace with your name and roll number
+    version='1.0.0',
+    author='Kunal',
+    author_email='kbhalla600@gmail.com',
+    description='A Python package to perform TOPSIS analysis',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Kunal-code-u/Topsis-Kunal-102216002',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy'
+    ],
+    entry_points={
+        'console_scripts': [
+            'topsis=Topsis_Kunal_102216002.topsis:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
