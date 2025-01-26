3425f093a909fc33508c184cdd38239454f4ac18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:14 +0000

    Add package discrete-dists/1.0.1/discrete_dists-1.0.1-cp311-cp311-win32.whl

---
+++ packages/discrete-dists/discrete_dists-1.0.1-cp311-cp311-win32.whl/discrete_dists/uniform.py
@@ -0,0 +1,94 @@
+from typing import overload
+import numpy as np
+import numpy.typing as npt
+import discrete_dists.utils.npu as npu
+from discrete_dists.distribution import Distribution, Support
+
+
+class Uniform(Distribution):
+    """
+    A uniform distribution defined over some support.
+    If only an integer support is given, assume support
+    is [0, support).
+    """
+    def __init__(self, support: Support | int):
+        if isinstance(support, int):
+            support = (0, support)
+
+        self._support: Support = support
+
+
+    # ------------------------
+    # -- Changes to support --
+    # ------------------------
+    @overload
+    def update(self, elements: np.ndarray) -> None: ...
+    @overload
+    def update(self, elements: np.ndarray, values: np.ndarray) -> None: ...
+    def update(self, elements: np.ndarray, values: np.ndarray | None = None):
+        """
+        Update the support of the distribution to cover the
+        given elements if these are outside the current support.
+        """
+        self._support = (
+            min(self._support[0], elements.min()),
+            max(self._support[1], elements.max())
+        )
+
+
+    @overload
+    def update_single(self, element: int) -> None: ...
+    @overload
+    def update_single(self, element: int, value: float) -> None: ...
+    def update_single(self, element: int, value: float = 0):
+        """
+        Update the support of the distribution to cover the
+        given element if it is outside the current support.
+        """
+        self._support = (
+            min(self._support[0], element),
+            max(self._support[1], element),
+        )
+
+
+    def update_support(self, support: Support | int):
+        """
+        Update the support of the distribution to the new
+        given support.
+        """
+        if isinstance(support, int):
+            self._support = (0, support)
+        else:
+            self._support = support
+
+
+    # --------------
+    # -- Samplers --
+    # --------------
+    def sample(self, rng: np.random.Generator, n: int):
+        """
+        Sample `n` values from the support of the distribution
+        with replacement.
+        """
+        size = self._support[1] - self._support[0]
+        if size < 2:
+            return self._support[0] + np.zeros(n, dtype=np.int64)
+
+        return rng.integers(*self._support, size=n)
+
+
+    def stratified_sample(self, rng: np.random.Generator, n: int):
+        """
+        Sample `n` evenly spaced values from the support
+        of the distribution with replacement.
+        """
+        return npu.stratified_sample_integers(rng, n, *self._support)
+
+
+    def probs(self, elements: npt.ArrayLike):
+        """
+        Get the probabilities of the given elements
+        under the current distribution.
+        """
+        d = self._support[1] - self._support[0]
+        return np.full_like(elements, fill_value=(1 / d), dtype=np.float64)
