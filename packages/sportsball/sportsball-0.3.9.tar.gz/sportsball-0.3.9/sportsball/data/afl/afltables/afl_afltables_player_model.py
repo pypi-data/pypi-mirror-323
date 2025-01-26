bc6be5ce3dcee5217eb61d408be21fbd996d00d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:38 +0000

    Add package sportsball/0.3.9/sportsball-0.3.9.tar.gz

---
+++ packages/sportsball/sportsball-0.3.9.tar.gz/sportsball-0.3.9/sportsball/data/afl/afltables/afl_afltables_player_model.py
@@ -0,0 +1,25 @@
+"""AFL AFLTables player model."""
+
+import os
+from urllib.parse import urlparse
+
+from ....cache import MEMORY
+from ...player_model import PlayerModel
+
+
+@MEMORY.cache
+def create_afl_afltables_player_model(
+    player_url: str, jersey: str, kicks: int | None
+) -> PlayerModel:
+    """Create a player model from AFL Tables."""
+    o = urlparse(player_url)
+    last_component = o.path.split("/")[-1]
+    identifier, _ = os.path.splitext(last_component)
+    jersey = "".join(filter(str.isdigit, jersey))
+    return PlayerModel(
+        identifier=identifier,
+        jersey=jersey,
+        kicks=kicks,
+        fumbles=None,
+        fumbles_lost=None,
+    )
