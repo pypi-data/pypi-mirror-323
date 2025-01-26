a1f25cf93f54349701284751541a25ad13e8bf17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:04:55 +0000

    Add package PyPetka-Script/2.4.2/pypetka_script-2.4.2.tar.gz

---
+++ packages/PyPetka-Script/pypetka_script-2.4.2.tar.gz/pypetka_script-2.4.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r', encoding='utf-8') as f:
+    return f.read()
+
+setup(
+  name='PyPetka-Script',
+  version='2.4.2',
+  author='Chorelin',
+  author_email='miheevila6@gmail.com',
+  description='Модуль для упрощения работы с Python, созданный командой PyPetka Team.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/IlyaChaek/PyPetka-Script',
+  packages=find_packages(),
+  install_requires=[],
+  classifiers=[
+    'Programming Language :: Python :: 3.13',
+    'License :: Freeware',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+  ],
+  keywords='Python language module PyPetka NewLanguage programming language development',
+  project_urls={
+    'Documentation': 'https://github.com/IlyaChaek/PyPetka-Script',
+  },
+  python_requires='>=3.6'
+)
