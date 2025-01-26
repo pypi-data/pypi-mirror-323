578ef32f396af64a19043ab4bf26a22e9eb25a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:37 +0000

    Add package android-notify/1.50.1/android_notify-1.50.1-py3-none-any.whl

---
+++ packages/android-notify/android_notify-1.50.1-py3-none-any.whl/android_notify/styles.py
@@ -0,0 +1,15 @@
+class NotificationStyles():
+    """ Safely Adding Styles"""
+    DEFAULT = "simple"
+    
+    PROGRESS = "progress"
+    INBOX = "inbox"
+    BIG_TEXT = "big_text"
+    
+    LARGE_ICON = "large_icon"
+    BIG_PICTURE = "big_picture"
+    BOTH_IMGS = "both_imgs"
+    
+    MESSAGING = "messaging" # TODO
+    CUSTOM = "custom" # TODO
+    
