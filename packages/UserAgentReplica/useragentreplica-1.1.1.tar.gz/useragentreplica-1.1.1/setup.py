0430ec07a94742d8355143d909dce1283203d37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:28 +0000

    Add package UserAgentReplica/1.1.1/useragentreplica-1.1.1.tar.gz

---
+++ packages/UserAgentReplica/useragentreplica-1.1.1.tar.gz/useragentreplica-1.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="UserAgentReplica",
+    version="1.1.1",
+    packages=find_packages(),
+    author="Pʀɪᴍᴇ Hʀɪᴛᴜ",
+    description="Generates safe and dummy usable user agent and xms user agent",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/Prime-Hritu/UserAgentReplica',
+    project_urls={ 'GitHub': 'https://github.com/Prime-Hritu/UserAgentReplica'}
+)
