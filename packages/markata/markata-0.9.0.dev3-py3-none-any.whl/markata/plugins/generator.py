cb6dcea98c7033fbe98bc93f9f018854ce65ec5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:37 +0000

    Add package markata/0.9.0.dev3/markata-0.9.0.dev3-py3-none-any.whl

---
+++ packages/markata/markata-0.9.0.dev3-py3-none-any.whl/markata/plugins/generator.py
@@ -0,0 +1,22 @@
+"""add generator meta tag"""
+
+from bs4 import BeautifulSoup
+
+from markata import Markata, __version__
+from markata.hookspec import hook_impl
+
+
+@hook_impl(trylast=True)
+def render(markata: Markata) -> None:
+    should_prettify = markata.config.prettify_html
+    for article in markata.iter_articles("add ssg tag"):
+        soup = BeautifulSoup(article.html, features="lxml")
+        tag = soup.new_tag("meta")
+        tag.attrs["content"] = f"markata {__version__}"
+        tag.attrs["name"] = "generator"
+        soup.head.append(tag)
+
+        if should_prettify:
+            article.html = soup.prettify()
+        else:
+            article.html = str(soup)
