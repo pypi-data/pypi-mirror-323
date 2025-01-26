17ebfd8916dd16bffb3cfa8408d5ba6484b101ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:35 +0000

    Add package openfund-trailing/1.0.6/openfund_trailing-1.0.6.tar.gz

---
+++ packages/openfund-trailing/openfund_trailing-1.0.6.tar.gz/openfund_trailing-1.0.6/src/trailing/main.py
@@ -0,0 +1,23 @@
+import os
+import json
+from trailing.chua_ok_all import MultiAssetTradingBot
+
+def main():
+    # os.environ["ENV"] = env
+    # os.environ["DEBUG"] = str(debug)
+    import os
+    openfund_config_path = os.getenv('openfund_config_path','config.json')
+    
+    with open(openfund_config_path, 'r') as f:
+        config_data = json.load(f)
+        
+    platform_config = config_data['okx']
+    feishu_webhook_url = config_data['feishu_webhook']
+    monitor_interval = config_data.get("monitor_interval", 4)  # 默认值为4秒
+
+    bot = MultiAssetTradingBot(platform_config, feishu_webhook=feishu_webhook_url, monitor_interval=monitor_interval)
+    bot.monitor_total_profit()
+
+if __name__ == "__main__":
+    main()
+    # main()
