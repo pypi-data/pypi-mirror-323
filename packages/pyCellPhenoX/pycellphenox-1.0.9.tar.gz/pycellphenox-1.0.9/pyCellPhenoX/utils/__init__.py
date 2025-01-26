197587c9f0a36157fc04bcefad51bb19df40a7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:04:42 +0000

    Add package pyCellPhenoX/1.0.9/pycellphenox-1.0.9.tar.gz

---
+++ packages/pyCellPhenoX/pycellphenox-1.0.9.tar.gz/pycellphenox-1.0.9/pyCellPhenoX/utils/__init__.py
@@ -0,0 +1,6 @@
+__project__ = "pyCellPhenoX"
+# These version placeholders are updated during build by poetry-dynamic-versioning
+__version__ = "1.0.9"
+__version_tuple__ = (1, 0, 9)
+__license__ = "MIT License"
+__author__ = "pyCellPhenoX Contributors"
