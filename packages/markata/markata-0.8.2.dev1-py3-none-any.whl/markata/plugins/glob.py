f8bc4060c190139b974225f23ca0e1cca418c75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 03:07:09 +0000

    Add package markata/0.8.2.dev1/markata-0.8.2.dev1-py3-none-any.whl

---
+++ packages/markata/markata-0.8.2.dev1-py3-none-any.whl/markata/plugins/glob.py
@@ -0,0 +1,87 @@
+"""Default glob plugin"""
+
+from pathlib import Path
+from typing import List, TYPE_CHECKING, Union
+from markata import background
+
+from more_itertools import flatten
+import pydantic
+
+from markata.hookspec import hook_impl, register_attr
+
+if TYPE_CHECKING:
+    from markata import Markata
+
+
+class GlobConfig(pydantic.BaseModel):
+    glob_patterns: Union[List[str], str] = ["**/*.md"]
+    use_gitignore: bool = True
+
+    @pydantic.validator("glob_patterns")
+    def convert_to_list(cls, v):
+        if not isinstance(v, list):
+            return v.split(",")
+        return v
+
+
+class Config(pydantic.BaseModel):
+    glob: GlobConfig = GlobConfig()
+
+
+@hook_impl
+@register_attr("post_models")
+def config_model(markata: "Markata") -> None:
+    markata.config_models.append(Config)
+
+
+@hook_impl
+@register_attr("content_directories", "files")
+def glob(markata: "Markata") -> None:
+    markata.files = list(
+        flatten(
+            [
+                Path().glob(str(pattern))
+                for pattern in markata.config.glob.glob_patterns
+            ],
+        ),
+    )
+    markata.content_directories = list({f.parent for f in markata.files})
+
+    try:
+        ignore = True
+    except KeyError:
+        ignore = True
+
+    if ignore and (Path(".gitignore").exists() or Path(".markataignore").exists()):
+        import pathspec
+
+        lines = []
+
+        if Path(".gitignore").exists():
+            lines.extend(Path(".gitignore").read_text().splitlines())
+
+        if Path(".markataignore").exists():
+            lines.extend(Path(".markataignore").read_text().splitlines())
+
+        key = markata.make_hash("glob", "spec", lines)
+        spec = markata.precache.get(key)
+        if spec is None:
+            spec = pathspec.PathSpec.from_lines("gitwildmatch", lines)
+            with markata.cache as cache:
+                cache.set(key, spec)
+
+        @background.task
+        def check_spec(file: str) -> bool:
+            key = markata.make_hash("glob", "check_spec", file)
+            check = markata.precache.get(key)
+            if check is not None:
+                return check
+
+            check = spec.match_file(str(file))
+            with markata.cache as cache:
+                cache.set(key, check)
+            return check
+
+        file_checks = [(file, check_spec(str(file))) for file in markata.files]
+        [check.result() for _, check in file_checks]
+        markata.files = [file for file, check in file_checks if check]
---
+++ packages/markata/markata-0.8.2.dev1-py3-none-any.whl/markata/plugins/publish_source.py
@@ -0,0 +1,94 @@
+"""
+Writes the final modified markdown and frontmatter to the output directory.
+Replacing the trailing slash if its there and adding .md will bring up the raw
+source.
+
+## Configuration
+
+The only configuration for the publish_source plugin is to make sure its in
+your list of hooks.
+
+
+``` toml
+[markata]
+
+# make sure its in your list of hooks
+hooks=[
+   "markata.plugins.publish_source",
+   ]
+```
+
+!!! note
+    publish_source is included by default, but if you have not included the
+    default set of hooks you will need to explicitly add it.
+"""
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import frontmatter
+import yaml
+from yaml.representer import RepresenterError
+
+from markata.hookspec import hook_impl
+
+if TYPE_CHECKING:
+    from markata import Markata
+
+
+def _save(output_dir: Path, article: frontmatter.Post) -> None:
+    """
+    saves the article to the output directory at its specified slug.
+    """
+    path = Path(
+        output_dir / Path(article["slug"]).parent / Path(article["path"]).name,
+    )
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text(article.dumps())
+
+
+def _strip_unserializable_values(
+    markata: "Markata",
+    article: frontmatter.Post,
+) -> frontmatter.Post:
+    """
+    Returns an article with only yaml serializable frontmatter.
+    """
+    _article = frontmatter.Post(
+        article.content,
+        **{k: v for k, v in article.metadata.items() if k != "content"},
+    )
+    kwargs = {
+        "Dumper": yaml.cyaml.CSafeDumper,
+        "default_flow_style": False,
+        "allow_unicode": True,
+    }
+    for key, value in article.metadata.items():
+        try:
+            yaml.dump({key: value}, **kwargs)
+        except RepresenterError:
+            del _article[key]
+    if markata.Post:
+        _article = markata.Post(**_article.metadata, path=str(article.path))
+    return _article
+
+
+@hook_impl
+def save(markata: "Markata") -> None:
+    """
+    Saves the final modified post to the output site as markdown.
+
+    !!! note
+        Any keys that are not yaml serializable will be stripped.
+
+    """
+    output_dir = Path(str(markata.config["output_dir"]))
+    for (
+        article
+    ) in markata.articles:  # iter_articles(description="saving source documents"):
+        try:
+            _save(output_dir, article)
+        except RepresenterError:
+            _article = _strip_unserializable_values(markata, article)
+
+            _save(output_dir, _article)
