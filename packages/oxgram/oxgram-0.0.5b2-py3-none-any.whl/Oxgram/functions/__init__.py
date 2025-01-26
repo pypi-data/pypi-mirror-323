828f7487ed53771dc4864c8a877cdcee41c717be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:06:23 +0000

    Add package oxgram/0.0.5b2/oxgram-0.0.5b2-py3-none-any.whl

---
+++ packages/oxgram/oxgram-0.0.5b2-py3-none-any.whl/Oxgram/functions/__init__.py
@@ -0,0 +1,11 @@
+from .exceptions import LimitedMessage
+from .exceptions import SecuredMessage
+from .exceptions import Accessblocked
+from .exceptions import CancelledTask
+from .exceptions import InvalidReply
+from .exceptions import Cancelled
+from .function01 import Pyrogram
+from .function05 import HMessage
+from .function02 import Flinks
+
+
