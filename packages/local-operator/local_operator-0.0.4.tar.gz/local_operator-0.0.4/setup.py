988327f1f826285a2d72e1953ea5c524fbe50619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:28 +0000

    Add package local-operator/0.0.4/local_operator-0.0.4.tar.gz

---
+++ packages/local-operator/local_operator-0.0.4.tar.gz/local_operator-0.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="local-operator",
+    packages=find_packages(),
+    py_modules=["local_operator.cli"],
+    entry_points={
+        "console_scripts": [
+            "local-operator = local_operator.cli",
+        ],
+    },
+    install_requires=[
+        "langchain-openai>=0.3.2",
+        "python-dotenv>=1.0.1",
+        "pydantic>=2.10.6",
+    ],
+    python_requires=">=3.12",
+    extras_require={
+        "dev": [
+            "black",
+            "isort",
+            "pylint",
+            "pyright",
+        ],
+    },
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
