40a3bebe46317ae8d192a36631b665b0095dccc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:05:31 +0000

    Add package netbox-ping/0.10/netbox_ping-0.10.tar.gz

---
+++ packages/netbox-ping/netbox_ping-0.10.tar.gz/netbox_ping-0.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='netbox-ping',
+    version='0.10',
+    description='A NetBox plugin for pinging and discovering IPs',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dendanskemine/netbox-ping',
+    author='Christian Rose',
+    license='Apache 2.0',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
