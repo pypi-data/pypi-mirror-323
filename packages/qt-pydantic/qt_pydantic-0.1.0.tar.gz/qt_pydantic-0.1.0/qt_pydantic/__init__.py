54b2fd4daf75cfabce2f37cffe76845a0cf65edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:15 +0000

    Add package qt-pydantic/0.1.0/qt_pydantic-0.1.0.tar.gz

---
+++ packages/qt-pydantic/qt_pydantic-0.1.0.tar.gz/qt_pydantic-0.1.0/qt_pydantic/__init__.py
@@ -0,0 +1,15 @@
+from .annotations import (
+    QSize,
+    QSizeF,
+    QPoint,
+    QPointF,
+    QRect,
+    QRectF,
+    QDate,
+    QDateTime,
+    QTime,
+    QUuid,
+    QColor,
+)
+
+__version__ = '0.1.0'
