0c07dd98d9ceac3762127accc4a0e6094b0eda1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:04:44 +0000

    Add package nexios/1.0.3/nexios-1.0.3-py3-none-any.whl

---
+++ packages/nexios/nexios-1.0.3-py3-none-any.whl/nexios/middlewares/base.py
@@ -0,0 +1,23 @@
+from nexios.http.response import NexioResponse
+class BaseMiddleware:
+    def __init__(self,*kwargs) -> None:
+        pass
+    async def __call__(self, request, response, next_middleware):
+        """Call the middleware process and pass control to the next middleware in the stack."""
+        result = await self.process_request(request, response)
+        if result:
+            
+            return result
+        await next_middleware()
+        
+        await self.process_response(request, response)
+        
+
+
+    async def process_request(self, request, response):
+        """Override this method in child classes to process the request before passing it along."""
+        pass
+
+    async def process_response(self, request, response):
+        """Override this method in child classes to process the response before returning it."""
+        pass
