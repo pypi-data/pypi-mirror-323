b996d425fb7acc87651cb871224dd01424951a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 15:04:27 +0000

    Add package prefect/3.1.15.dev3/prefect-3.1.15.dev3-py3-none-any.whl

---
+++ packages/prefect/prefect-3.1.15.dev3-py3-none-any.whl/prefect/server/ui/assets/useCan-BUbF5Oic.js
@@ -0,0 +1,2 @@
+import{cD as n}from"./index-BOc4S1oN.js";const c=Symbol("canInjectionKey");function a(){return n(c)}export{c,a as u};
+//# sourceMappingURL=useCan-BUbF5Oic.js.map
---
+++ packages/prefect/prefect-3.1.15.dev3-py3-none-any.whl/prefect/server/ui/assets/usePrefectApi-CG8j_h9K.js
@@ -0,0 +1,2 @@
+import{cD as r}from"./index-BOc4S1oN.js";import{p as e}from"./api-DgSSMEoO.js";function o(){return r(e)}export{o as u};
+//# sourceMappingURL=usePrefectApi-CG8j_h9K.js.map
