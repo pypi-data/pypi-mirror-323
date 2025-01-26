ff1fec7ecd29de4f60382f05b6bb314a5dfb6db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 03:07:08 +0000

    Add package directdb/1.2.3/directdb-1.2.3.tar.gz

---
+++ packages/directdb/directdb-1.2.3.tar.gz/directdb-1.2.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+import codecs
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.2.3'
+DESCRIPTION = 'An async package that makes database handling extremely easy!'
+
+# Setting up
+setup(
+    name="directdb",
+    version=VERSION,
+    author="Cannonball Chris",
+    author_email="cannonballchris8@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['asyncpg', 'aiosqlite'],
+    keywords=['db', 'bot', 'discord bot', 'database', 'postgresql', 'asyncpg', 'async', 'pgutils', 'nosql', 'sqlite', 'aiosqlite', 'discord.py'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
