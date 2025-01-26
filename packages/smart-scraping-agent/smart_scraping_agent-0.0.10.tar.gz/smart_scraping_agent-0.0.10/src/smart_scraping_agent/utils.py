03379595bf054b283e70c7bbe7437cdc47ab5944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 03:07:29 +0000

    Add package smart-scraping-agent/0.0.10/smart_scraping_agent-0.0.10.tar.gz

---
+++ packages/smart-scraping-agent/smart_scraping_agent-0.0.10.tar.gz/smart_scraping_agent-0.0.10/src/smart_scraping_agent/utils.py
@@ -0,0 +1,53 @@
+import json
+import re
+import os
+import logging
+import tiktoken
+from typing import List
+
+from langchain_core.messages import AIMessage
+
+
+def count_tokens(input_string: str) -> int:
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    tokens = tokenizer.encode(input_string)
+    return len(tokens)
+
+def validate_obj(obj):
+     return bool(
+          (products := obj.get('products')) \
+                and any(
+                    list(
+                        filter(
+                            lambda prod: (name := prod.get('name')) \
+                                            and prod.get('price_range') \
+                                                and prod.get('purchase_link') \
+                                                    and (re.search(r'\d', prod.get('price_range')) \
+                                                        and 'http' in prod.get('purchase_link')),
+                                                        products
+                                )
+                        )
+                    )
+                )
+
+def jsonify(container, filename, obj):
+    base_dir = 'output'
+    int_dir = 'json_contents'
+    results_dir = os.path.join(base_dir, int_dir, container)
+    os.makedirs(results_dir, exist_ok=True)
+    file_name = f'{filename}.json'
+    file_path = os.path.join(results_dir, file_name) 
+                                    
+    with open(file_path, 'w') as f:
+        f.write(json.dumps(obj, indent=4))
+    print(f'\nStored results at `{file_path}`.\n')
+
+def store_web_reader_contents(filename, contents):
+    base_dir = 'output'
+    int_dir = 'scraped_contents'
+    folder_path = os.path.join(base_dir, int_dir)
+    os.makedirs(folder_path, exist_ok= True)
+    file_path = os.path.join(folder_path, filename)
+    with open(file_path, 'w') as f:
+        f.write(contents)
+    logging.info(f'\n📝 Stored Scraped Contents at `{file_path}`.\n')
