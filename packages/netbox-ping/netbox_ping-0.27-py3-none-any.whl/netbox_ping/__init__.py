4228a461259dae5a524f6fee03270da84fdbfac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 05:04:52 +0000

    Add package netbox-ping/0.27/netbox_ping-0.27-py3-none-any.whl

---
+++ packages/netbox-ping/netbox_ping-0.27-py3-none-any.whl/netbox_ping/__init__.py
@@ -0,0 +1,29 @@
+from netbox.plugins import PluginConfig
+
+class Config(PluginConfig):
+    name = 'netbox_ping'
+    verbose_name = 'NetBox Ping'
+    description = 'Ping IPs and subnets'
+    version = '0.27'
+    author = 'Christian Rose'
+    default_settings = {
+        'coming_soon': True
+    }
+
+    # Register the custom table
+    ipaddress_table = 'netbox_ping.tables.CustomIPAddressTable'
+    
+    # Define which models support custom fields
+    custom_field_models = ['ipaddress']
+
+    # API settings
+    base_url = 'netbox-ping'
+
+    # Register API serializers
+    def ready(self):
+        from .api.serializers import PluginSettingsModelSerializer
+        self.serializer_classes = {
+            'PluginSettingsModel': PluginSettingsModelSerializer,
+        }
+
+config = Config
