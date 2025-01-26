fadead49171554afb750d0f4c9f411fcf6828ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 03:06:39 +0000

    Add package mgraph-ai-serverless/1.0.3/mgraph_ai_serverless-1.0.3-py3-none-any.whl

---
+++ packages/mgraph-ai-serverless/mgraph_ai_serverless-1.0.3-py3-none-any.whl/mgraph_ai_serverless/graph_engines/graphviz/Graphviz__Render.py
@@ -0,0 +1,14 @@
+import graphviz
+from graphviz                        import Digraph
+from osbot_utils.type_safe.Type_Safe import Type_Safe
+
+
+class Graphviz__Render(Type_Safe):
+
+    def render_dot(self, dot_source: str, directory='/tmp', filename='dot-render.png', output_format="png") -> bytes   :# Create a Digraph from the source
+        dot = graphviz.Source(dot_source)
+        kwargs = dict(directory = directory     ,
+                      filename  = filename      ,
+                      format    = output_format)
+
+        return dot.render(**kwargs)
---
+++ packages/mgraph-ai-serverless/mgraph_ai_serverless-1.0.3-py3-none-any.whl/mgraph_ai_serverless/testing/mgraph_ai_serverless__objs_for_tests.py
@@ -0,0 +1,5 @@
+from mgraph_ai_serverless.fast_api.MGraph_AI_Serverless__Fast_API import MGraph_AI_Serverless__Fast_API
+
+mgraph_ai_serverless__fast_api         = MGraph_AI_Serverless__Fast_API().setup()
+mgraph_ai_serverless__fast_api__app    = mgraph_ai_serverless__fast_api.app()
+mgraph_ai_serverless__fast_api__client = mgraph_ai_serverless__fast_api.client()
