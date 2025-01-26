77dc6b8e534bc3966394f0442af9658bf72d93e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 15:04:18 +0000

    Add package dns-exporter/1.1.0b1/dns_exporter-1.1.0b1.tar.gz

---
+++ packages/dns-exporter/dns_exporter-1.1.0b1.tar.gz/dns_exporter-1.1.0b1/src/tests/test_entrypoint.py
@@ -0,0 +1,32 @@
+"""Unit tests for entrypoint.py."""
+import time
+
+import dns_exporter.entrypoint
+import pytest
+from dns_exporter.entrypoint import main
+from dns_exporter.version import __version__
+
+mockargs = [
+    "-c",
+    "dns_exporter/dns_exporter_example.yml",
+    "-d",
+    "-p",
+    "25353",
+]
+
+
+def test_listen_port_busy(dns_exporter_example_config, caplog):
+    """Test calling main() on a port which is already busy."""
+    with pytest.raises(SystemExit):
+        dns_exporter.entrypoint.main(mockargs)
+    time.sleep(2)
+    assert "is in use?" in caplog.text
+
+
+def test_version(capsys):
+    """Make sure the -v command-line option returns the version."""
+    with pytest.raises(SystemExit) as e:
+        main(["-v"])
+    assert e.type == SystemExit, f"Exit was not as expected, it was {e.type}"
+    captured = capsys.readouterr()
+    assert __version__ in captured.out
