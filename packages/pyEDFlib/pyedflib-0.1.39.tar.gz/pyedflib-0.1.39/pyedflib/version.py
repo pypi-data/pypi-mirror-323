e4691bf7d7398f806ca74149b77571f0e17417eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 03:07:08 +0000

    Add package pyEDFlib/0.1.39/pyedflib-0.1.39.tar.gz

---
+++ packages/pyEDFlib/pyedflib-0.1.39.tar.gz/pyedflib-0.1.39/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.39'
+version = '0.1.39'
+full_version = '0.1.39'
+git_revision = '0c5037d6e4a1fdae7ed8df3d4ceea9031fbb6fc3'
+release = True
+
+if not release:
+    version = full_version
