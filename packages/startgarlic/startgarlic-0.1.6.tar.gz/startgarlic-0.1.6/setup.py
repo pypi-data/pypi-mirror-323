2beee0723cef3069ee9ea6c98867601ffd856c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:06:08 +0000

    Add package startgarlic/0.1.6/startgarlic-0.1.6.tar.gz

---
+++ packages/startgarlic/startgarlic-0.1.6.tar.gz/startgarlic-0.1.6/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+from wheel.bdist_wheel import bdist_wheel
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+class BdistWheelCommand(bdist_wheel):
+    def finalize_options(self):
+        bdist_wheel.finalize_options(self)
+        self.root_is_pure = False
+
+setup(
+    name="startgarlic",
+    version="0.1.6",
+    author="Bogdan Ciolac, May Elshater",
+    author_email="bogdan@startgarlic.com, may@startgarlic.com",
+    description="A RAG-based company information retrieval system",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/startgarlic/MVP",
+    packages=find_packages(),
+    # package_data={
+    #     'garlic': ['companies/*.xlsx'],
+    # },
+    install_requires=[
+         "numpy>=1.23.5,<2.0.0",
+         "pandas>=2.0.0",
+         "supabase-py",
+         "python-dotenv",
+         "sentence-transformers",
+         "langchain>=0.0.200",
+         "langchain-community", 
+         "langchain-huggingface",
+        #  "bcrypt",
+        #  "faiss-cpu",  # For vector storage
+        # "streamlit>=1.40.0"
+    ],
+    entry_points={
+        'console_scripts': [
+            'startgarlic=startgarlic.cli:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    cmdclass={
+        'bdist_wheel': BdistWheelCommand,
+    },
+    package_data={
+        'startgarlic': ['.env'],
+        'startgarlic.utils': ['config.py'],
+    },
+    include_package_data=True,
+)
---
+++ packages/startgarlic/startgarlic-0.1.6.tar.gz/startgarlic-0.1.6/startgarlic/__init__.py
@@ -0,0 +1,9 @@
+"""
+StartGarlic - A RAG-based company information retrieval system
+"""
+
+__version__ = "0.1.6"
+
+from .core import Garlic
+
+__all__ = ["Garlic"]
