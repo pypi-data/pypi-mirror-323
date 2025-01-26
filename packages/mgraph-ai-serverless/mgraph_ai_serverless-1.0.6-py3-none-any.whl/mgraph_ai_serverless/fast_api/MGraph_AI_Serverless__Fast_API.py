0fac86b814e4fc1e4ec09cf73232002ce2e44527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:06:20 +0000

    Add package mgraph-ai-serverless/1.0.6/mgraph_ai_serverless-1.0.6-py3-none-any.whl

---
+++ packages/mgraph-ai-serverless/mgraph_ai_serverless-1.0.6-py3-none-any.whl/mgraph_ai_serverless/fast_api/MGraph_AI_Serverless__Fast_API.py
@@ -0,0 +1,14 @@
+from mgraph_ai_serverless.graph_engines.graphviz.routes.Routes__Graphviz import Routes__Graphviz
+from osbot_fast_api.api.Fast_API                                         import Fast_API
+from mgraph_ai_serverless.fast_api.routes.Routes__Info                   import Routes__Info
+
+
+class MGraph_AI_Serverless__Fast_API(Fast_API):
+    base_path  : str  = '/'
+    enable_cors: bool = True
+
+    def setup_routes(self):
+        self.add_routes(Routes__Info    )
+        self.add_routes(Routes__Graphviz)
+
+
