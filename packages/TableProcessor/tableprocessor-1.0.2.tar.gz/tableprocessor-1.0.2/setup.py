beac6147799df725076fb89ede672fbb36dc2084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:13 +0000

    Add package TableProcessor/1.0.2/tableprocessor-1.0.2.tar.gz

---
+++ packages/TableProcessor/tableprocessor-1.0.2.tar.gz/tableprocessor-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TableProcessor",
+    version="1.0.2",
+    description="Procesamiento de tablas de datos",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "tqdm",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
