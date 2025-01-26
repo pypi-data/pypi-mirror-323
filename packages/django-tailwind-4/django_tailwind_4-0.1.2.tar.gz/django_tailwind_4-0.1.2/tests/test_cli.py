fae13af63ca6e529edbed2a20cac12445817cf6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 03:07:04 +0000

    Add package django-tailwind-4/0.1.2/django_tailwind_4-0.1.2.tar.gz

---
+++ packages/django-tailwind-4/django_tailwind_4-0.1.2.tar.gz/django_tailwind_4-0.1.2/tests/test_cli.py
@@ -0,0 +1,34 @@
+import os
+import uuid
+
+from django.core.management import call_command
+
+from tailwind.utils import get_app_path
+
+from .conftest import cleanup_theme_app_dir
+
+
+def test_tailwind_install_and_build(settings):
+    app_name = f'test_theme_{str(uuid.uuid1()).replace("-", "_")}'
+
+    call_command("tailwind", "init", "--app-name", app_name, "--no-input")
+
+    settings.INSTALLED_APPS += [app_name]
+    settings.TAILWIND_APP_NAME = app_name
+
+    assert os.path.isfile(os.path.join(get_app_path(app_name), "apps.py")), 'The "theme" app has been generated'
+
+    tailwind_config_path = os.path.join(get_app_path(app_name), "static_src", "tailwind.config.js")
+    assert os.path.isfile(tailwind_config_path), "tailwind.config.js is present"
+
+    call_command("tailwind", "install")
+    assert os.path.isdir(
+        os.path.join(get_app_path(app_name), "static_src", "node_modules")
+    ), "Tailwind has been installed from npm"
+
+    call_command("tailwind", "build")
+    assert os.path.isfile(
+        os.path.join(get_app_path(app_name), "static", "css", "dist", "styles.css")
+    ), "Tailwind has built a css/styles.css file"
+
+    cleanup_theme_app_dir(app_name)
