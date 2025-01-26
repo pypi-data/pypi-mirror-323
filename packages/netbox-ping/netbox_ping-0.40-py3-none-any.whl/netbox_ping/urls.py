970310fd664b6d7ab01b7ffed4bb10d0b5509136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 15:04:14 +0000

    Add package netbox-ping/0.40/netbox_ping-0.40-py3-none-any.whl

---
+++ packages/netbox-ping/netbox_ping-0.40-py3-none-any.whl/netbox_ping/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'netbox_ping'
+
+# Define a list of URL patterns to be imported by NetBox. Each pattern maps a URL to
+# a specific view so that it can be accessed by users.
+urlpatterns = [
+    path('', views.PingHomeView.as_view(), name='ping_home'),
+    path('ping-subnet/<int:prefix_id>/', views.PingSubnetView.as_view(), name='ping_subnet'),
+    path('scan-subnet/<int:prefix_id>/', views.ScanSubnetView.as_view(), name='scan_subnet'),
+    path('initialize/', views.InitializePluginView.as_view(), name='initialize_plugin'),
+    path('scan-all/', views.ScanAllView.as_view(), name='scan_all'),
+    path('update-settings/', views.UpdateSettingsView.as_view(), name='update_settings'),
+]
