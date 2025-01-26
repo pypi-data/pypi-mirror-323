76aac6b4b859a3f94c3e4d6ae90e3d8d9863127a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 03:07:10 +0000

    Add package wtpsplit/2.1.4/wtpsplit-2.1.4.tar.gz

---
+++ packages/wtpsplit/wtpsplit-2.1.4.tar.gz/wtpsplit-2.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wtpsplit",
+    version="2.1.4",
+    packages=find_packages(),
+    description="Universal Robust, Efficient and Adaptable Sentence Segmentation",
+    author="Markus Frohmann, Igor Sterner, Benjamin Minixhofer",
+    author_email="markus.frohmann@gmail.com",
+    install_requires=[
+        "onnxruntime>=1.13.1",
+        "transformers>=4.22.2",
+        "huggingface-hub",
+        "numpy>=1.0",
+        "scikit-learn>=1",
+        "tqdm",
+        "skops",
+        "pandas>=1",
+        "cached_property",  # for Py37
+        "mosestokenizer",
+        "adapters>=1.0.1",
+    ],
+    url="https://github.com/segment-any-text/wtpsplit",
+    package_data={"wtpsplit": ["data/*"]},
+    include_package_data=True,
+    license="MIT",
+)
