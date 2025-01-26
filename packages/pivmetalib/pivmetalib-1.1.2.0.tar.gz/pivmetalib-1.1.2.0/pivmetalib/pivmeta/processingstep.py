7b5e62ebc64247f04011b71c100336563e7a23d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:05:42 +0000

    Add package pivmetalib/1.1.2.0/pivmetalib-1.1.2.0.tar.gz

---
+++ packages/pivmetalib/pivmetalib-1.1.2.0.tar.gz/pivmetalib-1.1.2.0/pivmetalib/pivmeta/processingstep.py
@@ -0,0 +1,45 @@
+from ontolutils import urirefs, namespaces
+
+from pivmetalib import m4i
+
+
+@namespaces(pivmeta='https://matthiasprobst.github.io/pivmeta#')
+@urirefs(PIVAnalysis='pivmeta:PIVAnalysis')
+class PIVAnalysis(m4i.ProcessingStep):
+    """Pydantic Model for pivmeta:PIVAnalysis"""
+
+
+@namespaces(pivmeta='https://matthiasprobst.github.io/pivmeta#')
+@urirefs(PIVPostProcessing='pivmeta:PIVAnalysis')
+class PIVPostProcessing(PIVAnalysis):
+    """Pydantic Model for pivmeta:PIVPostProcessing"""
+
+
+@namespaces(pivmeta='https://matthiasprobst.github.io/pivmeta#')
+@urirefs(PIVPreProcessing='pivmeta:PIVPostProcessing')
+class PIVPreProcessing(PIVAnalysis):
+    """Pydantic Model for pivmeta:PIVPreProcessing"""
+
+
+@namespaces(pivmeta='https://matthiasprobst.github.io/pivmeta#')
+@urirefs(PIVEvaluation='pivmeta:PIVEvaluation')
+class PIVEvaluation(PIVAnalysis):
+    """Pydantic Model for pivmeta:PIVEvaluation"""
+
+
+@namespaces(pivmeta='https://matthiasprobst.github.io/pivmeta#')
+@urirefs(MaskGeneration='pivmeta:MaskGeneration')
+class MaskGeneration(PIVAnalysis):
+    """Pydantic Model for pivmeta:MaskGeneration"""
+
+
+@namespaces(pivmeta='https://matthiasprobst.github.io/pivmeta#')
+@urirefs(ImageRotation='pivmeta:ImageRotation')
+class ImageRotation(PIVAnalysis):
+    """Pydantic Model for pivmeta:ImageRotation"""
+
+
+@namespaces(pivmeta='https://matthiasprobst.github.io/pivmeta#')
+@urirefs(BackgroundImageGeneration='pivmeta:BackgroundImageGeneration')
+class BackgroundImageGeneration(PIVAnalysis):
+    """Pydantic Model for pivmeta:BackgroundImageGeneration"""
