2dbcc5f4c0bd2810d4b92b6f894f869d05c5610a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 03:06:36 +0000

    Add package dreamsboard/0.3.6/dreamsboard-0.3.6.tar.gz

---
+++ packages/dreamsboard/dreamsboard-0.3.6.tar.gz/dreamsboard-0.3.6/dreamsboard/dreams/task_step_md/prompts.py
@@ -0,0 +1,32 @@
+
+TASK_MD_TEMPLATE = """
+# {start_task_context} 
+
+
+{context_placeholder}
+ 
+"""
+
+
+
+TASK_STEP_MD_TITLE_TEMPLATE = """
+{task_step_name} [task_id:{task_step_level}]({task_step_id})
+
+{task_step_question_answer}
+"""
+
+
+TASK_STEP_MD_DESC_TEMPLATE = """
+{task_step_name} [task_id:{task_step_level}]({task_step_id}) {task_step_question_answer}
+"""
+
+
+
+TASK_STEP_MD_LIST_TEMPLATE = """
+- **{task_step_name} [task_id:{task_step_level}]({task_step_id})**:{task_step_question_answer}
+"""
+
+
+TASK_STEP_MD_TEMPLATE = """
+{task_step_name} [task_id:{task_step_level}]({task_step_id}), {task_step_question_answer}
+"""
