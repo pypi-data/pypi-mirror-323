6bf18205d9574722a981fad1c8c47149393609fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:05:13 +0000

    Add package netbox-ping/0.8/netbox_ping-0.8.tar.gz

---
+++ packages/netbox-ping/netbox_ping-0.8.tar.gz/netbox_ping-0.8/netbox_ping/__init__.py
@@ -0,0 +1,28 @@
+from netbox.plugins import PluginConfig
+
+class Config(PluginConfig):
+    name = 'netbox_ping'
+    verbose_name = 'NetBox Ping'
+    description = 'Ping IPs and subnets'
+    version = '0.8'
+    author = 'Christian Rose'
+    
+    # Required settings
+    base_url = 'netbox-ping'
+    
+    # Optional settings with defaults
+    default_settings = {
+        'coming_soon': True
+    }
+
+    # Register the custom table
+    ipaddress_table = 'netbox_ping.tables.CustomIPAddressTable'
+    
+    # Define which models support custom fields
+    custom_field_models = ['ipaddress']
+
+    # Register our models - this is the key change
+    django_apps = ('netbox_ping',)
+    installed_apps = ['netbox_ping']
+
+config = Config
