5dee480c582343df8ce23e3f2cdbbdae764eb976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:05:11 +0000

    Add package perfect-cmaps/1.0.1/perfect_cmaps-1.0.1.tar.gz

---
+++ packages/perfect-cmaps/perfect_cmaps-1.0.1.tar.gz/perfect_cmaps-1.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="perfect_cmaps",
+    version="1.0.1",
+    author="Mattias Ulmestrand",
+    author_email="m.ulmestrand@gmail.com",
+    description="Create amazing perceptually uniform colormaps with ease!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/m-ulmestrand/perfect-cmaps",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy",
+        "numba",
+        "pandas",
+        "matplotlib",
+        "colour-science",
+        "scipy",
+        "opencv-python-headless",
+        "importlib_resources",
+    ],
+)
