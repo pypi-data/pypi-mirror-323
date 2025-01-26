183f2914cf558a6c71fe2f9c7a7cad5fd4860500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:05:09 +0000

    Add package midea-local-ben/6.1.0/midea_local_ben-6.1.0-py3-none-any.whl

---
+++ packages/midea-local-ben/midea_local_ben-6.1.0-py3-none-any.whl/midealocal/devices/e6/__init__.py
@@ -0,0 +1,101 @@
+"""Midea local E6 device."""
+
+import logging
+from enum import StrEnum
+from typing import Any
+
+from midealocal.const import DeviceType, ProtocolVersion
+from midealocal.device import MideaDevice
+
+from .message import MessageE6Response, MessageQuery, MessageSet
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class DeviceAttributes(StrEnum):
+    """Midea E6 device attributes."""
+
+    main_power = "main_power"
+    heating_power = "heating_power"
+    heating_working = "heating_working"
+    bathing_working = "bathing_working"
+    min_temperature = "temperature_min"
+    max_temperature = "temperature_max"
+    heating_temperature = "heating_temperature"
+    bathing_temperature = "bathing_temperature"
+    heating_leaving_temperature = "heating_leaving_temperature"
+    bathing_leaving_temperature = "bathing_leaving_temperature"
+
+
+class MideaE6Device(MideaDevice):
+    """Midea E6 device."""
+
+    def __init__(
+        self,
+        name: str,
+        device_id: int,
+        ip_address: str,
+        port: int,
+        token: str,
+        key: str,
+        device_protocol: ProtocolVersion,
+        model: str,
+        subtype: int,
+        customize: str,  # noqa: ARG002
+    ) -> None:
+        """Initialize Midea E6 device."""
+        super().__init__(
+            name=name,
+            device_id=device_id,
+            device_type=DeviceType.E6,
+            ip_address=ip_address,
+            port=port,
+            token=token,
+            key=key,
+            device_protocol=device_protocol,
+            model=model,
+            subtype=subtype,
+            attributes={
+                DeviceAttributes.main_power: False,
+                DeviceAttributes.heating_power: True,
+                DeviceAttributes.heating_working: None,
+                DeviceAttributes.bathing_working: None,
+                DeviceAttributes.min_temperature: [30, 35],
+                DeviceAttributes.max_temperature: [80, 60],
+                DeviceAttributes.heating_temperature: 50,
+                DeviceAttributes.bathing_temperature: 40,
+                DeviceAttributes.heating_leaving_temperature: None,
+                DeviceAttributes.bathing_leaving_temperature: None,
+            },
+        )
+
+    def build_query(self) -> list[MessageQuery]:
+        """Midea E6 device build query."""
+        return [MessageQuery(self._message_protocol_version)]
+
+    def process_message(self, msg: bytes) -> dict[str, Any]:
+        """Midea E6 device process message."""
+        message = MessageE6Response(msg)
+        _LOGGER.debug("[%s] Received: %s", self.device_id, message)
+        new_status = {}
+        for status in self._attributes:
+            if hasattr(message, str(status)):
+                self._attributes[status] = getattr(message, str(status))
+                new_status[str(status)] = self._attributes[status]
+        return new_status
+
+    def set_attribute(self, attr: str, value: bool | int | str) -> None:
+        """Midea E6 device set attribute."""
+        if attr in [
+            DeviceAttributes.main_power,
+            DeviceAttributes.heating_power,
+            DeviceAttributes.heating_temperature,
+            DeviceAttributes.bathing_temperature,
+        ]:
+            message = MessageSet(self._message_protocol_version)
+            setattr(message, str(attr), value)
+            self.build_send(message)
+
+
+class MideaAppliance(MideaE6Device):
+    """Midea E6 appliance."""
---
+++ packages/midea-local-ben/midea_local_ben-6.1.0-py3-none-any.whl/midealocal/devices/e6/message.py
@@ -0,0 +1,110 @@
+"""Midea local E6 message."""
+
+from midealocal.const import DeviceType
+from midealocal.message import (
+    ListTypes,
+    MessageBody,
+    MessageRequest,
+    MessageResponse,
+    MessageType,
+)
+
+
+class MessageE6Base(MessageRequest):
+    """E6 message base."""
+
+    def __init__(
+        self,
+        protocol_version: int,
+        message_type: MessageType,
+    ) -> None:
+        """Initialize E6 message base."""
+        super().__init__(
+            device_type=DeviceType.E6,
+            protocol_version=protocol_version,
+            message_type=message_type,
+            body_type=ListTypes.X00,
+        )
+
+    @property
+    def body(self) -> bytearray:
+        """E6 message base body."""
+        return self._body
+
+    @property
+    def _body(self) -> bytearray:
+        raise NotImplementedError
+
+
+class MessageQuery(MessageE6Base):
+    """E6 message query."""
+
+    def __init__(self, protocol_version: int) -> None:
+        """Initialize E6 message query."""
+        super().__init__(
+            protocol_version=protocol_version,
+            message_type=MessageType.query,
+        )
+
+    @property
+    def _body(self) -> bytearray:
+        return bytearray([0x01, 0x01] + [0] * 28)
+
+
+class MessageSet(MessageE6Base):
+    """E6 message set."""
+
+    def __init__(self, protocol_version: int) -> None:
+        """Initialize E6 message set."""
+        super().__init__(
+            protocol_version=protocol_version,
+            message_type=MessageType.set,
+        )
+        self.main_power: bool | None = None
+        self.heating_temperature: int | None = None
+        self.bathing_temperature: int | None = None
+        self.heating_power: bool | None = None
+
+    @property
+    def _body(self) -> bytearray:
+        body: list[int] = []
+        if self.main_power is not None:
+            main_power = 0x01 if self.main_power else 0x02
+            body = [main_power, 0x01]
+        elif self.heating_temperature is not None:
+            body = [0x04, 0x13, self.heating_temperature]
+        elif self.bathing_temperature is not None:
+            body = [0x04, 0x12, self.bathing_temperature]
+        elif self.heating_power is not None:
+            heating_power = 0x01 if self.heating_power else 0x02
+            body = [0x04, 0x01, heating_power]
+        body_len = len(body)
+        return bytearray(body + [0] * (30 - body_len))
+
+
+class E6GeneralMessageBody(MessageBody):
+    """E6 message general body."""
+
+    def __init__(self, body: bytearray) -> None:
+        """Initialize E6 message general body."""
+        super().__init__(body)
+        self.main_power = (body[2] & 0x04) > 0
+        self.heating_working = (body[2] & 0x10) > 0
+        self.bathing_working = (body[2] & 0x20) > 0
+        self.heating_power = (body[4] & 0x01) > 0
+        self.min_temperature = [body[16], body[11]]
+        self.max_temperature = [body[15], body[10]]
+        self.heating_temperature = body[17]
+        self.bathing_temperature = body[12]
+        self.heating_leaving_temperature = body[14]
+        self.bathing_leaving_temperature = body[8]
+
+
+class MessageE6Response(MessageResponse):
+    """E6 message response."""
+
+    def __init__(self, message: bytes) -> None:
+        """Initialize E6 message response."""
+        super().__init__(bytearray(message))
+        self.set_body(E6GeneralMessageBody(super().body))
+        self.set_attr()
