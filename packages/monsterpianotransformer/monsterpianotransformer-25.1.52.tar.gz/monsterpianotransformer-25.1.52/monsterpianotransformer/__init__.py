5d18f2694ec0b0f04174bf7831c4a77e319eca65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:28 +0000

    Add package monsterpianotransformer/25.1.52/monsterpianotransformer-25.1.52.tar.gz

---
+++ packages/monsterpianotransformer/monsterpianotransformer-25.1.52.tar.gz/monsterpianotransformer-25.1.52/monsterpianotransformer/__init__.py
@@ -0,0 +1,17 @@
+from .models import MODELS_INFO
+
+from .model_loader import load_model
+
+from .midi_processors import midi_to_tokens, tokens_to_midi
+
+from .sample_midis import get_sample_midi_files
+
+from .monsterpianotransformer import generate, generate_long, generate_chord
+
+from .monsterpianotransformer import inpaint_pitches
+
+from .monsterpianotransformer import inpaint_velocities_simple, inpaint_velocities_seq2seq
+
+from .monsterpianotransformer import inpaint_timings
+
+from .monsterpianotransformer import inpaint_bridge
