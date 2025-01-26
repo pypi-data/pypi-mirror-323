930a0fd5b8e2a686e691d4be06b63215bfb0dfe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:06:18 +0000

    Add package bream/0.0.2/bream-0.0.2-py3-none-any.whl

---
+++ packages/bream/bream-0.0.2-py3-none-any.whl/bream/__init__.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from bream import coders, core
+from bream.core import (
+    Codec,
+    Coder,
+    DecodeError,
+    Document,
+    EncodeError,
+    JsonType,
+    SerialisationFormat,
+    TypeLabel,
+    TypeSpec,
+    decode,
+    decode_document,
+    encode,
+    encode_to_document,
+)
+
+__all__ = [
+    "Codec",
+    "Coder",
+    "DecodeError",
+    "Document",
+    "EncodeError",
+    "JsonType",
+    "SerialisationFormat",
+    "TypeLabel",
+    "TypeSpec",
+    "coders",
+    "core",
+    "decode",
+    "decode_document",
+    "encode",
+    "encode_to_document",
+]
