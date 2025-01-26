7397c74bfca35e3357944c8712af3414daf5ced6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:05:13 +0000

    Add package netbox-ping/0.6/netbox_ping-0.6.tar.gz

---
+++ packages/netbox-ping/netbox_ping-0.6.tar.gz/netbox_ping-0.6/netbox_ping/__init__.py
@@ -0,0 +1,37 @@
+from netbox.plugins import PluginConfig
+
+class Config(PluginConfig):
+    name = 'netbox_ping'
+    verbose_name = 'NetBox Ping'
+    description = 'Ping IPs and subnets'
+    version = '0.6'
+    author = 'Christian Rose'
+    default_settings = {
+        'coming_soon': True
+    }
+
+    # Register the custom table
+    ipaddress_table = 'netbox_ping.tables.CustomIPAddressTable'
+    
+    # Define which models support custom fields
+    custom_field_models = ['ipaddress']
+
+    # Register our models
+    django_apps = ('netbox_ping',)
+    
+    # Register our model using string reference
+    caching_config = {
+        'settings': {
+            'model': 'netbox_ping.PluginSettingsModel',
+            'key': 'netbox_ping.settings',
+        }
+    }
+
+    # API settings
+    base_url = 'netbox-ping'
+
+    def ready(self):
+        super().ready()
+        from . import signals
+
+config = Config
