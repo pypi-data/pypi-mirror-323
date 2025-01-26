8a1a603b37bb545a33ead2899ca897f2f3a87059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:06:20 +0000

    Add package mgraph-ai-serverless/1.0.6/mgraph_ai_serverless-1.0.6.tar.gz

---
+++ packages/mgraph-ai-serverless/mgraph_ai_serverless-1.0.6.tar.gz/mgraph_ai_serverless-1.0.6/mgraph_ai_serverless/graph_engines/graphviz/routes/Routes__Graphviz.py
@@ -0,0 +1,21 @@
+from fastapi                                                                        import Response
+from mgraph_ai_serverless.graph_engines.graphviz.models.Model__Graphviz__Render_Dot import Model__Graphviz__Render_Dot
+from mgraph_ai_serverless.graph_engines.graphviz.Graphviz__Render                   import Graphviz__Render
+from osbot_fast_api.api.Fast_API_Routes                                             import Fast_API_Routes
+
+ROUTES__GRAPHVIZ__RENDER = ['/render-dot']
+
+class Routes__Graphviz(Fast_API_Routes):
+    graphviz_render : Graphviz__Render
+    tag             : str = 'graphviz'
+
+    def render_dot(self, graphviz_render_dot: Model__Graphviz__Render_Dot) -> Response:
+        #try:
+            bytes_data = self.graphviz_render.render_dot(graphviz_render_dot)
+            output_format = graphviz_render_dot.output_format
+            return Response(content=bytes_data, media_type=f"image/{output_format.value}")
+        #except Exception as error:
+        #    return f"{error}"
+
+    def setup_routes(self):
+        self.add_route(self.render_dot, methods=['POST'])
