dc32aff7de27f77d76bca1f07eed96ba4d3011df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 15:04:28 +0000

    Add package birbtalk/0.1/birbtalk-0.1-py3-none-any.whl

---
+++ packages/birbtalk/birbtalk-0.1-py3-none-any.whl/birbtalk/langgpt.py
@@ -0,0 +1,46 @@
+langgpt = """# Role: {{name}}
+
+## Profile:
+  
+{% if author %}- Author: {{author}}\n{% endif %}
+{%- if version %}- Version: {{version}}\n{% endif %}
+{%- if language %}- Language: {{language}}{% endif %}
+- Description: {{description}}
+
+{%- if background %}\n\n### Background\n\n{{background}}{% endif %}
+
+{%- if rules %}\n\n## Rules
+{% for item in rules %} 
+{{loop.index}}. {{item}}
+{%- endfor %}
+{%- endif %}
+
+{%- if rules %}\n\n## Skills
+{% for item in rules %} 
+{{loop.index}}. {{item}}
+{%- endfor %}
+{%- endif %}
+
+{%- if workflow %}\n\n## Workflow
+{% for item in workflow %} 
+{{loop.index}}. {{item}}
+{%- endfor %}
+{%- endif %}
+
+{%- if examples %}\n\n## Examples
+{%- for item in examples %} 
+\n### {{item.title}}\n\n{{item.text}}
+{%- endfor %}
+{%- endif %}
+
+{%- if core_memories %}\n\n## Core memory
+{% for item in core_memories %} 
+{{loop.index}}. {{item}}
+{%- endfor %}
+{%- endif %}
+
+{%- if init_message %}\n\n## Initialization
+
+{{init_message}}
+{%- endif -%}
+"""
