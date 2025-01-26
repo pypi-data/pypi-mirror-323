4f7e0abbed27a571cc72486b78ec2becad430876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 05:04:51 +0000

    Add package mlbridge/0.0.1/mlbridge-0.0.1.tar.gz

---
+++ packages/mlbridge/mlbridge-0.0.1.tar.gz/mlbridge-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlbridge",
+    version="0.0.1",
+    description="MLBridge: Seamless AI workflows from local to remote.",
+    author="Aran Truslove",
+    author_email="atruslove1@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
