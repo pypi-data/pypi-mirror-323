12cf4ac750e65f8a93282d82fab0313bbd8cf4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 15:04:24 +0000

    Add package django-spire/0.1.9/django_spire-0.1.9-py3-none-any.whl

---
+++ packages/django-spire/django_spire-0.1.9-py3-none-any.whl/django_spire/template/app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from example.placeholder import views
+
+
+app_name = 'placeholder'
+
+urlpatterns = [
+    path('<int:pk>/delete/form', views.placeholder_delete_view, name='delete'),
+    path('<int:pk>/detail', views.placeholder_detail_view, name='detail'),
+    path('<int:pk>/form', views.placeholder_form_view, name='form'),
+    path('list/', views.placeholder_list_view, name='list'),
+]
