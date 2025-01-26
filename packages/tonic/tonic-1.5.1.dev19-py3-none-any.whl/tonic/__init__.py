bd9da0663151bd4b1c38a2daa94787407d975aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:04:56 +0000

    Add package tonic/1.5.1.dev19/tonic-1.5.1.dev19-py3-none-any.whl

---
+++ packages/tonic/tonic-1.5.1.dev19-py3-none-any.whl/tonic/__init__.py
@@ -0,0 +1,19 @@
+from pbr.version import VersionInfo
+
+from . import collation, datasets, io, slicers, transforms, utils
+from .cached_dataset import (
+    Aug_DiskCachedDataset,
+    CachedDataset,
+    DiskCachedDataset,
+    MemoryCachedDataset,
+)
+from .dataset import Dataset
+from .sliced_dataset import SlicedDataset
+
+try:
+    from . import prototype
+except (ImportError, NameError):
+    pass
+
+all = "__version__"
+__version__ = VersionInfo("tonic").release_string()
