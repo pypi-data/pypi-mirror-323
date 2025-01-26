526d97ee9228fe1e602f8bd1435a696da3b901d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 14:04:24 +0000

    Add package hydraflow/0.5.3/hydraflow-0.5.3.tar.gz

---
+++ packages/hydraflow/hydraflow-0.5.3.tar.gz/hydraflow-0.5.3/tests/run/test_filter.py
@@ -0,0 +1,17 @@
+import pytest
+
+from hydraflow.run_collection import RunCollection
+
+
+@pytest.fixture(scope="module")
+def rc(collect):
+    collect("run/filter.py", ["-m", "port=1,2,3"])
+    return collect("run/filter.py", ["-m", "port=1,2,4"])
+
+
+def test_rc_len(rc: RunCollection):
+    assert len(rc) == 4
+
+
+def test_params(rc: RunCollection):
+    assert rc.data.params["port"] == ["1", "2", "3", "4"]
