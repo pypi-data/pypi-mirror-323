244bbfe7d801c4585fc2fdf37248886264981d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-26 04:06:00 +0000

    Add package empire-chain/0.3.18/empire_chain-0.3.18.tar.gz

---
+++ packages/empire-chain/empire_chain-0.3.18.tar.gz/empire_chain-0.3.18/empire_chain/vector_stores/__init__.py
@@ -0,0 +1,20 @@
+class VectorStore:
+    def __init__(self, client):
+        self.client = client
+
+    def add(self, text: str, embedding: list[float]):
+        pass
+
+    def query(self, query_embedding: list[float], k: int = 10):
+        pass 
+
+def QdrantVectorStore(*args, **kwargs):
+    from empire_chain.vector_stores.qdrant import QdrantVectorStore as _QdrantVectorStore
+    try:
+        import qdrant_client
+        return _QdrantVectorStore(*args, **kwargs)
+    except ImportError:
+        raise ImportError(
+            "Could not import qdrant. Please install the necessary dependencies with: "
+            "pip install qdrant-client"
+        )
