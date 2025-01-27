294ed191171bf811ec8a6314b0d3ae879bc85cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 16:05:37 +0000

    Add package chanjo2/3.1.0/chanjo2-3.1.0.tar.gz

---
+++ packages/chanjo2/chanjo2-3.1.0.tar.gz/chanjo2-3.1.0/src/chanjo2/crud/intervals.py
@@ -0,0 +1,243 @@
+import logging
+from typing import List, Optional, Union
+
+from sqlalchemy import delete, or_, text
+from sqlalchemy.orm import Session, query
+from sqlalchemy.sql.expression import Delete
+
+from chanjo2.models import SQLExon, SQLGene, SQLTranscript
+from chanjo2.models.pydantic_models import (
+    Builds,
+    ExonBase,
+    GeneBase,
+    TranscriptBase,
+    TranscriptTag,
+)
+
+LOG = logging.getLogger(__name__)
+
+
+def delete_intervals_for_build(
+    db: Session, interval_type: Union[SQLGene, SQLTranscript, SQLExon], build: Builds
+) -> None:
+    """Delete intervals from a given table by specifying a genome build."""
+    statement: Delete = delete(interval_type).where(interval_type.build == build)
+    LOG.info(f"Executing statement: {statement}")
+    db.execute(statement)
+
+
+def count_intervals_for_build(
+    db: Session, interval_type: Union[SQLGene, SQLTranscript, SQLExon], build: Builds
+) -> int:
+    """Count intervals in table by specifying a genome build."""
+    return db.query(interval_type).where(interval_type.build == build).count()
+
+
+def _filter_intervals_by_build(
+    intervals: query.Query,
+    interval_type: Union[SQLGene, SQLTranscript, SQLExon],
+    build: Builds,
+) -> query.Query:
+    """Filter intervals by genome build."""
+    return intervals.filter(interval_type.build == build)
+
+
+def create_db_gene(gene: GeneBase) -> SQLGene:
+    """Create and return a SQL gene object."""
+    return SQLGene(
+        build=gene.build.value,
+        chromosome=gene.chromosome,
+        start=gene.start,
+        stop=gene.stop,
+        ensembl_id=gene.ensembl_id,
+        hgnc_symbol=gene.hgnc_symbol,
+        hgnc_id=gene.hgnc_id,
+    )
+
+
+def bulk_insert_genes(db: Session, genes: List[GeneBase]):
+    """Bulk insert genes into the database."""
+    db.bulk_save_objects([create_db_gene(gene) for gene in genes])
+    db.commit()
+
+
+def get_genes(
+    db: Session,
+    build: Builds,
+    ensembl_ids: Optional[List[str]],
+    hgnc_ids: Optional[List[int]],
+    hgnc_symbols: Optional[List[str]],
+    limit: Optional[int],
+) -> List[SQLGene]:
+    """Return genes according to specified fields."""
+    genes: query.Query = db.query(SQLGene)
+    if ensembl_ids:
+        ensembl_ids_placeholder = ", ".join(f"'{e}'" for e in ensembl_ids)
+        genes = genes.filter(
+            text(
+                f"""
+                EXISTS (
+                    SELECT 1
+                    FROM json_each(genes.ensembl_ids)
+                    WHERE value IN ({ensembl_ids_placeholder})
+                )
+                """
+            )
+        )
+    elif hgnc_ids:
+        genes: query.Query = genes.filter(SQLGene.hgnc_id.in_(hgnc_ids))
+    elif hgnc_symbols:
+        genes: query.Query = genes.filter(SQLGene.hgnc_symbol.in_(hgnc_symbols))
+
+    genes: query.Query = _filter_intervals_by_build(
+        intervals=genes, interval_type=SQLGene, build=build
+    )
+    if limit:
+        return genes.limit(limit).all()
+    return genes.all()
+
+
+def create_db_transcript(transcript: TranscriptBase) -> SQLTranscript:
+    """Create and return a SQL transcript object."""
+
+    return SQLTranscript(
+        chromosome=transcript.chromosome,
+        start=transcript.start,
+        stop=transcript.stop,
+        ensembl_id=transcript.ensembl_id,
+        ensembl_gene_id=transcript.ensembl_gene_id,
+        refseq_mrna=transcript.refseq_mrna,
+        refseq_mrna_pred=transcript.refseq_mrna_pred,
+        refseq_ncrna=transcript.refseq_ncrna,
+        refseq_mane_select=transcript.refseq_mane_select,
+        refseq_mane_plus_clinical=transcript.refseq_mane_plus_clinical,
+        build=transcript.build,
+    )
+
+
+def bulk_insert_transcripts(db: Session, transcripts: List[TranscriptBase]):
+    """Bulk insert transcripts into the database."""
+    db.bulk_save_objects(
+        [create_db_transcript(transcript=transcript) for transcript in transcripts]
+    )
+    db.commit()
+
+
+def get_hgnc_gene(db: Session, build: Builds, hgnc_id: int) -> SQLGene:
+    """Return a gene object by its HGNC ID."""
+    gene_query: query.Query = (
+        db.query(SQLGene)
+        .filter(SQLGene.hgnc_id == hgnc_id)
+        .filter(SQLGene.build == build)
+    )
+    return gene_query.first()
+
+
+def _filter_transcripts_by_tag(
+    transcripts: query.Query, transcript_tags: List[TranscriptTag] = []
+) -> query.Query:
+    """Return transcripts which contain one or more RefSeq tag."""
+
+    not_null_filters = []
+    for tag in transcript_tags:
+        not_null_filters.append(getattr(SQLTranscript, tag).isnot(None))
+    return transcripts.filter(or_(*not_null_filters))
+
+
+def set_sql_intervals(
+    db: Session,
+    interval_type: Union[SQLExon, SQLGene, SQLTranscript],
+    genes: List[SQLGene],
+    transcript_tags=Optional[List[TranscriptTag]],
+) -> List[Union[SQLGene, SQLTranscript, SQLExon]]:
+    """If SQL intervals are genes return them as they are, otherwise return genes, transcripts or exons."""
+    if interval_type == SQLGene:
+        sql_intervals: List[SQLGene] = genes
+    else:
+        sql_intervals: List[Union[SQLTranscript, SQLExon]] = get_gene_intervals(
+            db=db,
+            build=genes[0].build,
+            interval_type=interval_type,
+            ensembl_ids=None,
+            hgnc_ids=None,
+            hgnc_symbols=None,
+            ensembl_gene_ids=[
+                ensembl_id for gene in genes for ensembl_id in gene.ensembl_ids
+            ],
+            limit=None,
+            transcript_tags=transcript_tags,
+        )
+    return sql_intervals
+
+
+def get_gene_intervals(
+    db: Session,
+    build: Builds,
+    interval_type: Union[SQLTranscript, SQLExon],
+    ensembl_ids: Optional[List[str]] = [],
+    hgnc_ids: Optional[List[int]] = [],
+    hgnc_symbols: Optional[List[str]] = [],
+    ensembl_gene_ids: Optional[List[str]] = [],
+    limit: Optional[int] = None,
+    transcript_tags: Optional[List[TranscriptTag]] = [],
+) -> List[Union[SQLTranscript, SQLExon]]:
+    """Retrieve transcripts or exons from a list of genes."""
+
+    intervals = db.query(interval_type).filter(interval_type.build == build)
+
+    def get_ensembl_gene_ids_from_gene_filter(
+        filter_value: List[Union[str, int]], filter_column: str
+    ) -> List[str]:
+        """Helper function to get ensembl_gene_ids from either hgnc_ids or hgnc_symbols."""
+        genes = (
+            db.query(SQLGene.ensembl_ids).filter(filter_column.in_(filter_value)).all()
+        )
+        return [ensembl_id for gene in genes for ensembl_id in gene.ensembl_ids]
+
+    if ensembl_ids:
+        intervals: query.Query = intervals.filter(
+            interval_type.ensembl_id.in_(ensembl_ids)
+        )
+    elif hgnc_ids:
+        ensembl_gene_ids = get_ensembl_gene_ids_from_gene_filter(
+            hgnc_ids, SQLGene.hgnc_id
+        )
+    elif hgnc_symbols:
+        ensembl_gene_ids = get_ensembl_gene_ids_from_gene_filter(
+            hgnc_symbols, SQLGene.hgnc_symbol
+        )
+    if ensembl_gene_ids:
+        intervals = intervals.filter(
+            interval_type.ensembl_gene_id.in_(ensembl_gene_ids)
+        )
+
+    if interval_type == SQLTranscript and transcript_tags:
+        intervals = _filter_transcripts_by_tag(
+            transcripts=intervals, transcript_tags=transcript_tags
+        )
+
+    if limit:
+        return intervals.limit(limit).all()
+
+    return intervals.all()
+
+
+def create_db_exon(exon: ExonBase) -> SQLExon:
+    """Create and return a SQL exon object."""
+
+    return SQLExon(
+        chromosome=exon.chromosome,
+        start=exon.start,
+        stop=exon.stop,
+        rank_in_transcript=exon.rank_in_transcript,
+        ensembl_id=exon.ensembl_id,
+        ensembl_gene_id=exon.ensembl_gene_id,
+        ensembl_transcript_id=exon.ensembl_transcript_id,
+        build=exon.build,
+    )
+
+
+def bulk_insert_exons(db: Session, exons: List[ExonBase]) -> None:
+    """Bulk insert exons into the database."""
+    db.bulk_save_objects([create_db_exon(exon=exon) for exon in exons])
+    db.commit()
---
+++ packages/chanjo2/chanjo2-3.1.0.tar.gz/chanjo2-3.1.0/src/chanjo2/meta/handle_load_intervals.py
@@ -0,0 +1,257 @@
+import logging
+from typing import Iterator, List, Optional, Union
+
+import requests
+from schug.load.biomart import EnsemblBiomartClient
+from schug.models.common import Build as SchugBuild
+from sqlalchemy.orm import Session
+
+from chanjo2.constants import (
+    ENSEMBL_RESOURCE_CLIENT,
+    EXONS_FILE_HEADER,
+    GENES_FILE_HEADER,
+    TRANSCRIPTS_FILE_HEADER,
+)
+from chanjo2.crud.intervals import (
+    bulk_insert_exons,
+    bulk_insert_genes,
+    bulk_insert_transcripts,
+    count_intervals_for_build,
+    delete_intervals_for_build,
+)
+from chanjo2.models import SQLExon, SQLGene, SQLTranscript
+from chanjo2.models.pydantic_models import (
+    Builds,
+    ExonBase,
+    IntervalType,
+    TranscriptBase,
+)
+
+LOG = logging.getLogger(__name__)
+MAX_NR_OF_RECORDS = 10_000
+END_OF_PARSED_FILE: str = "[success]"
+
+
+def read_resource_lines(build: Builds, interval_type: IntervalType) -> Iterator[str]:
+    """Returns lines of a remote Ensembl Biomart resource (genes, transcripts or exons) in a given genome build."""
+
+    shug_client: EnsemblBiomartClient = ENSEMBL_RESOURCE_CLIENT[interval_type](
+        build=SchugBuild(build)
+    )
+    url: str = shug_client.build_url(xml=shug_client.xml)
+    response: requests.models.responses = requests.get(url, stream=True)
+    return response.iter_lines(decode_unicode=True)
+
+
+def _replace_empty_cols(line: str, nr_expected_columns: int) -> List[Union[str, None]]:
+    """Split line into columns, replacing empty columns with None values."""
+    cols = [
+        None if cell == "" else cell.replace("HGNC:", "") for cell in line.split("\t")
+    ]
+
+    # Make sure that expected nr of cols are returned if last cols are blank
+    cols += [None] * (nr_expected_columns - len(cols))
+    return cols
+
+
+async def update_genes(
+    build: Builds, session: Session, lines: Optional[Iterator] = None
+) -> Optional[int]:
+    """Loads genes into the database."""
+
+    def update_or_insert_gene(session, sql_gene):
+        # Try to find the gene in the database
+
+        existing_gene = (
+            session.query(SQLGene)
+            .filter_by(
+                chromosome=sql_gene.chromosome, start=sql_gene.start, stop=sql_gene.stop
+            )
+            .first()
+        )
+
+        if existing_gene:
+            # Gene exists, append the new ensembl_id to the existing ensembl_ids
+            existing_gene.ensembl_ids.append(sql_gene.ensembl_ids[0])
+        else:
+            # Gene does not exist, add a new record
+            session.add(sql_gene)
+
+    LOG.info(f"Loading gene intervals. Genome build --> {build}")
+    if lines is None:
+        lines: Iterator[str] = read_resource_lines(
+            build=build, interval_type=IntervalType.GENES
+        )
+
+    header = next(lines).split("\t")
+
+    if header != GENES_FILE_HEADER[build]:
+        raise ValueError(
+            f"Ensembl genes file has an unexpected format:{header}. Expected format: {GENES_FILE_HEADER[build]}"
+        )
+
+    for interval_type in [SQLExon, SQLTranscript, SQLGene]:
+        delete_intervals_for_build(db=session, interval_type=interval_type, build=build)
+
+    genes_bulk: List[SQLGene] = []
+
+    for line in lines:
+        if line == END_OF_PARSED_FILE:
+            break
+
+        items: List = _replace_empty_cols(line=line, nr_expected_columns=len(header))
+
+        try:
+            sql_gene = SQLGene(
+                build=build,
+                chromosome=items[0],
+                start=int(items[1]),
+                stop=int(items[2]),
+                ensembl_ids=[items[3]],
+                hgnc_symbol=items[4],
+                hgnc_id=items[5],
+            )
+
+            update_or_insert_gene(session, sql_gene)  # Update or insert the gene
+
+            if len(genes_bulk) > MAX_NR_OF_RECORDS:
+                bulk_insert_genes(db=session, genes=genes_bulk)
+                genes_bulk = []
+
+        except Exception as ex:
+            LOG.error(ex)
+            return
+
+    bulk_insert_genes(db=session, genes=genes_bulk)  # Load the remaining genes
+
+    nr_loaded_genes: int = count_intervals_for_build(
+        db=session, interval_type=SQLGene, build=build
+    )
+    LOG.info(f"{nr_loaded_genes} genes loaded into the database.")
+    return nr_loaded_genes
+
+
+async def update_transcripts(
+    build: Builds, session: Session, lines: Optional[Iterator] = None
+) -> Optional[int]:
+    """Loads transcripts into the database."""
+
+    LOG.info(f"Loading transcript intervals. Genome build --> {build}")
+
+    if lines is None:
+        lines: Iterator[str] = read_resource_lines(
+            build=build, interval_type=IntervalType.TRANSCRIPTS
+        )
+
+    header = next(lines).split("\t")
+    if header != TRANSCRIPTS_FILE_HEADER[build]:
+        raise ValueError(
+            f"Ensembl transcripts file has an unexpected format:{header}. Expected format: {TRANSCRIPTS_FILE_HEADER[build]}"
+        )
+
+    delete_intervals_for_build(db=session, interval_type=SQLTranscript, build=build)
+
+    transcripts_bulk: List[TranscriptBase] = []
+
+    for line in lines:
+        if line == END_OF_PARSED_FILE:
+            break
+
+        items: List = _replace_empty_cols(line=line, nr_expected_columns=len(header))
+
+        try:
+            transcript = TranscriptBase(
+                chromosome=items[0],
+                ensembl_gene_id=items[1],
+                ensembl_id=items[2],
+                start=int(items[3]),
+                stop=int(items[4]),
+                refseq_mrna=items[5],
+                refseq_mrna_pred=items[6],
+                refseq_ncrna=items[7],
+                refseq_mane_select=items[8] if build == Builds.build_38 else None,
+                refseq_mane_plus_clinical=(
+                    items[9] if build == Builds.build_38 else None
+                ),
+                build=build,
+            )
+            transcripts_bulk.append(transcript)
+
+            if len(transcripts_bulk) > MAX_NR_OF_RECORDS:
+                bulk_insert_transcripts(db=session, transcripts=transcripts_bulk)
+                transcripts_bulk = []
+
+        except Exception as ex:
+            LOG.error(ex)
+            return
+
+    bulk_insert_transcripts(
+        db=session, transcripts=transcripts_bulk
+    )  # Load the remaining transcripts
+
+    nr_loaded_transcripts: int = count_intervals_for_build(
+        db=session, interval_type=SQLTranscript, build=build
+    )
+    LOG.info(f"{nr_loaded_transcripts} transcripts loaded into the database.")
+    return nr_loaded_transcripts
+
+
+async def update_exons(
+    build: Builds, session: Session, lines: Optional[Iterator] = None
+) -> Optional[int]:
+    """Loads exons into the database."""
+
+    LOG.info(f"Loading exon intervals. Genome build --> {build}")
+
+    if lines is None:
+        lines: Iterator[str] = read_resource_lines(
+            build=build, interval_type=IntervalType.EXONS
+        )
+
+    header = next(lines).split("\t")
+    if header != EXONS_FILE_HEADER[build]:
+        raise ValueError(
+            f"Ensembl exons file has an unexpected format:{header}. Expected format: {EXONS_FILE_HEADER[build]}"
+        )
+
+    delete_intervals_for_build(db=session, interval_type=SQLExon, build=build)
+
+    exons_bulk: List[ExonBase] = []
+
+    for line in lines:
+        if line == END_OF_PARSED_FILE:
+            break
+
+        items: List = _replace_empty_cols(line=line, nr_expected_columns=len(header))
+
+        try:
+            # Load Exon interval into the database
+            exon = ExonBase(
+                chromosome=items[0],
+                ensembl_gene_id=items[1],
+                ensembl_transcript_id=items[2],
+                ensembl_id=items[3],
+                start=int(items[4]),
+                stop=int(items[5]),
+                rank_in_transcript=int(items[-1]),
+                build=build,
+            )
+
+            exons_bulk.append(exon)
+
+            if len(exons_bulk) > MAX_NR_OF_RECORDS:
+                bulk_insert_exons(db=session, exons=exons_bulk)
+                exons_bulk = []
+
+        except Exception as ex:
+            LOG.error(ex)
+            return
+
+    bulk_insert_exons(db=session, exons=exons_bulk)  # Load the remaining exons
+
+    nr_loaded_exons: int = count_intervals_for_build(
+        db=session, interval_type=SQLExon, build=build
+    )
+    LOG.info(f"{nr_loaded_exons} exons loaded into the database.")
+
+    return nr_loaded_exons
