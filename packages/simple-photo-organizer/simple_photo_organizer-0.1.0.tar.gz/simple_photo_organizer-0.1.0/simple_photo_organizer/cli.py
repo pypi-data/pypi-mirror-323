9f8eb91f85b3c540dd9ebe100b0bf8cbe2c890eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 14:05:02 +0000

    Add package simple-photo-organizer/0.1.0/simple_photo_organizer-0.1.0.tar.gz

---
+++ packages/simple-photo-organizer/simple_photo_organizer-0.1.0.tar.gz/simple_photo_organizer-0.1.0/simple_photo_organizer/cli.py
@@ -0,0 +1,23 @@
+import utils.helper_functions as hf
+import click
+
+@click.command()
+def main():
+
+    hf.install_exiftool()
+
+    src, dst = hf.locations()
+
+    folder_structure = hf.folder_structure()
+
+    pfd = hf.get_pfd(src)
+
+    # print(pfd)
+
+    hf.mkdir_tool(folder_structure=folder_structure, pfd=pfd, dst=dst)
+
+    hf.copy_and_rename(src=src, dst=dst, pfd=pfd, folder_structure=folder_structure)
+
+
+if __name__ == "__main__":
+    main()
