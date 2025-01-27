c55df54be85c5a02d5fee23768dbd50435a00ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:07:44 +0000

    Add package cgeniepy/0.14.8/cgeniepy-0.14.8-py3-none-any.whl

---
+++ packages/cgeniepy/cgeniepy-0.14.8-py3-none-any.whl/cgeniepy/__init__.py
@@ -0,0 +1,34 @@
+# from pint import UnitRegistry
+# import pathlib
+
+
+# ureg = UnitRegistry()
+# Q_ = ureg.Quantity
+# file_path = pathlib.Path(__file__).parent.parent / "data/context.txt"
+# ureg.load_definitions(file_path)
+
+
+from importlib.resources import files
+from cgeniepy.model import GenieModel
+from cgeniepy.ecology import EcoModel
+
+def sample_model(model_type='GenieModel', *args, **kwargs):
+    file_path=str(files('data').joinpath('sample_model'))
+    
+    if model_type == 'GenieModel':
+        model = GenieModel(file_path,*args, **kwargs)
+    elif model_type == 'EcoModel':
+        model = EcoModel(file_path,*args, **kwargs)
+    else:
+        raise ValueError('model_type must be either GenieModel or EcoModel')    
+
+    return model
+
+def load_obs(grid='worjh2'):
+    file_path=str(files('data').joinpath(grid+'_obs.nc'))
+    import xarray as xr
+    obs_data= xr.load_dataset(file_path)
+    return obs_data
+    
+
+    
