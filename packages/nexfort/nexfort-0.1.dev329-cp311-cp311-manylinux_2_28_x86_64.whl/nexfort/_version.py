cf14414956a306cf5d036e73dbb59e7cdc0e00f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:07:41 +0000

    Add package nexfort/0.1.dev329/nexfort-0.1.dev329-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/nexfort/nexfort-0.1.dev329-cp311-cp311-manylinux_2_28_x86_64.whl/nexfort/_version.py
@@ -0,0 +1,12 @@
+TYPE_CHECKING = False
+if TYPE_CHECKING:
+    from typing import Tuple, Union
+    VERSION_TUPLE = Tuple[Union[int, str], ...]
+else:
+    VERSION_TUPLE = object
+version: str
+__version__: str
+__version_tuple__: VERSION_TUPLE
+version_tuple: VERSION_TUPLE
+__version__ = version = '0.1.dev329'
+__version_tuple__ = version_tuple = (0, 1, 'dev329')
