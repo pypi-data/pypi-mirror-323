9960746caf2104a0c3d3597696cb9ca1070978a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 04:06:10 +0000

    Add package django-tailwind-4/0.1.4/django_tailwind_4-0.1.4.tar.gz

---
+++ packages/django-tailwind-4/django_tailwind_4-0.1.4.tar.gz/django_tailwind_4-0.1.4/tests/test_templatetags.py
@@ -0,0 +1,96 @@
+import time
+
+from django.template import Context, Template
+
+
+def test_tailwind_css_in_production(settings):
+    settings.TAILWIND_APP_NAME = "theme"
+    output = Template(
+        """
+        {% load tailwind_tags %}
+        {% tailwind_css %}
+        """
+    ).render(Context({}))
+
+    assert '<link rel="stylesheet" href="/static/css/dist/styles.css">' in output
+    assert "browser-sync/browser-sync-client.js" not in output
+
+
+def test_tailwind_css_in_production_with_version(settings):
+    settings.TAILWIND_APP_NAME = "theme"
+    output = Template(
+        """
+        {% load tailwind_tags %}
+        {% tailwind_css v=123 %}
+        """
+    ).render(Context({}))
+
+    assert '<link rel="stylesheet" href="/static/css/dist/styles.css?v=123">' in output
+    assert "browser-sync/browser-sync-client.js" not in output
+
+
+def test_tailwind_css_in_debug(settings):
+    settings.TAILWIND_APP_NAME = "theme"
+    settings.DEBUG = True
+    partial_time_version = str(int(time.time()))[:7]
+
+    output = Template(
+        """
+        {% load tailwind_tags %}
+        {% tailwind_css %}
+        """
+    ).render(Context({}))
+
+    assert f'<link rel="stylesheet" href="/static/css/dist/styles.css?v={partial_time_version}' in output
+    assert "browser-sync/browser-sync-client.js" not in output
+
+
+def test_tailwind_css_in_legacy_tailwind_dev_mode(settings):
+    settings.TAILWIND_APP_NAME = "theme"
+    settings.TAILWIND_DEV_MODE = True
+
+    output = Template(
+        """
+        {% load tailwind_tags %}
+        {% tailwind_css %}
+        """
+    ).render(Context({}))
+
+    assert '<link rel="stylesheet" href="/static/css/dist/styles.css">' in output
+    assert "//HOST:8383/browser-sync/browser-sync-client.js" in output
+
+
+def test_tailwind_css_in_debug_with_version(settings):
+    settings.TAILWIND_APP_NAME = "theme"
+    output = Template(
+        """
+        {% load tailwind_tags %}
+        {% tailwind_css v=123 %}
+        """
+    ).render(Context({}))
+
+    assert '<link rel="stylesheet" href="/static/css/dist/styles.css?v=123">' in output
+
+
+def test_tailwind_preload_css(settings):
+    settings.TAILWIND_APP_NAME = "theme"
+    output = Template(
+        """
+        {% load tailwind_tags %}
+        {% tailwind_preload_css %}
+        """
+    ).render(Context())
+
+    assert '<link rel="preload" href="/static/css/dist/styles.css" as="style">' in output
+
+
+def test_tailwind_preload_css_with_version(settings):
+    settings.TAILWIND_APP_NAME = "theme"
+    output = Template(
+        """
+        {% load tailwind_tags %}
+        {% tailwind_preload_css v=123 %}
+        """
+    ).render(Context())
+
+    assert '<link rel="preload" href="/static/css/dist/styles.css?v=123" as="style">' in output
