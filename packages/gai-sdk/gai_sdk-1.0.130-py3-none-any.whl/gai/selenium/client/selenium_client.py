63623e09a44b7760dc8802cbefe92d14b3e35eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 16:05:29 +0000

    Add package gai-sdk/1.0.130/gai_sdk-1.0.130-py3-none-any.whl

---
+++ packages/gai-sdk/gai_sdk-1.0.130-py3-none-any.whl/gai/selenium/client/selenium_client.py
@@ -0,0 +1,35 @@
+from gai.lib.common.http_utils import http_post_async, http_get_async
+from ..dtos import SearchRequest, SearchResponse, ScrapeRequest, ParsedResponse, CrawlRequest
+
+class SeleniumClient:
+    
+    async def search(self, query:str, n_results:int=10, period=None) -> SearchResponse:
+        # curl -X POST "http://localhost:12028/api/v1/googler" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"query\":\"python\",\"n_results\":5}"
+        
+        url = 'http://localhost:12028/api/v1/search'
+        params = SearchRequest(query=query, n_results=n_results, period=period).model_dump(exclude_none=True)
+        results = await http_post_async(url, data=params)
+        return SearchResponse(**results.json())
+    
+    async def scrape(self, root_url:str) -> ParsedResponse:
+        # curl -X POST "http://localhost:12028/api/v1/scrape" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"url\":\"https://www.bbc.com/news/world-europe-60575619\"}"
+        
+        url = 'http://localhost:12028/api/v1/scrape'
+        params = ScrapeRequest(url=root_url).model_dump(exclude_none=True)
+        results = await http_post_async(url, data=params)
+        return results.json()
+    
+    async def crawl(self, root_url:str, max_depth:int, max_count:int, include_external:bool=False):
+        # curl -X POST "http://localhost:12028/api/v1/crawl" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"root_url\":\"https://www.bbc.com/news/world-europe-60575619\",\"max_depth\":2,\"max_count\":5,\"include_external\":false}"
+        
+        url = 'http://localhost:12028/api/v1/crawl'
+        params = CrawlRequest(root_url=root_url, max_depth=max_depth, max_count=max_count, include_external=include_external).model_dump(exclude_none=True)
+        results = await http_post_async(url, data=params)
+        return results.json()
+    
+    async def get_crawl_job(self, job_id:str):
+        # curl -X GET "http://localhost:12028/api/v1/crawl/job/1" -H  "accept: application/json"
+        
+        url = f'http://localhost:12028/api/v1/crawl/job/{job_id}'
+        results = await http_get_async(url)
+        return results.json()
