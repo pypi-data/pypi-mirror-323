f008efaa6ad9b79f21cbbd7add215b005296796d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 04:05:30 +0000

    Add package Wsgi-15Watt/0.9.6/wsgi_15watt-0.9.6.tar.gz

---
+++ packages/Wsgi-15Watt/wsgi_15watt-0.9.6.tar.gz/wsgi_15watt-0.9.6/src/Wsgi-15Watt/__init__.py
@@ -0,0 +1,11 @@
+"""
+	Wsgi, fast simple and easy to use WSGI framework.
+
+	GIT: https://github.com/django15wattnet/Wsgi-15Watt
+
+	PIP: https://pypi.org/project/Wsgi-15Watt/
+
+	Homepage: https://wsgi.15watt.net/
+
+	EMail: pypi@15watt.net
+"""
