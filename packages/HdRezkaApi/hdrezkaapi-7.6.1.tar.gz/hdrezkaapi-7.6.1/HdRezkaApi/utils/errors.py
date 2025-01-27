fc8985ac255b3200410f547b02202b655a657b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 05:04:55 +0000

    Add package HdRezkaApi/7.6.1/hdrezkaapi-7.6.1.tar.gz

---
+++ packages/HdRezkaApi/hdrezkaapi-7.6.1.tar.gz/hdrezkaapi-7.6.1/HdRezkaApi/utils/errors.py
@@ -0,0 +1,11 @@
+class LoginRequiredError(Exception):
+	def __init__(self): super().__init__("Login is required to access this page.")
+
+class LoginFailed(Exception):
+	def __init__(self, msg): super().__init__(msg)
+
+class FetchFailed(Exception):
+	def __init__(self): super().__init__("Failed to fetch stream!")
+
+class HTTP(Exception):
+	def __init__(self, code, message=""): super().__init__(f"{code}: {message}")
