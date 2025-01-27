38261ab8a364654ec02e4b7696361efa010fdc8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:06:25 +0000

    Add package cdo-sdk-python/1.5.99/cdo_sdk_python-1.5.99-py3-none-any.whl

---
+++ packages/cdo-sdk-python/cdo_sdk_python-1.5.99-py3-none-any.whl/cdo-sdk-python/api/__init__.py
@@ -0,0 +1,25 @@
+# flake8: noqa
+
+# import apis into api package
+from cdo-sdk-python.api.ai_assistant_api import AIAssistantApi
+from cdo-sdk-python.api.asa_access_rules_api import ASAAccessRulesApi
+from cdo-sdk-python.api.access_groups_api import AccessGroupsApi
+from cdo-sdk-python.api.audit_logs_api import AuditLogsApi
+from cdo-sdk-python.api.change_requests_api import ChangeRequestsApi
+from cdo-sdk-python.api.changelogs_api import ChangelogsApi
+from cdo-sdk-python.api.cloud_delivered_fmc_api import CloudDeliveredFMCApi
+from cdo-sdk-python.api.command_line_interface_api import CommandLineInterfaceApi
+from cdo-sdk-python.api.commands_api import CommandsApi
+from cdo-sdk-python.api.connectors_api import ConnectorsApi
+from cdo-sdk-python.api.device_upgrades_api import DeviceUpgradesApi
+from cdo-sdk-python.api.inventory_api import InventoryApi
+from cdo-sdk-python.api.msp_api import MSPApi
+from cdo-sdk-python.api.meta_api import MetaApi
+from cdo-sdk-python.api.object_management_api import ObjectManagementApi
+from cdo-sdk-python.api.remote_access_monitoring_api import RemoteAccessMonitoringApi
+from cdo-sdk-python.api.search_api import SearchApi
+from cdo-sdk-python.api.tenant_management_api import TenantManagementApi
+from cdo-sdk-python.api.transactions_api import TransactionsApi
+from cdo-sdk-python.api.users_api import UsersApi
+from cdo-sdk-python.api.swagger_redirect_controller_api import SwaggerRedirectControllerApi
+
