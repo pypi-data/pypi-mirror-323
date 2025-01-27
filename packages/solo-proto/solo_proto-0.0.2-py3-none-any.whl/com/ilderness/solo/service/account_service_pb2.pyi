94cbc1a9db02d1ebaa1e9d305f0ac3d158e0ef47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:06:21 +0000

    Add package solo-proto/0.0.2/solo_proto-0.0.2-py3-none-any.whl

---
+++ packages/solo-proto/solo_proto-0.0.2-py3-none-any.whl/com/ilderness/solo/service/account_service_pb2.pyi
@@ -0,0 +1,18 @@
+from com.ilderness.solo.model import account_pb2 as _account_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class AccountRq(_message.Message):
+    __slots__ = ("account",)
+    ACCOUNT_FIELD_NUMBER: _ClassVar[int]
+    account: _account_pb2.Account
+    def __init__(self, account: _Optional[_Union[_account_pb2.Account, _Mapping]] = ...) -> None: ...
+
+class AccountRs(_message.Message):
+    __slots__ = ("account",)
+    ACCOUNT_FIELD_NUMBER: _ClassVar[int]
+    account: _account_pb2.Account
+    def __init__(self, account: _Optional[_Union[_account_pb2.Account, _Mapping]] = ...) -> None: ...
