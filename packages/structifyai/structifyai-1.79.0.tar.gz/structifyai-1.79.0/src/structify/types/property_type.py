b3f664b1ca87e8c1b316010387150fa886999d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 14:04:34 +0000

    Add package structifyai/1.79.0/structifyai-1.79.0.tar.gz

---
+++ packages/structifyai/structifyai-1.79.0.tar.gz/structifyai-1.79.0/src/structify/types/property_type.py
@@ -0,0 +1,27 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Union
+from typing_extensions import Literal, TypeAlias
+
+from pydantic import Field as FieldInfo
+
+from .._models import BaseModel
+
+__all__ = ["PropertyType", "Enum"]
+
+
+class Enum(BaseModel):
+    enum: List[str] = FieldInfo(alias="Enum")
+
+
+PropertyType: TypeAlias = Union[
+    Literal["String"],
+    Literal["Boolean"],
+    Enum,
+    Literal["Integer"],
+    Literal["Float"],
+    Literal["Date"],
+    Literal["URL"],
+    Literal["Money"],
+    Literal["Image"],
+]
