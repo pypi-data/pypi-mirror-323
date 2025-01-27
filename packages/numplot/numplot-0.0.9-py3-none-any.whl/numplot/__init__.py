f618c2200135811df4ab634d5a941eba6e026046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:07:53 +0000

    Add package numplot/0.0.9/numplot-0.0.9-py3-none-any.whl

---
+++ packages/numplot/numplot-0.0.9-py3-none-any.whl/numplot/__init__.py
@@ -0,0 +1,21 @@
+from .task_1 import t_1
+from .task_2 import t_2
+from .task_3 import t_3
+from .task_4 import t_4
+from .task_5_1 import t_5_1
+from .task_5_2 import t_5_2
+from .task_5_3 import t_5_3
+
+__all__ = ['t_1', 't_2', 't_3', 't_4', 't_5_1', 't_5_2', 't_5_3']
+
+def h():
+    text = """
+t_1 (1, 2, 3)
+t_2 (1, 2)
+t_3
+t_4
+t_5 
+      _1 (1, 2)
+      _2 (1)
+      _3 (1, 2, 3)"""
+    return text
---
+++ packages/numplot/numplot-0.0.9-py3-none-any.whl/numplot/task_5.py
@@ -0,0 +1,5 @@
+def task_5():
+    text = """
+    task_5"""
+
+    return text
