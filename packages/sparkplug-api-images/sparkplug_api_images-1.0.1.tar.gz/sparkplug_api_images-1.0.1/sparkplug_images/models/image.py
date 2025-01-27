35edf6a0a40f715c7fb9daf3dfa9485de8143ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 15:05:11 +0000

    Add package sparkplug-api-images/1.0.1/sparkplug_api_images-1.0.1.tar.gz

---
+++ packages/sparkplug-api-images/sparkplug_api_images-1.0.1.tar.gz/sparkplug_api_images-1.0.1/sparkplug_images/models/image.py
@@ -0,0 +1,38 @@
+# django
+from django.conf import settings
+from django.db import models
+
+# contrib
+from sorl.thumbnail import ImageField
+
+# sparkplug
+from sparkplug_core.models import BaseModel
+
+# app
+from .. import managers, uploads
+
+
+class Image(
+    BaseModel,
+):
+    creator = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="+",
+    )
+
+    file = ImageField(
+        upload_to=uploads.file_location,
+    )
+
+    objects = managers.ImageManager()
+
+    class Meta:
+        indexes = (models.Index(fields=["uuid"]),)
+
+    def __str__(self) -> str:
+        return self.uuid
+
+    def delete(self, *args, **kwargs) -> None:
+        self.file.delete()
+        super().delete(*args, **kwargs)
