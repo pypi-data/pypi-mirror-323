9b2c08713a336320718504c49427302ec185770e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 14:04:38 +0000

    Add package edmartzholamundoplayer/0.0.1/edmartzholamundoplayer-0.0.1-py3-none-any.whl

---
+++ packages/edmartzholamundoplayer/edmartzholamundoplayer-0.0.1-py3-none-any.whl/edmartzholamundoplayer/player.py
@@ -0,0 +1,23 @@
+"""
+Este modulo incluye la clase de de reproductor de musica
+"""
+
+
+class Player:
+    """
+    Esta clase crea un reproductor de musica
+    """
+
+    def play(self, song):
+        """
+        Reproduce la cancion que recibio en el constructor
+
+        Parameters:
+        song (str): este es un string con el path de la cancion
+
+        Returns:
+        Devuelve 1 si reproduce con exito, en caso de fracaso devuelve 0
+        """
+
+    def stop(self):
+        print("Stopping")
