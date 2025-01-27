7af3b55ab7efd86c81b92946ef9516f9e8b99baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 14:04:34 +0000

    Add package hcoop-meetbot/0.6.15/hcoop_meetbot-0.6.15-py3-none-any.whl

---
+++ packages/hcoop-meetbot/hcoop_meetbot-0.6.15-py3-none-any.whl/hcoopmeetbotlogic/release.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# vim: set ft=python ts=4 sw=4 expandtab:
+
+"""
+Version and release information.
+"""
+
+AUTHOR = "Kenneth J. Pronovici"
+EMAIL = "pronovic@ieee.org"
+COPYRIGHT = "2021-2022"
+VERSION = "0.6.2"
+DATE = "05 Sep 2022"
+URL = "https://github.com/pronovic/hcoop-meetbot"
+DOCS = "https://hcoop-meetbot.readthedocs.io/en/stable/"
