ba2e1a5b2f04cb71657ac58431c519b11d810627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 15:05:04 +0000

    Add package lucidicai/0.1.1/lucidicai-0.1.1-py3-none-any.whl

---
+++ packages/lucidicai/lucidicai-0.1.1-py3-none-any.whl/lucidicai/api.py
@@ -0,0 +1,25 @@
+import requests
+
+class LucidicAPI:
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.headers = {"Authorization": f"Api-Key {self.api_key}"}
+        self.base_url = "https://dashboard.lucidic.ai/demo/api/v1"
+        self.endpoints = {
+            "verifyAPIKey": "testendpoint",
+        }
+    def verifyAPIKey(self, testprompt="Tell me a joke"):
+        url = f'{self.base_url}/{self.endpoints["verifyAPIKey"]}'
+        try:
+            response = requests.get(
+                url,
+                headers=self.headers,
+                params={
+                    'prompt': testprompt,
+                }
+            )
+            response.raise_for_status()
+            return response.json()['response']
+        except Exception as e:
+            print(f"Error during API Call: {e}")
+            raise
