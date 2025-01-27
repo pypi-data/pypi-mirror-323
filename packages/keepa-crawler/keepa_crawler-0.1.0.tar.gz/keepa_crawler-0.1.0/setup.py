1871a18821546abb82bcdd08358b12ec6c5b05c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:07:42 +0000

    Add package keepa-crawler/0.1.0/keepa_crawler-0.1.0.tar.gz

---
+++ packages/keepa-crawler/keepa_crawler-0.1.0.tar.gz/keepa_crawler-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="keepa_crawler",
+    version="0.1.0",
+    package_dir={"keepa_crawler": "src"},
+    packages=["keepa_crawler"],
+    install_requires=[
+        'curl-cffi>=0.5.8',
+    ],
+    description="A client to crawl Keepa's historical Amazon product data",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Stan van Rooy",
+    author_email="stanvanrooy6@gmail.com",
+    url="https://github.com/stanvanrooy/keepa_crawler",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
