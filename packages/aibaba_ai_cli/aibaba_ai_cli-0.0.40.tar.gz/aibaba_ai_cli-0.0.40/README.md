1d81b0a3cb1262f20c94aa7e0b93eb7a29cc341d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 04:06:11 +0000

    Add package aibaba_ai_cli/0.0.40/aibaba_ai_cli-0.0.40.tar.gz

---
+++ packages/aibaba_ai_cli/aibaba_ai_cli-0.0.40.tar.gz/aibaba_ai_cli-0.0.40/README.md
@@ -0,0 +1,6 @@
+# aibaba_ai_cli
+
+This package implements the official CLI for Aibaba AI. Right now, it is most useful
+for getting started with Aibaba AI Templates!
+
+[CLI Docs](https://github.com/aibaba-ai/aibaba-ai/blob/master/libs/cli/DOCS.md)
---
+++ packages/aibaba_ai_cli/aibaba_ai_cli-0.0.40.tar.gz/aibaba_ai_cli-0.0.40/aibaba_ai_cli/package_template/package_template/chain.py
@@ -0,0 +1,17 @@
+from aibaba_ai_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant who speaks like a pirate",
+        ),
+        ("human", "{text}"),
+    ]
+)
+_model = ChatOpenAI()
+
+# if you update this, you MUST also update ../pyproject.toml
+# with the new `tool.aibaba_ai.export_attr`
+chain = _prompt | _model
