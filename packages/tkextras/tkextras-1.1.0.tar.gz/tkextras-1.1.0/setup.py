e9a4cbdcc2a51dd970da02e5f83040e960fc0705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 14:04:34 +0000

    Add package tkextras/1.1.0/tkextras-1.1.0.tar.gz

---
+++ packages/tkextras/tkextras-1.1.0.tar.gz/tkextras-1.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tkextras",
+    version="1.1.0",
+    description="A Tkinter utility module for enhanced widgets rendering",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Dietmar Steinle",
+    license="MIT",
+    url="https://github.com/whellcome/tkextras",
+    download_url='https://github.com/whellcome/tkextras/archive/master.zip',
+    project_urls={
+        "Documentation": "https://tkextras.readthedocs.io/",
+        "Source Code": "https://github.com/whellcome/tkextras/tree/master/tkextras",
+        "Examples": "https://github.com/whellcome/tkextras/tree/master/examples",
+    },
+    packages=find_packages(),
+    keywords=["tkinter", "ttk", "gui"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Tcl Extensions",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    install_requires=[
+        "pandas>=1.0",
+    ],
+    python_requires=">=3.9",
+)
