ae1b580e982570c901ce8de7fe3bcd91ec3f559f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:07:49 +0000

    Add package earningscall/1.1.0/earningscall-1.1.0.tar.gz

---
+++ packages/earningscall/earningscall-1.1.0.tar.gz/earningscall-1.1.0/scripts/get_single_transcript.py
@@ -0,0 +1,21 @@
+import earningscall  # noqa: F401
+
+from earningscall import get_company
+
+
+# TODO: Set your API key here:
+# earningscall.api_key = "YOUR SECRET API KEY GOES HERE"
+
+
+company = get_company("aapl")
+
+transcript = company.get_transcript(year=2021, quarter=3, level=2)
+
+speaker = transcript.speakers[1]
+speaker_label = speaker.speaker_info.name
+text = speaker.text
+print("Speaker:")
+print(f"  Name: {speaker.speaker_info.name}")
+print(f"  Title: {speaker.speaker_info.title}")
+print()
+print(f"Text: {text}")
