2220e60636825d10c7c97dd75b30a0664d263b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 15:05:24 +0000

    Add package openly-core/0.2.2/openly_core-0.2.2.tar.gz

---
+++ packages/openly-core/openly_core-0.2.2.tar.gz/openly_core-0.2.2/openly/devices/leak.py
@@ -0,0 +1,26 @@
+from openly.devices.base_device import BaseDevice
+from openly.exceptions import InvalidParametersError
+
+
+class LeakSensor(BaseDevice):
+    modes = ["yes", "no"]
+
+    def __init__(self, device_id: str | int, device_data: dict = {}) -> None:
+        super().__init__(device_id, device_data)
+
+        if hasattr(self, "status"):
+            self.mode = self.status.get("leakage", "no")
+
+    @property
+    def battery(self) -> int:
+        return self.status.get("battery", 0)
+
+    @property
+    def mode(self) -> str:
+        return self.status.get("leakage", "no")
+
+    @mode.setter
+    def mode(self, mode: str) -> None:
+        if mode not in self.modes:
+            raise InvalidParametersError("Invalid mode")
+        self.status["leakage"] = mode
