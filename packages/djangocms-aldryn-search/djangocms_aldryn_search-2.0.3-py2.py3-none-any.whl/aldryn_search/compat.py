98edf7c5c982cb7a760636c2f2d81060e535ebca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 16:37:50 +0000

    Add package djangocms-aldryn-search/2.0.3/djangocms_aldryn_search-2.0.3-py2.py3-none-any.whl

---
+++ packages/djangocms-aldryn-search/djangocms_aldryn_search-2.0.3-py2.py3-none-any.whl/aldryn_search/compat.py
@@ -0,0 +1,13 @@
+import cms
+
+from looseversion import LooseVersion
+
+
+GTE_CMS_35 = LooseVersion(cms.__version__) >= LooseVersion('3.5')
+
+
+def is_authenticated(user):
+    try:
+        return user.is_authenticated()  # Django<1.10
+    except TypeError:
+        return user.is_authenticated  # Django>=1.10
