6076bf7d33d344db8c95045f8dc0ee07c61b7794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 16:05:28 +0000

    Add package papertools/1.14/papertools-1.14.tar.gz

---
+++ packages/papertools/papertools-1.14.tar.gz/papertools-1.14/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='papertools',
+    version='1.14',
+    packages=find_packages(),
+    author='PaperTarsier692',
+    url='https://www.github.com/PaperTarsier692/papertools',
+    long_description='Small collection of various Python tools.', long_description_content_type='text/markdown'
+)
