7ddb455be9d2abc839915642d4d17107b0a82fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:07:42 +0000

    Add package VectorCode/0.2.3/vectorcode-0.2.3.tar.gz

---
+++ packages/VectorCode/vectorcode-0.2.3.tar.gz/vectorcode-0.2.3/src/vectorcode/subcommands/__init__.py
@@ -0,0 +1,8 @@
+from vectorcode.subcommands.check import check
+from vectorcode.subcommands.drop import drop
+from vectorcode.subcommands.init import init
+from vectorcode.subcommands.ls import ls
+from vectorcode.subcommands.query import query
+from vectorcode.subcommands.vectorise import vectorise
+
+__all__ = ["drop", "init", "query", "ls", "vectorise", "check"]
