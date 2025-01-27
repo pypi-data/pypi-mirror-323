b7a0b4a3ff4ab21d44cf7a1d1be3cf9c547a204b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:06:52 +0000

    Add package smart-calculator/0.1/smart_calculator-0.1.tar.gz

---
+++ packages/smart-calculator/smart_calculator-0.1.tar.gz/smart_calculator-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smart_calculator",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # none for this version
+    ],
+    author="Chukwunonso Smart Agbawo",
+    author_email= "chukwunonsosmartagbawo@gmail.com",
+    maintainer="Chukwunonso Smart Agbawo",
+    maintainer_email=  "chukwunonsosmartagbawo@gmail.com",
+)
