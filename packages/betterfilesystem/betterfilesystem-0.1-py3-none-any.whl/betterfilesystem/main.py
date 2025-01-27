c2de581180e6e11d05aa6d886ae5362c8edf5af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 15:04:59 +0000

    Add package betterfilesystem/0.1/betterfilesystem-0.1-py3-none-any.whl

---
+++ packages/betterfilesystem/betterfilesystem-0.1-py3-none-any.whl/betterfilesystem/main.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+def createfile(filename, filecontent):
+    with open(filename, "w") as f:
+        f.write(filecontent)
+
+def deletefile(filepath):
+    if os.path.exists(filepath):
+        os.remove(filepath)
+    else:
+        print(f"betterfilesystemerr-file {filepath} not found")
+
+def readfile(filepath):
+    filecontent = ""
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as f:
+            filecontent = f.read()
+    return filecontent
+
+def editfile(filename, filecontent):
+    createfile(filename, filecontent)
+
+def move(currentpath, movedpath):
+    if not os.path.exists(movedpath):
+        try:
+            os.mkdir(os.path.dirname(movedpath))
+            os.rename(currentpath, movedpath)
+        except FileExistsError:
+            os.rename(currentpath, movedpath)
+
+def create_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    else:
+        print("betterfilesystemerr-directory or file already there.")
+
+def delete_dir(directory):
+    if not os.path.exists(directory):
+        print("betterfilesystemerr-directory or file was not found.")
+    else:
+        shutil.rmtree(directory)
