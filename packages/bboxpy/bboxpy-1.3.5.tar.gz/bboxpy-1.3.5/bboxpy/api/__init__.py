1f9e2fed865d7837a3a59ed7f637469d64a69eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:07:49 +0000

    Add package bboxpy/1.3.5/bboxpy-1.3.5.tar.gz

---
+++ packages/bboxpy/bboxpy-1.3.5.tar.gz/bboxpy-1.3.5/bboxpy/api/__init__.py
@@ -0,0 +1,25 @@
+"""Provides API access to Bouygues Bbox."""
+
+from .ddns import Ddns
+from .device import Device
+from .iptv import IPTv
+from .lan import Lan
+from .parentalcontrol import ParentalControl
+from .remote import Remote
+from .services import Services
+from .voip import VOIP
+from .wan import Wan
+from .wifi import Wifi
+
+__all__ = [
+    "Ddns",
+    "Device",
+    "IPTv",
+    "Lan",
+    "ParentalControl",
+    "VOIP",
+    "Wan",
+    "Wifi",
+    "Services",
+    "Remote",
+]
