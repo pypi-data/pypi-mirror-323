a317907e5f4cb7e853f84f0606b987d684755c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 05:05:21 +0000

    Add package sklearn11/0.0.5/sklearn11-0.0.5.tar.gz

---
+++ packages/sklearn11/sklearn11-0.0.5.tar.gz/sklearn11-0.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sklearn11",
+    version="0.0.5",
+    author="olcay",
+    author_email="olcay.aydn25@gmail.com",
+    description="100 alcaz",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ozbere31/sklearn11",
+    packages=find_packages(),
+    include_package_data=True,  # Bu satırı ekleyin
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
