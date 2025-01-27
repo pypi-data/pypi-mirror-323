2b9b6d15c503a109d313c3985511a2d347025d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:08:01 +0000

    Add package zns-logging/1.0.2/zns_logging-1.0.2.tar.gz

---
+++ packages/zns-logging/zns_logging-1.0.2.tar.gz/zns_logging-1.0.2/tests/Test1.py
@@ -0,0 +1,9 @@
+from zns_logging import get_logger
+
+logger = get_logger(__name__, logging_level="DEBUG")
+
+logger.debug("This is a debug message")
+logger.info("This is an info message")
+logger.warning("This is a warning message")
+logger.error("This is an error message")
+logger.critical("This is a critical message")
