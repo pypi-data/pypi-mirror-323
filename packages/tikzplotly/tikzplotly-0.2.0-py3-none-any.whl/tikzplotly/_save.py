ce61c18f7effbd041e6af771de05f487131ca122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 04:05:26 +0000

    Add package tikzplotly/0.2.0/tikzplotly-0.2.0-py3-none-any.whl

---
+++ packages/tikzplotly/tikzplotly-0.2.0-py3-none-any.whl/tikzplotly/_save.py
@@ -0,0 +1,147 @@
+from pathlib import Path
+from .__about__ import __version__
+from ._tex import *
+from ._scatter import draw_scatter2d
+from ._heatmap import draw_heatmap
+from ._histogram import draw_histogram
+from ._axis import Axis
+from ._color import *
+from ._annotations import str_from_annotation
+from ._dataContainer import DataContainer
+from ._utils import sanitize_TeX_text
+from warnings import warn
+import re
+
+def get_tikz_code(
+        fig,
+        tikz_options = None,
+        axis_options = None,
+        include_disclamer = True,
+        img_name = "heatmap.png",
+    ):
+    """Get the tikz code of a figure.
+
+    Parameters
+    ----------
+    fig
+        Plotly figure
+    tikz_options, optional
+        options given to the tikzpicture environment, by default None
+    axis_options, optional
+        options given to the axis environment, by default None
+    include_disclamer, optional
+        include a disclamer in the code, by default True
+    img_name, optional
+        name of the PNG file for heatmap, by default "heatmap.png"
+
+    Returns
+    -------
+        string of tikz code
+    """
+    figure_data = fig.data
+    figure_layout = fig.layout
+    colors_set = set()
+    data_str = []
+
+    axis = Axis(figure_layout, colors_set, axis_options=axis_options)
+    data_container = DataContainer()
+
+    if len(figure_data) == 0:
+        warn("No data in figure.")
+
+    for trace in figure_data:
+
+        if trace.type == "scatter":
+            # Handle the case where x or y is empty
+            if trace.x is None and trace.y is None:
+                warn("Adding empty trace.")
+                data_str.append( "\\addplot coordinates {};\n" )
+                continue
+            else:
+                if trace.x is None:
+                    trace.x = list(range(len(trace.y)))
+                if trace.y is None:
+                    trace.y = list(range(len(trace.x)))
+
+            data_name_macro, y_name = data_container.addData(trace.x, trace.y, trace.name)
+            data_str.append( draw_scatter2d(data_name_macro, trace, y_name, axis, colors_set) )
+            if trace.name and trace['showlegend'] != False:
+                data_str.append( tex_add_legendentry(sanitize_TeX_text(trace.name)) )
+            if trace.line.color is not None:
+                colors_set.add(convert_color(trace.line.color)[:3])
+            if trace.fillcolor is not None:
+                colors_set.add(convert_color(trace.fillcolor)[:3])
+
+        elif trace.type == "heatmap":
+            # Handle the case where x, y or z is empty
+            if trace.z is None:
+                warn("Adding empty trace.")
+                data_str.append( "\\addplot coordinates {};\n" )
+                continue
+            data_str.append( draw_heatmap(trace, fig, img_name, axis) )
+
+        elif trace.type == "histogram":
+
+            data_str.append( draw_histogram(trace, axis, colors_set) )
+            if trace.name and trace['showlegend'] != False:
+                data_str.append( tex_add_legendentry(sanitize_TeX_text(trace.name)) )
+
+        else:
+            warn(f"Trace type {trace.type} is not supported yet.")
+
+    annotation_str = str_from_annotation(figure_layout.annotations, axis, colors_set)
+
+    code = """"""
+    stack_env = []
+
+    if include_disclamer:
+        code += tex_comment(f"This file was created with tikzplotly version {__version__}.")
+
+    if len(data_container.data) > 0:
+        code += data_container.exportData()
+        code += "\n"
+
+    code += tex_begin_environment("tikzpicture", stack_env, options=tikz_options)
+
+    if bool(colors_set): code += "\n"
+    color_list = list(colors_set)
+    color_list.sort()
+    for color in color_list:
+        code += tex_add_color(color[0], color[1], color[2])
+    if bool(colors_set): code += "\n"
+
+    code += axis.open_environment(stack_env)
+
+    if figure_layout.legend.title.text is not None and figure_layout.showlegend:
+        code += "\\addlegendimage{empty legend}\n"
+        code += tex_add_legendentry(sanitize_TeX_text(fig.layout.legend.title.text), options="yshift=5pt")
+
+    for trace_str in data_str:
+        code += trace_str
+
+    code += annotation_str
+
+    if figure_layout.showlegend == False:
+        code = re.sub(r"\\addlegendentry{.+}\n", "", code)
+
+    code += tex_end_all_environment(stack_env)
+
+    return code
+
+
+def save(filepath, *args, **kwargs):
+    """Save a figure to a file or a stream.
+
+    Parameters
+    ----------
+    filepath : str or Path
+        A string containing a path to a filename, or a Path object.
+    *args, **kwargs
+        Additional arguments are passed to the backend.
+    """
+    code = get_tikz_code(*args, **kwargs)
+    directory = Path(filepath).parent
+    if not directory.exists():
+        directory.mkdir(parents=True)
+    with open(filepath, "w") as fd:
+        fd.write(code)
---
+++ packages/tikzplotly/tikzplotly-0.2.0-py3-none-any.whl/tikzplotly/_utils.py
@@ -0,0 +1,177 @@
+import re
+import warnings
+from math import floor
+import numpy as np
+
+rep_digit = {'0': 'Z', '1': 'O', '2': 'T', '3': 'Th', '4': 'F', '5': 'Fi', '6': 'S', '7': 'Se', '8': 'E', '9': 'N'}
+rep_digit = dict((re.escape(k), v) for k, v in rep_digit.items())
+pattern_digit = re.compile("|".join(rep_digit.keys()))
+
+def replace_all_digits(text):
+    """Replace all digits in a string with their corresponding letter.
+
+    Parameters
+    ----------
+    text
+        string to replace digits in
+
+    Returns
+    -------
+        string with digits replaced by their corresponding letter
+    """
+    return pattern_digit.sub(lambda m: rep_digit[re.escape(m.group(0))], text)
+
+rep_mounts = {"January": '1', 'February': '2', 'March': '3', 'April': '4', 'May': '5', 'June': '6', 'July': '7', 'August': '8', 'September': '9', 'October': '10', 'November': '11', 'December': '12',
+              'january': 1, 'february': 2, 'march': 3, 'april': 4, 'may': 5, 'june': 6, 'july': 7, 'august': 8, 'september': 9, 'october': 10, 'november': 11, 'december': 12}
+rep_mounts = dict((re.escape(k), v) for k, v in rep_mounts.items())
+pattern_mounts = re.compile("|".join(rep_mounts.keys()))
+
+def replace_all_mounts(text):
+    """Replace all mounts in a string with their corresponding number.
+
+    Parameters
+    ----------
+    text
+        string to replace mounts in
+
+    Returns
+    -------
+        string with mounts replaced by their corresponding number
+    """
+    return pattern_mounts.sub(lambda m: rep_mounts[re.escape(m.group(0))], text)
+
+
+def sanitize_text(text: str, keep_space=False):
+    """
+    Sanitize the input text by removing or replacing unwanted characters.
+
+    Parameters
+    ----------
+    text (str): The input text to be sanitized.
+    keep_space (bool, optional): If True, spaces will be preserved in the sanitized text.
+                                 If False, spaces will be removed. Defaults to False.
+
+    Returns
+    -------
+    str: The sanitized text.
+    """
+    return "".join(sanitize_char(ch, keep_space=keep_space) for ch in text)
+
+def sanitize_char(ch, keep_space=False):
+    """
+    Sanitize a character by converting it to a hexadecimal representation if it is a special character, non-ASCII, or non-printable.
+
+    Parameters
+    ----------
+    ch (str): The character to sanitize.
+    keep_space (bool, optional): If True, spaces will be kept as is. Defaults to False.
+
+    Returns
+    -------
+    str: The sanitized character or its hexadecimal representation.
+    """
+    if keep_space and ch == " ": return " "
+    if ch in "[]{}= ": return f"x{ord(ch):x}"
+    # if not ascii, return hex
+    if ord(ch) > 127: return f"x{ord(ch):x}"
+    # if not printable, return hex
+    if not ch.isprintable(): return f"x{ord(ch):x}"
+    return ch
+
+def sanitize_TeX_text(text: str):
+    s = "".join(map(sanitize_TeX_char, text))
+    if '[' in s or ']' in s:
+        return "{" + s + "}"
+    return s
+
+def sanitize_TeX_char(ch):
+    if ch in "_{}": return f"\\{ch}"
+    # if not ascii, return hex
+    if ord(ch) > 127:
+        warnings.warn(f"Character {ch} has been replaced by \"x{ord(ch):x}\" in output file")
+        return f"x{ord(ch):x}"
+    # if not printable, return hex
+    if not ch.isprintable():
+        warnings.warn(f"Character {ch} has been replaced by \"x{ord(ch):x}\" in output file")
+        return f"x{ord(ch):x}"
+    return ch
+
+def px_to_pt(px):
+    """Convert size in pixel to a size in point
+
+    Parameters
+    ----------
+    px
+        size in pixel
+
+    Returns
+    -------
+    float
+        size in point
+    """
+    pt = px * .75
+    if floor(pt) == pt: return int(pt)
+    else: return pt
+
+
+def option_dict_to_str(options_dict, sep=" "):
+    """Convert a dictionary of options to a string of options for TikZ.
+
+    Parameters
+    ----------
+    options_dict
+        dictionary of options
+    sep, optional
+        separator between options, by default " "
+
+    Returns
+    -------
+    string
+        string of options for TikZ
+    """
+    options = ""
+    for key, value in options_dict.items():
+        if value is None:
+            options += f"{key},{sep}"
+        else:
+            options += f"{key}={value},{sep}"
+    if options == "":
+        return None
+    return options.strip()[:-1]
+
+def get_ticks_str(data, nticks=None):
+    """Get the ticks and ticklabels for the axis.
+
+    Parameters
+    ----------
+    data
+        data to be plotted
+    nticks
+        number of ticks
+
+    Returns
+    -------
+    tuple
+        ticks and ticklabels
+
+    Examples
+    --------
+    >>> get_ticks_str(["Sun", "Sat", "Thur", "Fri"])
+    ({0,1,2,3}, {"Sun", "Sat", "Thur", "Fri"})
+    """
+    indices = np.arange(len(data))
+    if nticks is not None:
+        data_ = data[::len(data)//(nticks-1)]
+        data = np.append(data_, [data[-1]])
+        indices_ = indices[::len(indices)//(nticks-1)]
+        indices = np.append(indices_, [indices[-1]])
+
+    ticks = "{"
+    ticklabels = "{"
+    for i, val in zip(indices, data):
+        ticks += str(i) + ","
+        ticklabels += str(val) + ","
+    ticks = ticks[:-1] + "}"
+    ticklabels = ticklabels[:-1] + "}"
+
+    return ticks, ticklabels
