129e1b2f7ae797e4bacb05c7b1de106982ae9517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:11:22 +0000

    Add package core-api-tools/0.0.6/core_api_tools-0.0.6.tar.gz

---
+++ packages/core-api-tools/core_api_tools-0.0.6.tar.gz/core_api_tools-0.0.6/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+with open('requirements.txt') as f:
+  r = [i.strip() for i in f.readlines() if i.strip()]
+
+setup(
+  name='core-api-tools',
+  version='0.0.6',
+  author='ZaharDimidov',
+  author_email='zahar_dimidov@mail.ru',
+  description='This is the simplest module for quick work with fastapi.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/zahardimidov/core-api-tools',
+  packages=['core_api_tools'],
+  install_requires=r,
+  classifiers=[
+    'Programming Language :: Python :: 3.12',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='fastapi',
+  project_urls={
+    'GitHub': 'https://github.com/zahardimidov/core-api-tools'
+  },
+  python_requires='>=3.10'
+)
+
+
