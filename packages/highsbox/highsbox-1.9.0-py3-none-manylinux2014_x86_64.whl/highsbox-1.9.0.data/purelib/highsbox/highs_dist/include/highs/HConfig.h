73c3ed108fa932aa70bdac391093e6c86ee049be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:07:37 +0000

    Add package highsbox/1.9.0/highsbox-1.9.0-py3-none-manylinux2014_x86_64.whl

---
+++ packages/highsbox/highsbox-1.9.0-py3-none-manylinux2014_x86_64.whl/highsbox-1.9.0.data/purelib/highsbox/highs_dist/include/highs/HConfig.h
@@ -0,0 +1,21 @@
+#ifndef HCONFIG_H_
+#define HCONFIG_H_
+
+#define FAST_BUILD
+#define ZLIB_FOUND
+/* #undef CUPDLP_CPU */
+#define CMAKE_BUILD_TYPE "Release"
+#define CMAKE_INSTALL_PREFIX "/project/HiGHS/highs_dist"
+/* #undef HIGHSINT64 */
+/* #undef HIGHS_NO_DEFAULT_THREADS */
+#define HIGHS_HAVE_MM_PAUSE
+#define HIGHS_HAVE_BUILTIN_CLZ
+/* #undef HIGHS_HAVE_BITSCAN_REVERSE */
+
+#define HIGHS_GITHASH "66f735e"
+#define HIGHS_VERSION_MAJOR 1
+#define HIGHS_VERSION_MINOR 9
+#define HIGHS_VERSION_PATCH 0
+#define HIGHS_DIR "/project/HiGHS"
+
+#endif /* HCONFIG_H_ */
