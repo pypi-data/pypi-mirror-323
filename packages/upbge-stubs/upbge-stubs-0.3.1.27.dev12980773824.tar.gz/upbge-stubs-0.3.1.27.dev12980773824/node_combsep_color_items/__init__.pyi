620628e000c742a470d6943d2bf2425f1e527a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 15:05:07 +0000

    Add package upbge-stubs/0.3.1.27.dev12980773824/upbge-stubs-0.3.1.27.dev12980773824.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev12980773824.tar.gz/upbge-stubs-0.3.1.27.dev12980773824/node_combsep_color_items/__init__.pyi
@@ -0,0 +1,26 @@
+"""
+
+
+Node Combsep Color Items
+^^^^^^^^^^^^^^^^^^^^^^^^
+
+:RGB:             
+  RGB.
+
+  Use RGB color processing.
+
+:HSV:             
+  HSV.
+
+  Use HSV color processing.
+
+:HSL:             
+  HSL.
+
+  Use HSL color processing.
+
+.. _rna-enum-node-combsep-color-items:
+
+"""
+
+import typing
