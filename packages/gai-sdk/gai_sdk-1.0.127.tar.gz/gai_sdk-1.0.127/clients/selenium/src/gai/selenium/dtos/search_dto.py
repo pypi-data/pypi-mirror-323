93f2ec7ad31b1a4867e4fd979c4d31863aa2c516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 15:05:16 +0000

    Add package gai-sdk/1.0.127/gai_sdk-1.0.127.tar.gz

---
+++ packages/gai-sdk/gai_sdk-1.0.127.tar.gz/gai_sdk-1.0.127/clients/selenium/src/gai/selenium/dtos/search_dto.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class SearchRequest(BaseModel):
+    query: str
+    n_results: int = 10
+    period: Optional[str]=None
+
+class SearchResult(BaseModel):
+    title: str
+    link: str
+    snippet: Optional[str]=""
+
+class SearchResponse(BaseModel):
+    result: list[SearchResult]
