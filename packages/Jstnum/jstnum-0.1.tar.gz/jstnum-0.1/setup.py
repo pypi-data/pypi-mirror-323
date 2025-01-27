94bfbe000b8ee4121cd6b2c0fa4687ae49fc8737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:06:19 +0000

    Add package Jstnum/0.1/jstnum-0.1.tar.gz

---
+++ packages/Jstnum/jstnum-0.1.tar.gz/jstnum-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Jstnum',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'better_profanity',
+        'textblob',
+        'scikit-learn',
+        'pyperclip',
+    ],
+    extras_require={
+        "email_support": ["smtplib"],
+    },
+    author='Jynoqtra',
+    author_email='Jynoqtra@gmail.com',
+    description='JynPopMod Python Module',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Jynoqtra/JynPopMod',
+    classifiers=[  
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+    ],
+)
