3f1feac6e74925519317f99f3b98b38505b3ea29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:07:26 +0000

    Add package qsalto/0.2.1/qsalto-0.2.1.tar.gz

---
+++ packages/qsalto/qsalto-0.2.1.tar.gz/qsalto-0.2.1/src/qsalto/matrices.py
@@ -0,0 +1,380 @@
+
+import numpy as np
+from math import comb as scomb
+from typing import Union, overload
+from . import single_entry
+from . import single_entry_precise
+
+try:
+    import mpmath as mp
+except ImportError:
+    import mp_not_available
+    mp = mp_not_available.mp_error()
+
+def get_single_entry(precise):
+    return single_entry_precise if precise else single_entry
+
+def comb(n, k):
+    if k < 0: return 0
+    else: return scomb(n, k)
+
+
+def assert_valid_entry(entry, n):
+    assert hasattr(entry, '__getitem__') and len(
+        entry) == 2, f"Invalid entry `{entry}`. Entry should be a pair of integers"
+    assert 0 <= entry[0] <= n and 0 <= entry[
+        1] <= n, f"Matrix entry `{entry}` out of range (n={n})"
+
+
+@overload
+def M(n: int, entry: None = None, precise=False) -> np.ndarray: ...
+@overload
+def M(n: int, entry: tuple, precise=False) -> float: ...
+
+@overload
+def M1(n: int, entry: None = None, precise=False) -> np.ndarray: ...
+@overload
+def M1(n: int, entry: tuple, precise=False) -> float: ...
+
+@overload
+def M2(n: int, entry: None = None, precise=False) -> np.ndarray: ...
+@overload
+def M2(n: int, entry: tuple, precise=False) -> float: ...
+
+@overload
+def T1(n: int, entry: None = None, precise=False) -> np.ndarray: ...
+@overload
+def T1(n: int, entry: tuple, precise=False) -> float: ...
+@overload
+def iT1(n: int, entry: None = None, precise=False) -> np.ndarray: ...
+@overload
+def iT1(n: int, entry: tuple, precise=False) -> float: ...
+
+@overload
+def T2(n: int, entry: None = None, precise=False) -> np.ndarray: ...
+@overload
+def T2(n: int, entry: tuple, precise=False) -> float: ...
+@overload
+def iT2(n: int, entry: None = None, precise=False) -> np.ndarray: ...
+@overload
+def iT2(n: int, entry: tuple, precise=False) -> float: ...
+
+@overload
+def T3(n: int, entry: None = None, precise=False) -> np.ndarray: ...
+@overload
+def T3(n: int, entry: tuple, precise=False) -> float: ...
+@overload
+def iT3(n: int, entry: None = None, precise=False) -> np.ndarray: ...
+@overload
+def iT3(n: int, entry: tuple, precise=False) -> float: ...
+
+
+def M(n: int, entry: Union[tuple, None] = None, precise=False) -> Union[np.ndarray, float]:
+    """Self-inverse transformation matrix between Shor-Laflamme distributions $a$ and $b$.
+
+    Parameters
+    ----------
+    n : int
+        Number of qubits
+    entry : Tuple[int, int] | None
+        If given, only the entry specified by these indices is computed and returned. 
+    precise : bool
+        If `true`, an `mp.matrix` returned, using current `mpmath` precision. 
+        
+    Returns
+    -------
+    np.ndarray | float
+        $(n+1)×(n+1)$ matrix or single entry.
+    """
+    if entry is not None:
+        assert_valid_entry(entry, n)
+        return get_single_entry(precise).M(n, *entry)
+
+    K = [[0] * (n + 1) for _ in range(n + 1)]
+    for k in range(0, n + 1):
+        K[0][k] = 1
+        K[k][-1] = comb(n, k) * (-1)**k
+    for k in range(1, n + 1):
+        for l in reversed(range(n)):
+            K[k][l] = 3 * K[k - 1][l + 1] + K[k - 1][l] + K[k][l + 1]
+    if precise:
+        return mp.matrix(K) / 2**n
+    else:
+        for k in range(n + 1):
+            for l in range(n + 1):
+                K[k][l] /= 2**n
+        K = np.array(K, dtype=float)
+        return K
+
+
+def M1(n: int, entry: Union[tuple, None] = None, precise=False) -> Union[np.ndarray, float]:
+    """Self-inverse transformation matrix between quantum weight enumerators $a'$ and $b'$.
+
+    Parameters
+    ----------
+    n : int
+        Number of qubits
+    entry : Tuple[int, int] | None
+        If given, only the entry specified by these indices is computed and returned. 
+    precise : bool
+        If `true`, an `mp.matrix` returned, using current `mpmath` precision. 
+
+    Returns
+    -------
+    np.ndarray | float
+        $(n+1)×(n+1)$ matrix or single entry.
+    """
+    if entry is not None:
+        assert_valid_entry(entry, n)
+        return get_single_entry(precise).M1(n, *entry)
+
+    if precise:
+        K = mp.matrix(n + 1)
+        for i in range(n + 1):
+            K[i, n - i] = 1
+        return K
+    else:
+        return np.fliplr(np.eye(n + 1))
+
+
+def M2(n: int, entry: Union[tuple, None] = None, precise=False) -> Union[np.ndarray, float]:
+    """Self-inverse transformation matrix between quantum weight enumerators $a''$ and $b''$.
+
+    Parameters
+    ----------
+    n : int
+        Number of qubits
+    entry : Tuple[int, int] | None
+        If given, only the entry specified by these indices is computed and returned. 
+    precise : bool
+        If `true`, an `mp.matrix` returned, using current `mpmath` precision. 
+
+    Returns
+    -------
+    np.ndarray | float
+        $(n+1)×(n+1)$ matrix or single entry.
+    """
+    if entry is not None:
+        assert_valid_entry(entry, n)
+        return get_single_entry(precise).M2(n, *entry)
+
+    if precise:
+        K = mp.eye(n + 1)
+        for i in range(1, n + 1, 2):
+            K[n - i, n - i] = -1
+        return K
+    else:
+        return np.diag(np.resize([1, -1], n+1)[::-1])
+
+
+def T2(n: int, entry: Union[tuple, None] = None, precise=False) -> Union[np.ndarray, float]:
+    """Transformation matrix from Shor-Laflamme enumerators $a$ to Rains shadow enumerators $a''$. 
+
+    Parameters
+    ----------
+    n : int
+        Number of qubits
+    entry : Tuple[int, int] | None
+        If given, only the entry specified by these indices is computed and returned. 
+    precise : bool
+        If `true`, an `mp.matrix` returned, using current `mpmath` precision. 
+
+    Returns
+    -------
+    np.ndarray | float
+        $(n+1)×(n+1)$ matrix or single entry.
+    """
+    if entry is not None:
+        assert_valid_entry(entry, n)
+        return get_single_entry(precise).T2(n, *entry)
+
+    K = M(n, precise=precise)
+    for col in range(1, n + 1, 2):
+        K[:, col] *= -1.
+    return K
+
+
+def iT2(n: int, entry: Union[tuple, None] = None, precise=False) -> Union[np.ndarray, float]:
+    """Transformation matrix from Rains shadow enumerators $a''$ to Shor-Laflamme enumerators $a$. 
+
+    Parameters
+    ----------
+    n : int
+        Number of qubits
+    entry : Tuple[int, int] | None
+        If given, only the entry specified by these indices is computed and returned. 
+    precise : bool
+        If `true`, an `mp.matrix` returned, using current `mpmath` precision. 
+
+    Returns
+    -------
+    np.ndarray | float
+        $(n+1)×(n+1)$ matrix or single entry.
+    """
+    if entry is not None:
+        assert_valid_entry(entry, n)
+        return get_single_entry(precise).iT2(n, *entry)
+
+    K = M(n, precise=precise)
+    for row in range(1, n + 1, 2):
+        K[row, :] *= -1.
+    return K
+
+
+def T1(n: int, entry: Union[tuple, None] = None, precise=False) -> Union[np.ndarray, float]:
+    """Transformation matrix from Shor-Laflamme enumerators $a$ to Rains unitary enumerators $a'$. 
+
+    Parameters
+    ----------
+    n : int
+        Number of qubits
+    entry : Tuple[int, int] | None
+        If given, only the entry specified by these indices is computed and returned. 
+    precise : bool
+        If `true`, an `mp.matrix` returned, using current `mpmath` precision. 
+
+    Returns
+    -------
+    np.ndarray | float
+        $(n+1)×(n+1)$ matrix or single entry.
+    """
+    if entry is not None:
+        assert_valid_entry(entry, n)
+        return get_single_entry(precise).T1(n, *entry)
+
+    K = [[0] * (n + 1) for _ in range(n + 1)]
+
+    for k in range(n + 1):
+        K[k][k] = 1
+        K[-1][k] = 1
+    for l in reversed(range(0, n)):
+        for k in range(l - 1, n):
+            K[k][l] = K[k][l + 1] + K[k + 1][l + 1]
+    if precise:
+        K = mp.matrix(K)
+    else:
+        K = np.array(K, dtype=float)
+
+    for k in range(n + 1):
+        K[k, :] *= 2**(n - k) / K[k, 0]
+    return K
+
+
+def iT1(n: int, entry: Union[tuple, None] = None, precise=False) -> Union[np.ndarray, float]:
+    """Transformation matrix from Rains unitary enumerators $a'$ to Shor-Laflamme enumerators $a$. 
+
+    Parameters
+    ----------
+    n : int
+        Number of qubits
+    entry : Tuple[int, int] | None
+        If given, only the entry specified by these indices is computed and returned. 
+    precise : bool
+        If `true`, an `mp.matrix` returned, using current `mpmath` precision. 
+
+    Returns
+    -------
+    np.ndarray | float
+        $(n+1)×(n+1)$ matrix or single entry.
+    """
+    if entry is not None:
+        assert_valid_entry(entry, n)
+        return get_single_entry(precise).iT1(n, *entry)
+
+    K = [[0] * (n+1) for _ in range(n + 1)]
+
+    for k in range(n + 1):
+        K[k][k] = 1
+        K[-1][k] = 1
+    for l in reversed(range(0, n)):
+        for k in range(l - 1, n):
+            K[k][l] = K[k][l + 1] + K[k + 1][l + 1]
+    if precise:
+        K = mp.matrix(K)
+    else:
+        K = np.array(K, dtype=float)
+
+    for l in reversed(range(n + 1)):
+        K[:, l] *= K[l, 0] / 2**(n - l)
+    for l in range(n + 1):
+        for k in range(l + 1, n + 1, 2):
+            K[k, l] *= -1
+    return K
+
+
+def T3(n: int, entry: Union[tuple, None] = None, precise=False) -> Union[np.ndarray, float]:
+    """Transformation matrix from Rains' unitary enumerators $a'$ to Rains' shadow enumerators $a''$. 
+
+    Parameters
+    ----------
+    n : int
+        Number of qubits
+    entry : Tuple[int, int] | None
+        If given, only the entry specified by these indices is computed and returned. 
+    precise : bool
+        If `true`, an `mp.matrix` returned, using current `mpmath` precision. 
+
+    Returns
+    -------
+    np.ndarray | float
+        $(n+1)×(n+1)$ matrix or single entry.
+    """
+    if entry is not None:
+        assert_valid_entry(entry, n)
+        return get_single_entry(precise).T3(n, *entry)
+
+    K = [[0] * (n + 1) for _ in range(n + 1)]
+    for k in range(0, n + 1):
+        K[0][k] = (-1)**k
+        K[k][0] = comb(n, k)
+    for k in range(1, n + 1):
+        for l in range(1, n + 1):
+            K[k][l] = K[k - 1][l - 1] - K[k - 1][l] - K[k][l - 1]
+
+    if precise:
+        K = mp.matrix(K)
+    else:
+        K = np.array(K, dtype=float)
+
+    for l in range(n + 1):
+        K[:, l] *= np.abs(K[l, 0])
+    return K / 2**n
+
+
+def iT3(n: int, entry: Union[tuple, None] = None, precise=False) -> Union[np.ndarray, float]:
+    """Transformation matrix from Rains' shadow enumerators $a''$ to Rains' unitary enumerators $a'$. 
+
+    Parameters
+    ----------
+    n : int
+        Number of qubits
+    entry : Tuple[int, int] | None
+        If given, only the entry specified by these indices is computed and returned. 
+    precise : bool
+        If `true`, an `mp.matrix` returned, using current `mpmath` precision. 
+
+    Returns
+    -------
+    np.ndarray | float
+        $(n+1)×(n+1)$ matrix or single entry.
+    """
+    if entry is not None:
+        assert_valid_entry(entry, n)
+        return get_single_entry(precise).iT3(n, *entry)
+
+    K = [[0] * (n + 1) for _ in range(n + 1)]
+    for k in range(0, n + 1):
+        K[0][k] = 1
+        K[k][0] = comb(n, k) * (-1)**k
+    for k in range(1, n + 1):
+        for l in range(1, n + 1):
+            K[k][l] = K[k - 1][l - 1] + K[k - 1][l] + K[k][l - 1]
+
+    if precise:
+        K = mp.matrix(K)
+    else:
+        K = np.array(K, dtype=float)
+
+    for k in range(n + 1):
+        K[k, :] *= 1 / np.abs(K[k, 0])
+    return K
---
+++ packages/qsalto/qsalto-0.2.1.tar.gz/qsalto-0.2.1/tests/test_matrices.py
@@ -0,0 +1,102 @@
+import unittest
+import numpy as np
+from src.qsalto import *
+from math import comb
+
+
+class TestData(unittest.TestCase):
+
+    def test_inverse(self):
+        for n in range(1, 12):
+            np.testing.assert_almost_equal(np.linalg.inv(M(n)), M(n))
+            np.testing.assert_almost_equal(np.linalg.inv(M1(n)), M1(n))
+            np.testing.assert_almost_equal(np.linalg.inv(M2(n)), M2(n))
+            np.testing.assert_almost_equal(np.linalg.inv(T1(n)), iT1(n))
+            np.testing.assert_almost_equal(np.linalg.inv(T2(n)), iT2(n))
+            np.testing.assert_almost_equal(np.linalg.inv(T3(n)), iT3(n))
+
+    def test_chaining(self):
+        for n in range(1, 30):
+            np.testing.assert_almost_equal(T3(n) @ T1(n), T2(n))
+            np.testing.assert_almost_equal(iT1(n) @ iT3(n), iT2(n))
+            np.testing.assert_almost_equal(T1(n) @ iT2(n), iT3(n))
+            np.testing.assert_almost_equal(T2(n) @ iT1(n), T3(n))
+
+    def test_M1_identity(self):
+        for n in range(1, 30):
+            np.testing.assert_almost_equal(T1(n) @ M(n) @ iT1(n), M1(n))
+
+    def test_M2_identity(self):
+        for n in range(1, 30):
+            np.testing.assert_almost_equal(T2(n) @ M(n) @ iT2(n), M2(n))
+
+    def test_separable(self):
+        for n in range(2, 20):
+            apd = np.full(n + 1, 1)
+            sld = iT1(n) @ apd
+            np.testing.assert_equal(sld, np.array([comb(n, i) / 2**n for i in np.arange(n+1)]))
+
+            tpd = T3(n) @ apd
+            np.testing.assert_almost_equal(tpd[n], 1)
+            np.testing.assert_almost_equal(tpd[:n], np.zeros(n))
+
+    def test_ghz(self):
+        for n in range(2, 30, 2):
+            apd = np.full(n + 1, .5)
+            apd[[0, n]] = 1
+
+            sld = iT1(n) @ apd
+            ks = np.arange(n+1)
+            ghz_sld = np.array([comb(n, i) * (1+(-1)**i) / 2 for i in ks])
+            ghz_sld[n] += 2**(n-1)
+            ghz_sld /= 2**n
+            np.testing.assert_equal(sld, ghz_sld)
+
+            tpd = T3(n) @ apd
+            np.testing.assert_almost_equal(tpd, ghz_sld)
+
+    def test_single_entry(self):
+        for t in [M, M1, M2, T1, iT1, T2, iT2, T3, iT3]:
+            with self.subTest("", t=t):
+                n = 11
+                matrix = t(n)
+                for i in range(n + 1):
+                    for j in range(n + 1):
+                        val = t(n, entry=(i, j))
+                        np.testing.assert_allclose(matrix[i, j], val)
+
+    def test_single_entry_precise(self):
+        import mpmath as mp
+        for t in [M, M1, M2, T1, iT1, T2, iT2, T3, iT3]:
+            with self.subTest("", t=t):
+                n = 20
+                matrix = t(n, precise=True)
+                for i in range(n + 1):
+                    for j in range(n + 1):
+                        val = t(n, entry=(i, j), precise=True)
+                        assert mp.fabs(matrix[i, j] - val) < 1e-10
+
+    def test_1000_qubits(self):
+        for t in [M, M1, M2, T1, iT1, T2, iT2, T3, iT3]:
+            with self.subTest("", t=t):
+                t(1000)
+
+    def test_numpy_int_for_n(self):
+        for t in [M, M1, M2, T1, iT1, T2, iT2, T3, iT3]:
+            with self.subTest("", t=t):
+                n = np.int64(11)
+                matrix = t(n)
+                val = t(n, entry=(0, 0))
+
+    def test_precise_mode(self):
+        for t in [M, M1, M2, T1, iT1, T2, iT2, T3, iT3]:
+            for n in [2, 11, 37, 39, 55, 63]:
+                with self.subTest("", t=t, n=n):
+                    K1 = t(n, precise=False)
+                    K2 = t(n, precise=True)
+                    np.testing.assert_allclose(K1, np.array(K2.tolist(), dtype=float))
+
+    def test_f(self):
+        a: np.ndarray = M(3)
+        b: float = M(3, entry=(2, 3))
+        c: np.ndarray = M(3, precise=True,)
