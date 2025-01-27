463b9827b874722f0f7f5567abeef20d580a1daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:07:48 +0000

    Add package aws-s3-controller/0.6.1/aws_s3_controller-0.6.1.tar.gz

---
+++ packages/aws-s3-controller/aws_s3_controller-0.6.1.tar.gz/aws_s3_controller-0.6.1/aws_s3_controller/aws_consts.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
+AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
+AWS_REGION = str(os.getenv('AWS_REGION'))
