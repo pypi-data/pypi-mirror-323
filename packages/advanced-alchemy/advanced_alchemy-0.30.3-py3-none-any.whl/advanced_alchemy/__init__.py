1ee76e021562e0728c1297a286842a6d9c61bc8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 04:05:04 +0000

    Add package advanced-alchemy/0.30.3/advanced_alchemy-0.30.3-py3-none-any.whl

---
+++ packages/advanced-alchemy/advanced_alchemy-0.30.3-py3-none-any.whl/advanced_alchemy/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from advanced_alchemy import (
+    alembic,
+    base,
+    cli,
+    config,
+    exceptions,
+    extensions,
+    filters,
+    mixins,
+    operations,
+    service,
+    types,
+    utils,
+)
+
+__all__ = (
+    "alembic",
+    "base",
+    "cli",
+    "config",
+    "exceptions",
+    "extensions",
+    "filters",
+    "mixins",
+    "operations",
+    "service",
+    "types",
+    "utils",
+)
