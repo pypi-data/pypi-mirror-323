842f142cb1effc0bfd616408871f76688f476e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 04:05:13 +0000

    Add package cxx-flow/0.3.4/cxx_flow-0.3.4.tar.gz

---
+++ packages/cxx-flow/cxx_flow-0.3.4.tar.gz/cxx_flow-0.3.4/cxx_flow/plugins/store/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2025 Marcin Zdun
+# This code is licensed under MIT license (see LICENSE for details)
+
+from . import store_both, store_packages, store_test
+
+__all__ = ["store_both", "store_test", "store_packages"]
