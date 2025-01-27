23073b49e62f6817e6eab1b418c24fe9d304b6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 04:05:46 +0000

    Add package swiftshadow/2.0.0/swiftshadow-2.0.0.tar.gz

---
+++ packages/swiftshadow/swiftshadow-2.0.0.tar.gz/swiftshadow-2.0.0/swiftshadow/helpers.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from typing import Literal
+
+from requests import get
+from swiftshadow.models import Proxy
+
+
+def checkProxy(proxy):
+    proxyDict = {proxy[1]: proxy[0]}
+    try:
+        resp = get(
+            f"{proxy[1]}://checkip.amazonaws.com", proxies=proxyDict, timeout=2
+        ).text
+        if resp.count(".") == 3:
+            return True
+        return False
+    except Exception:
+        return False
+
+
+def log(level, message):
+    level = level.upper()
+    print(
+        f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')} - [swiftshadow] - {level} : {message}"
+    )
+
+
+def plaintextToProxies(text: str, protocol: Literal["http", "https"]) -> list[Proxy]:
+    proxies: list[Proxy] = []
+    for line in text.splitlines():
+        try:
+            ip, port = line.split(":")
+        except ValueError:
+            continue
+        proxy = Proxy(ip=ip, port=int(port), protocol=protocol)
+        proxies.append(proxy)
+    return proxies
