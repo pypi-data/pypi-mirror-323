a5cc4d704c342c65b3619e570d3222d5ba4cf392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 04:05:45 +0000

    Add package LiveboxMonitor/1.5/LiveboxMonitor-1.5-py3-none-any.whl

---
+++ packages/LiveboxMonitor/LiveboxMonitor-1.5-py3-none-any.whl/LiveboxMonitor/__init__.py
@@ -0,0 +1,11 @@
+# #### Livebox Monitor ####
+
+__title__ = 'LiveboxMonitor'
+__description__ = 'Graphical user interface to monitor Livebox 4, 5, 6 & 7'
+__url__ = 'https://github.com/p-dor/LiveboxMonitor'
+__version__ = '1.5'
+__build__ = 0x010500
+__author__ = 'Pierre Dor'
+__author_email__ = 'dorpierre@orange.fr'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022-25 Pierre Dor'
