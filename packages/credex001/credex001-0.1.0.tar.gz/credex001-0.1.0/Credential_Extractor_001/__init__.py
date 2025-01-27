e59dd951bb30d69643ec3dd4646213d4287215d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 15:05:16 +0000

    Add package credex001/0.1.0/credex001-0.1.0.tar.gz

---
+++ packages/credex001/credex001-0.1.0.tar.gz/credex001-0.1.0/Credential_Extractor_001/__init__.py
@@ -0,0 +1,12 @@
+"""
+This module provides functions for converting HTML files to text format, 
+processing HTML files in a directory, and combining the resulting text files.
+
+Functions:
+    format_html_to_text(input_file) -> str
+    process_html_directory(input_dir, output_dir) -> None
+    combine_text_files(input_dir, output_file) -> None
+    extract(inp_folder='./yay', out_folder='./output_texts', out_file='combined.txt', rmtr=False) -> None
+"""
+
+from .extract import format_html_to_text, process_html_directory, combine_text_files, extract
