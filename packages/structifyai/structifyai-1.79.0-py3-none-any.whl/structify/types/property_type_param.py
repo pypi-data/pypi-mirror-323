e07c8c9b8fae47a5dffd3061405e94ae10cbf066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 14:04:34 +0000

    Add package structifyai/1.79.0/structifyai-1.79.0-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.79.0-py3-none-any.whl/structify/types/property_type_param.py
@@ -0,0 +1,27 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Union
+from typing_extensions import Literal, Required, Annotated, TypeAlias, TypedDict
+
+from .._utils import PropertyInfo
+
+__all__ = ["PropertyTypeParam", "Enum"]
+
+
+class Enum(TypedDict, total=False):
+    enum: Required[Annotated[List[str], PropertyInfo(alias="Enum")]]
+
+
+PropertyTypeParam: TypeAlias = Union[
+    Literal["String"],
+    Literal["Boolean"],
+    Enum,
+    Literal["Integer"],
+    Literal["Float"],
+    Literal["Date"],
+    Literal["URL"],
+    Literal["Money"],
+    Literal["Image"],
+]
---
+++ packages/structifyai/structifyai-1.79.0-py3-none-any.whl/structify/types/structure_enhance_params.py
@@ -0,0 +1,18 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import Required, TypedDict
+
+__all__ = ["StructureEnhanceParams"]
+
+
+class StructureEnhanceParams(TypedDict, total=False):
+    entity_id: Required[str]
+
+    allow_new_entities: bool
+
+    property_name: Optional[str]
+
+    relationship_name: Optional[str]
