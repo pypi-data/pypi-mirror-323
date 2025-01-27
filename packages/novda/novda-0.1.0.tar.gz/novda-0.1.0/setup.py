3e0705c7b504a2a994620f445b4623b4791a6303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:07:43 +0000

    Add package novda/0.1.0/novda-0.1.0.tar.gz

---
+++ packages/novda/novda-0.1.0.tar.gz/novda-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="novda",
+    version="0.1.0",
+    description="Ahmadjon & Abdulloh you are late",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Izzatullev Abror",
+    author_email="izzatullaev2001abror@gmail.com",
+    url="https://github.com/justbackend/novda",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
