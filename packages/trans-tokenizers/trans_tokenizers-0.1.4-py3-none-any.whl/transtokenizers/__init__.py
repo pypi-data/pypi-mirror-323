041eb3c2e26473cd4aa2fc74e5e371d2726b6960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 04:06:10 +0000

    Add package trans-tokenizers/0.1.4/trans_tokenizers-0.1.4-py3-none-any.whl

---
+++ packages/trans-tokenizers/trans_tokenizers-0.1.4-py3-none-any.whl/transtokenizers/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for transtokenizers."""
+
+__author__ = """Pieter Delobelle, François Remy"""
+__email__ = 'pieter.delobelle@kuleuven.be, francois.remy@ugent.be'
+__version__ = '0.1.4'
+
+from .transtokenizers import align, create_aligned_corpus, map_tokens, remap_model, smooth_mapping
