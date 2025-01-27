6bc4198cb26cbabb34f8592496725c796d47782d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:06:20 +0000

    Add package gapred/0.8.0/gapred-0.8.0.tar.gz

---
+++ packages/gapred/gapred-0.8.0.tar.gz/gapred-0.8.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gapred",  # Library name
+    version="0.8.0",  # Initial version
+    description="Self-made ML framework",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/radhofan/Gapred",  # Replace with your repository
+    packages=find_packages(),
+    install_requires=["numpy"],  # Dependencies
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
