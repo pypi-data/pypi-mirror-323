5823c1c3877863ec06f8a4005909a450f813ec17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 15:05:01 +0000

    Add package rustpython-ast-pyo3-wrapper/0.0.1/rustpython_ast_pyo3_wrapper-0.0.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/rustpython-ast-pyo3-wrapper/rustpython_ast_pyo3_wrapper-0.0.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/rustpython_ast_pyo3_wrapper/__init__.py
@@ -0,0 +1,5 @@
+from .rustpython_ast_pyo3_wrapper import *
+
+__doc__ = rustpython_ast_pyo3_wrapper.__doc__
+if hasattr(rustpython_ast_pyo3_wrapper, "__all__"):
+    __all__ = rustpython_ast_pyo3_wrapper.__all__
