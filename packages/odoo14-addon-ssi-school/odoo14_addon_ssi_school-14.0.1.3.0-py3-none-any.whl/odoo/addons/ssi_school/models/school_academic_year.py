a8b0dbae431483c14d25b24d1df71d5719c15636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 15:05:13 +0000

    Add package odoo14-addon-ssi-school/14.0.1.3.0/odoo14_addon_ssi_school-14.0.1.3.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-school/odoo14_addon_ssi_school-14.0.1.3.0-py3-none-any.whl/odoo/addons/ssi_school/models/school_academic_year.py
@@ -0,0 +1,21 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+
+from odoo import fields, models
+
+
+class SchoolAcademicYear(models.Model):
+    _name = "school_academic_year"
+    _inherit = ["mixin.master_data"]
+    _description = "School Academic Year"
+
+    date_start = fields.Date(
+        string="Date Start",
+        required=True,
+    )
+    date_end = fields.Date(
+        string="Date End",
+        required=True,
+    )
