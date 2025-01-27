55858e35a72f29d3319412afaf23f85218809ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 15:05:25 +0000

    Add package sdkit/2.0.21/sdkit-2.0.21-py3-none-any.whl

---
+++ packages/sdkit/sdkit-2.0.21-py3-none-any.whl/sdkit/models/model_loader/realesrgan.py
@@ -0,0 +1,60 @@
+import os
+
+import torch
+
+# hack for basicsr https://github.com/XPixelGroup/BasicSR/pull/650
+# credit: https://github.com/AUTOMATIC1111/stable-diffusion-webui/pull/14186/files
+
+import sys
+
+try:
+    import torchvision.transforms.functional_tensor
+except ImportError:
+    try:
+        import torchvision.transforms.functional as functional
+
+        sys.modules["torchvision.transforms.functional_tensor"] = functional
+    except ImportError:
+        pass
+# /hack
+
+from basicsr.archs.rrdbnet_arch import RRDBNet
+from realesrgan import RealESRGANer
+
+from sdkit import Context
+
+
+def load_model(context: Context, **kwargs):
+    model_path = context.model_paths.get("realesrgan")
+
+    RealESRGAN_models = {
+        "RealESRGAN_x4plus": RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4),
+        "RealESRGAN_x4plus_anime_6B": RRDBNet(
+            num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4
+        ),
+    }
+
+    model_to_use = os.path.basename(model_path)
+    model_to_use, _ = os.path.splitext(model_to_use)
+    model_to_use = RealESRGAN_models[model_to_use]
+
+    half = context.half_precision
+    model = RealESRGANer(
+        device=torch.device(context.device),
+        scale=4,
+        model_path=model_path,
+        model=model_to_use,
+        pre_pad=0,
+        half=half,
+        tile=256,
+    )
+    if "cuda" not in context.device:
+        model.model.to(context.device)
+
+    model.model.name = model_to_use
+
+    return model
+
+
+def unload_model(context: Context, **kwargs):
+    pass
