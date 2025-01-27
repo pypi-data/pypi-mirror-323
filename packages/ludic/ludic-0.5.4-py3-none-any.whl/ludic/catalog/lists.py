4b58cd2b93daa0446900f5ee98ad823ed2669f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 16:05:49 +0000

    Add package ludic/0.5.4/ludic-0.5.4-py3-none-any.whl

---
+++ packages/ludic/ludic-0.5.4-py3-none-any.whl/ludic/catalog/lists.py
@@ -0,0 +1,63 @@
+from typing import override
+
+from ludic.attrs import GlobalAttrs
+from ludic.components import Component
+from ludic.html import li, ol, ul
+from ludic.types import AnyChildren
+
+
+class ListAttrs(GlobalAttrs, total=False):
+    items: list[AnyChildren]
+
+
+class Item(Component[AnyChildren, GlobalAttrs]):
+    """Simple component simulating an item in a list.
+
+    Example usage:
+
+        Item("Item 1")
+    """
+
+    @override
+    def render(self) -> li:
+        return li(*self.children, **self.attrs)
+
+
+class List(Component[Item, ListAttrs]):
+    """Simple component simulating a list.
+
+    There is basically just an alias for the :class:`ul` element
+    without the requirement to pass `li` as children.
+
+    Example usage:
+
+        List(Item("Item 1"), Item("Item 2"))
+    """
+
+    @override
+    def render(self) -> ul:
+        if items := self.attrs.get("items"):
+            children = tuple(map(Item, items))
+        else:
+            children = self.children
+        return ul(*children, **self.attrs_for(ul))
+
+
+class NumberedList(Component[Item, ListAttrs]):
+    """Simple component simulating a numbered list.
+
+    There is basically just an alias for the :class:`ol` element
+    without the requirement to pass `li` as children.
+
+    Example usage:
+
+        NumberedList(Item("Item 1"), Item("Item 2"))
+    """
+
+    @override
+    def render(self) -> ol:
+        if items := self.attrs.get("items"):
+            children = tuple(map(Item, items))
+        else:
+            children = self.children
+        return ol(*children, **self.attrs_for(ol))
