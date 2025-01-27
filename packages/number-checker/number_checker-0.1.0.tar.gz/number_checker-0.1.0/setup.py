924ad20679d2b96308fa02d084a40f107121e63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:07:31 +0000

    Add package number-checker/0.1.0/number_checker-0.1.0.tar.gz

---
+++ packages/number-checker/number_checker-0.1.0.tar.gz/number_checker-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='number_checker',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A simple library to check if a number is 99',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/number_checker',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
