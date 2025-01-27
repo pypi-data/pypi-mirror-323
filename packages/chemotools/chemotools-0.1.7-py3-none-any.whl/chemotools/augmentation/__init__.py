4780052975dfa9e30ededa6631d8d31b074f82dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 05:05:11 +0000

    Add package chemotools/0.1.7/chemotools-0.1.7-py3-none-any.whl

---
+++ packages/chemotools/chemotools-0.1.7-py3-none-any.whl/chemotools/augmentation/__init__.py
@@ -0,0 +1,16 @@
+from .baseline_shift import BaselineShift
+from .exponential_noise import ExponentialNoise
+from .normal_noise import NormalNoise
+from .index_shift import IndexShift
+from .spectrum_scale import SpectrumScale
+from .uniform_noise import UniformNoise
+
+
+__all__ = [
+    "BaselineShift",
+    "ExponentialNoise",
+    "NormalNoise",
+    "IndexShift",
+    "SpectrumScale",
+    "UniformNoise",
+]
