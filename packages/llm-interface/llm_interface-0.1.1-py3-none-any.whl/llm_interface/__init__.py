492d94118000733585ee353b4f68e4bce9a4fb4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 04:05:39 +0000

    Add package llm-interface/0.1.1/llm_interface-0.1.1-py3-none-any.whl

---
+++ packages/llm-interface/llm_interface-0.1.1-py3-none-any.whl/llm_interface/__init__.py
@@ -0,0 +1,6 @@
+from .llm_config import llm_from_config
+from .llm_interface import LLMInterface
+from .llm_tool import tool
+
+__version__ = "0.1.0"
+__all__ = ["LLMInterface", "llm_from_config", "tool"]
