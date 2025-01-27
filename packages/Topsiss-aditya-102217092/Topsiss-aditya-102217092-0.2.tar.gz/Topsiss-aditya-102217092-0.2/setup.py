966723ef04ad2370b29befdb4462f351d61326c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 04:05:49 +0000

    Add package Topsiss-aditya-102217092/0.2/Topsiss-aditya-102217092-0.2.tar.gz

---
+++ packages/Topsiss-aditya-102217092/Topsiss-aditya-102217092-0.2.tar.gz/Topsiss-aditya-102217092-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsiss-aditya-102217092",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
