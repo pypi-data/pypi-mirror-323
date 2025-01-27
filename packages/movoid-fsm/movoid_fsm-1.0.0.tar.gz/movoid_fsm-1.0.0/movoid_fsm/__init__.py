4f727a0bbba9ab414d77799125a989049b000dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 04:06:01 +0000

    Add package movoid-fsm/1.0.0/movoid_fsm-1.0.0.tar.gz

---
+++ packages/movoid-fsm/movoid_fsm-1.0.0.tar.gz/movoid_fsm-1.0.0/movoid_fsm/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__.py
+# Author        : Sun YiFan-Movoid
+# Time          : 2025/1/25 22:05
+# Description   : 
+"""
+from .fsm import FSM, Fsm, FsmRule
