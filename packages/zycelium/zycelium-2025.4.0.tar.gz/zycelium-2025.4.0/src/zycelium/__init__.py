9362f6237ef3fd9703ec66cc3795635f3bdec1c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 15:05:02 +0000

    Add package zycelium/2025.4.0/zycelium-2025.4.0.tar.gz

---
+++ packages/zycelium/zycelium-2025.4.0.tar.gz/zycelium-2025.4.0/src/zycelium/__init__.py
@@ -0,0 +1,13 @@
+"""Zycelium: Personal Communication and Automation Network"""
+
+from .agent import Agent
+from .logging import get_logger
+
+__version__ = "2025.4.0"
+
+
+__all__ = [
+    "__version__",
+    "Agent",
+    "get_logger",
+]
