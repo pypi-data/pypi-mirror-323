0cfd3cbea46348dbc6926130a6336962882b0c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 15:05:03 +0000

    Add package gai-sdk/1.0.124/gai_sdk-1.0.124.tar.gz

---
+++ packages/gai-sdk/gai_sdk-1.0.124.tar.gz/gai_sdk-1.0.124/clients/selenium/src/gai/selenium/client/selenium_client.py
@@ -0,0 +1,12 @@
+from gai.lib.common.http_utils import http_post_async
+from ..dtos.search_dto import SearchRequest, SearchResponse
+
+class SeleniumClient:
+    
+    async def search(self, query:str, n_results:int=10, period=None) -> SearchResponse:
+        # curl -X POST "http://localhost:12028/api/v1/googler" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"query\":\"python\",\"n_results\":5}"
+        
+        url = 'http://localhost:12028/api/v1/search'
+        params = SearchRequest(query=query, n_results=n_results, period=period).model_dump(exclude_none=True)
+        results = await http_post_async(url, data=params)
+        return SearchResponse(**results.json())
