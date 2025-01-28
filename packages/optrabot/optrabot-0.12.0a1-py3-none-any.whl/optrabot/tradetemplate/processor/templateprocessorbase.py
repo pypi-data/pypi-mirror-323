6b0c28dae87002acf954f861db4fe4c13e5648f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:06:19 +0000

    Add package optrabot/0.12.0a1/optrabot-0.12.0a1-py3-none-any.whl

---
+++ packages/optrabot/optrabot-0.12.0a1-py3-none-any.whl/optrabot/tradetemplate/processor/templateprocessorbase.py
@@ -0,0 +1,53 @@
+import optrabot.config as optrabotcfg
+from loguru import logger
+from optrabot.broker.brokerfactory import BrokerFactory
+from optrabot.broker.order import OptionRight, Order
+from optrabot.config import Config
+from optrabot.signaldata import SignalData
+from optrabot.tradetemplate.templatefactory import Template
+from optrabot.trademanager import ManagedTrade
+
+"""
+Base class for all template processors
+"""
+class TemplateProcessorBase:
+
+	def __init__(self, template: Template):
+		"""
+		Initializes the template processor with the given template
+		"""
+		self._template = template
+		self._config: Config = optrabotcfg.appConfig
+
+	def composeEntryOrder(self, signalData: SignalData = None) -> Order:
+		"""
+		Composes the entry order based on the template and the optional signal data
+		"""
+		logger.debug('Creating entry order for template {}', self._template.name)
+
+	def composeTakeProfitOrder(self, managedTrade: ManagedTrade, fillPrice: float) -> Order:
+		"""
+		Composes the take profit order based on the template and the given fill price
+		"""
+		logger.debug('Creating take profit order for trade {}', managedTrade.trade.id)
+
+	def composeStopLossOrder(self, managedTrade: ManagedTrade, fillPrice: float) -> Order:
+		"""
+		Composes the stop loss order based on the template and the given fill price
+		"""
+		logger.debug('Creating stop loss order for trade {}', managedTrade.trade.id)
+
+	def hasTakeProfit(self) -> bool:
+		"""
+		Returns True if the template has a take profit defined
+		"""
+		return self._template.hasTakeProfit()
+	
+	def get_short_strike_from_delta(self, symbol: str, right: OptionRight, delta: int) -> float:
+		"""
+		Returns the short strike based on the given delta via the associated broker
+		connector and the buffered price data
+		"""
+		brokerConnector = BrokerFactory().getBrokerConnectorByAccount(self._template.account)
+		assert brokerConnector != None
+		return brokerConnector.get_strike_by_delta(symbol, right, delta)
