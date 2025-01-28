37fd19ea6184be7408237182a9d4ecf9cb0b3161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:06:14 +0000

    Add package xaiev/0.0.2/xaiev-0.0.2-py3-none-any.whl

---
+++ packages/xaiev/xaiev-0.0.2-py3-none-any.whl/xaiev/__init__.py
@@ -0,0 +1,21 @@
+from .release import __version__
+
+# the package is imported during installation
+# however installation happens in an isolated build environment
+# where no dependencies are installed.
+
+# this means: no importing the following modules will fail
+# during installation. This is OK, but only during installation
+
+try:
+    from . import core
+    from .core import *
+except ImportError:
+    import os
+
+    # detect whether installation is running
+    if "PIP_BUILD_TRACKER" in os.environ:
+        pass
+    else:
+        # raise the original exception
+        raise
