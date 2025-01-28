4e252f356eedf1bc1b799079bc6853d42a7bf320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:21:43 +0000

    Add package acl-cpp-python/0.4.0/acl-cpp-python-0.4.0.tar.gz

---
+++ packages/acl-cpp-python/acl-cpp-python-0.4.0.tar.gz/acl-cpp-python-0.4.0/tests/test_fenwicktree.py
@@ -0,0 +1,13 @@
+from acl_cpp.fenwicktree import fenwick_tree
+import pytest
+
+def test_fenwicktree():
+    a = fenwick_tree()
+    a = fenwick_tree(5)
+    a.add(0, 1)
+    assert a.sum(0, 5) == 1
+
+    with pytest.raises(ValueError):
+        a.add(-1, 0)
+    with pytest.raises(ValueError):
+        a.sum(1, 0)
