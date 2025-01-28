2a27ffb9be4afc73bf37586bdb1d8593cd734bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:06:26 +0000

    Add package DLite-Python/0.5.28/DLite_Python-0.5.28-cp39-cp39-musllinux_1_2_i686.whl

---
+++ packages/DLite-Python/DLite_Python-0.5.28-cp39-cp39-musllinux_1_2_i686.whl/dlite/paths.py
@@ -0,0 +1,13 @@
+dlite_INSTALL_ROOT = '/project/python/build/temp.linux-i686-cpython-39/{project}/python/build/lib.manylinux2014_x86_64-3.9'
+dlite_BUILD_ROOT = '/project/python/build/temp.linux-i686-cpython-39'
+
+dlite_PATH = ''
+dlite_LD_LIBRARY_PATH = '/project/python/build/temp.linux-i686-cpython-39/src;/project/python/build/temp.linux-i686-cpython-39/src/utils;/project/python/build/temp.linux-i686-cpython-39/src/pyembed'
+dlite_PYTHONPATH = '/project/python/build/temp.linux-i686-cpython-39/bindings/python/tmp/pip-build-env-7ml92imh/site'
+dlite_STORAGE_PLUGINS = '/project/python/build/temp.linux-i686-cpython-39/storages/json;/project/python/build/temp.linux-i686-cpython-39/storages/rdf;/project/python/build/temp.linux-i686-cpython-39/storages/python'
+dlite_MAPPING_PLUGINS = ''
+dlite_PYTHON_STORAGE_PLUGINS = '/project/storages/python/python-storage-plugins'
+dlite_PYTHON_MAPPING_PLUGINS = '/project/bindings/python/python-mapping-plugins'
+dlite_PYTHON_PROTOCOL_PLUGINS = '/project/bindings/python/python-protocol-plugins'
+dlite_TEMPLATES = '/project/tools/templates'
+dlite_STORAGES = '/project/examples/storages/*.json'
