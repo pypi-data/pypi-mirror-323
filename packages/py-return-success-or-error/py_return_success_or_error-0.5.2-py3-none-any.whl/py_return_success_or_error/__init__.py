8237ebb3c75e7181092394cc858c4ceee1d1e39a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 03:22:56 +0000

    Add package py-return-success-or-error/0.5.2/py_return_success_or_error-0.5.2-py3-none-any.whl

---
+++ packages/py-return-success-or-error/py_return_success_or_error-0.5.2-py3-none-any.whl/py_return_success_or_error/__init__.py
@@ -0,0 +1,17 @@
+from py_return_success_or_error.bases.usecase_base import (
+    UsecaseBase,
+    UsecaseBaseCallData,
+)
+from py_return_success_or_error.core.empty import EMPTY, Empty
+from py_return_success_or_error.core.return_success_or_error import (
+    ErrorReturn,
+    ReturnSuccessOrError,
+    SuccessReturn,
+)
+from py_return_success_or_error.interfaces.app_error import AppError, ErrorGeneric
+from py_return_success_or_error.interfaces.datasource import Datasource
+from py_return_success_or_error.interfaces.parameters_return_result import (
+    NoParams,
+    ParametersReturnResult,
+)
+
