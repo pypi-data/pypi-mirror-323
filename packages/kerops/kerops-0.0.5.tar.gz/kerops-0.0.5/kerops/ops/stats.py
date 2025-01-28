5c650dc2cb0fb501fbdd1da4fe9bbf8146e88d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 03:23:03 +0000

    Add package kerops/0.0.5/kerops-0.0.5.tar.gz

---
+++ packages/kerops/kerops-0.0.5.tar.gz/kerops-0.0.5/kerops/ops/stats.py
@@ -0,0 +1,63 @@
+from functools import reduce
+from math import ceil, floor, log2
+
+import torch
+from triton import next_power_of_2
+
+from ..kernels.stats import _Stats_cl3d_backward_impl, _Stats_cl3d_impl
+from ..settings import ConfigurableArg, configure, get_l1_cache
+
+
+@configure(l1_cache_bytes=get_l1_cache, num_warps=4)
+def Stats(x, *, l1_cache_bytes: ConfigurableArg, num_warps: ConfigurableArg):
+    num_channels = x.shape[1]
+    numel = x.numel()
+    assert x.ndim == 5
+    assert num_channels == next_power_of_2(num_channels)
+    assert x.dtype == torch.float16
+    assert x.is_contiguous(memory_format=torch.channels_last_3d)
+
+    MAX_SIZE = l1_cache_bytes // x.element_size()  # 32768 for fp16
+    numel_no_channels = reduce(lambda x, y: x * y, [s if idx != 1 else 1 for idx, s in enumerate(x.shape)], 1)
+    other = min(MAX_SIZE // num_channels, numel_no_channels)
+    other = int(2 ** (floor(log2(other))))
+    BLOCK_SIZE = num_channels * other
+    grid_size = ceil(numel / BLOCK_SIZE)
+
+    mean = torch.zeros(num_channels, dtype=torch.float32, device=x.device)
+    sqmean = torch.zeros(num_channels, dtype=torch.float32, device=x.device)
+
+    _Stats_cl3d_impl[(grid_size,)](x, mean, sqmean, numel_no_channels, num_channels, other, num_warps=num_warps)
+    return mean, sqmean
+
+
+@configure(l1_cache_bytes=get_l1_cache, num_warps=4)
+def StatsBackward(x, mean_grad, sqmean_grad, *, l1_cache_bytes: ConfigurableArg, num_warps: ConfigurableArg):
+    num_channels = x.shape[1]
+    numel = x.numel()
+    assert x.ndim == 5
+    assert num_channels == next_power_of_2(num_channels)
+    assert x.dtype == torch.float16
+    assert mean_grad.numel() == sqmean_grad.numel() == num_channels
+    assert x.is_contiguous(memory_format=torch.channels_last_3d)
+
+    MAX_SIZE = l1_cache_bytes // x.element_size()  # 32768 for fp16
+    numel_no_channels = reduce(lambda x, y: x * y, [s if idx != 1 else 1 for idx, s in enumerate(x.shape)], 1)
+    other = min(MAX_SIZE // num_channels, numel_no_channels)
+    other = int(2 ** (floor(log2(other))))
+    BLOCK_SIZE = num_channels * other
+    grid_size = ceil(numel / BLOCK_SIZE)
+
+    output_grad = torch.empty_like(x)
+
+    _Stats_cl3d_backward_impl[(grid_size,)](
+        x,
+        mean_grad,
+        sqmean_grad,
+        output_grad,
+        numel_no_channels,
+        num_channels=num_channels,
+        block_other=other,
+        num_warps=num_warps,
+    )
+    return output_grad
---
+++ packages/kerops/kerops-0.0.5.tar.gz/kerops-0.0.5/kerops/utils.py
@@ -0,0 +1,102 @@
+import torch
+from torch.nn import functional as F
+
+
+@torch.no_grad
+def allclose_two_stage(
+    input,
+    other,
+    rtol_strict=1e-4,
+    atol_strict=1e-4,
+    percentile_strict=0.99,
+    rtol_narrow=1e-3,
+    atol_narrow=1e-3,
+    debug_info=None,
+    precision=None,
+):
+    if isinstance(debug_info, str):
+        if debug_info == 'print':
+            debug_info = print
+        else:
+            raise ValueError(f'Unknown debug_info mode - {debug_info}')
+    elif debug_info is None:
+        debug_info = lambda x: None
+
+    assert input.shape == other.shape
+    assert input.dtype == other.dtype
+    assert input.device == other.device
+
+    if precision is None:
+        precision = input.dtype
+
+    input = input.reshape(-1)
+    other = other.reshape(-1)
+
+    strict_map = torch.abs(input.to(precision) - other.to(precision)) < atol_strict + rtol_strict * torch.abs(
+        other.to(precision)
+    )
+
+    size = input.numel()
+    num_true = strict_map.sum().item()
+
+    ratio = num_true / size
+    more_less = '>=' if ratio >= percentile_strict else '<'
+    debug_info(f'strict stage - {size=}, {num_true=}, {ratio=} {more_less} percentile_strict')
+
+    if ratio >= percentile_strict:
+        debug_info('Strict threshold has been passed')
+    else:
+        debug_info('Strict threshold has not been passed')
+
+    if size == num_true:
+        debug_info('Strict threshold has been passed completely, propose decreasing rtol_strict and atol_strict')
+        return True
+
+    if num_true / size > percentile_strict:
+        indices = torch.nonzero(~strict_map)
+
+        input = input[indices].to(precision)
+        other = other[indices].to(precision)
+
+        narrow_map = torch.abs(input - other) < atol_narrow + rtol_narrow * torch.abs(other)
+
+        if narrow_map.all():
+            debug_info('Soft threshold has been passed')
+            return True
+        else:
+            debug_info('Soft threshold has not been passed')
+
+    return False
+
+
+def weight_grad_similarity(input, other, rtol_cos=1e-4, atol_cos=1e-4, rtol_len=1e-3, atol_len=1e-3, debug_info=None):
+    if isinstance(debug_info, str):
+        if debug_info == 'print':
+            debug_info = print
+        else:
+            raise ValueError(f'Unknown debug_info mode - {debug_info}')
+    elif debug_info is None:
+        debug_info = lambda x: None
+
+    assert input.shape == other.shape
+    assert input.dtype == other.dtype
+    assert input.device == other.device
+
+    input_flat = input.reshape(-1)
+    other_flat = other.reshape(-1)
+
+    cos_sim = F.cosine_similarity(input_flat, other_flat, dim=0)
+
+    if not torch.allclose(torch.ones_like(cos_sim), cos_sim, rtol=rtol_cos, atol=atol_cos):
+        debug_info(f'Direction test failed - {cos_sim}')
+        return False
+
+    magnitudes_close = torch.allclose(torch.norm(input_flat), torch.norm(other_flat), rtol=rtol_len, atol=atol_len)
+
+    if not magnitudes_close:
+        debug_info(f'Magnitude test failed - {torch.norm(input_flat)=}, {torch.norm(other_flat)=}')
+        return False
+
+    debug_info('All stages passed')
+
+    return True
