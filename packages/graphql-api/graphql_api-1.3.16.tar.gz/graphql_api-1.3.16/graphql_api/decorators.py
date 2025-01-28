63dec81ab7a76928323dcdadccb6bd483f1bf96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 03:22:57 +0000

    Add package graphql-api/1.3.16/graphql_api-1.3.16.tar.gz

---
+++ packages/graphql-api/graphql_api-1.3.16.tar.gz/graphql_api-1.3.16/graphql_api/decorators.py
@@ -0,0 +1,18 @@
+from graphql_api.api import build_decorator
+
+
+def field(meta=None, mutable=False, directives=None):
+    return build_decorator(
+        None, meta, type="field", mutable=mutable, directives=directives
+    )
+
+
+def type(meta=None, abstract=False, interface=False, directives=None):
+    return build_decorator(
+        None,
+        meta,
+        type="object",
+        abstract=abstract,
+        interface=interface,
+        directives=directives,
+    )
