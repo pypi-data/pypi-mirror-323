fe7743473706ddda2418ca0f478beae3a58d9b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 03:22:54 +0000

    Add package mkdocs-puml-file/1.0.3/mkdocs_puml_file-1.0.3.tar.gz

---
+++ packages/mkdocs-puml-file/mkdocs_puml_file-1.0.3.tar.gz/mkdocs_puml_file-1.0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os.path
+
+def read(name):
+    mydir = os.path.abspath(os.path.dirname(__file__))
+    return open(os.path.join(mydir, name)).read()
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="mkdocs_puml_file",
+    version="1.0.3",
+    description="A MkDocs plugin that allows to embed PlantUML files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="mkdocs plantuml publishing documentation uml sequence diagram",
+    url="https://github.com/TomMeHo/mkdocs_puml_file",
+    author="Thomas Meder",
+    license="MIT",
+    #python_requires=">=3.10",
+    install_requires=["mkdocs"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests", "example"]
+    ),
+    entry_points={
+        "mkdocs.plugins": [ "puml-file = mkdocs_puml_file:PlantUmlFilePlugin", ] },
+)
