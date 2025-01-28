cda3f33a3b99a6562e444ac0edb77d52938994b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:10:12 +0000

    Add package netbox-ddns/1.5.0/netbox_ddns-1.5.0-py3-none-any.whl

---
+++ packages/netbox-ddns/netbox_ddns-1.5.0-py3-none-any.whl/netbox_ddns/__init__.py
@@ -0,0 +1,30 @@
+VERSION = '1.5.0'
+
+try:
+    from netbox.plugins import PluginConfig
+except ImportError:
+    # Dummy for when importing outside of netbox
+    class PluginConfig:
+        pass
+
+
+class NetBoxDDNSConfig(PluginConfig):
+    name = 'netbox_ddns'
+    verbose_name = 'Dynamic DNS'
+    version = VERSION
+    min_version = '4.0.0'
+    max_version = '4.2.999'
+    author = 'Sander Steffann'
+    author_email = 'sander@steffann.nl'
+    description = 'Dynamic DNS Connector for NetBox'
+    base_url = 'ddns'
+    required_settings = []
+    default_settings = {}
+
+    def ready(self):
+        super().ready()
+
+        from . import signals
+
+
+config = NetBoxDDNSConfig
