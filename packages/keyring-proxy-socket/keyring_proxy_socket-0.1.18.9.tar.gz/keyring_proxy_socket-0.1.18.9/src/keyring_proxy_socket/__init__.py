d2121cb1a1cd7efa4931bf5e6704af76cfc29534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:05:52 +0000

    Add package keyring-proxy-socket/0.1.18.9/keyring_proxy_socket-0.1.18.9.tar.gz

---
+++ packages/keyring-proxy-socket/keyring_proxy_socket-0.1.18.9.tar.gz/keyring_proxy_socket-0.1.18.9/src/keyring_proxy_socket/__init__.py
@@ -0,0 +1,22 @@
+from typing import override
+
+from jaraco.classes import properties
+from keyring_proxy.backend import ProxyBackend
+from keyring_proxy.socketproxy import SocketClient, default_socket_mgr_client
+from keyring_proxy.transport import TransportClient
+
+PRIORITY = 9.7
+
+
+class SocketProxyBackend(ProxyBackend):
+    socket_path: str | None = None
+    socket_host: str | None = None
+    socket_port: str | int | None = None
+
+    @override
+    def _get_transport(self) -> TransportClient:
+        return SocketClient(default_socket_mgr_client(self.socket_path, self.socket_host, self.socket_port))
+
+    @properties.classproperty
+    def priority(cls):
+        return PRIORITY
