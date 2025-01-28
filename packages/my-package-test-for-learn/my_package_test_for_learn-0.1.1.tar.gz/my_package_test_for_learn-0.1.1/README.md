d86f5d886a2cb3ff0c8489811801232ba20a6bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 03:05:46 +0000

    Add package my-package-test-for-learn/0.1.1/my_package_test_for_learn-0.1.1.tar.gz

---
+++ packages/my-package-test-for-learn/my_package_test_for_learn-0.1.1.tar.gz/my_package_test_for_learn-0.1.1/README.md
@@ -0,0 +1,22 @@
+# My Package
+
+`my_package` is a simple Python package for demonstration purposes.
+
+## Installation
+
+```bash
+pip install my-package-test-for-learn==0.1.0
+```
+
+or
+
+```bash
+pip install git+https://github.com/GauravKaklotar/my_package.git
+```
+
+
+## Usage
+
+```python
+from my_package import greet
+```
---
+++ packages/my-package-test-for-learn/my_package_test_for_learn-0.1.1.tar.gz/my_package_test_for_learn-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_package_test_for_learn",
+    version="0.1.1",
+    author="Gaurav Kaklotar",
+    author_email="gauravkaklotar2003@gmail.com",
+    description="A simple example Python package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/GauravKaklotar/my_package",  
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
