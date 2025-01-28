cfb14e2af07dd9cbcfff50d1244a5ed9593a0d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 03:05:48 +0000

    Add package django-bridge/0.3.2/django_bridge-0.3.2.tar.gz

---
+++ packages/django-bridge/django_bridge-0.3.2.tar.gz/django_bridge-0.3.2/django_bridge/test/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoBridgeTestAppConfig(AppConfig):
+    label = "django_bridge_test"
+    name = "django_bridge.test"
+    verbose_name = "Django Bridge tests"
