e5dc9a4433ce9e1d4369e8328d4443351a163557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:34:21 +0000

    Add package crawlee/0.5.3b10/crawlee-0.5.3b10.tar.gz

---
+++ packages/crawlee/crawlee-0.5.3b10.tar.gz/crawlee-0.5.3b10/src/crawlee/_utils/models.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from contextlib import suppress
+from datetime import timedelta
+from typing import Annotated, Any, Callable
+
+from pydantic import PlainSerializer, TypeAdapter, ValidationError, WrapValidator
+
+"""Utility types for Pydantic models."""
+
+
+def _timedelta_to_ms(td: timedelta | None) -> float | None:
+    if td == timedelta.max:
+        return float('inf')
+    if td is None:
+        return td
+    return int(round(td.total_seconds() * 1000))
+
+
+_number_parser = TypeAdapter(float)
+
+
+def _timedelta_from_ms(value: float | timedelta | Any | None, handler: Callable[[Any], timedelta]) -> timedelta | None:
+    if value == float('inf'):
+        return timedelta.max
+
+    # If the value is a string-encoded number, decode it
+    if isinstance(value, str):
+        with suppress(ValidationError):
+            value = _number_parser.validate_python(value)
+
+    if not isinstance(value, (int, float)):
+        return handler(value)
+
+    return timedelta(milliseconds=value)
+
+
+timedelta_ms = Annotated[timedelta, PlainSerializer(_timedelta_to_ms), WrapValidator(_timedelta_from_ms)]
