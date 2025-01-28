c499d753a62e32a01f22fbc6b0858277c546a5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 03:23:04 +0000

    Add package Xfer4sureTest-package/0.2.1/Xfer4sureTest-package-0.2.1.tar.gz

---
+++ packages/Xfer4sureTest-package/Xfer4sureTest-package-0.2.1.tar.gz/Xfer4sureTest-package-0.2.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Xfer4sureTest-package",
+    version="0.2.1",
+    packages=find_packages(),
+    package_data={
+        'Xfer4sureTest_package': ['__pycache__/*.pyc'],  # Include the .pyc files in the package
+    },
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'run-your-script = Xfer4sureTest_package.script:run',  # Add this to run `run` from script.pyc
+        ],
+    },
+)
+
