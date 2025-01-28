06c6387efded5430a7812f9902b898deb1e04210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:06:23 +0000

    Add package simbricks-utils/0.0.5/simbricks_utils-0.0.5.tar.gz

---
+++ packages/simbricks-utils/simbricks_utils-0.0.5.tar.gz/simbricks_utils-0.0.5/simbricks/utils/file.py
@@ -0,0 +1,49 @@
+# Copyright 2025 Max Planck Institute for Software Systems, and
+# National University of Singapore
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""Utility functions for operations on files and directories."""
+
+import os
+import asyncio
+import pathlib
+import shutil
+
+
+async def await_file(path: str, delay=0.05, verbose=False, timeout=30) -> None:
+    if verbose:
+        print(f"await_file({path})")
+    t = 0
+    while not os.path.exists(path):
+        if t >= timeout:
+            raise TimeoutError()
+        await asyncio.sleep(delay)
+        t += delay
+
+
+def mkdir(path: str) -> None:
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+
+
+def rmtree(path: str) -> None:
+    if os.path.isdir(path):
+        shutil.rmtree(path, ignore_errors=True)
+    elif os.path.exists(path):
+        os.unlink(path)
