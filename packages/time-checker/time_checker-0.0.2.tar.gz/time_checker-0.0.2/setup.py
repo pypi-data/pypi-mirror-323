c9c5f1cc91c47914644c798f8c1e9dbf34cb0e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 03:23:02 +0000

    Add package time-checker/0.0.2/time_checker-0.0.2.tar.gz

---
+++ packages/time-checker/time_checker-0.0.2.tar.gz/time_checker-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of the README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="time_checker",
+    version="0.0.2",
+    description="A reusable timer utility for Python scripts",
+    long_description=long_description,  # Add this line
+    long_description_content_type="text/markdown",  # Specify README format
+    author="Deep Korat",
+    author_email="deepkorat13@gmail.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
