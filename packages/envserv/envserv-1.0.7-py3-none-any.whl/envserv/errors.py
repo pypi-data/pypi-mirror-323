1871288e6c58aee49ad97ad505476f212b895fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 03:22:50 +0000

    Add package envserv/1.0.7/envserv-1.0.7-py3-none-any.whl

---
+++ packages/envserv/envserv-1.0.7-py3-none-any.whl/envserv/errors.py
@@ -0,0 +1,19 @@
+class EnvfileError(ImportError):
+    def __init__(self, *args: object, name: str | None = ..., path: str | None = ...) -> None:
+        super().__init__(*args, name=name, path=path)
+        
+class EnvVariableError(NameError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+        
+class EnvTypeError(TypeError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+        
+class EnvValueError(ValueError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+        
+class EnvOverwriteError(ValueError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
---
+++ packages/envserv/envserv-1.0.7-py3-none-any.whl/envserv/typing/variable.py
@@ -0,0 +1,19 @@
+class Variable:
+    def __init__(self,
+            var = ...,
+            alias:str|None = None,
+            overwrite:bool = True,
+            error:bool = True,
+        ) -> None:
+        self.var = var
+        self.alias = alias
+        self.overwrite = overwrite
+        self.error = error
+        
+def variable(
+        var = ...,
+        alias:str|None = None,
+        overwrite:bool = True,
+        error:bool = True
+    ) -> Variable:
+    return Variable(var=var, alias=alias, overwrite=overwrite, error=error)
