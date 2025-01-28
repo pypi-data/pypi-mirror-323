1984beae64bf1e0cbb4a85e2595aacfa55b69b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:05:53 +0000

    Add package pip-agents-core/0.0.2/pip_agents_core-0.0.2-py3-none-any.whl

---
+++ packages/pip-agents-core/pip_agents_core-0.0.2-py3-none-any.whl/pip_agents_core/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""pip_agents_core.__init__.
+
+Commons module initialization.
+    
+    :copyright: Enterprise Innovation Consulting 2024, see AUTHORS for more details.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__all__ = []
+
---
+++ packages/pip-agents-core/pip_agents_core-0.0.2-py3-none-any.whl/pip_agents_core/content/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    pip_agents_core.content.__init__
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    Todo...
+    
+    :copyright: Enterprise Innovation Consulting 2024, see AUTHORS for more details.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__all__ = []
