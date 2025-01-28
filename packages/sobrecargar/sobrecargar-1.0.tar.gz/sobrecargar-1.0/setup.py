640120a390beab4cfda4015b12c8966aaf64de8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:21:36 +0000

    Add package sobrecargar/1.0/sobrecargar-1.0.tar.gz

---
+++ packages/sobrecargar/sobrecargar-1.0.tar.gz/sobrecargar-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Medios_ARG',
+    description="setuptools.build_meta",
+    version='1.0',
+    author           = 'Hernán A. Teszkiewicz Novick',
+    author_email     = 'herni@cajadeideas.ar',
+    url= 'https://github.com/Hernanatn/sobrecargar.py',
+    download_url     =  'https://pypi.org/project/sobrecargar/0.1.9/#files',
+    packages=['sobrecargar'],
+)
