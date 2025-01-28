59c364f51b463e084a43e830703b2473ebab89b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:10:12 +0000

    Add package wizata-test-lib/0.1/wizata_test_lib-0.1.tar.gz

---
+++ packages/wizata-test-lib/wizata_test_lib-0.1.tar.gz/wizata_test_lib-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='wizata_test_lib',
+    version="0.1",
+    description='Wizata Sample Library',
+    author='Wizata S.A.',
+    author_email='info@wizata.com',
+    packages=['wizata_test_lib']
+)
