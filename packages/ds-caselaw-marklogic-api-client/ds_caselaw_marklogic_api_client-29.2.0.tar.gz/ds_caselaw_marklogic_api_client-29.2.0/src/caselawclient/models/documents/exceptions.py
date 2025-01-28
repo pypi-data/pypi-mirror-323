7e196ce62bb4cdc60cebb21a4632550e43bfd5f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:06:18 +0000

    Add package ds-caselaw-marklogic-api-client/29.2.0/ds_caselaw_marklogic_api_client-29.2.0.tar.gz

---
+++ packages/ds-caselaw-marklogic-api-client/ds_caselaw_marklogic_api_client-29.2.0.tar.gz/ds_caselaw_marklogic_api_client-29.2.0/src/caselawclient/models/documents/exceptions.py
@@ -0,0 +1,10 @@
+class CannotPublishUnpublishableDocument(Exception):
+    """A document which has failed publication safety checks in `Document.is_publishable` cannot be published."""
+
+
+class DocumentNotSafeForDeletion(Exception):
+    """A document which is not safe for deletion cannot be deleted."""
+
+
+class InvalidDocumentURIException(Exception):
+    """The document URI is not valid."""
