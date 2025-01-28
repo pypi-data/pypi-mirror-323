8c4011ff97259502eef5632da53141b5cbc19bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:21:58 +0000

    Add package RATapi/0.0.0.dev4/RATapi-0.0.0.dev4-cp310-cp310-win_amd64.whl

---
+++ packages/RATapi/RATapi-0.0.0.dev4-cp310-cp310-win_amd64.whl/RATapi/examples/normal_reflectivity/background_function.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def backgroundFunction(xdata, params):
+    # Split up the params array
+    Ao = params[0]
+    k = params[1]
+    back_const = params[2]
+
+    # Make an exponential decay background
+    background = Ao * np.exp(-k * np.array(xdata)) + back_const
+
+    return background
