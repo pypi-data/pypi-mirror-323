6ac1d8ada038f52e3be17b3c3a407287b6056463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:06:16 +0000

    Add package markata/0.9.0.dev4/markata-0.9.0.dev4-py3-none-any.whl

---
+++ packages/markata/markata-0.9.0.dev4-py3-none-any.whl/markata/plugins/flat_slug.py
@@ -0,0 +1,101 @@
+"""
+The articles slug is what determines the url to your page.  It should be
+sanitized of special characters that do not work in the browser.
+
+
+## Explicit Slug in Frontmatter
+
+If you explicitly set the slug in the frontmatter of a post, markata will not
+overwrite it.
+
+```markdown
+---
+title: My First Post
+slug: /my-post
+
+---
+
+This is my first post it will be at `<markata.config.url>/my-post/`
+reguardless of filename.
+```
+
+## Automatic Slug Based on Filename
+
+By default the flat_slug plugin will use the `stem` of your filename, which is
+the filename without the extension, unless you explicitly set your slug in
+frontmatter.
+
+* `/pages/my-post.md` becomes `<markata.config.url>/my-post/`
+* `/pages/blog/a-blog-post.md` becomes `<markata.config.url>/a-blog-post/`
+"""
+
+from pathlib import Path
+from typing import Optional, Any
+
+import pydantic
+
+from markata import Markata
+from markata.hookspec import hook_impl, register_attr
+
+
+class FlatSlugConfig(pydantic.BaseModel):
+    slugify: bool = True
+
+
+class Config(pydantic.BaseModel):
+    flat_slug: FlatSlugConfig = FlatSlugConfig()
+
+
+class FlatSlugPost(pydantic.BaseModel):
+    should_slugify: Optional[bool] = None
+    markata: Any = pydantic.Field(None, exclude=True)
+
+    model_config = pydantic.ConfigDict(
+        validate_assignment=False,
+        arbitrary_types_allowed=True,
+        extra="allow",
+        str_strip_whitespace=True,
+        validate_default=True,
+        coerce_numbers_to_str=True,
+        populate_by_name=True,
+    )
+
+    @pydantic.field_validator("should_slugify", mode="before")
+    @classmethod
+    def default_slugify(cls, v: Optional[bool], info) -> bool:
+        if v is None:
+            markata = info.data.get("markata")
+            if markata is None:
+                return True  # Default to True if no markata instance
+            return markata.config.flat_slug.slugify
+        return v
+
+
+@hook_impl()
+@register_attr("config_models")
+def config_model(markata: Markata) -> None:
+    markata.config_models.append(Config)
+
+
+@hook_impl
+@register_attr("post_models")
+def post_model(markata: "Markata") -> None:
+    markata.post_models.append(FlatSlugPost)
+
+
+@hook_impl(tryfirst=True)
+def pre_render(markata: "Markata") -> None:
+    """
+    Sets the article slug if one is not already set in the frontmatter.
+    """
+    from slugify import slugify
+
+    for article in markata.iter_articles(description="creating slugs"):
+        stem = article.get(
+            "slug",
+            Path(article.get("path", article.get("title", ""))).stem,
+        )
+        if article.should_slugify:
+            article.slug = "/".join([slugify(s) for s in stem.split("/")])
+        else:
+            article.slug = stem
---
+++ packages/markata/markata-0.9.0.dev4-py3-none-any.whl/markata/plugins/rss.py
@@ -0,0 +1,77 @@
+"""Default glob plugin"""
+
+import datetime
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import pytz
+
+
+from markata.hookspec import hook_impl
+
+if TYPE_CHECKING:
+    from markata import Markata
+    from feedgen.feed import FeedGenerator
+
+    class MarkataRss(Markata):
+        fg: "FeedGenerator"
+        rss: str
+
+
+@hook_impl(trylast=True)
+def render(markata: "MarkataRss") -> None:
+    from feedgen.feed import FeedGenerator
+
+    fg = FeedGenerator()
+    url = markata.config.url or ""
+    title = markata.config.title
+    name = markata.config.author_name
+    email = markata.config.author_email
+    icon = str(markata.config.icon)
+    lang = markata.config.lang
+    rss_description = markata.config.rss_description or "rss feed"
+
+    fg.id(str(url) + "/rss.xml")
+    fg.title(title)
+    fg.author(
+        {
+            "name": name,
+            "email": email,
+        },
+    )
+    fg.link(href=str(url), rel="alternate")
+    fg.logo(icon)
+    fg.subtitle(rss_description)
+    fg.link(href=str(url) + "/rss.xml", rel="self")
+    fg.language(lang)
+
+    try:
+        all_posts = sorted(markata.articles, key=lambda x: x["date"], reverse=True)
+        posts = [post for post in all_posts if post["published"] == "True"]
+    except BaseException:
+        posts = markata.articles
+
+    for article in posts:
+        fe = fg.add_entry()
+        fe.id(str(url + "/" + article.slug))
+        fe.title(article.title)
+        fe.published(
+            datetime.datetime.combine(
+                article.date or datetime.datetime.min.date(),
+                datetime.datetime.min.time(),
+                pytz.UTC,
+            )
+        )
+        fe.description(article.description)
+        fe.summary(article.long_description)
+        fe.link(href=str(url) + "/" + article.slug)
+        fe.content(article.article_html.translate(dict.fromkeys(range(32))))
+
+    markata.fg = fg
+    markata.rss = fg.rss_str(pretty=True)
+
+
+@hook_impl
+def save(markata: "MarkataRss") -> None:
+    output_dir = Path(markata.config["output_dir"])
+    markata.fg.rss_file(str(output_dir / "rss.xml"))
