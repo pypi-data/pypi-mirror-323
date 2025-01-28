e895273ca2dc7b5a2ee67a899186306af3930f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:06:02 +0000

    Add package comwatt-client-legacy/0.1.1/comwatt_client_legacy-0.1.1.tar.gz

---
+++ packages/comwatt-client-legacy/comwatt_client_legacy-0.1.1.tar.gz/comwatt_client_legacy-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='comwatt-client-legacy',
+    version='0.1.1',
+    author='Matéo Greil',
+    author_email='contact@greil.fr',
+    description='Python Client for Comwatt API (legacy: go.energy.comwatt)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.comw/MateoGreil/python-comwatt-client-legacy",
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
