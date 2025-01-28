6b6f4acc422603bcc15e30604dba2c2ff3ea0af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-28 04:21:49 +0000

    Add package scaleway-async/2.8.0/scaleway_async-2.8.0.tar.gz

---
+++ packages/scaleway-async/scaleway_async-2.8.0.tar.gz/scaleway_async-2.8.0/scaleway_async/webhosting/v1/__init__.py
@@ -0,0 +1,189 @@
+# This file was automatically generated. DO NOT EDIT.
+# If you have any remark or suggestion do not hesitate to open an issue.
+from .types import DnsRecordStatus
+from .types import DnsRecordType
+from .types import DnsRecordsStatus
+from .types import HostingStatus
+from .content import HOSTING_TRANSIENT_STATUSES
+from .types import HostingSummaryStatus
+from .content import HOSTING_SUMMARY_TRANSIENT_STATUSES
+from .types import ListDatabaseUsersRequestOrderBy
+from .types import ListDatabasesRequestOrderBy
+from .types import ListFtpAccountsRequestOrderBy
+from .types import ListHostingsRequestOrderBy
+from .types import ListMailAccountsRequestOrderBy
+from .types import ListOffersRequestOrderBy
+from .types import ListWebsitesRequestOrderBy
+from .types import NameserverStatus
+from .types import OfferOptionName
+from .types import OfferOptionWarning
+from .types import PlatformPlatformGroup
+from .types import PlatformControlPanelUrls
+from .types import OfferOption
+from .types import PlatformControlPanel
+from .types import CreateDatabaseRequestUser
+from .types import CreateHostingRequestDomainConfiguration
+from .types import OfferOptionRequest
+from .types import SyncDomainDnsRecordsRequestRecord
+from .types import DnsRecord
+from .types import Nameserver
+from .types import HostingUser
+from .types import Offer
+from .types import Platform
+from .types import ControlPanel
+from .types import DatabaseUser
+from .types import Database
+from .types import FtpAccount
+from .types import HostingSummary
+from .types import MailAccount
+from .types import Website
+from .types import CheckUserOwnsDomainResponse
+from .types import ControlPanelApiListControlPanelsRequest
+from .types import DatabaseApiAssignDatabaseUserRequest
+from .types import DatabaseApiChangeDatabaseUserPasswordRequest
+from .types import DatabaseApiCreateDatabaseRequest
+from .types import DatabaseApiCreateDatabaseUserRequest
+from .types import DatabaseApiDeleteDatabaseRequest
+from .types import DatabaseApiDeleteDatabaseUserRequest
+from .types import DatabaseApiGetDatabaseRequest
+from .types import DatabaseApiGetDatabaseUserRequest
+from .types import DatabaseApiListDatabaseUsersRequest
+from .types import DatabaseApiListDatabasesRequest
+from .types import DatabaseApiUnassignDatabaseUserRequest
+from .types import DnsApiCheckUserOwnsDomainRequest
+from .types import DnsApiGetDomainDnsRecordsRequest
+from .types import DnsApiSyncDomainDnsRecordsRequest
+from .types import DnsRecords
+from .types import FtpAccountApiChangeFtpAccountPasswordRequest
+from .types import FtpAccountApiCreateFtpAccountRequest
+from .types import FtpAccountApiListFtpAccountsRequest
+from .types import FtpAccountApiRemoveFtpAccountRequest
+from .types import Hosting
+from .types import HostingApiCreateHostingRequest
+from .types import HostingApiCreateSessionRequest
+from .types import HostingApiDeleteHostingRequest
+from .types import HostingApiGetHostingRequest
+from .types import HostingApiGetResourceSummaryRequest
+from .types import HostingApiListHostingsRequest
+from .types import HostingApiResetHostingPasswordRequest
+from .types import HostingApiUpdateHostingRequest
+from .types import ListControlPanelsResponse
+from .types import ListDatabaseUsersResponse
+from .types import ListDatabasesResponse
+from .types import ListFtpAccountsResponse
+from .types import ListHostingsResponse
+from .types import ListMailAccountsResponse
+from .types import ListOffersResponse
+from .types import ListWebsitesResponse
+from .types import MailAccountApiChangeMailAccountPasswordRequest
+from .types import MailAccountApiCreateMailAccountRequest
+from .types import MailAccountApiListMailAccountsRequest
+from .types import MailAccountApiRemoveMailAccountRequest
+from .types import OfferApiListOffersRequest
+from .types import ResetHostingPasswordResponse
+from .types import ResourceSummary
+from .types import Session
+from .types import WebsiteApiListWebsitesRequest
+from .api import WebhostingV1ControlPanelAPI
+from .api import WebhostingV1DatabaseAPI
+from .api import WebhostingV1DnsAPI
+from .api import WebhostingV1OfferAPI
+from .api import WebhostingV1HostingAPI
+from .api import WebhostingV1FtpAccountAPI
+from .api import WebhostingV1MailAccountAPI
+from .api import WebhostingV1WebsiteAPI
+
+__all__ = [
+    "DnsRecordStatus",
+    "DnsRecordType",
+    "DnsRecordsStatus",
+    "HostingStatus",
+    "HOSTING_TRANSIENT_STATUSES",
+    "HostingSummaryStatus",
+    "HOSTING_SUMMARY_TRANSIENT_STATUSES",
+    "ListDatabaseUsersRequestOrderBy",
+    "ListDatabasesRequestOrderBy",
+    "ListFtpAccountsRequestOrderBy",
+    "ListHostingsRequestOrderBy",
+    "ListMailAccountsRequestOrderBy",
+    "ListOffersRequestOrderBy",
+    "ListWebsitesRequestOrderBy",
+    "NameserverStatus",
+    "OfferOptionName",
+    "OfferOptionWarning",
+    "PlatformPlatformGroup",
+    "PlatformControlPanelUrls",
+    "OfferOption",
+    "PlatformControlPanel",
+    "CreateDatabaseRequestUser",
+    "CreateHostingRequestDomainConfiguration",
+    "OfferOptionRequest",
+    "SyncDomainDnsRecordsRequestRecord",
+    "DnsRecord",
+    "Nameserver",
+    "HostingUser",
+    "Offer",
+    "Platform",
+    "ControlPanel",
+    "DatabaseUser",
+    "Database",
+    "FtpAccount",
+    "HostingSummary",
+    "MailAccount",
+    "Website",
+    "CheckUserOwnsDomainResponse",
+    "ControlPanelApiListControlPanelsRequest",
+    "DatabaseApiAssignDatabaseUserRequest",
+    "DatabaseApiChangeDatabaseUserPasswordRequest",
+    "DatabaseApiCreateDatabaseRequest",
+    "DatabaseApiCreateDatabaseUserRequest",
+    "DatabaseApiDeleteDatabaseRequest",
+    "DatabaseApiDeleteDatabaseUserRequest",
+    "DatabaseApiGetDatabaseRequest",
+    "DatabaseApiGetDatabaseUserRequest",
+    "DatabaseApiListDatabaseUsersRequest",
+    "DatabaseApiListDatabasesRequest",
+    "DatabaseApiUnassignDatabaseUserRequest",
+    "DnsApiCheckUserOwnsDomainRequest",
+    "DnsApiGetDomainDnsRecordsRequest",
+    "DnsApiSyncDomainDnsRecordsRequest",
+    "DnsRecords",
+    "FtpAccountApiChangeFtpAccountPasswordRequest",
+    "FtpAccountApiCreateFtpAccountRequest",
+    "FtpAccountApiListFtpAccountsRequest",
+    "FtpAccountApiRemoveFtpAccountRequest",
+    "Hosting",
+    "HostingApiCreateHostingRequest",
+    "HostingApiCreateSessionRequest",
+    "HostingApiDeleteHostingRequest",
+    "HostingApiGetHostingRequest",
+    "HostingApiGetResourceSummaryRequest",
+    "HostingApiListHostingsRequest",
+    "HostingApiResetHostingPasswordRequest",
+    "HostingApiUpdateHostingRequest",
+    "ListControlPanelsResponse",
+    "ListDatabaseUsersResponse",
+    "ListDatabasesResponse",
+    "ListFtpAccountsResponse",
+    "ListHostingsResponse",
+    "ListMailAccountsResponse",
+    "ListOffersResponse",
+    "ListWebsitesResponse",
+    "MailAccountApiChangeMailAccountPasswordRequest",
+    "MailAccountApiCreateMailAccountRequest",
+    "MailAccountApiListMailAccountsRequest",
+    "MailAccountApiRemoveMailAccountRequest",
+    "OfferApiListOffersRequest",
+    "ResetHostingPasswordResponse",
+    "ResourceSummary",
+    "Session",
+    "WebsiteApiListWebsitesRequest",
+    "WebhostingV1ControlPanelAPI",
+    "WebhostingV1DatabaseAPI",
+    "WebhostingV1DnsAPI",
+    "WebhostingV1OfferAPI",
+    "WebhostingV1HostingAPI",
+    "WebhostingV1FtpAccountAPI",
+    "WebhostingV1MailAccountAPI",
+    "WebhostingV1WebsiteAPI",
+]
