6265ecf3defe44eb08020fdfd39ac50f4d4e5755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 01:53:45 +0000

    Add package g4f/0.4.3.3/g4f-0.4.3.3.tar.gz

---
+++ packages/g4f/g4f-0.4.3.3.tar.gz/g4f-0.4.3.3/g4f/Provider/needs_auth/CopilotAccount.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from ..Copilot import Copilot
+
+class CopilotAccount(Copilot):
+    needs_auth = True
+    use_nodriver = True
+    parent = "Copilot"
+    default_model = "Copilot"
+    default_vision_model = default_model
+    models = [default_model]
+    image_models = models
+    model_aliases = {
+        "dall-e-3": default_model
+    }
---
+++ packages/g4f/g4f-0.4.3.3.tar.gz/g4f-0.4.3.3/g4f/Provider/needs_auth/Custom.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from .OpenaiTemplate import OpenaiTemplate
+
+class Custom(OpenaiTemplate):
+    label = "Custom Provider"
+    working = True
+    needs_auth = False
+    api_base = "http://localhost:8080/v1"
+    sort_models = False
