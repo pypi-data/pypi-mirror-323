f59f38da5c849b454faa7f09d9af3026b0a45ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 02:05:42 +0000

    Add package livesession/0.0.0/livesession-0.0.0.tar.gz

---
+++ packages/livesession/livesession-0.0.0.tar.gz/livesession-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='livesession',
+    version='0.0.0',
+    packages=find_packages(),
+    description='LiveSession Python SDK',
+    author='LiveSession',
+    author_email='hello@livesession.io'
+)
