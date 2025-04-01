c12ec6a0f784686914136635f30fa2c785c7572b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 01:50:55 +0000

    Add package monitored-ioloop/0.0.14/monitored_ioloop-0.0.14.tar.gz

---
+++ packages/monitored-ioloop/monitored_ioloop-0.0.14.tar.gz/monitored_ioloop-0.0.14/monitored_ioloop/monitored_asyncio.py
@@ -0,0 +1,53 @@
+import asyncio
+import typing
+from asyncio import Handle
+
+from monitored_ioloop.monitored_ioloop_base import BaseMonitoredEventLoopPolicy
+from monitored_ioloop.monitoring import (
+    wrap_callback_with_monitoring,
+    IoLoopMonitorState,
+    IoLoopInnerState,
+)
+
+
+class MonitoredSelectorEventLoop(asyncio.SelectorEventLoop):
+    def __init__(
+        self,
+        monitor_callback: typing.Callable[[IoLoopMonitorState], None],
+        *args: typing.Any,
+        **kwargs: typing.Any,
+    ):
+        super().__init__(*args, **kwargs)
+        self._monitor_callback = monitor_callback
+        self._state = IoLoopInnerState(handles_count=0)
+
+    def call_soon(
+        self,
+        callback: typing.Callable[..., typing.Any],
+        *args: typing.Any,  # type: ignore
+        **kwargs: typing.Any,
+    ) -> Handle:
+        callback_with_monitoring = wrap_callback_with_monitoring(
+            callback, self._monitor_callback, self._state
+        )
+
+        handle = super().call_soon(callback_with_monitoring, *args, **kwargs)
+        callback_with_monitoring.set_handle(handle)
+        return handle
+
+
+class MonitoredAsyncIOEventLoopPolicy(BaseMonitoredEventLoopPolicy):
+    """Event loop policy.
+
+    The preferred way to make your application use monitored asyncio selector based ioloop:
+
+    >>> import asyncio
+    >>> import monitored_ioloop
+    >>> asyncio.set_event_loop_policy(monitored_ioloop.MonitoredAsyncIOEventLoopPolicy())
+    >>> asyncio.get_event_loop()
+    <uvloop.Loop running=False closed=False debug=False>
+    """
+
+    def _loop_factory(self) -> MonitoredSelectorEventLoop:
+        loop = MonitoredSelectorEventLoop(self._monitor_callback)
+        return loop
