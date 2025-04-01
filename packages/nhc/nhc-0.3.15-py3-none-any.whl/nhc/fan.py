cf382852a56dee9eaec0b9e9da6221cbbb328c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 01:52:48 +0000

    Add package nhc/0.3.15/nhc-0.3.15-py3-none-any.whl

---
+++ packages/nhc/nhc-0.3.15-py3-none-any.whl/nhc/fan.py
@@ -0,0 +1,21 @@
+from .action import NHCAction
+from .const import PRESET_MODES, MODES
+
+class NHCFan(NHCAction):
+  def __init__(self, controller, action):
+    super().__init__(controller, action)
+
+  @property
+  def modes(self) -> list:
+    return MODES
+
+  @property
+  def mode(self) -> str:
+    for mode, value in PRESET_MODES.items():
+        if value == self._state:
+            return mode
+
+    return PRESET_MODES['low']
+
+  def set_mode(self, preset_mode: str) -> None:
+      self._controller.execute(self.id, PRESET_MODES[preset_mode])
