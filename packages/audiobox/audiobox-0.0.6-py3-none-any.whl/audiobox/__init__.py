eb46e9b97ef0c487a192482574fe2af8bb2efdbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 02:07:10 +0000

    Add package audiobox/0.0.6/audiobox-0.0.6-py3-none-any.whl

---
+++ packages/audiobox/audiobox-0.0.6-py3-none-any.whl/audiobox/__init__.py
@@ -0,0 +1,27 @@
+from audiobox.audiobox import (
+    play_music, 
+    sfx, 
+    wait, 
+    generate_example_files, 
+    music_file, 
+    music_on, 
+    get_audio_length, 
+    add_to_playlist,
+    remove_from_playlist, 
+    clear_playlist,
+    play_playlist
+)
+
+__all__ = [
+    "play_music", 
+    "sfx", 
+    "wait", 
+    "generate_example_files", 
+    "music_file", 
+    "music_on", 
+    "get_audio_length", 
+    "add_to_playlist",
+    "remove_from_playlist", 
+    "clear_playlist",
+    "play_playlist"
+]
