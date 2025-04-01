2e8c8fdb6e2271d30c8a5f918f96ebf1ea121bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 01:53:25 +0000

    Add package NuCS/4.8.0/nucs-4.8.0.tar.gz

---
+++ packages/NuCS/nucs-4.8.0.tar.gz/nucs-4.8.0/tests/propagators/test_count_eq.py
@@ -0,0 +1,54 @@
+###############################################################################
+# __   _            _____    _____
+# | \ | |          / ____|  / ____|
+# |  \| |  _   _  | |      | (___
+# | . ` | | | | | | |       \___ \
+# | |\  | | |_| | | |____   ____) |
+# |_| \_|  \__,_|  \_____| |_____/
+#
+# Fast constraint solving in Python  - https://github.com/yangeorget/nucs
+#
+# Copyright 2024-2025 - Yan Georget
+###############################################################################
+import numpy as np
+
+from nucs.constants import PROP_CONSISTENCY, PROP_ENTAILMENT, PROP_INCONSISTENCY
+from nucs.numpy_helper import new_parameters_by_values, new_shr_domains_by_values
+from nucs.propagators.count_eq_propagator import compute_domains_count_eq
+
+
+class TestCountEQ:
+    def test_compute_domains_1(self) -> None:
+        domains = new_shr_domains_by_values([(1, 4), (3, 5), (3, 6), (6, 8), 3, 5, 1])
+        data = new_parameters_by_values([5])
+        assert compute_domains_count_eq(domains, data) == PROP_CONSISTENCY
+        assert np.all(domains == np.array([[1, 4], [3, 4], [3, 6], [6, 8], [3, 3], [5, 5], [1, 1]]))
+
+    def test_compute_domains_2(self) -> None:
+        domains = new_shr_domains_by_values([(1, 4), (3, 5), (3, 6), (6, 8), 3, 5, 2])
+        data = new_parameters_by_values([5])
+        assert compute_domains_count_eq(domains, data) == PROP_CONSISTENCY
+        assert np.all(domains == np.array([[1, 4], [3, 5], [3, 6], [6, 8], [3, 3], [5, 5], [2, 2]]))
+
+    def test_compute_domains_3(self) -> None:
+        domains = new_shr_domains_by_values([(1, 4), (3, 5), (3, 6), (6, 8), 3, 5, 0])
+        data = new_parameters_by_values([5])
+        assert compute_domains_count_eq(domains, data) == PROP_INCONSISTENCY
+
+    def test_compute_domains_4(self) -> None:
+        domains = new_shr_domains_by_values([(1, 4), 5, (3, 6), (6, 8), 3, 5, (1, 2)])
+        data = new_parameters_by_values([5])
+        assert compute_domains_count_eq(domains, data) == PROP_CONSISTENCY
+        assert np.all(domains == np.array([[1, 4], [5, 5], [3, 6], [6, 8], [3, 3], [5, 5], [2, 2]]))
+
+    def test_compute_domains_5(self) -> None:
+        domains = new_shr_domains_by_values([(1, 4), (3, 5), (3, 6), (6, 8), 3, 5, (-1, 10)])
+        data = new_parameters_by_values([5])
+        assert compute_domains_count_eq(domains, data) == PROP_CONSISTENCY
+        assert np.all(domains == np.array([[1, 4], [3, 5], [3, 6], [6, 8], [3, 3], [5, 5], [1, 3]]))
+
+    def test_compute_domains_6(self) -> None:
+        domains = new_shr_domains_by_values([2, (0, 1), (3, 4), 2, 2, (2, 4)])
+        data = new_parameters_by_values([2])
+        assert compute_domains_count_eq(domains, data) == PROP_ENTAILMENT
+        assert np.all(domains == np.array([[2, 2], [0, 1], [3, 4], [2, 2], [2, 2], [3, 3]]))
---
+++ packages/NuCS/nucs-4.8.0.tar.gz/nucs-4.8.0/tests/propagators/test_gcc.py
@@ -0,0 +1,54 @@
+###############################################################################
+# __   _            _____    _____
+# | \ | |          / ____|  / ____|
+# |  \| |  _   _  | |      | (___
+# | . ` | | | | | | |       \___ \
+# | |\  | | |_| | | |____   ____) |
+# |_| \_|  \__,_|  \_____| |_____/
+#
+# Fast constraint solving in Python  - https://github.com/yangeorget/nucs
+#
+# Copyright 2024-2025 - Yan Georget
+###############################################################################
+import numpy as np
+
+from nucs.constants import PROP_CONSISTENCY
+from nucs.numpy_helper import new_parameters_by_values, new_shr_domains_by_values
+from nucs.propagators.gcc_propagator import compute_domains_gcc
+
+
+class TestGCC:
+    def test_compute_domains_0(self) -> None:
+        domains = new_shr_domains_by_values([0])
+        assert compute_domains_gcc(domains, new_parameters_by_values([0, 1, 1])) == PROP_CONSISTENCY
+        assert np.all(domains == np.array([[0, 0]]))
+
+    def test_compute_domains_1(self) -> None:
+        domains = new_shr_domains_by_values([0, 1])
+        assert compute_domains_gcc(domains, new_parameters_by_values([0, 1, 1, 1, 1])) == PROP_CONSISTENCY
+        assert np.all(domains == np.array([[0, 0], [1, 1]]))
+
+    def test_compute_domains_2(self) -> None:
+        domains = new_shr_domains_by_values([0, (0, 1)])
+        assert compute_domains_gcc(domains, new_parameters_by_values([0, 1, 1, 1, 1])) == PROP_CONSISTENCY
+        assert np.all(domains == np.array([[0, 0], [1, 1]]))
+
+    def test_compute_domains_3(self) -> None:
+        domains = new_shr_domains_by_values([0, 2, (1, 2)])
+        assert compute_domains_gcc(domains, new_parameters_by_values([0] + [1] * 6)) == PROP_CONSISTENCY
+        assert np.all(domains == np.array([[0, 0], [2, 2], [1, 1]]))
+
+    def test_compute_domains_4(self) -> None:
+        domains = new_shr_domains_by_values([0, (0, 4), (0, 4), (0, 4), (0, 4)])
+        assert compute_domains_gcc(domains, new_parameters_by_values([0] + [1] * 10)) == PROP_CONSISTENCY
+        assert np.all(domains == np.array([[0, 0], [1, 4], [1, 4], [1, 4], [1, 4]]))
+
+    def test_compute_domains_5(self) -> None:
+        domains = new_shr_domains_by_values([(3, 6), (3, 4), (2, 5), (2, 4), (3, 4), (1, 6)])
+        assert compute_domains_gcc(domains, new_parameters_by_values([1] + [1] * 12)) == PROP_CONSISTENCY
+        assert np.all(domains == np.array([[6, 6], [3, 4], [5, 5], [2, 2], [3, 4], [1, 1]]))
+
+    def test_compute_domains_6(self) -> None:
+        domains = new_shr_domains_by_values([(3, 4), (2, 4), (3, 4), (2, 5), (3, 6), (1, 6)])
+        assert compute_domains_gcc(domains, new_parameters_by_values([1] + [0] * 6 + [1] * 6)) == PROP_CONSISTENCY
+        assert np.all(domains == np.array([[3, 4], [2, 2], [3, 4], [5, 5], [6, 6], [1, 1]]))
