12eb95edf0fd1162c0a7ac41cfd5ae8c31af9647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 01:50:54 +0000

    Add package spread-scoring-utilities/0.1.0/spread_scoring_utilities-0.1.0-py3-none-any.whl

---
+++ packages/spread-scoring-utilities/spread_scoring_utilities-0.1.0-py3-none-any.whl/scoring_utils/__init__.py
@@ -0,0 +1,21 @@
+"""
+-----------------------------------------------------
+Sports Betting Algorithm (PyPy-ready)
+-----------------------------------------------------
+This module contains a number of helper functions
+to numerically and semantically manage sports
+score betting.
+-----------------------------------------------------
+"""
+
+from .scoring_utils import (
+    calculate_ambient_noise_factor,
+    analyze_performance_feedback,
+    calculate_resource_adjustment_factor,
+    generate_capacity_projection,
+    forecast_environmental_conditions,
+    calculate_supplementary_resource_budget,
+    simulate_resource_degradation,
+    score_spreading,
+    score_spreading_clipped
+)
