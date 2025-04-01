c24ced0e980b33992b174ae282ee322e0119bd9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 01:51:59 +0000

    Add package hd2api.py/0.0.1.16.2/hd2api_py-0.0.1.16.2.tar.gz

---
+++ packages/hd2api.py/hd2api_py-0.0.1.16.2.tar.gz/hd2api_py-0.0.1.16.2/src/hd2api/models/Position.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from pydantic import Field
+
+from .ABC.model import BaseApiModel
+
+
+class Position(BaseApiModel):
+    """
+    A position on the galactic war map, relative to Super Earth.
+
+    """
+
+    x: Optional[float] = Field(alias="x", default=None, description="The X coordinate.")
+    y: Optional[float] = Field(alias="y", default=None, description="The Y coordinate.")
