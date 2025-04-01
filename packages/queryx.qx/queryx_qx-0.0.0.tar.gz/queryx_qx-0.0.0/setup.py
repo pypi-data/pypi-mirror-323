85f0a6eee3d98acfdb8f30dcccfce62978a7f418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 01:54:13 +0000

    Add package queryx.qx/0.0.0/queryx_qx-0.0.0.tar.gz

---
+++ packages/queryx.qx/queryx_qx-0.0.0.tar.gz/queryx_qx-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='queryx.qx',
+    version='0.0.0',
+    packages=find_packages(),
+    description='queryx qx',
+    author='Patryk Zdunowski',
+    author_email='zdunekhere@gmail.com'
+)
