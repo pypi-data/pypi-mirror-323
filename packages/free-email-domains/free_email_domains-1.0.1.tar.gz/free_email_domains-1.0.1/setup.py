d2b442b6d5188f7a0657cd36f1dc83aafb6df741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 02:07:21 +0000

    Add package free-email-domains/1.0.1/free_email_domains-1.0.1.tar.gz

---
+++ packages/free-email-domains/free_email_domains-1.0.1.tar.gz/free_email_domains-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='free_email_domains',
+    version='1.0.1',
+    author='Luke Renton',
+    description='A package containing a list of free email domains.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LukeRenton/free-email-domain-list",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
