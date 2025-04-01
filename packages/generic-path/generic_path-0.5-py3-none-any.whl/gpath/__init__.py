21538cfb5e28cdee67019660b9cf9457206b3a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 02:07:21 +0000

    Add package generic-path/0.5/generic_path-0.5-py3-none-any.whl

---
+++ packages/generic-path/generic_path-0.5-py3-none-any.whl/gpath/__init__.py
@@ -0,0 +1,11 @@
+"""
+	GPath is a robust, generalised abstract file path that provides path manipulations independent from the local environment, maximising cross-platform compatibility.
+"""
+
+
+__version__ = '0.5'
+
+from . import platform, render
+from ._gpath import GPath, GPathLike
+
+__all__ = ('GPath', 'GPathLike', 'platform', 'render')
