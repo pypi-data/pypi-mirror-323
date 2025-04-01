5ba5dc2a9f41ad5c264fc1f1c1ab545f25ede47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 02:07:26 +0000

    Add package OpenHosta/2.1.5/OpenHosta-2.1.5-py3-none-any.whl

---
+++ packages/OpenHosta/OpenHosta-2.1.5-py3-none-any.whl/OpenHosta/exec/predict/model/hosta_model.py
@@ -0,0 +1,36 @@
+from abc import ABC
+from typing import Optional
+
+import torch
+from torch import nn
+from .neural_network_types import ArchitectureType
+
+
+class HostaModel(ABC, nn.Module):
+    def __init__(self):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.layers = []
+        self.architecture_type: Optional[ArchitectureType] = None
+        super().__init__()
+
+    def trainer(self, train_set, epochs):
+        pass
+
+    def forward(self, x):
+        # for layer in self.layers:
+        #     x = layer(x)
+        # return x
+        pass
+
+    def validate(self, validation_set):
+        pass
+
+    def inference(self, x):
+        pass
+
+    def init_weights(self, path: str):
+        self.load_state_dict(torch.load(path, weights_only=True, map_location=self.device))
+        self.eval()
+
+    def save_weights(self, path: str):
+        torch.save(self.state_dict(), path)
